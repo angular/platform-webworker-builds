4337dfaea57dd04f327d658cf28c68c8e5a5b82f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-11-30 22:05:01 +0000

    e628b66 feat(build): record angular version in the dom (#13164)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-beta.0-3e73bea
+ * @license Angular v2.3.0-beta.0-e628b66
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -3495,6 +3495,12 @@
           .concat(customProviders)));
   }
 
+  /**
+   * @stable
+   */
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-beta.0-e628b66');
+
+  exports.VERSION = VERSION;
   exports.ClientMessageBroker = ClientMessageBroker;
   exports.ClientMessageBrokerFactory = ClientMessageBrokerFactory;
   exports.FnArg = FnArg;
