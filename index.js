7191841fad0daceca9bba0c38c9e3ee05f7b09b7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-01 21:38:43 +0000

    25a6da2 refactor(compiler-cli): refactor compiler host parameters (#13147)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-5908b66');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-25a6da2');
 export { ClientMessageBroker, ClientMessageBrokerFactory, FnArg, UiArguments, MessageBus, PRIMITIVE, ReceivedMessage, ServiceMessageBroker, ServiceMessageBrokerFactory, WORKER_UI_LOCATION_PROVIDERS, WORKER_APP_LOCATION_PROVIDERS, WorkerAppModule, platformWorkerApp, platformWorkerUi, bootstrapWorkerUi } from './src/platform-webworker';
 //# sourceMappingURL=index.js.map
