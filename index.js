9331358129cd4a1057ca0580763cd77f14202a1e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2016-12-07 00:32:10 +0000

    25c2141 fix(language-service): remove incompletely used parameter from `createLanguageServiceFromTypescript()` (#13278)

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ import { Version } from '@angular/core';
 /**
  * @stable
  */
-export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-393c100');
+export var /** @type {?} */ VERSION = new Version('2.3.0-rc.0-25c2141');
 export { ClientMessageBroker, ClientMessageBrokerFactory, FnArg, UiArguments, MessageBus, PRIMITIVE, ReceivedMessage, ServiceMessageBroker, ServiceMessageBrokerFactory, WORKER_UI_LOCATION_PROVIDERS, WORKER_APP_LOCATION_PROVIDERS, WorkerAppModule, platformWorkerApp, platformWorkerUi, bootstrapWorkerUi } from './src/platform-webworker';
 //# sourceMappingURL=index.js.map
