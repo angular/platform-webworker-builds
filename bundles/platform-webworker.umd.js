a023c3c9a98b4e7175067201e822e585eff79071
Victor Berchet <victor@suumit.com>
2016-12-08 19:37:41 +0000

    65965c2 docs(changelog): fix a typo (#13298)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v2.3.0-13b41bd
+ * @license Angular v2.3.0-65965c2
  * (c) 2010-2016 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1776,7 +1776,7 @@
   /**
    * @stable
    */
-  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-13b41bd');
+  var /** @type {?} */ VERSION = new _angular_core.Version('2.3.0-65965c2');
 
   var MessageBasedPlatformLocation = (function () {
       /**
