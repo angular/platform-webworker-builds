3a102247a0e178da3b940578ceeb090205ebb9d6
Miško Hevery <misko@hevery.com>
2017-02-09 23:18:06 +0000

    45cc444 fix(core): Remove ChangeDetectorRef Paramter from KeyValueDifferFactory and IterableDifferFactory (#14311)

---
+++ src/private_import_platform-browser.js
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { __platform_browser_private__ as _ } from '@angular/platform-browser';
+export var /** @type {?} */ BROWSER_SANITIZATION_PROVIDERS = _.BROWSER_SANITIZATION_PROVIDERS;
+export var /** @type {?} */ BrowserPlatformLocation = _.BrowserPlatformLocation;
+export var /** @type {?} */ getDOM = _.getDOM;
+export var /** @type {?} */ BrowserDomAdapter = _.BrowserDomAdapter;
+export var /** @type {?} */ BrowserGetTestability = _.BrowserGetTestability;
+export var /** @type {?} */ DomRootRenderer = _.DomRootRenderer;
+export var /** @type {?} */ DomRootRenderer_ = _.DomRootRenderer_;
+export var /** @type {?} */ DomEventsPlugin = _.DomEventsPlugin;
+export var /** @type {?} */ DomSharedStylesHost = _.DomSharedStylesHost;
+export var /** @type {?} */ SharedStylesHost = _.SharedStylesHost;
+export var /** @type {?} */ KeyEventsPlugin = _.KeyEventsPlugin;
+export var /** @type {?} */ HammerGesturesPlugin = _.HammerGesturesPlugin;
+export var /** @type {?} */ DomAdapter = _.DomAdapter;
+export var /** @type {?} */ setRootDomAdapter = _.setRootDomAdapter;
+export var /** @type {?} */ WebAnimationsDriver = _.WebAnimationsDriver;
+//# sourceMappingURL=private_import_platform-browser.js.map
