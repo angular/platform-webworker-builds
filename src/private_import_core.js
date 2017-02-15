3df7ded2ed571d83f37b2ef92ccbdfc180ca0095
GitHub <noreply@github.com>
2017-02-15 16:57:18 +0000

    59b2b65 build: update .pullapprove

---
+++ src/private_import_core.js
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { __core_private__ as r } from '@angular/core/index';
+export const /** @type {?} */ RenderDebugInfo = r.RenderDebugInfo;
+export const /** @type {?} */ ReflectionCapabilities = r.ReflectionCapabilities;
+export const /** @type {?} */ DebugDomRootRenderer = r.DebugDomRootRenderer;
+export const /** @type {?} */ reflector = r.reflector;
+export const /** @type {?} */ NoOpAnimationPlayer = r.NoOpAnimationPlayer;
+export const /** @type {?} */ AnimationPlayer = r.AnimationPlayer;
+export const /** @type {?} */ AnimationSequencePlayer = r.AnimationSequencePlayer;
+export const /** @type {?} */ AnimationGroupPlayer = r.AnimationGroupPlayer;
+export const /** @type {?} */ AnimationKeyframe = r.AnimationKeyframe;
+export const /** @type {?} */ AnimationStyles = r.AnimationStyles;
+export const /** @type {?} */ prepareFinalAnimationStyles = r.prepareFinalAnimationStyles;
+export const /** @type {?} */ balanceAnimationKeyframes = r.balanceAnimationKeyframes;
+export const /** @type {?} */ clearStyles = r.clearStyles;
+export const /** @type {?} */ collectAndResolveStyles = r.collectAndResolveStyles;
+export const /** @type {?} */ APP_ID_RANDOM_PROVIDER = r.APP_ID_RANDOM_PROVIDER;
+//# sourceMappingURL=private_import_core.js.map
