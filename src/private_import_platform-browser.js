4dffdde89ed5dae9b00e98ad4546ea542bcee643
GitHub <noreply@github.com>
2017-02-15 16:58:53 +0000

    3a2e5f5 build: update tbosch/vicb in pullapprove

---
+++ src/private_import_platform-browser.js
@@ -0,0 +1,24 @@
+/**
+ * @license
+ * Copyright Google Inc. All Rights Reserved.
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE file at https://angular.io/license
+ */
+import { __platform_browser_private__ as _ } from '@angular/platform-browser/index';
+export const /** @type {?} */ BROWSER_SANITIZATION_PROVIDERS = _.BROWSER_SANITIZATION_PROVIDERS;
+export const /** @type {?} */ BrowserPlatformLocation = _.BrowserPlatformLocation;
+export const /** @type {?} */ getDOM = _.getDOM;
+export const /** @type {?} */ BrowserDomAdapter = _.BrowserDomAdapter;
+export const /** @type {?} */ BrowserGetTestability = _.BrowserGetTestability;
+export const /** @type {?} */ DomRootRenderer = _.DomRootRenderer;
+export const /** @type {?} */ DomRootRenderer_ = _.DomRootRenderer_;
+export const /** @type {?} */ DomEventsPlugin = _.DomEventsPlugin;
+export const /** @type {?} */ DomSharedStylesHost = _.DomSharedStylesHost;
+export const /** @type {?} */ SharedStylesHost = _.SharedStylesHost;
+export const /** @type {?} */ KeyEventsPlugin = _.KeyEventsPlugin;
+export const /** @type {?} */ HammerGesturesPlugin = _.HammerGesturesPlugin;
+export const /** @type {?} */ DomAdapter = _.DomAdapter;
+export const /** @type {?} */ setRootDomAdapter = _.setRootDomAdapter;
+export const /** @type {?} */ WebAnimationsDriver = _.WebAnimationsDriver;
+//# sourceMappingURL=private_import_platform-browser.js.map
