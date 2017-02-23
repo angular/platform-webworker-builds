9b7963ff0e6aef09f76cbb2c618eef112d0a3dbd
Igor Minar <iminar@google.com>
2017-02-23 21:28:21 +0000

    187f7b6 refactor(animations): always remove elements right away unless they have a leave animation

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -17,7 +17,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 /**
- * @license Angular v4.0.0-beta.8-4301dce
+ * @license Angular v4.0.0-beta.8-187f7b6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -2102,7 +2102,7 @@ function _resolveDefaultAnimationDriver() {
 /**
  * @stable
  */
-var /** @type {?} */VERSION = new Version('4.0.0-beta.8-4301dce');
+var /** @type {?} */VERSION = new Version('4.0.0-beta.8-187f7b6');
 
 var MessageBasedPlatformLocation = function () {
     /**
--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-4301dce
+ * @license Angular v4.0.0-beta.8-187f7b6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1625,7 +1625,7 @@ function _resolveDefaultAnimationDriver() {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-4301dce');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-187f7b6');
 
 class MessageBasedPlatformLocation {
     /**
