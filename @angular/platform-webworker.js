ea0cb97363d150ee95b7f42f82a56a975a44bc77
Igor Minar <iminar@google.com>
2017-02-24 17:58:33 +0000

    93ddd38 refactor(animations): make modules and bundles into plural Animations (#14695)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-32c2fd5
+ * @license Angular v4.0.0-beta.8-93ddd38
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1889,7 +1889,7 @@ function _resolveDefaultAnimationDriver() {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-32c2fd5');
+const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-93ddd38');
 
 class MessageBasedPlatformLocation {
     /**
