1b2423d2e5e79a9d431549ad1bfc66fe2c613afa
Igor Minar <igor@angularjs.org>
2017-02-25 02:29:25 +0000

    c7d1b36 docs: update release schedule due to bad 4.0.0-rc.0 release

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-beta.8-2602b03
+ * @license Angular v4.0.0-rc.1-c7d1b36
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1889,7 +1889,7 @@ function _resolveDefaultAnimationDriver() {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-beta.8-2602b03');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-c7d1b36');
 
 class MessageBasedPlatformLocation {
     /**
