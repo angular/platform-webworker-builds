43f42de498f7baf6999d15e03905b4341923d372
Igor Minar <iminar@google.com>
2017-02-27 19:48:41 +0000

    f356041 docs: update version in changelog to reflect RC.1 (not 0) (#14740)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1935,7 +1935,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-77682a3');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f356041');
 
     var MessageBasedPlatformLocation = function () {
         /**
