8ee83e5dc0a1d309e938a600cae2c5dd20003c5d
Igor Minar <iminar@google.com>
2017-02-27 19:47:28 +0000

    77682a3 fix(compiler): quote `animation` in `RendererTypeV2` and skip if empty (#14773)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1935,7 +1935,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-213e210');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-77682a3');
 
     var MessageBasedPlatformLocation = function () {
         /**
