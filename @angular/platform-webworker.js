223abee3e6020b9f244a416a970d8dfef15caa68
Igor Minar <iminar@google.com>
2017-02-27 23:13:01 +0000

    5caab71 refactor: make identifiers for generated code small to improve dev size

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-f356041
+ * @license Angular v4.0.0-rc.1-5caab71
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1889,7 +1889,7 @@ function _resolveDefaultAnimationDriver() {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-f356041');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-5caab71');
 
 class MessageBasedPlatformLocation {
     /**
--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1935,7 +1935,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-f356041');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5caab71');
 
     var MessageBasedPlatformLocation = function () {
         /**
