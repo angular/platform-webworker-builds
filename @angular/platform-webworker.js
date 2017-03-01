b58de5338d87aac442de94d0cd2ef201b7a33331
Igor Minar <iminar@google.com>
2017-03-01 16:23:38 +0000

    bc9e1de docs: changelog - fix else example (#14766)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-fc9e6b2
+ * @license Angular v4.0.0-rc.1-bc9e1de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1557,7 +1557,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-fc9e6b2');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-bc9e1de');
 
 class MessageBasedPlatformLocation {
     /**
--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1677,7 +1677,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-fc9e6b2');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-bc9e1de');
 
     var MessageBasedPlatformLocation = function () {
         /**
