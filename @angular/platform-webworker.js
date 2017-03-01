bde5ec24adbf6c3f59a2b870288f7cb17f8d6508
Igor Minar <iminar@google.com>
2017-03-01 23:48:38 +0000

    dd49901 ci: add NGBUILDS_IO_KEY to .travis.yaml for ngbuilds.io integration

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-a6996a9
+ * @license Angular v4.0.0-rc.1-dd49901
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1557,7 +1557,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-a6996a9');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-dd49901');
 
 class MessageBasedPlatformLocation {
     /**
