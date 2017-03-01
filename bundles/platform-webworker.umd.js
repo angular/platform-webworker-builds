3583cec49685b163fc111d38b60699b886a384b4
Igor Minar <iminar@google.com>
2017-03-01 16:34:09 +0000

    49b462e test: closure integration test for change detection (#14691)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1677,7 +1677,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-d1182af');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-49b462e');
 
     var MessageBasedPlatformLocation = function () {
         /**
