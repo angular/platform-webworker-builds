15b256f21bcca658cabd0825eb25615f235c3395
Igor Minar <iminar@google.com>
2017-03-01 16:25:42 +0000

    5753de5 fix(core): fix `isComponentView()` and `isEmbeddedView()` tests (#14789)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1677,7 +1677,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-bc9e1de');
+    var /** @type {?} */VERSION = new _core.Version('4.0.0-rc.1-5753de5');
 
     var MessageBasedPlatformLocation = function () {
         /**
