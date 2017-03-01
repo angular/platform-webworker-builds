f74a84b0a560384e0a71768ef5bdf890acd84d5c
Igor Minar <iminar@google.com>
2017-03-01 02:46:12 +0000

    5ba55b0 fix(compiler): fix identifier names of `EMPTY_MAP` / `EMPTY_ARRAY` (#14806)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.1-126fda2
+ * @license Angular v4.0.0-rc.1-5ba55b0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1557,7 +1557,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-126fda2');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.1-5ba55b0');
 
 class MessageBasedPlatformLocation {
     /**
