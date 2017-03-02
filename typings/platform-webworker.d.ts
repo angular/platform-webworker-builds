d010822d1628a9991ae380e2d6473666aa4edf93
Igor Minar <igor@angularjs.org>
2017-03-02 07:21:29 +0000

    180b705 release: cut the 4.0.0-rc.2 release

---
+++ typings/platform-webworker.d.ts
@@ -0,0 +1,12 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public_api';
+export { ON_WEB_WORKER as ɵj } from './src/web_workers/shared/api';
+export { ClientMessageBrokerFactory_ as ɵa } from './src/web_workers/shared/client_message_broker';
+export { RenderStore as ɵh } from './src/web_workers/shared/render_store';
+export { Serializer as ɵb } from './src/web_workers/shared/serializer';
+export { ServiceMessageBrokerFactory_ as ɵc } from './src/web_workers/shared/service_message_broker';
+export { WebWorkerRendererFactoryV2 as ɵi } from './src/web_workers/worker/renderer';
+export { createMessageBus as ɵe, errorHandler as ɵd, setupWebWorker as ɵf } from './src/worker_app';
+export { _WORKER_UI_PLATFORM_PROVIDERS as ɵg } from './src/worker_render';
