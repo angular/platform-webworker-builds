232c75a81eae80fa6fe98d159718cec173f0b150
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 16:54:27 +0000

    fa1920a feat(aio): enable deep-linking on deployed apps (until prerendering is done) (#15049)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-71cd295
+ * @license Angular v4.0.0-rc.3-fa1920a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1428,7 +1428,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-71cd295');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-fa1920a');
 
 class MessageBasedPlatformLocation {
     /**
