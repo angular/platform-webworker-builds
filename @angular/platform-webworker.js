0eaf4998df3dff0fb3286b701c96bc5dd4565d40
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:13:41 +0000

    893652a fix(aio): upgrade to zone.js 0.7.8 (#15099)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-6559425
+ * @license Angular v4.0.0-rc.3-893652a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-6559425');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-893652a');
 
 class MessageBasedPlatformLocation {
     /**
