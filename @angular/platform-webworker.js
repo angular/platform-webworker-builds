39e1deae9aa847ad957183ab71ea362956dcc48f
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:19:26 +0000

    e7dab7e feat(aio): hard code a "Home" image link into the toolbar

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-893652a
+ * @license Angular v4.0.0-rc.3-e7dab7e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-893652a');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-e7dab7e');
 
 class MessageBasedPlatformLocation {
     /**
