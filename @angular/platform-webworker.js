377cf68275dbe77aaf09c8c63d9c6fcb9946fdde
Chuck Jazdzewski <chuckj@google.com>
2017-03-13 20:55:15 +0000

    75147ff fix(aio): remove invalid brace from CSS (#15122)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-018e5c9
+ * @license Angular v4.0.0-rc.3-75147ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-018e5c9');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-75147ff');
 
 class MessageBasedPlatformLocation {
     /**
--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-018e5c9
+ * @license Angular v4.0.0-rc.3-75147ff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1482,7 +1482,7 @@
     /**
      * @stable
      */
-    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-018e5c9');
+    var /** @type {?} */ VERSION = new _angular_core.Version('4.0.0-rc.3-75147ff');
     var MessageBasedPlatformLocation = (function () {
         /**
          * @param {?} _brokerFactory
