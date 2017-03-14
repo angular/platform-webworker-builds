86aa12705732464f639a00351289d2368fd42865
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-14 14:09:49 +0000

    2f250c1 refactor(aio): move window:resize handler into component

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-d4205bb
+ * @license Angular v4.0.0-rc.3-2f250c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-d4205bb');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-2f250c1');
 
 class MessageBasedPlatformLocation {
     /**
