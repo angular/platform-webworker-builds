8d181273abd57c1e73a61f0b05880f0ed1d7dc81
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 23:44:26 +0000

    13686bb fix: element injector vs module injector (#15044)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-f093501
+ * @license Angular v4.0.0-rc.3-13686bb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f093501');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-13686bb');
 
 class MessageBasedPlatformLocation {
     /**
