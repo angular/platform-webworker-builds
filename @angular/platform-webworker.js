af18c3c7690386bc3f201a446a35e604507ba693
Chuck Jazdzewski <chuckj@google.com>
2017-03-14 01:03:34 +0000

    eedca09 build(aio): generate the api-list.json file from the API docs

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-ab40fcb
+ * @license Angular v4.0.0-rc.3-eedca09
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ab40fcb');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-eedca09');
 
 class MessageBasedPlatformLocation {
     /**
