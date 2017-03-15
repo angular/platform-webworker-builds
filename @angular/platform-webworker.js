46ad1de6b1e1cc8ee632a1f1f2b9e97842b30abb
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 00:29:09 +0000

    959a03a fix(compiler): fix utf8encode, move to sharted utils, add tests (#15076)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-3b1956b
+ * @license Angular v4.0.0-rc.3-959a03a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3b1956b');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-959a03a');
 
 class MessageBasedPlatformLocation {
     /**
