8c30a984dfcfcd856ac26b2c9e5b9856a883770d
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 20:57:23 +0000

    3f38c6f fix(animations): always fire callbacks even for noop animations (#15170)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-80112a9
+ * @license Angular v4.0.0-rc.3-3f38c6f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1441,7 +1441,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-80112a9');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-3f38c6f');
 
 class MessageBasedPlatformLocation {
     /**
