164c6a77c600bcac8d3f84025e5317bc599c2f41
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 20:41:16 +0000

    322bf7a Revert "refactor(upgrade): don’t rely on compiler internals"

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-a3e32fb
+ * @license Angular v4.0.0-rc.3-322bf7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1441,7 +1441,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-a3e32fb');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-322bf7a');
 
 class MessageBasedPlatformLocation {
     /**
