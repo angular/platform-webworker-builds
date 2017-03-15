a1b1a8b3be16ba6a4ab84304130bac4097210f3f
Chuck Jazdzewski <chuckj@google.com>
2017-03-15 22:45:31 +0000

    f1b33ab fix(http): Make ResponseOptionsArgs an interface (#14607) (#14623)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-029d0f2
+ * @license Angular v4.0.0-rc.3-f1b33ab
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1441,7 +1441,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-029d0f2');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f1b33ab');
 
 class MessageBasedPlatformLocation {
     /**
