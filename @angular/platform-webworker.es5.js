6a5044214393c4a1cb9222561251e3cfcc6a63b3
Igor Minar <igor@angularjs.org>
2017-03-15 04:26:06 +0000

    ec548ad feat(aio): hide search results when click outside search elements

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.0.0-rc.3-423bfb0
+ * @license Angular v4.0.0-rc.3-ec548ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1479,7 +1479,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-423bfb0');
+var /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ec548ad');
 var MessageBasedPlatformLocation = (function () {
     /**
      * @param {?} _brokerFactory
--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-423bfb0
+ * @license Angular v4.0.0-rc.3-ec548ad
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1427,7 +1427,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-423bfb0');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-ec548ad');
 
 class MessageBasedPlatformLocation {
     /**
