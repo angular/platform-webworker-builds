56e53938ed36553e2113a1d57922d8bb4c3cffe7
Chuck Jazdzewski <chuckj@google.com>
2017-03-16 16:30:53 +0000

    f92d644 ci: disable Android 4.1, 4.2 and 4.3 in SauceLabs (#14461)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-923d0c5
+ * @license Angular v4.0.0-rc.3-f92d644
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1441,7 +1441,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @stable
  */
-const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-923d0c5');
+const /** @type {?} */ VERSION = new Version('4.0.0-rc.3-f92d644');
 
 class MessageBasedPlatformLocation {
     /**
