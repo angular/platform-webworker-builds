0b83ce7e36028ed2803ba25bfbff0efcadaa3768
Miško Hevery <misko@hevery.com>
2017-03-17 20:46:03 +0000

    f925910 fix(animations): make sure easing values work with web-animations (#15195)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-2d78c8c
+ * @license Angular v4.0.0-rc.5-f925910
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1428,7 +1428,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-2d78c8c');
+const VERSION = new Version('4.0.0-rc.5-f925910');
 
 class MessageBasedPlatformLocation {
     /**
