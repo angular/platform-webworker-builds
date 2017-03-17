acc36d6f4e37c9db04b5f1d32330229fd44e6f21
Chuck Jazdzewski <chuckj@google.com>
2017-03-17 00:50:05 +0000

    410aa33 build: fix paths to typings files so tsickle resolves imports correctly

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.3-994089d
+ * @license Angular v4.0.0-rc.3-410aa33
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1428,7 +1428,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.3-994089d');
+const VERSION = new Version('4.0.0-rc.3-410aa33');
 
 class MessageBasedPlatformLocation {
     /**
