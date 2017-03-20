7fca8680395f5b8a8392e268f0a80b855e9e4389
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-20 12:28:29 +0000

    6d4f5ce build(aio): update to dgeni-packages 0.17.0

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-d1e82b0
+ * @license Angular v4.0.0-rc.5-6d4f5ce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1428,7 +1428,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-d1e82b0');
+const VERSION = new Version('4.0.0-rc.5-6d4f5ce');
 
 class MessageBasedPlatformLocation {
     /**
