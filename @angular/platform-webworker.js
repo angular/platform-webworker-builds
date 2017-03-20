439dc53adce884f468f7008ad28a9306a5d7d74f
Victor Berchet <victor@suumit.com>
2017-03-20 18:06:10 +0000

    d3eda7a feat(router): add `ParamMap.keys` to get a list of parameters

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-5da021c
+ * @license Angular v4.0.0-rc.5-d3eda7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-5da021c');
+const VERSION = new Version('4.0.0-rc.5-d3eda7a');
 
 /**
  * @license
