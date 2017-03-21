9f8af447aa277c7f4cfc5a59fa1b79d4d57a3d6d
Miško Hevery <misko@hevery.com>
2017-03-21 03:42:09 +0000

    de3d2ee fix(platform-server): interpret Native view encapsulation as Emulated on the server (#15155)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-d3eda7a
+ * @license Angular v4.0.0-rc.5-de3d2ee
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-d3eda7a');
+const VERSION = new Version('4.0.0-rc.5-de3d2ee');
 
 /**
  * @license
