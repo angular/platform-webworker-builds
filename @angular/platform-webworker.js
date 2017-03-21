f931590ad54037fed2a52fba54a5b16259c28c3c
Miško Hevery <misko@hevery.com>
2017-03-21 04:26:58 +0000

    2489e4b fix(animations): correct the main entry path in package.json (#15300)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-de3d2ee
+ * @license Angular v4.0.0-rc.5-2489e4b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-de3d2ee');
+const VERSION = new Version('4.0.0-rc.5-2489e4b');
 
 /**
  * @license
