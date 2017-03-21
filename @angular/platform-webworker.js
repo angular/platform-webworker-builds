6132dead0d33d47fd0a553be981e1a030992ea0b
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-21 17:16:35 +0000

    f40f915 build(aio): upgrade to rxjs@5.2.0

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-11d5309
+ * @license Angular v4.0.0-rc.5-f40f915
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-11d5309');
+const VERSION = new Version('4.0.0-rc.5-f40f915');
 
 /**
  * @license
