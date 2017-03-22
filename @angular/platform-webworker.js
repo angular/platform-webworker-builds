f5b12a0189d8c2641626ad05d0bb864ffb74374e
Igor Minar <iminar@google.com>
2017-03-22 22:29:51 +0000

    a50d79d fix(compiler-cli): adding missing format xliff for the extractor (#15386)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-64285a2
+ * @license Angular v4.0.0-rc.5-a50d79d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-64285a2');
+const VERSION = new Version('4.0.0-rc.5-a50d79d');
 
 /**
  * @license
