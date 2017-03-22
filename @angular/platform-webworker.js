3a6b93f2dbc1fb145092a6e0e0ee02a0bd61f130
Miško Hevery <misko@hevery.com>
2017-03-22 03:45:03 +0000

    1bcbcfd revert: build(aio): implement prerendering (#15346)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-5efc860
+ * @license Angular v4.0.0-rc.5-1bcbcfd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-5efc860');
+const VERSION = new Version('4.0.0-rc.5-1bcbcfd');
 
 /**
  * @license
