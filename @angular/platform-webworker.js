040f10eaf7008f4ef418fdd9f4036aa0e9a92117
Victor Berchet <victor@suumit.com>
2017-03-23 18:04:34 +0000

    08f2f08 fix(router): should pass new data to Observable when query params change (#15387)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-376088d
+ * @license Angular v4.0.0-rc.6-08f2f08
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-376088d');
+const VERSION = new Version('4.0.0-rc.6-08f2f08');
 
 /**
  * @license
