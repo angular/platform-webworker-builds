0ab694eb91453cdc5f4a2962ac9e9d903d0bf330
Victor Berchet <victor@suumit.com>
2017-03-23 17:44:44 +0000

    376088d build(aio): delete content folder before doc-gen (#15414)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-73808dd
+ * @license Angular v4.0.0-rc.6-376088d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-73808dd');
+const VERSION = new Version('4.0.0-rc.6-376088d');
 
 /**
  * @license
