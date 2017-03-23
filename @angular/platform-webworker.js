7f835c0614254786382d07a252487ea015886528
Victor Berchet <victor@suumit.com>
2017-03-23 22:11:13 +0000

    b800a0c fix: prevent strictNullChecks support until #15432 is fixed (#15434)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.6-c8ab5cb
+ * @license Angular v4.0.0-rc.6-b800a0c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.6-c8ab5cb');
+const VERSION = new Version('4.0.0-rc.6-b800a0c');
 
 /**
  * @license
