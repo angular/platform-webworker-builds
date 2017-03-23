60af4cd01b4e4d3543a27b21f53adb5168b19cfa
Igor Minar <iminar@google.com>
2017-03-23 00:32:32 +0000

    edb2571 docs: spelling error (#15406)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-rc.5-26f6bd4
+ * @license Angular v4.0.0-rc.5-edb2571
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-rc.5-26f6bd4');
+const VERSION = new Version('4.0.0-rc.5-edb2571');
 
 /**
  * @license
