779c78c7f983644513958becee5bb05f08b81c1c
Igor Minar <iminar@google.com>
2017-03-24 15:38:06 +0000

    d92930e docs: fixed broken links (#15455)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-c2892da
+ * @license Angular v4.0.0-d92930e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-c2892da');
+const VERSION = new Version('4.0.0-d92930e');
 
 /**
  * @license
