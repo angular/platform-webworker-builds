39e1a187ceaea6e1e2c12820f49c3d90d2bcf628
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 13:54:11 +0000

    6605dd1 docs(aio): correct spelling in overview

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-837ed78
+ * @license Angular v4.0.0-6605dd1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-837ed78');
+const VERSION = new Version('4.0.0-6605dd1');
 
 /**
  * @license
