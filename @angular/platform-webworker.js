5bc967766bebdfe4df2cff435297e8d972108d9f
Victor Berchet <victor@suumit.com>
2017-03-27 17:02:32 +0000

    816b389 docs: in doc comments, replace [aA]ngular2 with Angular (#15463)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-6605dd1
+ * @license Angular v4.0.0-816b389
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-6605dd1');
+const VERSION = new Version('4.0.0-816b389');
 
 /**
  * @license
