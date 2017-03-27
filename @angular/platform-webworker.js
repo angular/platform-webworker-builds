a9b6f8bd6978a46c6f140ed4ec029e78c22defb1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 08:38:00 +0000

    a6fd22c docs(aio): fix typo

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-1616cae
+ * @license Angular v4.0.0-a6fd22c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-1616cae');
+const VERSION = new Version('4.0.0-a6fd22c');
 
 /**
  * @license
