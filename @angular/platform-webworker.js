0b03112605f37f208538f418be9acee137afb2b1
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-27 09:31:11 +0000

    52ea193 build(aio): left align code regions

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-228238e
+ * @license Angular v4.0.0-52ea193
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-228238e');
+const VERSION = new Version('4.0.0-52ea193');
 
 /**
  * @license
