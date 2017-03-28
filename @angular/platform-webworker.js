17e988cfdee73507e2c1c340279bce5aecc4be39
Pete Bacon Darwin <pete@bacondarwin.com>
2017-03-28 10:08:44 +0000

    9995191 fix(aio): selectedNodes should work for URLs ending with a slash

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9e0d7be
+ * @license Angular v4.0.0-9995191
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9e0d7be');
+const VERSION = new Version('4.0.0-9995191');
 
 /**
  * @license
