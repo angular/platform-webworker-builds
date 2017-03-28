f32e6a3559093896dd379174c5b370cdb92ed9b1
Victor Berchet <victor@suumit.com>
2017-03-28 17:20:59 +0000

    a5c972a fix(language-service): be resilient to invalidate ordering (#15470)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-8e03f65
+ * @license Angular v4.0.0-a5c972a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-8e03f65');
+const VERSION = new Version('4.0.0-a5c972a');
 
 /**
  * @license
