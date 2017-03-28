425a1a50f072b79564b4cc5f1813dd67de77fcbf
Victor Berchet <victor@suumit.com>
2017-03-28 00:55:05 +0000

    a9ae4da ci: add tbosch and vicb as approvers of the language service (#15530)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-15662ef
+ * @license Angular v4.0.0-a9ae4da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-15662ef');
+const VERSION = new Version('4.0.0-a9ae4da');
 
 /**
  * @license
