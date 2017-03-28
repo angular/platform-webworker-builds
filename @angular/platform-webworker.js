c6d367d3ec9ab7c0690a8d097488dc24431ce8ca
Victor Berchet <victor@suumit.com>
2017-03-28 20:51:32 +0000

    aa16ccd fix(core): check for undefined on normalizeDebugBindingValue (#15503)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-d438b88
+ * @license Angular v4.0.0-aa16ccd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-d438b88');
+const VERSION = new Version('4.0.0-aa16ccd');
 
 /**
  * @license
