1323deb9560eb037bc50f9f92f5520fd4166ae4c
Victor Berchet <victor@suumit.com>
2017-03-28 21:59:08 +0000

    861953c feat(aio): add LiveExampleComponent (#15544)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-aa16ccd
+ * @license Angular v4.0.0-861953c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-aa16ccd');
+const VERSION = new Version('4.0.0-861953c');
 
 /**
  * @license
