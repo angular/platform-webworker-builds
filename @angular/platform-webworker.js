72dd983ac14b939d8979b4a27ec88bc92d79ed9b
Victor Berchet <victor@suumit.com>
2017-03-29 16:53:12 +0000

    910c0d9 fix(core): Update types for TypeScript nullability support (#15472)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-4916278
+ * @license Angular v4.0.0-910c0d9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-4916278');
+const VERSION = new Version('4.0.0-910c0d9');
 
 /**
  * @license
