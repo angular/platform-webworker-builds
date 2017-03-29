4b9bcf1891c760c5da807fffc286ea31e3cfdf2c
Victor Berchet <victor@suumit.com>
2017-03-29 18:03:27 +0000

    902bb2f fix(compiler): throw when a component defines both template and templateUrl (#15572)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-23bf348
+ * @license Angular v4.0.0-902bb2f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-23bf348');
+const VERSION = new Version('4.0.0-902bb2f');
 
 /**
  * @license
