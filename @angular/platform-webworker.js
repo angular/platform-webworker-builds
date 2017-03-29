58995f82d6178742e27193281f7e6ddd1ff19925
Victor Berchet <victor@suumit.com>
2017-03-29 21:35:53 +0000

    9f2acf5 feat(aio): refactors AppComponent and its kids + sidenav hiding (#15570)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-9c77a7c
+ * @license Angular v4.0.0-9f2acf5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-9c77a7c');
+const VERSION = new Version('4.0.0-9f2acf5');
 
 /**
  * @license
