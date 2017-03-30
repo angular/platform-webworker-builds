acf2ece1e3b9fd77ecc1904fb8f05261e0f97577
Victor Berchet <victor@suumit.com>
2017-03-30 00:00:32 +0000

    6b79ab5 docs: add changelog for 4.0.1

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.0.0-ca66530
+ * @license Angular v4.0.1-6b79ab5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.0.0-ca66530');
+const VERSION = new Version('4.0.1-6b79ab5');
 
 /**
  * @license
