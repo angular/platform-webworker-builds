8449931f2b95bb07c02850dad5a6c1d5e927c633
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-03-31 18:55:19 +0000

    5fbb0d0 fix(language-service): resolve any parameter types to any result

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-7c2ce29
+ * @license Angular v4.1.0-beta.0-5fbb0d0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-7c2ce29');
+const VERSION = new Version('4.1.0-beta.0-5fbb0d0');
 
 /**
  * @license
