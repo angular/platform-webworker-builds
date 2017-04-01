9f54c881a736799862da8242f18ac1ed9c64773d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-04-01 00:47:07 +0000

    08941aa build(aio): process marketing docs that have .md extension

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-03fb766
+ * @license Angular v4.1.0-beta.0-08941aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-03fb766');
+const VERSION = new Version('4.1.0-beta.0-08941aa');
 
 /**
  * @license
