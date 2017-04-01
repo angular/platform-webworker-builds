b37f1875fc0ebbca983b5f88825c88695dfe1d64
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-01 07:01:31 +0000

    ad639d7 feat(aio): add contribute marketing page

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-e1c2e50
+ * @license Angular v4.1.0-beta.0-ad639d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-e1c2e50');
+const VERSION = new Version('4.1.0-beta.0-ad639d7');
 
 /**
  * @license
