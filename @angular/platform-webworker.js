3bba2a728baa408c9ea09d874f78b9d53ea9f1c3
Matias Niemelä <matias@yearofmoo.com>
2017-04-05 01:38:18 +0000

    c933b75 feat(animations): wip: introduce a wave of new animation features

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-64f1bf6
+ * @license Angular v4.1.0-beta.0-c933b75
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-64f1bf6');
+const VERSION = new Version('4.1.0-beta.0-c933b75');
 
 /**
  * @license
