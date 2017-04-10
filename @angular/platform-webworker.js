a275fc5ea2e4e7c4db054bd074160d895ffc242c
Hans <hans@hansl.ca>
2017-04-10 21:42:43 +0000

    09d9f5f fix(compiler): Update types for TypeScript nullability support

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-4c566db
+ * @license Angular v4.1.0-beta.0-09d9f5f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-4c566db');
+const VERSION = new Version('4.1.0-beta.0-09d9f5f');
 
 /**
  * @license
