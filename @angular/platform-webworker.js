737f6713478b2a98ea685b36ec6877b88586a589
Hans <hans@hansl.ca>
2017-04-11 18:32:27 +0000

    91b2e39 ci: add jasonaden to the router

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-a487563
+ * @license Angular v4.1.0-beta.0-91b2e39
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-a487563');
+const VERSION = new Version('4.1.0-beta.0-91b2e39');
 
 /**
  * @license
