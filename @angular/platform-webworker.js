3cc763d428859c427ee25bf5dccd7d0c8c6990ac
Hans <hans@hansl.ca>
2017-04-11 23:18:48 +0000

    04fb29b test(aio): add test for escaped newline in CodeComponent

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-3ced940
+ * @license Angular v4.1.0-beta.0-04fb29b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-3ced940');
+const VERSION = new Version('4.1.0-beta.0-04fb29b');
 
 /**
  * @license
