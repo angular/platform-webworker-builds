d045a74376d01d0e1c35bac24544181a6fe82ac5
Hans Larsen <hans@hansl.ca>
2017-04-12 01:30:53 +0000

    38d75d4 feat(animations): Update types for TypeScript nullability support

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-04fb29b
+ * @license Angular v4.1.0-beta.0-38d75d4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-04fb29b');
+const VERSION = new Version('4.1.0-beta.0-38d75d4');
 
 /**
  * @license
