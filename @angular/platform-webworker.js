a13c8a857823fc09bb56c3b9a79886b4aa266a01
GitHub <noreply@github.com>
2017-04-12 19:41:19 +0000

    4a052cd build(aio): upgrade to @angular/material#2.0.0-beta.3 (#15606)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-28c2a71
+ * @license Angular v4.1.0-beta.0-4a052cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-28c2a71');
+const VERSION = new Version('4.1.0-beta.0-4a052cd');
 
 /**
  * @license
