46cea176eaeaa250dec4538517ad8e4f6296054e
Igor Minar <iminar@google.com>
2017-04-12 19:52:45 +0000

    8f1359d docs(aio): make events.html current (#15913)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.0-8659bd2
+ * @license Angular v4.1.0-beta.0-8f1359d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.0-8659bd2');
+const VERSION = new Version('4.1.0-beta.0-8f1359d');
 
 /**
  * @license
