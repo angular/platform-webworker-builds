adbb999953c02a1612abcec622cb7e59d8034744
Tobias Bosch <tbosch1009@gmail.com>
2017-04-13 19:13:12 +0000

    fdb3f26 refactor(compiler): cleanup (#15960)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-14b7dfa
+ * @license Angular v4.1.0-beta.1-fdb3f26
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-14b7dfa');
+const VERSION = new Version('4.1.0-beta.1-fdb3f26');
 
 /**
  * @license
