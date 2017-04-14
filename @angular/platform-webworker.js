7ff6d8b953a5dd74d151358ccb2809fe74f1c229
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 21:47:12 +0000

    56c46d7 fix(router): Update types for TypeScript nullability support

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-c36ec9b
+ * @license Angular v4.1.0-beta.1-56c46d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-c36ec9b');
+const VERSION = new Version('4.1.0-beta.1-56c46d7');
 
 /**
  * @license
