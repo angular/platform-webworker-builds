6acedbcd58532a61fa874776ff9db686290db413
Tobias Bosch <tbosch1009@gmail.com>
2017-04-14 22:27:07 +0000

    6649743 fix(forms): Update types for TypeScript nullability support

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-56c46d7
+ * @license Angular v4.1.0-beta.1-6649743
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-56c46d7');
+const VERSION = new Version('4.1.0-beta.1-6649743');
 
 /**
  * @license
