acf2f60e2d42d032660e792cb0e09d9893a4b26c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:18 +0000

    8479e9e feat(aio): high res screen spacing

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-092f0df
+ * @license Angular v4.1.0-beta.1-8479e9e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-092f0df');
+const VERSION = new Version('4.1.0-beta.1-8479e9e');
 
 /**
  * @license
