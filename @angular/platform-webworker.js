f77053e0611e606ab82cb3b4dbfd24e54629dcca
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-15 09:39:48 +0000

    c0b1bbe feat(aio): add log rotation in preview server

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-8479e9e
+ * @license Angular v4.1.0-beta.1-c0b1bbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-8479e9e');
+const VERSION = new Version('4.1.0-beta.1-c0b1bbe');
 
 /**
  * @license
