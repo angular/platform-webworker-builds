86d8b4283d22aecb795714762969d9e3e8c209ed
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-16 10:49:19 +0000

    11b54f6 feat(aio): edit page link styling and placement

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-4a3c66f
+ * @license Angular v4.1.0-beta.1-11b54f6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-4a3c66f');
+const VERSION = new Version('4.1.0-beta.1-11b54f6');
 
 /**
  * @license
