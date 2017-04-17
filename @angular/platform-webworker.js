38341ef034bfb498610024d00f470ccb4ae9ed6d
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:35:05 +0000

    82417b3 fix(router): prevent `RouterLinkActive` from causing an infinite CD loop

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-5b141fb
+ * @license Angular v4.1.0-beta.1-82417b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-5b141fb');
+const VERSION = new Version('4.1.0-beta.1-82417b3');
 
 /**
  * @license
