ccc136e19d388c308c47a94c3804860ad8e98126
Tobias Bosch <tbosch1009@gmail.com>
2017-04-17 17:13:15 +0000

    2688842 Revert "fix(http): Update types for TypeScript nullability support"

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-2ddf3bc
+ * @license Angular v4.1.0-beta.1-2688842
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1503,7 +1503,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-2ddf3bc');
+const VERSION = new Version('4.1.0-beta.1-2688842');
 
 /**
  * @license
