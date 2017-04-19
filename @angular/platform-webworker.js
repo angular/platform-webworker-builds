349b6c8bece7804f3aef371a53b5bec26fe0e30e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 15:12:37 +0000

    e07b7ea docs(aio): fix broken regions

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-73c7503
+ * @license Angular v4.1.0-beta.1-e07b7ea
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-73c7503');
+const VERSION = new Version('4.1.0-beta.1-e07b7ea');
 
 /**
  * @license
