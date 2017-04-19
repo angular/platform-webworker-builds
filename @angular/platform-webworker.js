2a4a7f45d35730e8e092bfae109d1fe7bbfd33c0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 08:37:35 +0000

    e951612 fix(aio): set the pageId to the file-not-found URL if the doc is not available

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-b668c2c
+ * @license Angular v4.1.0-beta.1-e951612
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-b668c2c');
+const VERSION = new Version('4.1.0-beta.1-e951612');
 
 /**
  * @license
