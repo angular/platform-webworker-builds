c8d4fefbe6f04d5e35f834ea29746258c401b65e
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-19 09:38:17 +0000

    8fa4453 docs(aio): Testing Guide: Second matcher parameter

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -4,7 +4,7 @@ var __extends = (this && this.__extends) || function (d, b) {
     d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
 };
 /**
- * @license Angular v4.1.0-beta.1-c2fca3d
+ * @license Angular v4.1.0-beta.1-8fa4453
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1557,7 +1557,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.1.0-beta.1-c2fca3d');
+var VERSION = new Version('4.1.0-beta.1-8fa4453');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
