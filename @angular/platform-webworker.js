2512012332603b160c44af8387cb805dcfd2048a
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-20 09:34:17 +0000

    f902581 fix(aio): handle clicks deep within anchor tags

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-745731e
+ * @license Angular v4.1.0-beta.1-f902581
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-745731e');
+const VERSION = new Version('4.1.0-beta.1-f902581');
 
 /**
  * @license
