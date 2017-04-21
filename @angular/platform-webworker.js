ef0c7c38f33e89bb99c30e7fd604fb63db3b7886
Miško Hevery <misko@hevery.com>
2017-04-21 17:27:42 +0000

    062fc4a docs(aio): change hardcoded links to the old site (#16196)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-cf034f7
+ * @license Angular v4.1.0-rc.0-062fc4a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-cf034f7');
+const VERSION = new Version('4.1.0-rc.0-062fc4a');
 
 /**
  * @license
