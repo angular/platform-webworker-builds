78a5741493de17102a3ca770e1d418274cdf188b
Matias Niemelä <matias@yearofmoo.com>
2017-04-21 04:29:31 +0000

    70384db feat(animations): wip: introduce a wave of new animation features

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-beta.1-47acf3d
+ * @license Angular v4.1.0-beta.1-70384db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-beta.1-47acf3d');
+const VERSION = new Version('4.1.0-beta.1-70384db');
 
 /**
  * @license
