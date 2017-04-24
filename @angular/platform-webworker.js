8e993e6ba55d602a806c391d0306e29ce1bfc01c
Pete Bacon Darwin <pete@bacondarwin.com>
2017-04-24 14:02:13 +0000

    d7719aa perf(aio): improve unit test rebuild time

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-rc.0-54e587a
+ * @license Angular v4.1.0-rc.0-d7719aa
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-rc.0-54e587a');
+const VERSION = new Version('4.1.0-rc.0-d7719aa');
 
 /**
  * @license
