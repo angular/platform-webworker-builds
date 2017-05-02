394750289f6521bc5499c71b300c879813ca5809
Matias Niemelä <matias@yearofmoo.com>
2017-05-02 22:30:16 +0000

    79ed0e7 feat(aio): display API icons in search results

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-04dc248
+ * @license Angular v4.1.0-79ed0e7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-04dc248');
+const VERSION = new Version('4.1.0-79ed0e7');
 
 /**
  * @license
