5b6ef0684c37b782ddd260777737f6c8e116731e
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 16:54:33 +0000

    9684d78 build: update `concurrently` to latest version

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-71f5b73
+ * @license Angular v4.1.0-9684d78
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-71f5b73');
+const VERSION = new Version('4.1.0-9684d78');
 
 /**
  * @license
