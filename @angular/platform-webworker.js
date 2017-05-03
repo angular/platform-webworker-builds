0e6a1e529a6baa1f048764bd0fbf71b5e78e53d8
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 20:50:01 +0000

    9ca2b4c feat(aio): set “avoid” class by convention (filename has “.avoid.”)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8760bf7
+ * @license Angular v4.1.0-9ca2b4c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-8760bf7');
+const VERSION = new Version('4.1.0-9ca2b4c');
 
 /**
  * @license
