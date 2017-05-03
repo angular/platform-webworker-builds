7b66d556a279a2334a68fe5d18d60131a6d83dfe
Matias Niemelä <matias@yearofmoo.com>
2017-05-03 21:08:13 +0000

    55b8de9 build(aio): lint examples

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-eb56ab3
+ * @license Angular v4.1.0-55b8de9
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-eb56ab3');
+const VERSION = new Version('4.1.0-55b8de9');
 
 /**
  * @license
