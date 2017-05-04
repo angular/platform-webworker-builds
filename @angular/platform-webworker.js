d9f9d64e18175f19bb0620c7b9171e46ea73e4ec
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 20:32:35 +0000

    1092292 test(compiler-cli): add test for missingTranslation parameter

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-8931e71
+ * @license Angular v4.1.0-1092292
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-8931e71');
+const VERSION = new Version('4.1.0-1092292');
 
 /**
  * @license
@@ -1887,6 +1887,14 @@ class WebWorkerRendererFactory2 {
         ]);
         return renderer;
     }
+    /**
+     * @return {?}
+     */
+    begin() { }
+    /**
+     * @return {?}
+     */
+    end() { }
     /**
      * @param {?} fnName
      * @param {?} fnArgs
