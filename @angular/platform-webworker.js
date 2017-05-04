c97759405a06b0c0427d1d909cebdbe3161d0e58
Matias Niemelä <matias@yearofmoo.com>
2017-05-04 02:48:55 +0000

    77b8a76 feat(aio): toc styling

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.0-55b8de9
+ * @license Angular v4.1.0-77b8a76
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.0-55b8de9');
+const VERSION = new Version('4.1.0-77b8a76');
 
 /**
  * @license
