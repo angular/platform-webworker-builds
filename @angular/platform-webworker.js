2d95d624b02273aaecfa7f85750f463ff67c43ff
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 18:36:26 +0000

    547c363 feat: add .ngsummary.ts files to support AOT unit tests

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-d27588b
+ * @license Angular v4.2.0-beta.0-547c363
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-d27588b');
+const VERSION = new Version('4.2.0-beta.0-547c363');
 
 /**
  * @license
