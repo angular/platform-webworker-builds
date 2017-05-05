de97d064f8a90c85c387dd0c8ca19d583cd006b5
Matias Niemelä <matias@yearofmoo.com>
2017-05-05 21:00:30 +0000

    42dc2c1 refactor(aio): remove redundant test

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-9da6340
+ * @license Angular v4.2.0-beta.0-42dc2c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-9da6340');
+const VERSION = new Version('4.2.0-beta.0-42dc2c1');
 
 /**
  * @license
