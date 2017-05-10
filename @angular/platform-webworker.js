df1153d0ede0ed518147a0892f1067be1454e3a9
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-10 14:25:33 +0000

    e242e20 fix(aio): style TOC scrollbar similar to other scrollbars

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.0-f148ebe
+ * @license Angular v4.2.0-beta.0-e242e20
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.0-f148ebe');
+const VERSION = new Version('4.2.0-beta.0-e242e20');
 
 /**
  * @license
