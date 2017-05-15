5086bf16e225ec9b2500d80dbbdf88210f107d75
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-15 20:30:16 +0000

    ea2b24f fix: add typescript 2.3.2 typings test (#16738)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-a9d9aa1
+ * @license Angular v4.2.0-beta.1-ea2b24f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-a9d9aa1');
+const VERSION = new Version('4.2.0-beta.1-ea2b24f');
 
 /**
  * @license
