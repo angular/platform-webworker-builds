0dd6e691b71c5d2bd1334587d6b16b6e54f718e6
Jason Aden <jasonaden@google.com>
2017-05-17 22:32:55 +0000

    6f039d7 build: do not create the bundles when updating the public API

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.1.2-62a8618
+ * @license Angular v4.1.2-6f039d7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.1.2-62a8618');
+const VERSION = new Version('4.1.2-6f039d7');
 
 /**
  * @license
