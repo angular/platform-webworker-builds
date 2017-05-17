8984cdc2fb33e909a6a86b2278649aa220592a27
Matias Niemelä <matias@yearofmoo.com>
2017-05-17 23:47:11 +0000

    712630c perf(animations): reduce size of animations bundle

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-beta.1-6416e79
+ * @license Angular v4.2.0-beta.1-712630c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-beta.1-6416e79');
+const VERSION = new Version('4.2.0-beta.1-712630c');
 
 /**
  * @license
