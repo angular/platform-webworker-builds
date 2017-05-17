bb9b0f5fc487d4889a35a8f70a58a556e0848432
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-17 00:58:14 +0000

    54a6e4f refactor(animations): make animation testing work with fixture.whenRenderingDone

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-d761059
+ * @license Angular v4.2.0-beta.1-54a6e4f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1562,7 +1562,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-d761059');
+var VERSION = new Version('4.2.0-beta.1-54a6e4f');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
