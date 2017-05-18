daf20fd729cab6c4afbcf1075cd835d5ef0641b7
Matias Niemelä <matias@yearofmoo.com>
2017-05-18 02:48:35 +0000

    198edb3 refactor(router): don't run the change detection every time an outlet is activated

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-712630c
+ * @license Angular v4.2.0-beta.1-198edb3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1562,7 +1562,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-712630c');
+var VERSION = new Version('4.2.0-beta.1-198edb3');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
