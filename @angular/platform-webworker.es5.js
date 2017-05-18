7d4c830a763d794e5b7ab13225e40bed3961d964
Jason Aden <jasonaden@users.noreply.github.com>
2017-05-18 20:18:29 +0000

    7d9f96a fix(animations): make sure reuseable animation subtitutions work without default params (#16875)

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-86b7bd9
+ * @license Angular v4.2.0-beta.1-7d9f96a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1562,7 +1562,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-86b7bd9');
+var VERSION = new Version('4.2.0-beta.1-7d9f96a');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
