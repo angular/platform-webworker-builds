c4fd2abbe781df1ac55964521d0bd61b84be9943
Matias Niemelä <matias@yearofmoo.com>
2017-05-19 21:04:30 +0000

    f1a9e3c feat(animations): introduce routeable animation support

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-b10029c
+ * @license Angular v4.2.0-beta.1-f1a9e3c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1562,7 +1562,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-b10029c');
+var VERSION = new Version('4.2.0-beta.1-f1a9e3c');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
