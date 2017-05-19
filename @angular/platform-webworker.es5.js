c5b3d2be8cdfdcf784c96b1f7c1fe98d01cb89b0
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-19 09:00:56 +0000

    b836aca docs(aio): rework of the upgrade guide

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -9,7 +9,7 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 /**
- * @license Angular v4.2.0-beta.1-7d9f96a
+ * @license Angular v4.2.0-beta.1-b836aca
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1562,7 +1562,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.0-beta.1-7d9f96a');
+var VERSION = new Version('4.2.0-beta.1-b836aca');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
