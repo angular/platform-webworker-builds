f1627c929d4d1b696d87bc9a654ed8e5b812ff16
Pete Bacon Darwin <pete@bacondarwin.com>
2017-05-26 05:19:06 +0000

    0c691af build(aio): start using tsc importHelpers option for the app

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-5aa53d7
+ * @license Angular v4.2.0-rc.0-0c691af
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-5aa53d7');
+const VERSION = new Version('4.2.0-rc.0-0c691af');
 
 /**
  * @license
