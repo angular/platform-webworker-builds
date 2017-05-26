4d06de66aee2da35ea608675b5af093efde6d6a7
Chuck Jazdzewski <chuckj@google.com>
2017-05-26 00:02:39 +0000

    2538094 fix(aio): group topbar items into a single sidenav node when narrow screen (#17001)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.0-4c5e28e
+ * @license Angular v4.2.0-rc.0-2538094
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.0-4c5e28e');
+const VERSION = new Version('4.2.0-rc.0-2538094');
 
 /**
  * @license
