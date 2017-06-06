8a0325073277e267be2a7ac52c8617dcacd56cc4
Pete Bacon Darwin <pete@bacondarwin.com>
2017-06-06 15:59:05 +0000

    3a121a6 build(aio): migrate contribute page to html

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-c06f4fc
+ * @license Angular v4.2.0-rc.2-3a121a6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-c06f4fc');
+const VERSION = new Version('4.2.0-rc.2-3a121a6');
 
 /**
  * @license
