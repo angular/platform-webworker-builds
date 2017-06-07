a52e4914ad98f385ec3b972ad2335b57ca29a7cf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-07 18:59:56 +0000

    bb46f54 refactor(aio): use the SelectMenuComponent for all select menus

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-a39f7d6
+ * @license Angular v4.2.0-rc.2-bb46f54
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-a39f7d6');
+const VERSION = new Version('4.2.0-rc.2-bb46f54');
 
 /**
  * @license
