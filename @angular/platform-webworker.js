e81912e935ff8bc96ae76f8577f2aff1e1e579c7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-08 22:55:47 +0000

    cf0a9e0 docs: add changelog for 4.2.0

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-576d6d8
+ * @license Angular v4.2.0-cf0a9e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-576d6d8');
+const VERSION = new Version('4.2.0-cf0a9e0');
 
 /**
  * @license
