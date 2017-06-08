89429578602121fb8905e3ee2e7192f820e294d4
Igor Minar <iminar@google.com>
2017-06-08 19:12:27 +0000

    67ef0f0 fix(aio): api list column overflow fix (#17300)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-rc.2-e9886d7
+ * @license Angular v4.2.0-rc.2-67ef0f0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-rc.2-e9886d7');
+const VERSION = new Version('4.2.0-rc.2-67ef0f0');
 
 /**
  * @license
