3dd332799510c8d88d950ce86b5eff9752563ba3
Igor Minar <iminar@google.com>
2017-06-09 21:43:35 +0000

    65d49d5 fix(aio): temporarily remove link to source from the API pages (#17371)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.0-33df13a
+ * @license Angular v4.2.0-65d49d5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1586,7 +1586,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.0-33df13a');
+var VERSION = new _angular_core.Version('4.2.0-65d49d5');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
