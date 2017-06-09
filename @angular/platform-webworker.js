a218f4c63156ab8e488b4245d4d28678276d6142
Igor Minar <iminar@google.com>
2017-06-09 22:10:07 +0000

    d56b7ed docs(aio): reorganise the sidenav menu (#16934)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.0-c2d31fb
+ * @license Angular v4.2.0-d56b7ed
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.0-c2d31fb');
+const VERSION = new Version('4.2.0-d56b7ed');
 
 /**
  * @license
