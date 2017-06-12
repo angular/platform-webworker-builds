edd0c9bbe1d163e52ac087df408cca32cf2949e7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:18:57 +0000

    234268e ci(aio): deploy commits on the stable branch to production

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-7a2d2ef
+ * @license Angular v4.2.1-234268e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-7a2d2ef');
+const VERSION = new Version('4.2.1-234268e');
 
 /**
  * @license
