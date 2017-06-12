4ca8124a722fb20845b880e91c4fd8fe13132f64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-06-12 23:20:36 +0000

    0268818 build(aio): update Google Analytics property id for the prod environment

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.1-234268e
+ * @license Angular v4.2.1-0268818
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.1-234268e
+ * @license Angular v4.2.1-0268818
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1586,7 +1586,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.1-234268e');
+var VERSION = new _angular_core.Version('4.2.1-0268818');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
