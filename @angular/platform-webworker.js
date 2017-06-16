d5207ee30fca1fe4d968149c91d5e4e5e333f485
Hans <hans@hansl.ca>
2017-06-16 19:35:22 +0000

    fe126cb fix(aio): remove unused icon reference and file

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-adc1b12
+ * @license Angular v4.2.2-fe126cb
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-adc1b12');
+const VERSION = new Version('4.2.2-fe126cb');
 
 /**
  * @license
