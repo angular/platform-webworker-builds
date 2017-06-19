37b7ae3c0394235ddb6222490537c197fbabdeb9
Hans <hans@hansl.ca>
2017-06-19 19:06:28 +0000

    aa92f3a docs(RELEASE_SCHEDULE): update the release schedule w/ recent regression patch releases

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-afbb6bb
+ * @license Angular v4.2.2-aa92f3a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-afbb6bb');
+const VERSION = new Version('4.2.2-aa92f3a');
 
 /**
  * @license
