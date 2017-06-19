46ce32567f3be7ed0339fed1c5d45bbdef403b63
Hans Larsen <hans@hansl.ca>
2017-06-19 23:03:08 +0000

    2e8efda fix(aio): leave results panel open when opening search result on new page

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-09512c7
+ * @license Angular v4.2.3-2e8efda
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-09512c7');
+const VERSION = new Version('4.2.3-2e8efda');
 
 /**
  * @license
