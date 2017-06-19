21d875023a6483d0fd4a96e3faab4ff8b0d9b15f
Hans <hans@hansl.ca>
2017-06-19 18:04:57 +0000

    4f37f86 fix(aio): switch from `innerText` to `textContent` to support older browsers

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.2-3093c55
+ * @license Angular v4.2.2-4f37f86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.2-3093c55');
+const VERSION = new Version('4.2.2-4f37f86');
 
 /**
  * @license
