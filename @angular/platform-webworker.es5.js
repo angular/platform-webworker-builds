c9901638bdb287431ff76524526c03a65e2166d9
Hans Larsen <hans@hansl.ca>
2017-06-20 20:28:00 +0000

    77860a0 fix: argument destructuring sometimes breaks strictNullChecks

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-63a5f33
+ * @license Angular v4.2.3-77860a0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1553,7 +1553,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-63a5f33');
+var VERSION = new Version('4.2.3-77860a0');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
