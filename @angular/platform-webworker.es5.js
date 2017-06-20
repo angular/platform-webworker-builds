29ab15b74047cccc54837966f17790a0c62598fb
Hans Larsen <hans@hansl.ca>
2017-06-20 21:53:51 +0000

    59299de fix(compiler-cli): find lazy routes in nested module import arrays

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.2.3-f997937
+ * @license Angular v4.2.3-59299de
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1553,7 +1553,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.2.3-f997937');
+var VERSION = new Version('4.2.3-59299de');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
