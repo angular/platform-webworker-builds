986e4230e10cd330e34ab7414da9fdd1bfc240c0
Hans Larsen <hans@hansl.ca>
2017-06-22 00:44:08 +0000

    8a09015 docs: add changelog for 4.2.4

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.3-d203136
+ * @license Angular v4.2.4-8a09015
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.3-d203136');
+const VERSION = new Version('4.2.4-8a09015');
 
 /**
  * @license
