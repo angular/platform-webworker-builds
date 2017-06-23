cebf861d9c8de8c54ced12b7b576974821203aef
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:19:41 +0000

    87206e1 fix(aio): preserve newlines when copying code

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-494a0d0
+ * @license Angular v4.3.0-beta.0-87206e1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-494a0d0');
+const VERSION = new Version('4.3.0-beta.0-87206e1');
 
 /**
  * @license
