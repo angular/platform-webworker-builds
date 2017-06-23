c4837357b659880f46898b2e5346029dad5eb944
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:19:51 +0000

    849200b docs: remove unnecessary newline

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-87206e1
+ * @license Angular v4.3.0-beta.0-849200b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-87206e1');
+const VERSION = new Version('4.3.0-beta.0-849200b');
 
 /**
  * @license
