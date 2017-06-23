e1615a422aea51e94ce2560d2282e04cd18c836f
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 17:49:27 +0000

    9344c86 docs: fix “under to hood” typo in changelog.md

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-353de51
+ * @license Angular v4.2.4-9344c86
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.4-353de51');
+const VERSION = new Version('4.2.4-9344c86');
 
 /**
  * @license
