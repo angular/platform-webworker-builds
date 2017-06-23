3b044a8a44f45a80c6b1e0129f54f586449fb9c9
Matias Niemelä <matias@yearofmoo.com>
2017-06-23 19:26:12 +0000

    6191d53 docs(aio): Update resources.json - fixed language error

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-414c7e9
+ * @license Angular v4.3.0-beta.0-6191d53
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-414c7e9');
+const VERSION = new Version('4.3.0-beta.0-6191d53');
 
 /**
  * @license
