650d9920f89df3d1eef8f7ddb9ee3e842ea73eb9
Matias Niemelä <matias@yearofmoo.com>
2017-06-24 00:53:47 +0000

    263c1a1 refactor: remove unused imports of the deprecated Renderer

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-43c187b
+ * @license Angular v4.3.0-beta.0-263c1a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-43c187b');
+const VERSION = new Version('4.3.0-beta.0-263c1a1');
 
 /**
  * @license
