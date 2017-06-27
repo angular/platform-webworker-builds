3272975017ba3afc74b0da3738b23c2bd83697a8
Matias Niemelä <matias@yearofmoo.com>
2017-06-27 20:54:45 +0000

    7205d7a fix(aio): build `scripts-js` before creating a new docker image for the preview server

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.4-ffbdf74
+ * @license Angular v4.2.4-7205d7a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.4-ffbdf74');
+const VERSION = new Version('4.2.4-7205d7a');
 
 /**
  * @license
