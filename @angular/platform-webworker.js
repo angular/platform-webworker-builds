41e83a5dfa1dd02f123bd18865c6d03174aaedce
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 16:54:45 +0000

    4268c82 feat(aio): use shorter URLs for previews

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-af14b1e
+ * @license Angular v4.3.0-beta.0-4268c82
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-af14b1e');
+const VERSION = new Version('4.3.0-beta.0-4268c82');
 
 /**
  * @license
