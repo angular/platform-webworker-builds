bf14e4ae8d0d57f232b9b6a0b488acc1d92e18d0
Matias Niemelä <matias@yearofmoo.com>
2017-06-29 17:06:06 +0000

    96b1703 feat(aio): cross reference readme and docs styleguide

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.0-e47a77f
+ * @license Angular v4.3.0-beta.0-96b1703
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.0-e47a77f');
+const VERSION = new Version('4.3.0-beta.0-96b1703');
 
 /**
  * @license
