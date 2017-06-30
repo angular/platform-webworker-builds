a786ccab1973be1f6fff21156bbedbd25f222825
Stephen Fluin <stephen.fluin@gmail.com>
2017-06-30 20:29:46 +0000

    6b6a8e5 docs(aio): Add resources contributor info

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-69ec333
+ * @license Angular v4.3.0-beta.1-6b6a8e5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-69ec333');
+const VERSION = new Version('4.3.0-beta.1-6b6a8e5');
 
 /**
  * @license
