dc6d1493b20978ecd1d4963cd9998b43d2a0f338
Peter Bacon Darwin <pete@bacondarwin.com>
2017-06-30 11:53:15 +0000

    27b87e8 docs(aio): fix quickstart-cli bad HTML escape chars

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-7080fcc
+ * @license Angular v4.2.5-27b87e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.2.5-7080fcc');
+const VERSION = new Version('4.2.5-27b87e8');
 
 /**
  * @license
