91d395207684b505dac95225144445962187d735
Peter Bacon Darwin <pete@bacondarwin.com>
2017-07-03 07:53:36 +0000

    37b2000 ci(aio): use valid database path for Firebase payload size upload

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.2.5-571adbe
+ * @license Angular v4.2.5-37b2000
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.2.5-571adbe
+ * @license Angular v4.2.5-37b2000
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1586,7 +1586,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.2.5-571adbe');
+var VERSION = new _angular_core.Version('4.2.5-37b2000');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
