77bef485767e215b32e57937040ea1a88f68e3bd
Jason Aden <jasonaden@angular.io>
2017-07-06 17:57:09 +0000

    105e920 fix(animations): properly handle cancelled animation style application

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-71ee0c5
+ * @license Angular v4.3.0-beta.1-105e920
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-71ee0c5');
+const VERSION = new Version('4.3.0-beta.1-105e920');
 
 /**
  * @license
