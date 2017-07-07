fd0ac33e6aaaab71691f2c54327028edf0db2d01
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 16:18:44 +0000

    cb16e9c fix(tsc-wrapped): emit exports metadata in flat modules (#17893)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-6bae73c
+ * @license Angular v4.3.0-beta.1-cb16e9c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-6bae73c');
+const VERSION = new Version('4.3.0-beta.1-cb16e9c');
 
 /**
  * @license
