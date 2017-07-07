e5a33f3a3a7343aa4e58c19c0619c2d99e406c23
Jason Aden <jasonaden@angular.io>
2017-07-07 16:13:24 +0000

    11db3bd fix(upgrade): bring the dynamic version closer to the static one (#17971)

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v4.3.0-beta.1-3b2d2c4
+ * @license Angular v4.3.0-beta.1-11db3bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1553,7 +1553,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('4.3.0-beta.1-3b2d2c4');
+var VERSION = new Version('4.3.0-beta.1-11db3bd');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
