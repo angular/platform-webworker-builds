3968169f7fbe247919594ebb336c74f11a1457d1
Jason Aden <jasonaden@users.noreply.github.com>
2017-07-07 23:51:07 +0000

    671a175 fix(compiler-cli): fix relative source paths on windows for extracted msg (#17915)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-beta.1-9c3386b
+ * @license Angular v4.3.0-beta.1-671a175
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-beta.1-9c3386b');
+const VERSION = new Version('4.3.0-beta.1-671a175');
 
 /**
  * @license
