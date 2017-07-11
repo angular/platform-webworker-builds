84f74f745eb5700cd8d7bca21c62cb566fce7e9f
Igor Minar <iminar@google.com>
2017-07-11 19:43:23 +0000

    1b1a6ba docs: fix 404 link to SFEIR website (#17913)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-df06e8b
+ * @license Angular v4.3.0-rc.0-1b1a6ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-df06e8b');
+const VERSION = new Version('4.3.0-rc.0-1b1a6ba');
 
 /**
  * @license
