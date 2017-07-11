d6ab1105e45159bd188cd9d531975060805c8f6a
Igor Minar <iminar@google.com>
2017-07-11 19:05:10 +0000

    e131f6b docs: various minor formatting changes (#18046)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-e28f097
+ * @license Angular v4.3.0-rc.0-e131f6b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-e28f097');
+const VERSION = new Version('4.3.0-rc.0-e131f6b');
 
 /**
  * @license
