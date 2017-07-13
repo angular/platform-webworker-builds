ca5ba1aaac304273723f6d03e729cb7d3a2b804d
Igor Minar <igor@angularjs.org>
2017-07-13 22:53:43 +0000

    72143e8 revert: ci(aio): exclude changes in aio/content folder

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-45ffe54
+ * @license Angular v4.3.0-rc.0-72143e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-45ffe54');
+const VERSION = new Version('4.3.0-rc.0-72143e8');
 
 /**
  * @license
