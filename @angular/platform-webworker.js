a1095b9bd9c6b05cffc01340771ad2f9d39f2238
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-14 18:00:23 +0000

    01a2688 fix(animations): make sure @.disabled works in non-animation components

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-rc.0-8e56c3c
+ * @license Angular v4.3.0-rc.0-01a2688
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-rc.0-8e56c3c');
+const VERSION = new Version('4.3.0-rc.0-01a2688');
 
 /**
  * @license
