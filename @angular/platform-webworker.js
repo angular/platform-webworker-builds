7b324c2d7ff26845c40207c53611ddc25112c4f7
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-17 21:39:37 +0000

    c6ad212 docs(aio): fix HttpClient's interceptor sample

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-8c81c62
+ * @license Angular v4.3.0-c6ad212
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-8c81c62');
+const VERSION = new Version('4.3.0-c6ad212');
 
 /**
  * @license
