bd6ee71e04bab97eb933001dc9be5b75afdd993e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 21:39:23 +0000

    4ce29f3 fix(platform-server): provide XhrFactory for HttpClient

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-f19bd5f
+ * @license Angular v4.3.0-4ce29f3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-f19bd5f');
+const VERSION = new Version('4.3.0-4ce29f3');
 
 /**
  * @license
