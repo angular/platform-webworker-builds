9dc68c58e20ff6995e56d191fafa50ebd488839e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:18:27 +0000

    3b588fe docs: fix typo

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-e03adb9
+ * @license Angular v4.3.0-3b588fe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-e03adb9');
+const VERSION = new Version('4.3.0-3b588fe');
 
 /**
  * @license
