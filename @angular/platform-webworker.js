2959235d85f0a1338d1b9d3700b547e51c44b88a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 22:40:20 +0000

    d22f8f5 fix(animations): always camelcase style property names that contain auto styles

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-8076482
+ * @license Angular v4.3.0-d22f8f5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-8076482');
+const VERSION = new Version('4.3.0-d22f8f5');
 
 /**
  * @license
