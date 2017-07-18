7ee51f6675c5605cbb18a814490af4358b030860
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-18 18:35:05 +0000

    256bc8a docs(http): Make name of injected HttpTestingController consistent

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.0-59c23c7
+ * @license Angular v4.3.0-256bc8a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.0-59c23c7');
+const VERSION = new Version('4.3.0-256bc8a');
 
 /**
  * @license
