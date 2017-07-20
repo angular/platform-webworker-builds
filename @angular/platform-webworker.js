0487939401fb4821a9fe7f200a036c69bedc229b
Miško Hevery <misko@hevery.com>
2017-07-20 17:27:20 +0000

    8d28191 fix(aio): invalid formatting in architecure.md (#18159)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-31cb418
+ * @license Angular v5.0.0-beta.0-8d28191
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-31cb418');
+const VERSION = new Version('5.0.0-beta.0-8d28191');
 
 /**
  * @license
