64bd341d26b9be241e669344ed0469f4f5c52764
Miško Hevery <misko@hevery.com>
2017-07-20 16:56:39 +0000

    31cb418 docs(aio): Fix http guide

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-8de44cf
+ * @license Angular v5.0.0-beta.0-31cb418
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.0-8de44cf');
+const VERSION = new Version('5.0.0-beta.0-31cb418');
 
 /**
  * @license
