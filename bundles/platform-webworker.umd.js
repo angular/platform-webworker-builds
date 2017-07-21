1e130f767664fcb9cb78810e901d594ab42642d6
Miško Hevery <misko@hevery.com>
2017-07-21 17:41:38 +0000

    a6c635e ci: force precise on Travis (#18282)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.0-54e0244
+ * @license Angular v5.0.0-beta.0-a6c635e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.0-54e0244
+ * @license Angular v5.0.0-beta.0-a6c635e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1586,7 +1586,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.0-54e0244');
+var VERSION = new _angular_core.Version('5.0.0-beta.0-a6c635e');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
