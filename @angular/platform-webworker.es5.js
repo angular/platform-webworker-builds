3174c8ce055101f0629abde13ffa9030f2490248
Miško Hevery <misko@hevery.com>
2017-07-26 17:12:21 +0000

    205abe8 build: fix broken bazel build (#18335)

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.0-b582e2b
+ * @license Angular v5.0.0-beta.0-205abe8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1528,7 +1528,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.0-b582e2b');
+var VERSION = new Version('5.0.0-beta.0-205abe8');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
