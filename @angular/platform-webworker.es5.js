bceaa2486e84e4effbf7b225ef0c7aad8a05cf9a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-28 22:26:27 +0000

    0fb7484 refactor(aio): move content-specific images to `content/images/`

--- @angular/platform-webworker.es5.js
+++ @angular/platform-webworker.es5.js
@@ -1,6 +1,6 @@
 import * as tslib_1 from "tslib";
 /**
- * @license Angular v5.0.0-beta.1-5808153
+ * @license Angular v5.0.0-beta.1-0fb7484
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1528,7 +1528,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-beta.1-5808153');
+var VERSION = new Version('5.0.0-beta.1-0fb7484');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-5808153
+ * @license Angular v5.0.0-beta.1-0fb7484
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1481,7 +1481,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-5808153');
+const VERSION = new Version('5.0.0-beta.1-0fb7484');
 
 /**
  * @fileoverview added by tsickle
