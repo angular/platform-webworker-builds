9e322a1e41ab79ce231229c428975905ca21e39c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 23:15:06 +0000

    9479a10 build: enable TSLint on the packages folder

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-e64b54b
+ * @license Angular v5.0.0-beta.1-9479a10
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1481,7 +1481,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.1-e64b54b');
+const VERSION = new Version('5.0.0-beta.1-9479a10');
 
 /**
  * @fileoverview added by tsickle
