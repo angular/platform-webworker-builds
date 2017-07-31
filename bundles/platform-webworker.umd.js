25b24b3044107c4a45483c9713c4ebfc4d42f397
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-07-31 19:58:16 +0000

    a11542a docs(aio): fixed list format in FormArray section

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.1-b6c4af6
+ * @license Angular v5.0.0-beta.1-a11542a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.1-b6c4af6
+ * @license Angular v5.0.0-beta.1-a11542a
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1561,7 +1561,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.1-b6c4af6');
+var VERSION = new _angular_core.Version('5.0.0-beta.1-a11542a');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
