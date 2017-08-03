6a94bbc6e04882c4438e360c5715ea5aa3bf3d74
Victor Berchet <victor@suumit.com>
2017-08-03 18:24:38 +0000

    e7e7622 fix(compiler): ignore @import in multi-line css (#18452)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-4176832
+ * @license Angular v4.3.3-e7e7622
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-4176832');
+const VERSION = new Version('4.3.3-e7e7622');
 
 /**
  * @license
