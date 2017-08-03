afc3b603c48ed6ebb9e50e95fe21064731a55341
Victor Berchet <victor@suumit.com>
2017-08-03 00:42:09 +0000

    ea07856 refactor(upgrade): compile upgrade cleanly with TypeScript 2.4 (#18461)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-7c47b62
+ * @license Angular v5.0.0-beta.2-ea07856
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.2-7c47b62
+ * @license Angular v5.0.0-beta.2-ea07856
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1561,7 +1561,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.2-7c47b62');
+var VERSION = new _angular_core.Version('5.0.0-beta.2-ea07856');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
