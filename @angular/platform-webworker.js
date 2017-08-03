8f3916df3c40a5d57ce2647b6b49bcbef21580c7
Victor Berchet <victor@suumit.com>
2017-08-03 01:16:35 +0000

    3a227a1 refactor(router): compile router cleanly with TypeScript 2.4 (#18465)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-81cb5bc
+ * @license Angular v5.0.0-beta.2-3a227a1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1481,7 +1481,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-81cb5bc');
+const VERSION = new Version('5.0.0-beta.2-3a227a1');
 
 /**
  * @fileoverview added by tsickle
