d74f4f4a5b8f866359888eb5ade6c1cfe44702b5
Victor Berchet <victor@suumit.com>
2017-08-07 18:04:55 +0000

    3212f8c docs(aio): typo & update the bio (#18559)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-c421cca
+ * @license Angular v4.3.3-3212f8c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-c421cca');
+const VERSION = new Version('4.3.3-3212f8c');
 
 /**
  * @license
