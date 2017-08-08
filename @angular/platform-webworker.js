773aabdeb8c8017e3342f02b69d7c8037fbb485f
Victor Berchet <victor@suumit.com>
2017-08-08 21:32:17 +0000

    f0c3ed0 ci(aio): fix deploying to firebase (#18590)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.3-e0660b1
+ * @license Angular v4.3.3-f0c3ed0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.3-e0660b1');
+const VERSION = new Version('4.3.3-f0c3ed0');
 
 /**
  * @license
