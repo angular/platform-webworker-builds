4e1472f8e5e947d15f33383412b00a446a5d94f5
Victor Berchet <victor@suumit.com>
2017-08-09 22:03:32 +0000

    7f2037f test(aio): fix running docs examples against local builds (#18520)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.2-fd6ae57
+ * @license Angular v5.0.0-beta.2-7f2037f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1502,7 +1502,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.2-fd6ae57');
+const VERSION = new Version('5.0.0-beta.2-7f2037f');
 
 /**
  * @fileoverview added by tsickle
