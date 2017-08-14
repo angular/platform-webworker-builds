84e8a9bad6bc0e3994b1125cd7d69ae39cddf1a4
Hans <hans@hansl.ca>
2017-08-14 20:54:14 +0000

    d2c0d98 perf(core): add option to remove blank text nodes from compiled templates

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-088532b
+ * @license Angular v5.0.0-beta.3-d2c0d98
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1502,7 +1502,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-088532b');
+const VERSION = new Version('5.0.0-beta.3-d2c0d98');
 
 /**
  * @fileoverview added by tsickle
