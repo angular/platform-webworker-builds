5a57ec958cc82f9265b5c084681d74d757e00a8a
Hans <hans@hansl.ca>
2017-08-15 23:11:50 +0000

    856278c docs(core): correct code examples for ChangeDetectorRef

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-5e840e1
+ * @license Angular v5.0.0-beta.3-856278c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.3-5e840e1
+ * @license Angular v5.0.0-beta.3-856278c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1582,7 +1582,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.3-5e840e1');
+var VERSION = new _angular_core.Version('5.0.0-beta.3-856278c');
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes} checked by tsc
