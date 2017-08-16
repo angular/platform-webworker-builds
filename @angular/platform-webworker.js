bab49d76372e10ecb0d9e2b3c51cecb0a58e5729
Hans <hans@hansl.ca>
2017-08-16 00:08:17 +0000

    0a73e8d feat(common): mark NgTemplateOutlet API as stable

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-1cfa79c
+ * @license Angular v5.0.0-beta.3-0a73e8d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1502,7 +1502,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-1cfa79c');
+const VERSION = new Version('5.0.0-beta.3-0a73e8d');
 
 /**
  * @fileoverview added by tsickle
