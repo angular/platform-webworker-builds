230b73e365b88b42fc353d7ff0a652b989a45f29
Hans Larsen <hans@hansl.ca>
2017-08-16 18:26:59 +0000

    75d484e docs: add changelog for 4.3.5

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-32ff21c
+ * @license Angular v5.0.0-beta.3-75d484e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1502,7 +1502,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-32ff21c');
+const VERSION = new Version('5.0.0-beta.3-75d484e');
 
 /**
  * @fileoverview added by tsickle
