de2d2ae8e014a62091cdabe01c9eee6258d15b14
Hans <hans@hansl.ca>
2017-08-16 18:54:08 +0000

    bc22ff1 fix(language-service): remove tsickle dependency

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.3-75d484e
+ * @license Angular v5.0.0-beta.3-bc22ff1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1502,7 +1502,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.3-75d484e');
+const VERSION = new Version('5.0.0-beta.3-bc22ff1');
 
 /**
  * @fileoverview added by tsickle
