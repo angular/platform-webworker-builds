48a07b0130c1eecc065a0f7465d60a22ef300509
Miško Hevery <misko@hevery.com>
2017-08-18 18:32:50 +0000

    a56468c refactor(platform-webworker): remove deprecated `PRIMITIVE` (#18761)

--- src/platform-webworker.d.ts
+++ src/platform-webworker.d.ts
@@ -9,7 +9,7 @@ import { PlatformRef, StaticProvider } from '@angular/core';
 export { VERSION } from './version';
 export { ClientMessageBroker, ClientMessageBrokerFactory, FnArg, UiArguments } from './web_workers/shared/client_message_broker';
 export { MessageBus, MessageBusSink, MessageBusSource } from './web_workers/shared/message_bus';
-export { PRIMITIVE, SerializerTypes } from './web_workers/shared/serializer';
+export { SerializerTypes } from './web_workers/shared/serializer';
 export { ReceivedMessage, ServiceMessageBroker, ServiceMessageBrokerFactory } from './web_workers/shared/service_message_broker';
 export { WORKER_UI_LOCATION_PROVIDERS } from './web_workers/ui/location_providers';
 export { WORKER_APP_LOCATION_PROVIDERS } from './web_workers/worker/location_providers';
