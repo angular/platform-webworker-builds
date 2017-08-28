a29a58d926214a9bcf28e3c9a2fe1df8ee184a38
Miško Hevery <misko@hevery.com>
2017-08-28 21:46:46 +0000

    655b495 build: reenable npm install for air builds

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.4-92dbaf3
+ * @license Angular v5.0.0-beta.4-655b495
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1495,7 +1495,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.4-92dbaf3');
+const VERSION = new Version('5.0.0-beta.4-655b495');
 
 /**
  * @fileoverview added by tsickle
