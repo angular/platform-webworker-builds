bda7a3a875db0ab1244f4c901091186244549d36
Jason Aden <jasonaden@google.com>
2017-08-31 22:58:55 +0000

    ee04217 refactor(compiler-cli): expose ngtools api separately (#18978)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.5-a1293b2
+ * @license Angular v5.0.0-beta.5-ee04217
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1495,7 +1495,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-beta.5-a1293b2');
+const VERSION = new Version('5.0.0-beta.5-ee04217');
 
 /**
  * @fileoverview added by tsickle
