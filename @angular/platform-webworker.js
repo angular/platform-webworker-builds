50dde7da1273aecc509fb9e90495d1198ab61dfd
Jason Aden <jasonaden@google.com>
2017-09-01 17:54:21 +0000

    1cc3fe2 fix(animations): do not leak DOM nodes/styling for host triggered animations (#18853)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.3.6-ba7d70e
+ * @license Angular v4.3.6-1cc3fe2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.3.6-ba7d70e');
+const VERSION = new Version('4.3.6-1cc3fe2');
 
 /**
  * @license
