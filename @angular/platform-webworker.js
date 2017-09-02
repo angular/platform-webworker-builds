9c610c2bb72d74d2c637b3387d1528ee569dc65c
Misko Hevery <misko@hevery.com>
2017-09-02 05:48:22 +0000

    aeb98db docs: add changelog for 4.4.0-RC.0

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.0-RC.0-8036d05
+ * @license Angular v4.4.0-RC.0-aeb98db
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.0-RC.0-8036d05');
+const VERSION = new Version('4.4.0-RC.0-aeb98db');
 
 /**
  * @license
