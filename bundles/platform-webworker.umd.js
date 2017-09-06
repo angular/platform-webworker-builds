3afca511e722abe5909a804a5b2d8eee459bd05a
Miško Hevery <misko@hevery.com>
2017-09-06 04:39:30 +0000

    15945c8 Revert "fix(animations): ensure animateChild() works with all inner leave animations (#19006)"

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-66f0ab0
+ * @license Angular v5.0.0-beta.6-15945c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.6-66f0ab0
+ * @license Angular v5.0.0-beta.6-15945c8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1597,7 +1597,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-66f0ab0');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-15945c8');
 
 /**
  * @fileoverview added by tsickle
