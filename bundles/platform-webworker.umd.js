eb439ac57f406e35e0dfe88f97d1a1da633be0a8
Matias Niemelä <matias@yearofmoo.com>
2017-09-08 21:43:45 +0000

    626555c fix(tsc-wrapped): fix metadata symbol reference

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.6-8f79150
+ * @license Angular v5.0.0-beta.6-626555c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.6-8f79150
+ * @license Angular v5.0.0-beta.6-626555c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1597,7 +1597,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.6-8f79150');
+var VERSION = new _angular_core.Version('5.0.0-beta.6-626555c');
 
 /**
  * @fileoverview added by tsickle
