9f72e1ed10b97606495686afdb0ba266a760d706
Igor Minar <igor@angularjs.org>
2017-09-19 04:36:36 +0000

    49d122e release: cut the 4.4.2 release

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.1-ff87294
+ * @license Angular v4.4.2-49d122e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.1-ff87294');
+const VERSION = new Version('4.4.2-49d122e');
 
 /**
  * @license
