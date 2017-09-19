727589e8127f4936b957a27af4a2c92fbc14fc0a
Igor Minar <igor@angularjs.org>
2017-09-19 23:00:34 +0000

    e17128e docs: cherry-pick changelog for 4.4.2 and 4.4.3

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-b6b18c1
+ * @license Angular v5.0.0-beta.7-e17128e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-b6b18c1
+ * @license Angular v5.0.0-beta.7-e17128e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-b6b18c1');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-e17128e');
 
 /**
  * @fileoverview added by tsickle
