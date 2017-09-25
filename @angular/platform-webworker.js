b82e3d9b739f1fd0962cfaa14d5f15889087c336
Victor Berchet <victor@suumit.com>
2017-09-25 19:35:04 +0000

    5c99b01 build(aio): run the upload server as a non-previleged user

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-04bc525
+ * @license Angular v4.4.3-5c99b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-04bc525');
+const VERSION = new Version('4.4.3-5c99b01');
 
 /**
  * @license
