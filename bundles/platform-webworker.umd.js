a610121e22377a5ae6f4e6a0b0937eac8f250b00
Victor Berchet <victor@suumit.com>
2017-09-26 17:04:24 +0000

    2cf52c1 test: Enable sass compiler for bazel integration test (#19357)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-beta.7-27c6638
+ * @license Angular v5.0.0-beta.7-2cf52c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-beta.7-27c6638
+ * @license Angular v5.0.0-beta.7-2cf52c1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-beta.7-27c6638');
+var VERSION = new _angular_core.Version('5.0.0-beta.7-2cf52c1');
 
 /**
  * @fileoverview added by tsickle
