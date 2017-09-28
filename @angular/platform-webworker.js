d416db4134bee57a524e8fb5de27498f7b0ec09a
Victor Berchet <victor@suumit.com>
2017-09-28 19:26:12 +0000

    7fc2dce fix(compiler): do not consider a reference with members as a reference (#19466)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.3-c3b39ba
+ * @license Angular v4.4.3-7fc2dce
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.3-c3b39ba');
+const VERSION = new Version('4.4.3-7fc2dce');
 
 /**
  * @license
