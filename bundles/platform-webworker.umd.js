beae8d4ccf292d5cbd900eaae0e462bbe05c5789
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-10-02 15:45:58 +0000

    745b59f perf(compiler): only emit changed files for incremental compilation

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.0-0038712
+ * @license Angular v5.0.0-rc.0-745b59f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.0-0038712
+ * @license Angular v5.0.0-rc.0-745b59f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.0-0038712');
+var VERSION = new _angular_core.Version('5.0.0-rc.0-745b59f');
 
 /**
  * @fileoverview added by tsickle
