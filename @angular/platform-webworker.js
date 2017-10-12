6cb5119f628eb8140e3b59821f8794d451b9de7d
Chuck Jazdzewski <chuckj@google.com>
2017-10-12 17:12:15 +0000

    f27f6e4 docs(aio): improve the Angular Connect homepage banner (#19684)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.4-a15abbb
+ * @license Angular v4.4.4-f27f6e4
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.4-a15abbb');
+const VERSION = new Version('4.4.4-f27f6e4');
 
 /**
  * @license
