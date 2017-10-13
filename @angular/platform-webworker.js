0750d6a63fd9dafd3cb6ab410bcaa94f399523c8
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 17:05:43 +0000

    f983a6c fix(animations): properly support boolean-based transitions and state changes (#19672)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-18f1b01
+ * @license Angular v4.4.5-f983a6c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-18f1b01');
+const VERSION = new Version('4.4.5-f983a6c');
 
 /**
  * @license
