ef95ea7f3a70eca8d5e68719f79170e4d060baa9
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 16:59:55 +0000

    18f1b01 build(aio): consider `devDependencies` when overwriting dependencies of local Angular packages (#19687)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-591dcc2
+ * @license Angular v4.4.5-18f1b01
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-591dcc2');
+const VERSION = new Version('4.4.5-18f1b01');
 
 /**
  * @license
