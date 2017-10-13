cde3140c84e1f7398ba7f913d25536b2a319308e
Tobias Bosch <tbosch1009@gmail.com>
2017-10-13 22:50:56 +0000

    1be22df ci(aio): raise payload limit to accommodate the new search feature (#19704)

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.5-f983a6c
+ * @license Angular v4.4.5-1be22df
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.5-f983a6c');
+const VERSION = new Version('4.4.5-1be22df');
 
 /**
  * @license
