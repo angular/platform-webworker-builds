0da9dcae02b8b9f0af4a84cbdc09a06c91fba1c6
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 05:42:45 +0000

    005a78b refactor: allow compilation with TypeScript 2.5 (#19966)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.9-7553ce9
+ * @license Angular v5.0.0-rc.9-005a78b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.9-7553ce9
+ * @license Angular v5.0.0-rc.9-005a78b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.9-7553ce9');
+var VERSION = new _angular_core.Version('5.0.0-rc.9-005a78b');
 
 /**
  * @fileoverview added by tsickle
