7d5dc853f2274ac144e5cf57bca263bdafd47aa0
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 04:47:31 +0000

    f5b7f2b docs: add changelog for 5.0.0-rc.9

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-cf5fce8
+ * @license Angular v5.0.0-rc.9-f5b7f2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-cf5fce8');
+var VERSION = new _angular_core.Version('5.0.0-rc.9-f5b7f2b');
 
 /**
  * @fileoverview added by tsickle
