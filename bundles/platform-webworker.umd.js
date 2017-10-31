248eca54913ca81c287927a5962c9c0c2dd923d5
Matias Niemelä <matias@yearofmoo.com>
2017-10-31 00:58:07 +0000

    fd37f3f fix(compiler): always use relative paths to refer to generated code

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-rc.8-85e95cc
+ * @license Angular v5.0.0-rc.8-fd37f3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-rc.8-85e95cc
+ * @license Angular v5.0.0-rc.8-fd37f3f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-rc.8-85e95cc');
+var VERSION = new _angular_core.Version('5.0.0-rc.8-fd37f3f');
 
 /**
  * @fileoverview added by tsickle
