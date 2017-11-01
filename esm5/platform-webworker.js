bf44581024532c9c2e6fef399eeb64649a8e4dc9
Matias Niemelä <matias@yearofmoo.com>
2017-11-01 21:46:15 +0000

    ca129ba docs(aio): upgrade to v5 and cli 1.5

--- esm5/platform-webworker.js
+++ esm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-171ae15
+ * @license Angular v5.0.0-ca129ba
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1779,7 +1779,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-171ae15');
+var VERSION = new Version('5.0.0-ca129ba');
 
 /**
  * @fileoverview added by tsickle
