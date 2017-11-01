e5bc700c6246c3cced578513512671838e16d971
Igor Minar <igor@angular.io>
2017-11-01 23:46:09 +0000

    2ea76cc build: change the version in package.json to 5.1.0-beta

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-27ae0f9
+ * @license Angular v5.1.0-beta.0-2ea76cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-27ae0f9
+ * @license Angular v5.1.0-beta.0-2ea76cc
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-27ae0f9');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-2ea76cc');
 
 /**
  * @fileoverview added by tsickle
