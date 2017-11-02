f19bcb7707ccaaaa8ba2462e130a940a05e9b71f
Igor Minar <igor@angular.io>
2017-11-02 00:40:10 +0000

    18197bd docs(aio): fix linting for universal (#20086)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-c75740e
+ * @license Angular v5.0.0-18197bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-c75740e
+ * @license Angular v5.0.0-18197bd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-c75740e');
+var VERSION = new _angular_core.Version('5.0.0-18197bd');
 
 /**
  * @fileoverview added by tsickle
