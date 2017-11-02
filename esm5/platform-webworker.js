378d2e25febb735d682fe77fef1ea3124a64ba41
Victor Berchet <victor@suumit.com>
2017-11-02 23:43:36 +0000

    678d1cf fix(core): __symbol__ should return __zone_symbol__ without zone.js loaded (#19541)

--- esm5/platform-webworker.js
+++ esm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-25843fe
+ * @license Angular v5.0.0-678d1cf
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1779,7 +1779,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.0.0-25843fe');
+var VERSION = new Version('5.0.0-678d1cf');
 
 /**
  * @fileoverview added by tsickle
