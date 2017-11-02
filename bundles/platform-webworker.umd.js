c523997c2cf0aa06069ec932d35ca0258524bf5d
Victor Berchet <victor@suumit.com>
2017-11-02 22:39:47 +0000

    068348e build(aio): do not fail the initial doc-gen when running docs-watch

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-5b16ce9
+ * @license Angular v5.1.0-beta.0-068348e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-5b16ce9');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-068348e');
 
 /**
  * @fileoverview added by tsickle
