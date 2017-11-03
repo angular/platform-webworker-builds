e19500044c107093f80edb7b9537acc322cb9f4c
Victor Berchet <victor@suumit.com>
2017-11-03 19:03:39 +0000

    e6f16a7 docs(aio): Fix typo in Architecture Overview. (#20073)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.0-678d1cf
+ * @license Angular v5.0.0-e6f16a7
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.0-678d1cf');
+var VERSION = new _angular_core.Version('5.0.0-e6f16a7');
 
 /**
  * @fileoverview added by tsickle
