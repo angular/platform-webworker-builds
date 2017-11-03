46434e380f9d145b61396f58130cb5613111207c
Victor Berchet <victor@suumit.com>
2017-11-03 19:53:05 +0000

    a99eb16 test(aio): make e2e tests less flakey

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-faa6212
+ * @license Angular v5.1.0-beta.0-a99eb16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-faa6212
+ * @license Angular v5.1.0-beta.0-a99eb16
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-faa6212');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-a99eb16');
 
 /**
  * @fileoverview added by tsickle
