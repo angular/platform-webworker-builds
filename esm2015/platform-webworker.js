e53e3f79c42b13cb65794e331b9643e50f0e22f1
Victor Berchet <victor@suumit.com>
2017-11-06 20:06:17 +0000

    511e56d docs(common): fix mis-ordered lines (#20221)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.0-1f1741c
+ * @license Angular v5.0.0-511e56d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1479,7 +1479,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.0-1f1741c');
+const VERSION = new Version('5.0.0-511e56d');
 
 /**
  * @fileoverview added by tsickle
