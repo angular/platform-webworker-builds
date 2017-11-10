d7113b792074430639d8de3ac03d6d770a873ba6
Jason Aden <jasonaden@google.com>
2017-11-10 23:06:28 +0000

    24cf8b3 fix(core): ensure init lifecycle events are called (#20258)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-161f88f
+ * @license Angular v5.1.0-beta.0-24cf8b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-161f88f
+ * @license Angular v5.1.0-beta.0-24cf8b3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-161f88f');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-24cf8b3');
 
 /**
  * @fileoverview added by tsickle
