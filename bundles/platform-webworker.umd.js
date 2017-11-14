681e79e7222b9f5dd802b34ace497225e4b94799
Jason Aden <jasonaden@google.com>
2017-11-14 18:25:18 +0000

    b9bd320 docs(aio): fix broken link in guide/component-interaction (#20411)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-24cf8b3
+ * @license Angular v5.1.0-beta.0-b9bd320
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-24cf8b3
+ * @license Angular v5.1.0-beta.0-b9bd320
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-24cf8b3');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-b9bd320');
 
 /**
  * @fileoverview added by tsickle
