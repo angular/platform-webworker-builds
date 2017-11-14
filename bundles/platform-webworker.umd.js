7bd22636734ff0e462fd731d2a5af32af0c1db87
Jason Aden <jasonaden@google.com>
2017-11-14 22:07:12 +0000

    20aafff fix(animations): ensure final state() styles are applied within @.disabled animations (#20267)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-12af6d3
+ * @license Angular v5.1.0-beta.0-20aafff
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-12af6d3');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-20aafff');
 
 /**
  * @fileoverview added by tsickle
