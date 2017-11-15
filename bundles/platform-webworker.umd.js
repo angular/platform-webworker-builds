c9173e7432f6cab023b1bcc96e5ce23e4389c2fa
Jason Aden <jasonaden@google.com>
2017-11-15 20:19:01 +0000

    2d6126e style(core): fix comment format for linter

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.1-bc4b4b5
+ * @license Angular v5.0.1-2d6126e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.1-bc4b4b5');
+var VERSION = new _angular_core.Version('5.0.1-2d6126e');
 
 /**
  * @fileoverview added by tsickle
