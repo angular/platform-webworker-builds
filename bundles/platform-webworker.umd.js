657cf629f10bc94ccd4d4099476b3cfb5bc46d44
Miško Hevery <misko@hevery.com>
2017-11-16 01:01:31 +0000

    f8658cd Revert "fix(animations): always fire inner trigger callbacks even if blocked by parent animations (#19753)"

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.0-043e408
+ * @license Angular v5.1.0-beta.0-f8658cd
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.0-043e408');
+var VERSION = new _angular_core.Version('5.1.0-beta.0-f8658cd');
 
 /**
  * @fileoverview added by tsickle
