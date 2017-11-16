9d664fb971e55a7f5bed12096ed42cedf40b417c
Miško Hevery <misko@hevery.com>
2017-11-16 04:50:55 +0000

    1285792 fix(aio): fix window title on Home page (#20440)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.1-2d6126e
+ * @license Angular v5.0.1-1285792
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.1-2d6126e');
+var VERSION = new _angular_core.Version('5.0.1-1285792');
 
 /**
  * @fileoverview added by tsickle
