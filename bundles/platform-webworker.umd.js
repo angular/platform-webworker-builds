dc6c0e203ba7ea0e6957f21d69aa4c44d188fd2b
Miško Hevery <misko@hevery.com>
2017-11-17 22:10:11 +0000

    365712e docs(aio): fix typo in Attribute Directives documentation (#20143)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-1861e41
+ * @license Angular v5.1.0-beta.1-365712e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-1861e41');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-365712e');
 
 /**
  * @fileoverview added by tsickle
