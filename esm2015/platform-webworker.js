51207582669cc27d95f165d1d37e2bc37901ca71
Miško Hevery <misko@hevery.com>
2017-11-18 00:42:51 +0000

    82aace6 fix(core): should support event.stopImmediatePropagation (#20469)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.2-b1f8eb1
+ * @license Angular v5.0.2-82aace6
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1479,7 +1479,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.2-b1f8eb1');
+const VERSION = new Version('5.0.2-82aace6');
 
 /**
  * @fileoverview added by tsickle
