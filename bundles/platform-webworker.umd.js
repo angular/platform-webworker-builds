c3264a13b8bc8515128399a3ec980405a6540481
Miško Hevery <misko@hevery.com>
2017-11-18 00:37:40 +0000

    69c53c3 fix(forms): updateOn should check if change occurred (#20358)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-365712e
+ * @license Angular v5.1.0-beta.1-69c53c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-365712e
+ * @license Angular v5.1.0-beta.1-69c53c3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-365712e');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-69c53c3');
 
 /**
  * @fileoverview added by tsickle
