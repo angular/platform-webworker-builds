8ce2bc9e5f0736ac9742ceb1f148419f68193ea9
Miško Hevery <misko@hevery.com>
2017-11-22 20:10:09 +0000

    ffb6dbe fix(animations): always fire start and done callbacks in order for noop animations (#20570)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-0e012c9
+ * @license Angular v5.1.0-beta.1-ffb6dbe
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-0e012c9');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-ffb6dbe');
 
 /**
  * @fileoverview added by tsickle
