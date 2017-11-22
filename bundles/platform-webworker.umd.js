cf6f9f419ecbdb56fc328dea9557d0f608482a96
Miško Hevery <misko@hevery.com>
2017-11-22 20:12:52 +0000

    0444e13 docs(aio): add ngATL header (#20590)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.1-ffb6dbe
+ * @license Angular v5.1.0-beta.1-0444e13
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.1-ffb6dbe
+ * @license Angular v5.1.0-beta.1-0444e13
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.1-ffb6dbe');
+var VERSION = new _angular_core.Version('5.1.0-beta.1-0444e13');
 
 /**
  * @fileoverview added by tsickle
