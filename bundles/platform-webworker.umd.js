d9c0b5a8881313a3d5c4624dcad379a0e767daad
Miško Hevery <misko@hevery.com>
2017-11-27 20:03:38 +0000

    a53a040 build(aio): prevent comments in code from leaking into doc-gen code snippets (#20607)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-beta.2-330bb2a
+ * @license Angular v5.1.0-beta.2-a53a040
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-beta.2-330bb2a');
+var VERSION = new _angular_core.Version('5.1.0-beta.2-a53a040');
 
 /**
  * @fileoverview added by tsickle
