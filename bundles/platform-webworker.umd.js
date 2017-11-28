f8dc8881a5315dc928c0cfc49bca3895d6511f52
Miško Hevery <misko@hevery.com>
2017-11-28 01:13:32 +0000

    c7b211c fix(animations): ensure multi-level leave animations work (#19455)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-2a5b619
+ * @license Angular v5.0.3-c7b211c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.3-2a5b619
+ * @license Angular v5.0.3-c7b211c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.3-2a5b619');
+var VERSION = new _angular_core.Version('5.0.3-c7b211c');
 
 /**
  * @fileoverview added by tsickle
