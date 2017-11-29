1cd501ead6944f6edf8b50dbe3fd3e624e9607dc
Miško Hevery <misko@hevery.com>
2017-11-29 22:47:49 +0000

    647ca64 docs(aio): Updating with Ignite UI for Angular (#20663)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.3-c7b211c
+ * @license Angular v5.0.3-647ca64
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.3-c7b211c');
+var VERSION = new _angular_core.Version('5.0.3-647ca64');
 
 /**
  * @fileoverview added by tsickle
