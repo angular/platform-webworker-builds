c035c0ea5e21aa19a81b3bff57b6bb7dbe580c89
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-01 23:40:15 +0000

    9dc310e docs: add changelog for 5.0.5

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.4-445d833
+ * @license Angular v5.0.5-9dc310e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1479,7 +1479,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.4-445d833');
+const VERSION = new Version('5.0.5-9dc310e');
 
 /**
  * @fileoverview added by tsickle
