ed7983c85515058a7548f560a34deb13098103b9
Igor Minar <igor@angularjs.org>
2017-12-04 23:16:16 +0000

    7e7ff2e ci: fix the payload-size checking scripts (#20683)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-rc.0-d507057
+ * @license Angular v5.1.0-rc.1-7e7ff2e
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-rc.0-d507057');
+var VERSION = new _angular_core.Version('5.1.0-rc.1-7e7ff2e');
 
 /**
  * @fileoverview added by tsickle
