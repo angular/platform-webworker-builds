53f0906b35d131524cb957ef09adb004033dde52
Igor Minar <igor@angularjs.org>
2017-12-06 06:08:26 +0000

    a2fde2b ci(core): Improve the payload size message (#20786)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.0.5-e37f7c0
+ * @license Angular v5.0.5-a2fde2b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1811,7 +1811,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.0.5-e37f7c0');
+var VERSION = new _angular_core.Version('5.0.5-a2fde2b');
 
 /**
  * @fileoverview added by tsickle
