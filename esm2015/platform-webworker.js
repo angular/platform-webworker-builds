b955759a3ec17e4f25ce70b309b40e0ddf43be29
Igor Minar <igor@angularjs.org>
2017-12-06 06:20:58 +0000

    9efb5da docs(aio): Fix typo (#20732)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.0.5-860ef8c
+ * @license Angular v5.0.5-9efb5da
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1479,7 +1479,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.0.5-860ef8c');
+const VERSION = new Version('5.0.5-9efb5da');
 
 /**
  * @fileoverview added by tsickle
