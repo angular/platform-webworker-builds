3b7fbb3d36d0ff819d6f0caf2e98f97b2767ae72
Igor Minar <igor@angularjs.org>
2017-12-06 15:06:00 +0000

    be99449 docs(core): add docs for i18n tokens (#17920)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-6911a25
+ * @license Angular v5.1.0-rc.1-be99449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-rc.1-6911a25
+ * @license Angular v5.1.0-rc.1-be99449
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-rc.1-6911a25');
+var VERSION = new _angular_core.Version('5.1.0-rc.1-be99449');
 
 /**
  * @fileoverview added by tsickle
