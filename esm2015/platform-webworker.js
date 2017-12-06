2bd38f6ee0d4848b0eb0b06616aee1df1622993b
Igor Minar <igor@angularjs.org>
2017-12-06 16:45:23 +0000

    b78ada1 fix(animations): ensure DOM is cleaned up after multiple @trigger leave animations finish (#20740)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-rc.1-be99449
+ * @license Angular v5.1.0-rc.1-b78ada1
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-rc.1-be99449');
+const VERSION = new Version('5.1.0-rc.1-b78ada1');
 
 /**
  * @fileoverview added by tsickle
