4b14f3fd6d5b266b8c6c6b7135e10b53b13edf3b
Jason Aden <jasonaden@google.com>
2017-12-07 21:49:09 +0000

    9dd60a5 build: upgrade bazel rules to latest (#20768)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-6727336
+ * @license Angular v5.1.0-9dd60a5
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-6727336');
+const VERSION = new Version('5.1.0-9dd60a5');
 
 /**
  * @fileoverview added by tsickle
