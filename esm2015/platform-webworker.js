af0e9809b32f25e6752fd35d50a6e5975a200fd9
Jason Aden <jasonaden@google.com>
2017-12-08 18:22:34 +0000

    e6a2805 fix(animations): properly recover and cleanup DOM when CD failures occur (#20719)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-baeec4d
+ * @license Angular v5.1.0-e6a2805
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-baeec4d');
+const VERSION = new Version('5.1.0-e6a2805');
 
 /**
  * @fileoverview added by tsickle
