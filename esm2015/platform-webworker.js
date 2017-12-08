b835978fe0fb08b349a673eeef7250511ac19285
Jason Aden <jasonaden@google.com>
2017-12-08 01:29:47 +0000

    d8cc09b fix(router): NavigatonError and NavigationCancel should be emitted after resetting the URL (#20803)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-ef534c0
+ * @license Angular v5.1.0-d8cc09b
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-ef534c0');
+const VERSION = new Version('5.1.0-d8cc09b');
 
 /**
  * @fileoverview added by tsickle
