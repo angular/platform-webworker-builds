7dc2a4d6fefaa4c2890ca5c28cf4a4faa3c2d77d
Jason Aden <jasonaden@google.com>
2017-12-08 23:20:48 +0000

    13e663c fix(animations): ensure multi-level route leave animations are queryable (#20787)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-3ce3b4d
+ * @license Angular v5.1.0-13e663c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-3ce3b4d
+ * @license Angular v5.1.0-13e663c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-3ce3b4d');
+var VERSION = new _angular_core.Version('5.1.0-13e663c');
 
 /**
  * @fileoverview added by tsickle
--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-3ce3b4d
+ * @license Angular v5.1.0-13e663c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-3ce3b4d');
+const VERSION = new Version('5.1.0-13e663c');
 
 /**
  * @fileoverview added by tsickle
