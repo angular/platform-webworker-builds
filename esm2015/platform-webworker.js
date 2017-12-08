cfc8b2fbe0103e3120dbd419f8355ca57257885d
Jason Aden <jasonaden@google.com>
2017-12-08 18:25:40 +0000

    53b9de3 docs(aio): fix typo for missing quote (#20888) PR Close #20888

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-e6a2805
+ * @license Angular v5.1.0-53b9de3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.0-e6a2805');
+const VERSION = new Version('5.1.0-53b9de3');
 
 /**
  * @fileoverview added by tsickle
