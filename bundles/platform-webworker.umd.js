c235ba707345fa8002aee0c36527c31fe4081c49
Jason Aden <jasonaden@google.com>
2017-12-12 00:26:28 +0000

    81d497c build: pin ChromeDriver version (#20940)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.0-70cd124
+ * @license Angular v5.1.0-81d497c
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.0-70cd124');
+var VERSION = new _angular_core.Version('5.1.0-81d497c');
 
 /**
  * @fileoverview added by tsickle
