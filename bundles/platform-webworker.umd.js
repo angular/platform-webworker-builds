fb46f1d8d806a4ef32e3f16c085da304ab3717f5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-15 16:15:37 +0000

    267ebf3 fix(common): fix a Closure compilation issue.

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.1-49c45f3
+ * @license Angular v5.1.1-267ebf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.1-49c45f3
+ * @license Angular v5.1.1-267ebf3
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.1-49c45f3');
+var VERSION = new _angular_core.Version('5.1.1-267ebf3');
 
 /**
  * @fileoverview added by tsickle
