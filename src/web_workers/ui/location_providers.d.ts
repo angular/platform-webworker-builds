681f0c52f219700e8420823c932ab39349adaf6f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2017-12-20 23:57:42 +0000

    abca7c0 docs: add changelog for 5.2.0-beta.1

---
+++ src/web_workers/ui/location_providers.d.ts
@@ -0,0 +1,7 @@
+import { StaticProvider } from '@angular/core';
+/**
+ * A list of {@link Provider}s. To use the router in a Worker enabled application you must
+ * include these providers when setting up the render thread.
+ * @experimental
+ */
+export declare const WORKER_UI_LOCATION_PROVIDERS: StaticProvider[];
