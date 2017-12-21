d67e5e1593764394950728844935dc36c8094be5
Igor Minar <igor@angularjs.org>
2017-12-21 21:35:57 +0000

    0487a9f build: update pullapprove: (#21117)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-871ece6
+ * @license Angular v5.2.0-beta.1-0487a9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-871ece6
+ * @license Angular v5.2.0-beta.1-0487a9f
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-871ece6');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-0487a9f');
 
 /**
  * @fileoverview added by tsickle
