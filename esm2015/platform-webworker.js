1960d2614658b1732f5250fbd5e800fd8c977e27
Igor Minar <igor@angularjs.org>
2017-12-22 17:39:02 +0000

    8c99175 fix(router): fix wildcard route with lazy loaded module (again) (#18139)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-fa0e8ef
+ * @license Angular v5.1.2-8c99175
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.2-fa0e8ef');
+const VERSION = new Version('5.1.2-8c99175');
 
 /**
  * @fileoverview added by tsickle
