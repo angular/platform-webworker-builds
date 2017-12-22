e834e19bccb3f1b54c24a15e7340c2fd001f9c17
Igor Minar <igor@angularjs.org>
2017-12-22 04:30:37 +0000

    bc66d27 docs(service-worker): fix word wrap (#21114)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-27fc458
+ * @license Angular v5.2.0-beta.1-bc66d27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-27fc458
+ * @license Angular v5.2.0-beta.1-bc66d27
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-27fc458');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-bc66d27');
 
 /**
  * @fileoverview added by tsickle
