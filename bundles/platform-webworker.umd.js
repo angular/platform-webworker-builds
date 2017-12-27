ff080b1eb24f71ca5c2cff1220c6227a04ba5671
Igor Minar <igor@angular.io>
2017-12-27 21:43:43 +0000

    d8abf70 aio: Sort in the api type dropdown (#21176)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-efd9c09
+ * @license Angular v5.2.0-beta.1-d8abf70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-efd9c09
+ * @license Angular v5.2.0-beta.1-d8abf70
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-efd9c09');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-d8abf70');
 
 /**
  * @fileoverview added by tsickle
