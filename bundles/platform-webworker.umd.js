85d43676552560223644f0a137920315b668491c
Igor Minar <igor@angular.io>
2017-12-27 21:35:39 +0000

    66043e0 docs(aio): Sort in the api type dropdown (#21030) (#21176)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.2-12702b2
+ * @license Angular v5.1.2-66043e0
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.2-12702b2');
+var VERSION = new _angular_core.Version('5.1.2-66043e0');
 
 /**
  * @fileoverview added by tsickle
