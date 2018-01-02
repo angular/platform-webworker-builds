878b07f5f8ceb5e8b2c65d46e813bc53ab63250d
Miško Hevery <misko@hevery.com>
2018-01-02 19:03:18 +0000

    3e47ea2 fix(language-service): ignore null metadatas (#20557)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-beta.1-bc7a6d7
+ * @license Angular v5.2.0-beta.1-3e47ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-beta.1-bc7a6d7
+ * @license Angular v5.2.0-beta.1-3e47ea2
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-beta.1-bc7a6d7');
+var VERSION = new _angular_core.Version('5.2.0-beta.1-3e47ea2');
 
 /**
  * @fileoverview added by tsickle
