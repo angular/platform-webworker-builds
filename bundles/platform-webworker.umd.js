54143fa2fb07d4b205ffd34642f84e11a67a37b1
Kara Erickson <karakara@google.com>
2018-01-04 22:36:10 +0000

    81823e8 build(aio): fix example protractor config for Travis (#21309)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-rc.0-ef78538
+ * @license Angular v5.2.0-rc.0-81823e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-rc.0-ef78538
+ * @license Angular v5.2.0-rc.0-81823e8
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-rc.0-ef78538');
+var VERSION = new _angular_core.Version('5.2.0-rc.0-81823e8');
 
 /**
  * @fileoverview added by tsickle
