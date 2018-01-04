a4d7b78c52e31e87fea5dd6b1c3c565072ac57b8
Kara Erickson <karakara@google.com>
2018-01-04 23:53:41 +0000

    03d16fc docs(aio): add missing imports to interceptor example (#21259)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v5.2.0-rc.0-b5934fc
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v5.2.0-rc.0-03d16fc
+ * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
 import { CommonModule, DOCUMENT, LOCATION_INITIALIZED, PlatformLocation, ɵPLATFORM_WORKER_APP_ID, ɵPLATFORM_WORKER_UI_ID } from '@angular/common';
@@ -1488,7 +1488,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.0-rc.0-b5934fc');
+const VERSION = new Version('5.2.0-rc.0-03d16fc');
 
 /**
  * @fileoverview added by tsickle
