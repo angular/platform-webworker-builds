c24237d6c2780bf911c0ca2c45562dabfcfd2080
Kara Erickson <karakara@google.com>
2018-01-05 22:40:49 +0000

    a1af559 build(core): update payload size of hello world (#21340)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-e2cc4db
+ * @license Angular v5.1.3-a1af559
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-e2cc4db');
+const VERSION = new Version('5.1.3-a1af559');
 
 /**
  * @fileoverview added by tsickle
