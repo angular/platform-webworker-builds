4cdefcd19320db0987aa96e2e4943932dd77953d
Kara Erickson <karakara@google.com>
2018-01-05 19:54:15 +0000

    c809545 test(aio): wait for Angular to avoid flake in e2e test (#21047)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-d3c0425
+ * @license Angular v5.1.3-c809545
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-d3c0425');
+const VERSION = new Version('5.1.3-c809545');
 
 /**
  * @fileoverview added by tsickle
