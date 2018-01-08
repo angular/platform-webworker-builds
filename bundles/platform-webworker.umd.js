352f1c8abf298cff42c061e5f43c0749436e55d6
Kara Erickson <karakara@google.com>
2018-01-08 18:35:20 +0000

    8b197b2 build(core): improve payload size failure message (#21346)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-8f3ea33
+ * @license Angular v5.1.3-8b197b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.1.3-8f3ea33
+ * @license Angular v5.1.3-8b197b2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1825,7 +1825,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.1.3-8f3ea33');
+var VERSION = new _angular_core.Version('5.1.3-8b197b2');
 
 /**
  * @fileoverview added by tsickle
