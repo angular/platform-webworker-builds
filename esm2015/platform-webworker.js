34a1310f5b30c14d056290cdd5dde60dbc73c814
Kara Erickson <karakara@google.com>
2018-01-09 20:43:07 +0000

    96efb4b ci: temporary work-around for Travis issue (#21422)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.1.3-7c34807
+ * @license Angular v5.1.3-96efb4b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1493,7 +1493,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.1.3-7c34807');
+const VERSION = new Version('5.1.3-96efb4b');
 
 /**
  * @fileoverview added by tsickle
