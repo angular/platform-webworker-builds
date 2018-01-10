bfa4fac152de10e4cda9ce5f0756213aa05755c6
Alex Eagle <alexeagle@google.com>
2018-01-10 22:48:33 +0000

    137b9c7 fix(aio): preserve static Observable methods (#21351)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-2ad92ed
+ * @license Angular v5.2.0-137b9c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-2ad92ed
+ * @license Angular v5.2.0-137b9c7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-2ad92ed');
+var VERSION = new _angular_core.Version('5.2.0-137b9c7');
 
 /**
  * @fileoverview added by tsickle
