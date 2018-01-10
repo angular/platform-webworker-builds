de10f7b57fd12bb712317ca9b114c61babe90eac
Alex Eagle <alexeagle@google.com>
2018-01-10 23:51:02 +0000

    a33ff2c docs(ivy): add Ivy as a recognized label (#21428)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-92984ba
+ * @license Angular v5.2.0-a33ff2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-92984ba');
+var VERSION = new _angular_core.Version('5.2.0-a33ff2c');
 
 /**
  * @fileoverview added by tsickle
