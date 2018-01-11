972e627984d5fe43c02ad1dd891df6aed9c023d1
Alex Eagle <alexeagle@google.com>
2018-01-11 15:23:05 +0000

    b49d54e docs(forms): clarify note in reactive forms doc (#21134)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-55fd82e
+ * @license Angular v5.2.0-b49d54e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-55fd82e');
+var VERSION = new _angular_core.Version('5.2.0-b49d54e');
 
 /**
  * @fileoverview added by tsickle
