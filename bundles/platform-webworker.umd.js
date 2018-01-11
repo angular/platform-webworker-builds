69e5d8a5562f7cc67f2e9b033f0496137c3c7f54
Alex Eagle <alexeagle@google.com>
2018-01-11 00:42:03 +0000

    e64b1e9 fix(compiler): make `.ngsummary.json` files idempotent (#21448)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-6be9c04
+ * @license Angular v5.2.0-e64b1e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-6be9c04');
+var VERSION = new _angular_core.Version('5.2.0-e64b1e9');
 
 /**
  * @fileoverview added by tsickle
