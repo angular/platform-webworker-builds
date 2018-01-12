39bf092d6a2e808f104cc64f1b4a6ce64b5416b7
Alex Eagle <alexeagle@google.com>
2018-01-12 18:25:32 +0000

    9005a6f feat(aio): implement survey notification link (#21371)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-3c6a506
+ * @license Angular v5.2.0-9005a6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-3c6a506
+ * @license Angular v5.2.0-9005a6f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-3c6a506');
+var VERSION = new _angular_core.Version('5.2.0-9005a6f');
 
 /**
  * @fileoverview added by tsickle
