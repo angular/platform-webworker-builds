804b06806de5e47d183cde7b5e456c643c0103d1
Alex Eagle <alexeagle@google.com>
2018-01-12 18:49:15 +0000

    63478d7 ci: add angular robot config file (#21489)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.0-4f31492
+ * @license Angular v5.2.0-63478d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.0-4f31492
+ * @license Angular v5.2.0-63478d7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.0-4f31492');
+var VERSION = new _angular_core.Version('5.2.0-63478d7');
 
 /**
  * @fileoverview added by tsickle
