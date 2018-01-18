c7085977c4e321da24a61da23e991f3881c3bf61
Chuck Jazdzewski <chuckj@google.com>
2018-01-18 01:35:15 +0000

    af2a843 docs: edit entry component FAQ (#21487)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.9.9-6-beta.0-e79e98a
+ * @license Angular v5.9.9-6-beta.0-af2a843
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.9.9-6-beta.0-e79e98a
+ * @license Angular v5.9.9-6-beta.0-af2a843
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.9.9-6-beta.0-e79e98a');
+var VERSION = new _angular_core.Version('5.9.9-6-beta.0-af2a843');
 
 /**
  * @fileoverview added by tsickle
