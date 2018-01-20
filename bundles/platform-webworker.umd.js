ed6fa630033f3f2c41a27fa57fd7d017142715c7
Misko Hevery <misko@hevery.com>
2018-01-20 05:01:24 +0000

    d7dbdc5 docs: fix stray div and reformat paragraph (#21676)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-554129d
+ * @license Angular v5.2.1-d7dbdc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.1-554129d
+ * @license Angular v5.2.1-d7dbdc5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.1-554129d');
+var VERSION = new _angular_core.Version('5.2.1-d7dbdc5');
 
 /**
  * @fileoverview added by tsickle
