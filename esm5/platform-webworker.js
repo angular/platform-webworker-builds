d5ba444ed6cf54406c4fb6050f59f4676fcc42c2
Miško Hevery <misko@hevery.com>
2018-01-22 23:54:57 +0000

    4b68fdc build: Update to latest rules_typescript. (#21675)

--- esm5/platform-webworker.js
+++ esm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.1-c12ea3a
+ * @license Angular v5.2.1-4b68fdc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1788,7 +1788,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.1-c12ea3a');
+var VERSION = new Version('5.2.1-4b68fdc');
 
 /**
  * @fileoverview added by tsickle
