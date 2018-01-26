d6f2ed6fc4de7f83692f60569a3596fa277c0aad
Jason Aden <jasonaden@google.com>
2018-01-26 23:10:37 +0000

    676d9c2 fix(language-service): ensure correct paths are passed to TypeScript (#21812)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-3b7bab7
+ * @license Angular v6.0.0-beta.1-676d9c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-3b7bab7
+ * @license Angular v6.0.0-beta.1-676d9c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-3b7bab7');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-676d9c2');
 
 /**
  * @fileoverview added by tsickle
