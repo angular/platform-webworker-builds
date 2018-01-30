a158ec4b283a261b234712de01d8784778391d66
Jason Aden <jasonaden@google.com>
2018-01-30 06:17:14 +0000

    02483a0 Revert: "refactor(ivy): save check methods separately"

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-120bdee
+ * @license Angular v6.0.0-beta.1-02483a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-120bdee
+ * @license Angular v6.0.0-beta.1-02483a0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-120bdee');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-02483a0');
 
 /**
  * @fileoverview added by tsickle
