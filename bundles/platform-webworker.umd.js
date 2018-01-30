78a23c0f8ade8651458d10430666cfc1ffac427a
Jason Aden <jasonaden@google.com>
2018-01-30 20:22:21 +0000

    285dd6b feat(ivy): observable QueryList (#21859)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.1-b10540a
+ * @license Angular v6.0.0-beta.1-285dd6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.1-b10540a
+ * @license Angular v6.0.0-beta.1-285dd6b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.1-b10540a');
+var VERSION = new _angular_core.Version('6.0.0-beta.1-285dd6b');
 
 /**
  * @fileoverview added by tsickle
