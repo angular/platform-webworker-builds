a0fe9d5d77965eb0676ee185fb4fdee0b3699fcf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:56:48 +0000

    102d06b docs: consistency fix in describing a custom tag (#21747)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.3-aa9ba7f
+ * @license Angular v5.2.3-102d06b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.3-aa9ba7f');
+var VERSION = new _angular_core.Version('5.2.3-102d06b');
 
 /**
  * @fileoverview added by tsickle
