0853eb9f66badc90553143bf9dd3ed6ecc122290
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-02 16:25:08 +0000

    743d8bc feat(ivy): add canonical example of a pipe. (#21834)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-f816666
+ * @license Angular v6.0.0-beta.2-743d8bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.2-f816666
+ * @license Angular v6.0.0-beta.2-743d8bc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-f816666');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-743d8bc');
 
 /**
  * @fileoverview added by tsickle
