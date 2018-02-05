018aad95477790157b24ebb6e4d6ae43428aaa7c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-02-05 22:10:54 +0000

    9d3326c docs: clarify npm/yarn commands, add blank lines to mix md/html in table (#21606)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.2-0846784
+ * @license Angular v6.0.0-beta.2-9d3326c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.2-0846784
+ * @license Angular v6.0.0-beta.2-9d3326c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.2-0846784');
+var VERSION = new _angular_core.Version('6.0.0-beta.2-9d3326c');
 
 /**
  * @fileoverview added by tsickle
