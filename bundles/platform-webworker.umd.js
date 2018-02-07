0d0d913f6e36d96cd9783e340b84dd510a8e8f20
Miško Hevery <misko@hevery.com>
2018-02-07 21:46:15 +0000

    7a406a3 feat(aio): report `logger.error` calls to Google Analytics (#22011)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.3-e442881
+ * @license Angular v6.0.0-beta.3-7a406a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.3-e442881
+ * @license Angular v6.0.0-beta.3-7a406a3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.3-e442881');
+var VERSION = new _angular_core.Version('6.0.0-beta.3-7a406a3');
 
 /**
  * @fileoverview added by tsickle
