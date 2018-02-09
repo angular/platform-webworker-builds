e6cf2d8834dc0192ea0e5410c6da50e4f1732419
Miško Hevery <misko@hevery.com>
2018-02-09 21:59:40 +0000

    ac815f7 docs: fix typo in http.md (#22058)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-854f0ff
+ * @license Angular v5.2.4-ac815f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-854f0ff');
+var VERSION = new _angular_core.Version('5.2.4-ac815f7');
 
 /**
  * @fileoverview added by tsickle
