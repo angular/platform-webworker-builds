dd87e77a762e11b99002a2d29f1d10d0e90acba6
Miško Hevery <misko@hevery.com>
2018-02-10 01:36:22 +0000

    9b280ee build: merge-pr new checks that all requested changes have been addressed (#21817)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-ac815f7
+ * @license Angular v5.2.4-9b280ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-ac815f7
+ * @license Angular v5.2.4-9b280ee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-ac815f7');
+var VERSION = new _angular_core.Version('5.2.4-9b280ee');
 
 /**
  * @fileoverview added by tsickle
