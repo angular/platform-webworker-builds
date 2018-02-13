5da1b6cb866d6a9106eb587345228483ed4d77c9
Miško Hevery <misko@hevery.com>
2018-02-13 21:41:52 +0000

    1b8ea10 fix(compiler): make unary plus operator consistent to JavaScript (#22154)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.4-da6ab91
+ * @license Angular v5.2.4-1b8ea10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.4-da6ab91
+ * @license Angular v5.2.4-1b8ea10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.4-da6ab91');
+var VERSION = new _angular_core.Version('5.2.4-1b8ea10');
 
 /**
  * @fileoverview added by tsickle
