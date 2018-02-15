c9233738944501b33a5b1b18304243fc7f6d2fcf
Victor Berchet <victor@suumit.com>
2018-02-15 22:27:50 +0000

    6a57264 Revert: "build: allow bazel build ... (#22168)"

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-1e3e0fa
+ * @license Angular v5.2.5-6a57264
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-1e3e0fa
+ * @license Angular v5.2.5-6a57264
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-1e3e0fa');
+var VERSION = new _angular_core.Version('5.2.5-6a57264');
 
 /**
  * @fileoverview added by tsickle
