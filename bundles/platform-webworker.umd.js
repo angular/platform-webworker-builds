c1765a6a3774e66cffc3d5a8190c98e7bce6e2f8
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 00:27:11 +0000

    1e3e0fa fix(aio): improve printing styles (#19651)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-0c88d5d
+ * @license Angular v5.2.5-1e3e0fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-0c88d5d
+ * @license Angular v5.2.5-1e3e0fa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-0c88d5d');
+var VERSION = new _angular_core.Version('5.2.5-1e3e0fa');
 
 /**
  * @fileoverview added by tsickle
