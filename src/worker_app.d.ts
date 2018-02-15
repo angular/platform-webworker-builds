90308122fad02327cd646cddef1f2d930fabaf88
Matias Niemelä <matias@yearofmoo.com>
2018-02-15 05:10:55 +0000

    dac2e6e Revert "build: allow bazel build ... (#22168)"

---
+++ src/worker_app.d.ts
@@ -0,0 +1,16 @@
+import { ErrorHandler, NgZone, PlatformRef, StaticProvider } from '@angular/core';
+import { MessageBus } from './web_workers/shared/message_bus';
+/**
+ * @experimental
+ */
+export declare const platformWorkerApp: (extraProviders?: StaticProvider[] | undefined) => PlatformRef;
+export declare function errorHandler(): ErrorHandler;
+export declare function createMessageBus(zone: NgZone): MessageBus;
+export declare function setupWebWorker(): void;
+/**
+ * The ng module for the worker app side.
+ *
+ * @experimental
+ */
+export declare class WorkerAppModule {
+}
