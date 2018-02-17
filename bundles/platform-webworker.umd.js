4e17d194c0595182f2998cb00c9a7ff050468435
Victor Berchet <victor@suumit.com>
2018-02-17 02:25:59 +0000

    861250b docs: fix changelog errors (#22228)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-81c1e0a
+ * @license Angular v5.2.5-861250b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-81c1e0a');
+var VERSION = new _angular_core.Version('5.2.5-861250b');
 
 /**
  * @fileoverview added by tsickle
