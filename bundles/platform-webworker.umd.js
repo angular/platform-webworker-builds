6c591dc8f8c5840260f926a4930bacdd33a288b5
Victor Berchet <victor@suumit.com>
2018-02-18 21:41:21 +0000

    4ec40c6 fix(aio): improve announcement-bar layout with wide logos (#22272)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-7fa2d4b
+ * @license Angular v6.0.0-beta.4-4ec40c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-7fa2d4b
+ * @license Angular v6.0.0-beta.4-4ec40c6
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-7fa2d4b');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-4ec40c6');
 
 /**
  * @fileoverview added by tsickle
