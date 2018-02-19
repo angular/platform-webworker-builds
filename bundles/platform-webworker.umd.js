8ed1993b0934147bc9e02d2f67104b295cba7f28
Victor Berchet <victor@suumit.com>
2018-02-19 04:10:55 +0000

    cdfedc1 fix(common): fix merge error in ng_if.ts

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-c726d1d
+ * @license Angular v5.2.5-cdfedc1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-c726d1d');
+var VERSION = new _angular_core.Version('5.2.5-cdfedc1');
 
 /**
  * @fileoverview added by tsickle
