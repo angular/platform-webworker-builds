136c347cf0b17e1ede7aad1f2857ab4fe85989b4
Victor Berchet <victor@suumit.com>
2018-02-19 05:18:11 +0000

    bf07837 test(common): fix ngIf tests

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-cdfedc1');
+var VERSION = new _angular_core.Version('5.2.5-bf07837');
 
 /**
  * @fileoverview added by tsickle
--- esm5/platform-webworker.js
+++ esm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-cdfedc1
+ * @license Angular v5.2.5-bf07837
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1788,7 +1788,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('5.2.5-cdfedc1');
+var VERSION = new Version('5.2.5-bf07837');
 
 /**
  * @fileoverview added by tsickle
