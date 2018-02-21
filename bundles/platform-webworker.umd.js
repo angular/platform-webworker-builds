3172e7cddca36dc259b3e7701f21feb65502df3c
Victor Berchet <victor@suumit.com>
2018-02-21 19:37:34 +0000

    dfdade2 docs(aio): Wrong code example. Form status field was added later in the guide. (#21275)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-c2f78e1
+ * @license Angular v5.2.5-dfdade2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-c2f78e1
+ * @license Angular v5.2.5-dfdade2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-c2f78e1');
+var VERSION = new _angular_core.Version('5.2.5-dfdade2');
 
 /**
  * @fileoverview added by tsickle
