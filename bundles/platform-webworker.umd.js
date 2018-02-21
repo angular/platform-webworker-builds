55a137324e1b331d181fb0e57ca1bac79c1fa2e1
Victor Berchet <victor@suumit.com>
2018-02-21 23:22:49 +0000

    a7ebf5a fix(core): properly handle function without prototype in reflector (#22284)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.4-b42921b
+ * @license Angular v6.0.0-beta.4-a7ebf5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.4-b42921b
+ * @license Angular v6.0.0-beta.4-a7ebf5a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.4-b42921b');
+var VERSION = new _angular_core.Version('6.0.0-beta.4-a7ebf5a');
 
 /**
  * @fileoverview added by tsickle
