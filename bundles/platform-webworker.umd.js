4d39e756e6daac84d92a5f5b99920e009298fb14
Victor Berchet <victor@suumit.com>
2018-02-21 19:27:57 +0000

    484802c build: make git revert messages valid (#22339)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-ee53577
+ * @license Angular v5.2.5-484802c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-ee53577
+ * @license Angular v5.2.5-484802c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-ee53577');
+var VERSION = new _angular_core.Version('5.2.5-484802c');
 
 /**
  * @fileoverview added by tsickle
