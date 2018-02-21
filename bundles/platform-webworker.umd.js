83fac99e30e55fccf158d449b92237a66d88e898
Victor Berchet <victor@suumit.com>
2018-02-21 19:29:28 +0000

    c2f78e1 docs(http): fix a typo in code comment (#22327)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-484802c
+ * @license Angular v5.2.5-c2f78e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.5-484802c
+ * @license Angular v5.2.5-c2f78e1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.5-484802c');
+var VERSION = new _angular_core.Version('5.2.5-c2f78e1');
 
 /**
  * @fileoverview added by tsickle
