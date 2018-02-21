3cef42332929b038dcc9618564249fb808b13afc
Victor Berchet <victor@suumit.com>
2018-02-21 23:15:13 +0000

    612cfec docs(aio): updates directive event hooks real capabilities (#16654)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.5-dfdade2
+ * @license Angular v5.2.5-612cfec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1488,7 +1488,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.5-dfdade2');
+const VERSION = new Version('5.2.5-612cfec');
 
 /**
  * @fileoverview added by tsickle
