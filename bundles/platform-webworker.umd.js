0061ae91dc805f355e51330c9304bcffba05e8f2
Victor Berchet <victor@suumit.com>
2018-02-22 19:41:43 +0000

    da1b4d5 docs: fix ngmodules-jsmodules pre-req (#22316)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.6-aa100f6
+ * @license Angular v5.2.6-da1b4d5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-aa100f6');
+var VERSION = new _angular_core.Version('5.2.6-da1b4d5');
 
 /**
  * @fileoverview added by tsickle
