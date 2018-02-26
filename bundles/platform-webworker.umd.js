8961b7304ad6d86eadbccf44b83448a1a81349ce
Alex Eagle <alexeagle@google.com>
2018-02-26 21:56:37 +0000

    2a38d93 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-a9a0e27
+ * @license Angular v5.2.6-2a38d93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.6-a9a0e27
+ * @license Angular v5.2.6-2a38d93
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-a9a0e27');
+var VERSION = new _angular_core.Version('5.2.6-2a38d93');
 
 /**
  * @fileoverview added by tsickle
