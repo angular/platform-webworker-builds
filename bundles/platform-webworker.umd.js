5aba05c78626d7d044690252e938cc4ea65ba4e0
Alex Eagle <alexeagle@google.com>
2018-02-26 22:00:37 +0000

    c593d69 docs(aio): fix doc typo referring to `httpOptions` (#22456)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.5-5a32d7e
+ * @license Angular v6.0.0-beta.5-c593d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.5-5a32d7e
+ * @license Angular v6.0.0-beta.5-c593d69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.5-5a32d7e');
+var VERSION = new _angular_core.Version('6.0.0-beta.5-c593d69');
 
 /**
  * @fileoverview added by tsickle
