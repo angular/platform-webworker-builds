e0fbaa5167cc0da093ddcfafa02f6bf4e1626c89
Alex Eagle <alexeagle@google.com>
2018-02-27 02:07:36 +0000

    de02a7a fix(platform-server): generate correct stylings for camel case names (#22263)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.6-2a38d93
+ * @license Angular v5.2.6-de02a7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.6-2a38d93
+ * @license Angular v5.2.6-de02a7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.6-2a38d93');
+var VERSION = new _angular_core.Version('5.2.6-de02a7a');
 
 /**
  * @fileoverview added by tsickle
