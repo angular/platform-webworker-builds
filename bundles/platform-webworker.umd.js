716dcbac5fcbf8ed4726e1b60f2d01ea131fde99
Alex Eagle <alexeagle@google.com>
2018-03-01 17:05:20 +0000

    fe0d53f build: Add support for bazelOptions.maxCacheSizeMb in ngc-wrapped. (#22511)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.7-f8e70fb
+ * @license Angular v5.2.7-fe0d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.7-f8e70fb
+ * @license Angular v5.2.7-fe0d53f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.7-f8e70fb');
+var VERSION = new _angular_core.Version('5.2.7-fe0d53f');
 
 /**
  * @fileoverview added by tsickle
