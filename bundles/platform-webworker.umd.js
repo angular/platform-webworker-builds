ef801a04bbc4e7e2f3fd214501d2a9ec3be34858
Alex Eagle <alexeagle@google.com>
2018-03-01 17:51:18 +0000

    ec445b5 ci: speed up lint job on CircleCI (#22526)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-ab790f3
+ * @license Angular v6.0.0-beta.6-ec445b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.6-ab790f3
+ * @license Angular v6.0.0-beta.6-ec445b5
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-ab790f3');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-ec445b5');
 
 /**
  * @fileoverview added by tsickle
