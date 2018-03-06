fa66850678ec87efc5bfbd95cbedc9d04262e240
Alex Eagle <alexeagle@google.com>
2018-03-06 05:43:43 +0000

    2a1e3d1 docs: add info about the WebStorm plugin to BAZEL.md (#22572) PR Close #22572

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.6-2c2b62f
+ * @license Angular v6.0.0-beta.6-2a1e3d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.6-2c2b62f
+ * @license Angular v6.0.0-beta.6-2a1e3d1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.6-2c2b62f');
+var VERSION = new _angular_core.Version('6.0.0-beta.6-2a1e3d1');
 
 /**
  * @fileoverview added by tsickle
