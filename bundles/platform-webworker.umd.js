c2c95e895349f27d6deb021c928d3f9865e913e1
Kara Erickson <karakara@google.com>
2018-03-08 19:20:52 +0000

    73c203f feat(ivy): support host attribute and property bindings (#22334)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-c499c8f
+ * @license Angular v6.0.0-beta.7-73c203f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-c499c8f
+ * @license Angular v6.0.0-beta.7-73c203f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-c499c8f');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-73c203f');
 
 /**
  * @fileoverview added by tsickle
