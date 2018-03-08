483e5debe04bd67992e6146d5968781cd932fde8
Kara Erickson <karakara@google.com>
2018-03-08 19:19:23 +0000

    c499c8f build: update angular-bot g3sync paths (#22637)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-4c089c1
+ * @license Angular v6.0.0-beta.7-c499c8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-4c089c1
+ * @license Angular v6.0.0-beta.7-c499c8f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-4c089c1');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-c499c8f');
 
 /**
  * @fileoverview added by tsickle
