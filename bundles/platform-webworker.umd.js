d1590c7c9272001a1ed566e059f24589783facc7
Kara Erickson <karakara@google.com>
2018-03-09 01:06:47 +0000

    5412e10 docs(core): replace ancient live demos (#22665)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-489fec1
+ * @license Angular v6.0.0-beta.7-5412e10
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-489fec1');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-5412e10');
 
 /**
  * @fileoverview added by tsickle
