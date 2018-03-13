487f505d52403bc33580137569fa4454114f5867
Kara Erickson <karakara@google.com>
2018-03-13 19:35:41 +0000

    34e355a build(bazel): ng_package rxjs/operators rollup config (#22744)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-58b94e6
+ * @license Angular v6.0.0-beta.7-34e355a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-58b94e6');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-34e355a');
 
 /**
  * @fileoverview added by tsickle
