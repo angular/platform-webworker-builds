4ec585c0f0ae4031518708570567e1b5779f2dc5
Kara Erickson <karakara@google.com>
2018-03-14 18:14:01 +0000

    fe96caf fix(aio): constrain error logging to improve reporting (#22713)

--- esm2015/platform-webworker.js
+++ esm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.8-ad674da
+ * @license Angular v5.2.8-fe96caf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1488,7 +1488,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('5.2.8-ad674da');
+const VERSION = new Version('5.2.8-fe96caf');
 
 /**
  * @fileoverview added by tsickle
