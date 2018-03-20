e1a0e4ce917a3d23faabee0504c83f5e0863093e
Igor Minar <igor@angular.io>
2018-03-20 05:14:29 +0000

    4648597 build(aio): temporarily pin elements to 6.0.0-beta.8 (#22573)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-beta.7-2b3de63
+ * @license Angular v6.0.0-beta.7-4648597
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-beta.7-2b3de63
+ * @license Angular v6.0.0-beta.7-4648597
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-beta.7-2b3de63');
+var VERSION = new _angular_core.Version('6.0.0-beta.7-4648597');
 
 /**
  * @fileoverview added by tsickle
