e5bd443e8ad82f77855c3cebc629f63c0f298ab4
Igor Minar <igor@angular.io>
2018-03-21 20:36:31 +0000

    8c2a578 fix(service-worker): fix LruList bugs (#22769)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-4f2c51f
+ * @license Angular v6.0.0-rc.0-8c2a578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-4f2c51f
+ * @license Angular v6.0.0-rc.0-8c2a578
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-4f2c51f');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-8c2a578');
 
 /**
  * @fileoverview added by tsickle
