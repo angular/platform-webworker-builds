4ea97529f15bee436775bb5efc2e0661171c0085
Matias Niemelä <matias@yearofmoo.com>
2018-03-22 23:41:29 +0000

    e44f69c refactor(ivy): move dir flags to tnode (#22901)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-e5e1b0d
+ * @license Angular v6.0.0-rc.0-e44f69c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-e5e1b0d
+ * @license Angular v6.0.0-rc.0-e44f69c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-e5e1b0d');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-e44f69c');
 
 /**
  * @fileoverview added by tsickle
