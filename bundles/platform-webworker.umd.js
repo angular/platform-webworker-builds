da9eb84e50798f37f24a8e5e9135d11e80ec8e8c
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 18:35:13 +0000

    4be8b3f docs: update available platforms for `test.sh` (#22958)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-8d1e640
+ * @license Angular v5.2.9-4be8b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-8d1e640
+ * @license Angular v5.2.9-4be8b3f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-8d1e640');
+var VERSION = new _angular_core.Version('5.2.9-4be8b3f');
 
 /**
  * @fileoverview added by tsickle
