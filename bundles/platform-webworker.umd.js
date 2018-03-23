fb54e1e1dbf32b5f4bd57bfbe42348b556f48003
Miško Hevery <misko@hevery.com>
2018-03-23 20:29:38 +0000

    4f0cae0 build: allow bazel build //... (#22168)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-65f296a
+ * @license Angular v6.0.0-rc.0-4f0cae0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-65f296a
+ * @license Angular v6.0.0-rc.0-4f0cae0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-65f296a');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-4f0cae0');
 
 /**
  * @fileoverview added by tsickle
