bc0576074e82e3e5f111d7a10462f1dca7f68f18
Matias Niemelä <matias@yearofmoo.com>
2018-03-23 01:50:50 +0000

    5ab9d4d test(compiler): allow asserting matching identifier names (#22835)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-49396ca
+ * @license Angular v6.0.0-rc.0-5ab9d4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-49396ca
+ * @license Angular v6.0.0-rc.0-5ab9d4d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-49396ca');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-5ab9d4d');
 
 /**
  * @fileoverview added by tsickle
