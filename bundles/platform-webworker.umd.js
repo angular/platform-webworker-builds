865fba44b222fe094fda9860a7c6d90e439d5462
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-28 17:27:19 +0000

    4e004f3 ci(compiler-cli): run compiler-cli tests in bazel (#22997)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.0-d9dc46e
+ * @license Angular v6.0.0-rc.0-4e004f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.0-d9dc46e
+ * @license Angular v6.0.0-rc.0-4e004f3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.0-d9dc46e');
+var VERSION = new _angular_core.Version('6.0.0-rc.0-4e004f3');
 
 /**
  * @fileoverview added by tsickle
