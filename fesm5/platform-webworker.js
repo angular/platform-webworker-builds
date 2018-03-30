7e2bc392e9468bea5cdf8d27d2a78161ac6f269c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-03-30 23:14:58 +0000

    9cd4465 refactor(compiler): generate flat css selectors (#23074)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-6cb1adf
+ * @license Angular v6.0.0-rc.1-9cd4465
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1788,7 +1788,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-6cb1adf');
+var VERSION = new Version('6.0.0-rc.1-9cd4465');
 
 /**
  * @fileoverview added by tsickle
