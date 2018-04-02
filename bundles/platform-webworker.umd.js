c38602746fcfaa575c3a2097f424fdec31e8620f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-02 17:12:34 +0000

    85d3b59 refactor(ivy): generate pipe names instead of defs (#23104)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-7a1c437
+ * @license Angular v6.0.0-rc.1-85d3b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-7a1c437
+ * @license Angular v6.0.0-rc.1-85d3b59
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-7a1c437');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-85d3b59');
 
 /**
  * @fileoverview added by tsickle
