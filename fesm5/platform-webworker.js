456c15256f3f8218266bdb7ba3bf4806589390c3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-03 18:29:39 +0000

    fab6b39 fix(core): inject() should always work in an NgModule injection scope (#23148)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-7ca7720
+ * @license Angular v6.0.0-rc.1-fab6b39
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1788,7 +1788,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-7ca7720');
+var VERSION = new Version('6.0.0-rc.1-fab6b39');
 
 /**
  * @fileoverview added by tsickle
