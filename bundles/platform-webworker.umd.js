42261abf6201b74a54b4cdc09a41189baea79b64
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 15:45:09 +0000

    550433a feat(compiler-cli): lower loadChildren fields to allow dynamic module paths (#23088)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-4506230
+ * @license Angular v6.0.0-rc.1-550433a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-4506230
+ * @license Angular v6.0.0-rc.1-550433a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-4506230');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-550433a');
 
 /**
  * @fileoverview added by tsickle
