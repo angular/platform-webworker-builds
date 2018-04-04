20b2d0b015657a0cab2275cf72c05dfa8443dca1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-04-04 21:56:17 +0000

    6dd8f6e docs(aio): remove invalid nav item (#23175)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-32a41bc
+ * @license Angular v6.0.0-rc.1-6dd8f6e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1788,7 +1788,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new Version('6.0.0-rc.1-32a41bc');
+var VERSION = new Version('6.0.0-rc.1-6dd8f6e');
 
 /**
  * @fileoverview added by tsickle
