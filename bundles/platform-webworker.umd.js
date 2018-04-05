41314630713c62807f42f24a43032f3ad48aaaf6
Igor Minar <igor@angular.io>
2018-04-05 23:54:14 +0000

    b5f41f2 docs: fixed live example for the lifecycle hooks. (#23201)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.2-629629d
+ * @license Angular v6.0.0-rc.2-b5f41f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.2-629629d
+ * @license Angular v6.0.0-rc.2-b5f41f2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.2-629629d');
+var VERSION = new _angular_core.Version('6.0.0-rc.2-b5f41f2');
 
 /**
  * @fileoverview added by tsickle
