de6ca03f9e958ed58364f7a282485a7fddccdce0
Igor Minar <igor@angular.io>
2018-04-05 17:03:17 +0000

    9141424 docs(router): remove unnecessary `{@link Injector}` jsdoc tags (#23187)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.1-92821e3
+ * @license Angular v6.0.0-rc.1-9141424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.1-92821e3
+ * @license Angular v6.0.0-rc.1-9141424
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.1-92821e3');
+var VERSION = new _angular_core.Version('6.0.0-rc.1-9141424');
 
 /**
  * @fileoverview added by tsickle
