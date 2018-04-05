06acfadb6a46300d7a75d9cdf5e6bbb1ad5d4949
Igor Minar <igor@angular.io>
2018-04-05 17:29:57 +0000

    8aa49ac fix(aio): update trusted GitHub teams (angular-core --> team) (#23181)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-3900c36
+ * @license Angular v5.2.9-8aa49ac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-3900c36');
+var VERSION = new _angular_core.Version('5.2.9-8aa49ac');
 
 /**
  * @fileoverview added by tsickle
