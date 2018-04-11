9dcf028e6b0be52e42d4993d651f0838d35c5994
Victor Berchet <victor@suumit.com>
2018-04-11 22:59:46 +0000

    ce40e85 refactor(common): update CLDR data to v33.0.0 (#23265) PR Close #23265

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-a4ac872
+ * @license Angular v6.0.0-rc.3-ce40e85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-a4ac872
+ * @license Angular v6.0.0-rc.3-ce40e85
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-a4ac872');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-ce40e85');
 
 /**
  * @fileoverview added by tsickle
