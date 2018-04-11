d4064315fc5daf7c1e92f71f34a7a67eef8179e4
Igor Minar <igor@angular.io>
2018-04-11 06:23:15 +0000

    b3a10e0 build: update Bazel dependency to 0.11.1 (#23297)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.3-0cb4f12
+ * @license Angular v6.0.0-rc.3-b3a10e0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.3-0cb4f12');
+var VERSION = new _angular_core.Version('6.0.0-rc.3-b3a10e0');
 
 /**
  * @fileoverview added by tsickle
