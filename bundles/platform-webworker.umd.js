4427d70ab205151af2b366915e4a097071ad106d
Igor Minar <igor@angular.io>
2018-04-12 06:38:19 +0000

    c5b6e31 build: fix aio size tracking, we need to use node_modules local to aio (#23328)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.9-22686f8
+ * @license Angular v5.2.9-c5b6e31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.9-22686f8
+ * @license Angular v5.2.9-c5b6e31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.9-22686f8');
+var VERSION = new _angular_core.Version('5.2.9-c5b6e31');
 
 /**
  * @fileoverview added by tsickle
