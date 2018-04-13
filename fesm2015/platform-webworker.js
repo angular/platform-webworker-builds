36e4a4c0799dddd79bb78264652267a49e508da6
Igor Minar <igor@angular.io>
2018-04-13 23:52:23 +0000

    7e581da ci: remove travis e2e_2 job (#22810)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-2c09b70
+ * @license Angular v6.0.0-rc.4-7e581da
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1488,7 +1488,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.4-2c09b70');
+const VERSION = new Version('6.0.0-rc.4-7e581da');
 
 /**
  * @fileoverview added by tsickle
