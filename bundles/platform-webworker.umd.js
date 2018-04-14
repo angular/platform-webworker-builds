fac6787f8b325b849be63cd7460e0499d5cd9952
Igor Minar <igor@angular.io>
2018-04-14 06:30:38 +0000

    674c3de revert: refactor(ivy): make return value of define(Component|Directive|Pipe|Injector|Injectable) private (#23371)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.4-33630dd
+ * @license Angular v6.0.0-rc.4-674c3de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.4-33630dd
+ * @license Angular v6.0.0-rc.4-674c3de
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.4-33630dd');
+var VERSION = new _angular_core.Version('6.0.0-rc.4-674c3de');
 
 /**
  * @fileoverview added by tsickle
