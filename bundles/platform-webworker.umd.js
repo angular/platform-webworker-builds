32eca9676aa1013810b0eeb4fa83a7429d15c1bb
Jason Aden <jasonaden@google.com>
2018-04-17 21:40:22 +0000

    fd48e53 docs(aio): add front page campaign for the ng-conf live stream (#23391)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-fe312cc
+ * @license Angular v6.0.0-rc.5-fd48e53
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-fe312cc
+ * @license Angular v6.0.0-rc.5-fd48e53
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-fe312cc');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-fd48e53');
 
 /**
  * @fileoverview added by tsickle
