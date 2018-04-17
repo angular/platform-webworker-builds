fbbb4ebec9ac9cf51a31ab1c89352a9ddc645c0b
Jason Aden <jasonaden@google.com>
2018-04-17 21:24:48 +0000

    5a1ddee refactor(ivy): speed up bound text nodes (#23386)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-7f612fc
+ * @license Angular v6.0.0-rc.5-5a1ddee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-7f612fc
+ * @license Angular v6.0.0-rc.5-5a1ddee
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-7f612fc');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-5a1ddee');
 
 /**
  * @fileoverview added by tsickle
