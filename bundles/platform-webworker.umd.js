07e6c997a39b93c2e9f742f12cffc66ca3be833c
Victor Berchet <victor@suumit.com>
2018-04-23 21:04:45 +0000

    391bfce docs(aio): Add UpgradingAngularJS to education resources (#23169)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-151fb66
+ * @license Angular v6.0.0-rc.5-391bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-151fb66
+ * @license Angular v6.0.0-rc.5-391bfce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-151fb66');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-391bfce');
 
 /**
  * @fileoverview added by tsickle
