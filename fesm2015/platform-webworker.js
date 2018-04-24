0d7588505965cc5681bb3459c96443a89ce4a687
Victor Berchet <victor@suumit.com>
2018-04-24 18:36:31 +0000

    dab5df9 ci(aio): fix `deploy-to-firebase` script (#23470)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+74.sha-1c9200e
+ * @license Angular v6.0.0-rc.5+77.sha-dab5df9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1489,7 +1489,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+74.sha-1c9200e');
+const VERSION = new Version('6.0.0-rc.5+77.sha-dab5df9');
 
 /**
  * @fileoverview added by tsickle
