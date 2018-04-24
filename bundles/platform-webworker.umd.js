c725b65c479f6c1dd0a1b419144edcf036580b5c
Victor Berchet <victor@suumit.com>
2018-04-24 21:59:35 +0000

    f721b06 style: format code

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5-0bc8443
+ * @license Angular v6.0.0-rc.5-f721b06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.5-0bc8443
+ * @license Angular v6.0.0-rc.5-f721b06
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.5-0bc8443');
+var VERSION = new _angular_core.Version('6.0.0-rc.5-f721b06');
 
 /**
  * @fileoverview added by tsickle
