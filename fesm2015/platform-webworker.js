a8a2d539251a139dfb41c5914353083628109d2f
Igor Minar <igor@angular.io>
2018-04-25 20:55:17 +0000

    db77d8d feat(ivy): support injection flags at runtime (#23518)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+83.sha-f567e18
+ * @license Angular v6.0.0-rc.5+85.sha-db77d8d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1489,7 +1489,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+83.sha-f567e18');
+const VERSION = new Version('6.0.0-rc.5+85.sha-db77d8d');
 
 /**
  * @fileoverview added by tsickle
