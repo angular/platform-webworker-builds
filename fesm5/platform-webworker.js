fdb688a7ee3e4949c842fdf764f678c766a86200
Igor Minar <igor@angular.io>
2018-04-26 02:25:29 +0000

    1a44a0b feat(ivy): support lifecycle hooks of ViewContainerRef (#23396)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+88.sha-b1f040f
+ * @license Angular v6.0.0-rc.5+89.sha-1a44a0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1051,7 +1051,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new Version('6.0.0-rc.5+88.sha-b1f040f');
+var VERSION = new Version('6.0.0-rc.5+89.sha-1a44a0b');
 
 /**
  * @license
