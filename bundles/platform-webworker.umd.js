b0021437f10f1723dda26085f2200ac6c4275566
Igor Minar <igor@angular.io>
2018-05-01 18:04:44 +0000

    f11daa2 docs(aio): update Egghead.io URL (#23598)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.6-31a435e
+ * @license Angular v6.0.0-rc.6-f11daa2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-rc.6-31a435e
+ * @license Angular v6.0.0-rc.6-f11daa2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-rc.6-31a435e');
+var VERSION = new _angular_core.Version('6.0.0-rc.6-f11daa2');
 
 /**
  * @fileoverview added by tsickle
