0c13d490f29175ff714c9235cbf0535c3a71ad53
Igor Minar <igor@angular.io>
2018-05-03 21:49:54 +0000

    9027818 docs(aio): Upgrade server-side rendering example to Angular V6 (#23649)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-5bd8c68
+ * @license Angular v6.0.0-9027818
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1488,7 +1488,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-5bd8c68');
+const VERSION = new Version('6.0.0-9027818');
 
 /**
  * @fileoverview added by tsickle
