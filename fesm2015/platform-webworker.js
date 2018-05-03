6759442c8bc174ed634dc35d27a2d6bb1f864771
Igor Minar <igor@angular.io>
2018-05-03 22:34:14 +0000

    2c4850d fix(aio): correct project id for deployment of archive sites

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+124.sha-2ef4760
+ * @license Angular v6.0.0-rc.5+125.sha-2c4850d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1489,7 +1489,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-const VERSION = new Version('6.0.0-rc.5+124.sha-2ef4760');
+const VERSION = new Version('6.0.0-rc.5+125.sha-2c4850d');
 
 /**
  * @fileoverview added by tsickle
