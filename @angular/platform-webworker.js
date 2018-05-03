c2d5e78128fb119523d8287e21069773f29383e0
Igor Minar <igor@angular.io>
2018-05-03 22:30:39 +0000

    b19216d fix(aio): correct project id for deployment of archive sites

--- @angular/platform-webworker.js
+++ @angular/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.7-84fc1a3
+ * @license Angular v4.4.7-b19216d
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1505,7 +1505,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-const VERSION = new Version('4.4.7-84fc1a3');
+const VERSION = new Version('4.4.7-b19216d');
 
 /**
  * @license
