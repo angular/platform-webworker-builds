f274aba7fa0117269fe9fc0d67274daf5a008be9
Igor Minar <igor@angular.io>
2018-05-04 22:32:57 +0000

    0187501 test: fix firebase deployment script test

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v4.4.7-b19216d
+ * @license Angular v4.4.7-0187501
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v4.4.7-b19216d
+ * @license Angular v4.4.7-0187501
  * (c) 2010-2017 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1586,7 +1586,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('4.4.7-b19216d');
+var VERSION = new _angular_core.Version('4.4.7-0187501');
 /**
  * @license
  * Copyright Google Inc. All Rights Reserved.
