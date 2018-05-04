9f04667dbbe478752d76b89d5454b08a687d009d
Igor Minar <igor@angular.io>
2018-05-04 23:07:16 +0000

    bbc416c build: update bazel to 0.13 (#23623)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-07f2098
+ * @license Angular v6.0.0-bbc416c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v6.0.0-07f2098
+ * @license Angular v6.0.0-bbc416c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new _angular_core.Version('6.0.0-07f2098');
+var VERSION = new _angular_core.Version('6.0.0-bbc416c');
 
 /**
  * @fileoverview added by tsickle
