749dfef133d2742d633ffb4631c901415e81e991
Igor Minar <igor@angular.io>
2018-05-08 21:20:15 +0000

    fe7f48c docs(aio): Upgrade example dependencies to Angular V6 (#23660)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0+28.sha-5581e97
+ * @license Angular v6.0.0+30.sha-fe7f48c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1489,7 +1489,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-const VERSION = new Version('6.0.0+28.sha-5581e97');
+const VERSION = new Version('6.0.0+30.sha-fe7f48c');
 
 /**
  * @fileoverview added by tsickle
