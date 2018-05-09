17ee39f89539610474c2a2549225b08c77aeb2ee
Igor Minar <igor@angular.io>
2018-05-09 19:32:35 +0000

    f1e4a15 refactor(service-worker): sort manifest `url`/`hashTable` entries (#23586)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.0-rc.5+150.sha-08e7efc
+ * @license Angular v6.0.0-rc.5+154.sha-f1e4a15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1119,7 +1119,7 @@ function spawnWebWorker(uri, instance) {
 /**
  *
  */
-var VERSION = new core.Version('6.0.0-rc.5+150.sha-08e7efc');
+var VERSION = new core.Version('6.0.0-rc.5+154.sha-f1e4a15');
 
 /**
  * @license
