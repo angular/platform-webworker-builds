364e064b135f3c0e3a52967802371a261d974cd1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-05-15 19:37:01 +0000

    bc27d4a fix(service-worker): correctly handle requests with empty `clientId` (#23625)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-65dba9d
+ * @license Angular v5.2.10-bc27d4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.10-65dba9d
+ * @license Angular v5.2.10-bc27d4a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.10-65dba9d');
+var VERSION = new _angular_core.Version('5.2.10-bc27d4a');
 
 /**
  * @fileoverview added by tsickle
