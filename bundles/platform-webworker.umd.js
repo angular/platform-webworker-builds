fafced23b6baaab9cb5b9f3cd3a93572a4af1542
Miško Hevery <misko@hevery.com>
2018-06-07 23:11:10 +0000

    7031972 docs(aio): Added resource link to Amexio Canvas Web Based IDE (#24336)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.0.4+1.sha-cfa5b65
+ * @license Angular v6.0.4+2.sha-7031972
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1103,7 +1103,7 @@ function spawnWebWorker(uri, instance) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-var VERSION = new core.Version('6.0.4+1.sha-cfa5b65');
+var VERSION = new core.Version('6.0.4+2.sha-7031972');
 
 /**
  * @license
