c15d4934303a6a104b4a9191e27545197962a076
Miško Hevery <misko@hevery.com>
2018-06-13 21:13:33 +0000

    a577c9e docs: edit api doc comments for new template and style (#23682)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.0+43.sha-70ef061
+ * @license Angular v6.1.0-beta.0+55.sha-a577c9e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1482,7 +1482,7 @@ function spawnWebWorker(uri, instance) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.0+43.sha-70ef061');
+const VERSION = new Version('6.1.0-beta.0+55.sha-a577c9e');
 
 /**
  * @fileoverview added by tsickle
