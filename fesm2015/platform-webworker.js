afbc9ee4e504cbf6fbec583003fd550176f6da32
Miško Hevery <misko@hevery.com>
2018-06-14 22:08:38 +0000

    27bc7dc feat(ivy): ngtsc compiles @Component, @Directive, @NgModule (#24427)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+29.sha-0f7e4fa
+ * @license Angular v6.1.0-beta.1+30.sha-27bc7dc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1482,7 +1482,7 @@ function spawnWebWorker(uri, instance) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-const VERSION = new Version('6.1.0-beta.1+29.sha-0f7e4fa');
+const VERSION = new Version('6.1.0-beta.1+30.sha-27bc7dc');
 
 /**
  * @fileoverview added by tsickle
