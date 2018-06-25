d842e6fe959e6412a168de20be9984f18432163d
Pete Bacon Darwin <pete@bacondarwin.com>
2018-06-25 13:49:22 +0000

    db21dbf test(ivy): fix ngcc Esm2015ReflectionHost tests

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.1+44.sha-368a7ed
+ * @license Angular v6.1.0-beta.1+45.sha-db21dbf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -973,7 +973,7 @@ function spawnWebWorker(uri, instance) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.1+44.sha-368a7ed');
+const VERSION = new Version('6.1.0-beta.1+45.sha-db21dbf');
 
 /**
  * @license
