ae88ad90ed0f5dcae4e966ea56c2fdaf2396475e
Miško Hevery <misko@hevery.com>
2018-07-03 16:07:31 +0000

    c94a2c9 build(bazel): update to latest rules_typescript (#24737)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+26.sha-948e223
+ * @license Angular v6.1.0-beta.3+27.sha-c94a2c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -973,7 +973,7 @@ function spawnWebWorker(uri, instance) {
  * @description
  * Entry point for all public APIs of the common package.
  */
-const VERSION = new Version('6.1.0-beta.3+26.sha-948e223');
+const VERSION = new Version('6.1.0-beta.3+27.sha-c94a2c9');
 
 /**
  * @license
