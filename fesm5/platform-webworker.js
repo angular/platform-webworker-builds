67a5a2e07d26af27cba24dd0ff2495be820b0fa2
Miško Hevery <misko@hevery.com>
2018-07-11 02:54:39 +0000

    a527c69 fix(common): do not round factional seconds (#24831)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-beta.3+79.sha-8057664
+ * @license Angular v6.1.0-beta.3+80.sha-a527c69
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ function spawnWebWorker(uri, instance) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-beta.3+79.sha-8057664');
+var VERSION = new Version('6.1.0-beta.3+80.sha-a527c69');
 
 /**
  * @license
