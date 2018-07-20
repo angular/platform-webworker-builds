a9f2d0d678bad1dcbd3c2642a3afc654d097ba30
Victor Berchet <victor@suumit.com>
2018-07-20 20:00:18 +0000

    8620373 fix(service-worker): don't include sourceMappingURL in ngsw-worker (#24877)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0-rc.3+17.sha-bb58138
+ * @license Angular v6.1.0-rc.3+41.sha-8620373
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ function spawnWebWorker(uri, instance) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.0-rc.3+17.sha-bb58138');
+var VERSION = new Version('6.1.0-rc.3+41.sha-8620373');
 
 /**
  * @license
