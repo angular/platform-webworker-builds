dcef8ff9aa7eb5451a7aa7ca18275d0f036aa27b
Igor Minar <igor@angular.io>
2018-07-27 19:50:53 +0000

    ce98634 build(compiler-cli): update tsickle dependency to support TypeScript 2.9 (#25152)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+18.sha-3426784
+ * @license Angular v6.1.0+19.sha-ce98634
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+18.sha-3426784');
+const VERSION = new Version('6.1.0+19.sha-ce98634');
 
 /**
  * @fileoverview added by tsickle
