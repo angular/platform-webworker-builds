79e181f717d82beba6b8d142c88e35eba21049e5
Igor Minar <igor@angular.io>
2018-07-31 20:57:40 +0000

    fa38828 docs(aio): add short description for entryComponents (#21360)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+18.sha-875efa8
+ * @license Angular v6.1.0+21.sha-fa38828
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+18.sha-875efa8');
+const VERSION = new Version('6.1.0+21.sha-fa38828');
 
 /**
  * @fileoverview added by tsickle
