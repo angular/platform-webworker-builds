180af655f6bdcd6716fa83bc31b4cbc33ec8e304
Igor Minar <igor@angular.io>
2018-08-01 20:27:26 +0000

    7a4fb44 docs(aio): add Kevin Yang to GDE resources (#24791)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+74.sha-a9ecf4b
+ * @license Angular v6.1.0+77.sha-7a4fb44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+74.sha-a9ecf4b');
+const VERSION = new Version('6.1.0+77.sha-7a4fb44');
 
 /**
  * @fileoverview added by tsickle
