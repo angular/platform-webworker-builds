9e02386aef3986350d3ffdfac39e252c62d903cb
Igor Minar <igor@angular.io>
2018-08-01 20:07:22 +0000

    a83b9f7 docs(changelog): remove reverted feature entry (#25206)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.0+22.sha-d4bf2da
+ * @license Angular v6.1.0+26.sha-a83b9f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.0+22.sha-d4bf2da');
+const VERSION = new Version('6.1.0+26.sha-a83b9f7');
 
 /**
  * @fileoverview added by tsickle
