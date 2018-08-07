9b293dd5bfc465998f6b54321d828cd43adccd10
Kara Erickson <karakara@google.com>
2018-08-07 20:03:27 +0000

    2f23533 docs(aio): Angular course in Portuguese #21836

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.1+26.sha-054fbbe
+ * @license Angular v6.1.1+27.sha-2f23533
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1030,7 +1030,7 @@ function spawnWebWorker(uri, instance) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('6.1.1+26.sha-054fbbe');
+var VERSION = new Version('6.1.1+27.sha-2f23533');
 
 /**
  * @license
