58e0e380bb6e3a3e9dac6b078572c8b769a4f885
Kara Erickson <karakara@google.com>
2018-08-08 21:52:37 +0000

    a3f1e2c style: format .bzl files with buildifier (#23544)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.1+1.sha-1039bea
+ * @license Angular v7.0.0-beta.1+7.sha-a3f1e2c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.1+1.sha-1039bea');
+const VERSION = new Version('7.0.0-beta.1+7.sha-a3f1e2c');
 
 /**
  * @fileoverview added by tsickle
