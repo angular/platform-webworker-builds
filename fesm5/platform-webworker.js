f69694c94bf5056f642c1b853956a8259a6d7584
Ben Lesh <ben@benlesh.com>
2018-08-14 01:43:52 +0000

    97d8b5e fix(ivy): reordering how root is acquired (#25470)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.0.0-beta.1+25.sha-4a4d6fb
+ * @license Angular v7.0.0-beta.1+26.sha-97d8b5e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, Injectable, RenderComponentType, ɵstringify, EventEmitter, RendererFactory2, ErrorHandler, Injector, NgZone, PLATFORM_ID, PLATFORM_INITIALIZER, Testability, createPlatformFactory, isDevMode, platformCore, ɵAPP_ID_RANDOM_PROVIDER, Version, APP_INITIALIZER, ApplicationModule, NgModule } from '@angular/core';
-import { __extends, __read, __spread } from 'tslib';
+import { __read, __spread, __extends } from 'tslib';
 import { ɵPLATFORM_WORKER_UI_ID, DOCUMENT as DOCUMENT$1, PlatformLocation, LOCATION_INITIALIZED, CommonModule, ViewportScroller, ɵNullViewportScroller, ɵPLATFORM_WORKER_APP_ID } from '@angular/common';
 import { DOCUMENT, EVENT_MANAGER_PLUGINS, EventManager, HAMMER_GESTURE_CONFIG, HammerGestureConfig, ɵBROWSER_SANITIZATION_PROVIDERS, ɵBrowserDomAdapter, ɵBrowserGetTestability, ɵDomEventsPlugin, ɵDomRendererFactory2, ɵDomSharedStylesHost, ɵHammerGesturesPlugin, ɵKeyEventsPlugin, ɵSharedStylesHost, ɵBrowserPlatformLocation, ɵDomAdapter, ɵsetRootDomAdapter } from '@angular/platform-browser';
 
@@ -1030,7 +1030,7 @@ function spawnWebWorker(uri, instance) {
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.io/license
  */
-var VERSION = new Version('7.0.0-beta.1+25.sha-4a4d6fb');
+var VERSION = new Version('7.0.0-beta.1+26.sha-97d8b5e');
 
 /**
  * @license
