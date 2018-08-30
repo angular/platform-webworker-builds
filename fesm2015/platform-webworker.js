a80370be1625367ece9d2f41e3e0354adcd529dd
Matias Niemelä <matias@yearofmoo.com>
2018-08-30 19:01:06 +0000

    2e09115 docs: edit and organize di guide (#21915)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6
+ * @license Angular v6.1.6+1.sha-2e09115
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6');
+const VERSION = new Version('6.1.6+1.sha-2e09115');
 
 /**
  * @fileoverview added by tsickle
