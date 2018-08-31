0393091e9dfa15abf4a7b44ff6f25c261b4348ef
Miško Hevery <misko@hevery.com>
2018-08-31 17:23:30 +0000

    a065dc2 fix(compiler): remove bogus lines

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.10-bc27d4a
+ * @license Angular v5.2.11-a065dc2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.10-bc27d4a
+ * @license Angular v5.2.11-a065dc2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.10-bc27d4a');
+var VERSION = new _angular_core.Version('5.2.11-a065dc2');
 
 /**
  * @fileoverview added by tsickle
