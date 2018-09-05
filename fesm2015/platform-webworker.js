6cb0cbc00059b68bc755358dab83bec17938ff42
Misko Hevery <misko@hevery.com>
2018-09-05 19:20:14 +0000

    46de203 refactor(docs-infra): simplify custom-element polyfill setup (#25806)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.6+10.sha-ad7be50
+ * @license Angular v6.1.6+14.sha-46de203
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.6+10.sha-ad7be50');
+const VERSION = new Version('6.1.6+14.sha-46de203');
 
 /**
  * @fileoverview added by tsickle
