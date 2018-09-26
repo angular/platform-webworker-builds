41c421bad546565c62695824176cf18bd516da36
Kara Erickson <karakara@google.com>
2018-09-26 23:03:47 +0000

    a01acec fix(docs-infra): use correct parameters for paginated requests to GitHub (#25671)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-beta.6+89.sha-4597582
+ * @license Angular v7.0.0-beta.7+18.sha-a01acec
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-beta.6+89.sha-4597582');
+const VERSION = new Version('7.0.0-beta.7+18.sha-a01acec');
 
 /**
  * @fileoverview added by tsickle
