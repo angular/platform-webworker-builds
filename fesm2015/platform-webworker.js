191fb9f4bfd3568244aeaad05daf9c4d23faba31
Jason Aden <jasonaden@google.com>
2018-10-08 21:12:04 +0000

    ed04e99 docs: update docs to reflect the changes in RxJS 6 (#26238)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v6.1.9+72.sha-623adbb
+ * @license Angular v6.1.9+73.sha-ed04e99
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('6.1.9+72.sha-623adbb');
+const VERSION = new Version('6.1.9+73.sha-ed04e99');
 
 /**
  * @fileoverview added by tsickle
