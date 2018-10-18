4ecf2a7adbe9cdc2ff01480744a0c6d048e9be18
Igor Minar <igor@angular.io>
2018-10-18 20:04:04 +0000

    bbf96db feat(docs-infra): add v6 to the aio version picker

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+99.sha-72c2578
+ * @license Angular v7.0.0-rc.1+100.sha-bbf96db
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+99.sha-72c2578');
+const VERSION = new Version('7.0.0-rc.1+100.sha-bbf96db');
 
 /**
  * @fileoverview added by tsickle
