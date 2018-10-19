a688bc2b7315b0474f3efb1a34d010fb95016f64
Alex Rickabaugh <arick@arick-macbookpro2.roam.corp.google.com>
2018-10-19 19:13:22 +0000

    07fc4c2 docs(service-worker): updated browser support for Service Worker (#26408)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.0-rc.1+108.sha-aa55d88
+ * @license Angular v7.0.0-rc.1+107.sha-07fc4c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1456,7 +1456,7 @@ function spawnWebWorker(uri, instance) {
  * @suppress {checkTypes,extraRequire,uselessCode} checked by tsc
  */
 /** @type {?} */
-const VERSION = new Version('7.0.0-rc.1+108.sha-aa55d88');
+const VERSION = new Version('7.0.0-rc.1+107.sha-07fc4c2');
 
 /**
  * @fileoverview added by tsickle
