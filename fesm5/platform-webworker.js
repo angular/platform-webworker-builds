d3a8852309115cf48688cb77f365ac4683b0db6a
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 18:32:17 +0000

    8a4ec8e docs: describe your change... (#26784) PR Close #26784

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.0+65.sha-95743e3
+ * @license Angular v7.1.0-beta.0+66.sha-8a4ec8e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -978,7 +978,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+65.sha-95743e3');
+var VERSION = new Version('7.1.0-beta.0+66.sha-8a4ec8e');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
