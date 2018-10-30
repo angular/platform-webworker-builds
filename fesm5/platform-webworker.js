485f51af83ffd4c8b4d6f2f63e5ebfac0db28e36
Matias Niemelä <matias@yearofmoo.com>
2018-10-30 20:55:24 +0000

    678c281 docs: fix typo (#26776) PR Close #26776

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,11 +1,11 @@
 /**
- * @license Angular v7.1.0-beta.0+72.sha-634e9e9
+ * @license Angular v7.1.0-beta.0+71.sha-678c281
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
 import { InjectionToken, Injectable, RenderComponentType, ɵstringify, EventEmitter, RendererFactory2, ErrorHandler, Injector, NgZone, PLATFORM_ID, PLATFORM_INITIALIZER, Testability, createPlatformFactory, isDevMode, platformCore, ɵAPP_ID_RANDOM_PROVIDER, Version, APP_INITIALIZER, ApplicationModule, NgModule } from '@angular/core';
-import { __decorate, __metadata, __extends, __spread, __read } from 'tslib';
+import { __decorate, __metadata, __extends, __read, __spread } from 'tslib';
 import { ɵPLATFORM_WORKER_UI_ID, DOCUMENT as DOCUMENT$1, PlatformLocation, LOCATION_INITIALIZED, CommonModule, ViewportScroller, ɵNullViewportScroller, ɵPLATFORM_WORKER_APP_ID } from '@angular/common';
 import { DOCUMENT, EVENT_MANAGER_PLUGINS, EventManager, HAMMER_GESTURE_CONFIG, HammerGestureConfig, ɵBROWSER_SANITIZATION_PROVIDERS, ɵBrowserDomAdapter, ɵBrowserGetTestability, ɵDomEventsPlugin, ɵDomRendererFactory2, ɵDomSharedStylesHost, ɵHammerGesturesPlugin, ɵKeyEventsPlugin, ɵSharedStylesHost, ɵBrowserPlatformLocation, ɵDomAdapter, ɵsetRootDomAdapter } from '@angular/platform-browser';
 
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.0+72.sha-634e9e9');
+var VERSION = new Version('7.1.0-beta.0+71.sha-678c281');
 
 /**
  * @license
