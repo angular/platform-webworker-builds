ac3e5d7db8e041c77d26e7d3edae1402e1d7fcff
Kara Erickson <karakara@google.com>
2018-11-01 21:15:24 +0000

    2810482 build: update `canonical-path` dependency (#26719)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.2
+ * @license Angular v7.0.2+1.sha-2810482
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.2');
+var VERSION = new Version('7.0.2+1.sha-2810482');
 
 /**
  * @license
