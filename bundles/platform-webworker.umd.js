a7caa80d035a64c79b084da80e4c8a3560e5e3b4
Kara Erickson <karakara@google.com>
2018-11-02 20:47:35 +0000

    516af6c docs: minor edits and corrections to cli intro (#26654)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+63.sha-9c2d0d0
+ * @license Angular v7.1.0-beta.1+64.sha-516af6c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.1+63.sha-9c2d0d0');
+    var VERSION = new i0.Version('7.1.0-beta.1+64.sha-516af6c');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
