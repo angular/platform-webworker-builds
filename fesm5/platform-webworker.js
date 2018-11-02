e303c6e0c948d0bac0e98881fbcb329f6eed212b
Kara Erickson <karakara@google.com>
2018-11-02 18:09:42 +0000

    c016066 fix(ivy): ngcc should not break lifecycle hooks (#26856)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+60.sha-beabfb7
+ * @license Angular v7.1.0-beta.1+61.sha-c016066
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.1+60.sha-beabfb7');
+var VERSION = new Version('7.1.0-beta.1+61.sha-c016066');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
