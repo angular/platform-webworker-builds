536e645ded88131c44a354e18814bb345ec1ba5a
Kara Erickson <karakara@google.com>
2018-11-05 20:02:21 +0000

    f9a4abb fix: missing semi-colon and empty line (#26945)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.1+71.sha-4e4bca6
+ * @license Angular v7.1.0-beta.1+72.sha-f9a4abb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.1+71.sha-4e4bca6');
+    var VERSION = new i0.Version('7.1.0-beta.1+72.sha-f9a4abb');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
