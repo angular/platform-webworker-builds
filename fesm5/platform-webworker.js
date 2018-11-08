20231bac68d158b3a4df24e4e61fc5720fedc106
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:25:54 +0000

    006a43f docs: correct https://angularconsole.com/ link (#26999) PR Close #26999

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3
+ * @license Angular v7.0.3+2.sha-006a43f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3');
+var VERSION = new Version('7.0.3+2.sha-006a43f');
 
 /**
  * @license
