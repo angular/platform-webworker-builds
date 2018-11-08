4f8f637207c9c66b4dce850cf79819f8e48c7c95
Andrew Kushnir <akushnir@google.com>
2018-11-08 19:32:20 +0000

    19546c2 build: add 'yarn list-fixme-ivy-targets' to list all the fixme-ivy-* targest (#26834)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+3.sha-791d192
+ * @license Angular v7.1.0-beta.2+4.sha-19546c2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+3.sha-791d192');
+var VERSION = new Version('7.1.0-beta.2+4.sha-19546c2');
 
 /**
  * @license
