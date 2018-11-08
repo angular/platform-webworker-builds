b0e3776d4a8676fe4f478b0085dc6ed70fdb0086
Andrew Kushnir <akushnir@google.com>
2018-11-08 21:36:09 +0000

    1529ecd docs: add Angular subreddit to resources.json (#26976)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+5.sha-0d972d9
+ * @license Angular v7.1.0-beta.2+7.sha-1529ecd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0-beta.2+5.sha-0d972d9');
+    var VERSION = new i0.Version('7.1.0-beta.2+7.sha-1529ecd');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+5.sha-0d972d9
+ * @license Angular v7.1.0-beta.2+7.sha-1529ecd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+5.sha-0d972d9');
+var VERSION = new Version('7.1.0-beta.2+7.sha-1529ecd');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
