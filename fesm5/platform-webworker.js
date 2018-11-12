7334753b9f3c03052d0da321fca55e4822ec1dc9
Andrew Kushnir <akushnir@google.com>
2018-11-12 21:24:16 +0000

    e5c9f7a fix(ivy): provide NgZone to ComponentRef (#26898)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+26.sha-f80c600
+ * @license Angular v7.1.0-beta.2+21.sha-e5c9f7a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+26.sha-f80c600');
+var VERSION = new Version('7.1.0-beta.2+21.sha-e5c9f7a');
 
 /**
  * @license
