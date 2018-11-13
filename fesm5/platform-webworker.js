ab8eaa9d4310a4db708700c99505eb472ab59b12
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:25:32 +0000

    0ada23a fix(compiler-cli): only pass canonical genfile paths to compiler host (#27062)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-beta.2+35.sha-1b97971
+ * @license Angular v7.1.0-beta.2+34.sha-0ada23a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-beta.2+35.sha-1b97971');
+var VERSION = new Version('7.1.0-beta.2+34.sha-0ada23a');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
