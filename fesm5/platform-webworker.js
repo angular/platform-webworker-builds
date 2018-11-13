905cbf91299f588819779d3522f2a7885fee6b00
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:13:27 +0000

    bff5c24 docs: fix typo (#26878) PR Close #26878

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+17.sha-53197de
+ * @license Angular v7.0.3+18.sha-bff5c24
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+17.sha-53197de');
+var VERSION = new Version('7.0.3+18.sha-bff5c24');
 
 /**
  * @license
