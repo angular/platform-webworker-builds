cca65fac34d016f98599119c7e1459e3aeb0f350
Andrew Kushnir <akushnir@google.com>
2018-11-13 19:19:30 +0000

    2ec0541 docs: new doc for core directives (#26923)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+20.sha-de7b554
+ * @license Angular v7.0.3+22.sha-2ec0541
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+20.sha-de7b554');
+var VERSION = new Version('7.0.3+22.sha-2ec0541');
 
 /**
  * @license
