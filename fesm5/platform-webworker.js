d5b4bdd03588edba57c203a27cbccd1390b646a8
Andrew Kushnir <akushnir@google.com>
2018-11-14 20:46:52 +0000

    9c7428e build(bazel): turn on --nolegacy-external-runfiles (#26770)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.0.3+23.sha-4348c47
+ * @license Angular v7.0.3+25.sha-9c7428e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.0.3+23.sha-4348c47');
+var VERSION = new Version('7.0.3+25.sha-9c7428e');
 
 /**
  * @license
