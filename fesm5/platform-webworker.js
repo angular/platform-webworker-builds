b13207be7a95d311236c3654a14cb747a283697b
Misko Hevery <misko@hevery.com>
2018-11-17 20:43:20 +0000

    81e975a build: update to latest Bazel rules_typescript (#27138)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+15.sha-20ea5b5
+ * @license Angular v7.1.0-rc.0+16.sha-81e975a.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+15.sha-20ea5b5');
+var VERSION = new Version('7.1.0-rc.0+16.sha-81e975a.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
