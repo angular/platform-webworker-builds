0fab2dc93a433b4045faf36d80f0dedef0272764
Misko Hevery <misko@hevery.com>
2018-11-20 19:21:11 +0000

    34306c3 docs(forms): close markdown backticks (#27189)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0-rc.0+24.sha-391767f.with-local-changes
+ * @license Angular v7.1.0-rc.0+26.sha-34306c3.with-local-changes
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0-rc.0+24.sha-391767f.with-local-changes');
+var VERSION = new Version('7.1.0-rc.0+26.sha-34306c3.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
