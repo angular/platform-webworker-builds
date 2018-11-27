5927785850aecff4c7ec18d70def21646fb824bb
Jason Aden <jasonaden@google.com>
2018-11-27 18:45:48 +0000

    d767e0b fix(ivy): consider providers for view/content queries (#27156)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+11.sha-f45aedc
+ * @license Angular v7.1.0+13.sha-d767e0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+11.sha-f45aedc');
+var VERSION = new Version('7.1.0+13.sha-d767e0b');
 
 /**
  * @license
