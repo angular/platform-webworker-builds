f61ddf695e51ba6e9fc560d4133d7e5df3a80473
Igor Minar <igor@angular.io>
2018-11-30 06:22:08 +0000

    2389a68 test(platform-server): import ivyEnabled from @angular/private/testing (#27302)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,10 +1,10 @@
 /**
- * @license Angular v7.1.0+68.sha-c331fc6
+ * @license Angular v7.1.0+67.sha-2389a68
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
 
-import { InjectionToken, Injectable, Version, ɵstringify, RenderComponentType, EventEmitter, PLATFORM_INITIALIZER, Injector, NgZone, APP_INITIALIZER, defineInjectable, inject, ɵsetClassMetadata, createPlatformFactory, platformCore, PLATFORM_ID, ErrorHandler, ɵdefineNgModule, ApplicationModule, defineInjector, RendererFactory2, NgModule, ɵAPP_ID_RANDOM_PROVIDER, Testability, isDevMode } from '@angular/core';
+import { InjectionToken, Injectable, Version, ɵstringify, RenderComponentType, EventEmitter, APP_INITIALIZER, NgZone, PLATFORM_INITIALIZER, Injector, defineInjectable, inject, ɵsetClassMetadata, RendererFactory2, ErrorHandler, ɵAPP_ID_RANDOM_PROVIDER, Testability, PLATFORM_ID, createPlatformFactory, platformCore, isDevMode, ɵdefineNgModule, ApplicationModule, defineInjector, NgModule } from '@angular/core';
 import { __spread, __extends, __read } from 'tslib';
 import { ɵPLATFORM_WORKER_UI_ID, DOCUMENT as DOCUMENT$1, PlatformLocation, LOCATION_INITIALIZED, CommonModule, ViewportScroller, ɵNullViewportScroller, ɵPLATFORM_WORKER_APP_ID } from '@angular/common';
 import { DOCUMENT, EVENT_MANAGER_PLUGINS, EventManager, HAMMER_GESTURE_CONFIG, HammerGestureConfig, ɵBROWSER_SANITIZATION_PROVIDERS, ɵBrowserDomAdapter, ɵBrowserGetTestability, ɵDomEventsPlugin, ɵDomRendererFactory2, ɵDomSharedStylesHost, ɵHammerGesturesPlugin, ɵKeyEventsPlugin, ɵSharedStylesHost, ɵBrowserPlatformLocation, ɵDomAdapter, ɵsetRootDomAdapter } from '@angular/platform-browser';
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+68.sha-c331fc6');
+var VERSION = new Version('7.1.0+67.sha-2389a68');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
