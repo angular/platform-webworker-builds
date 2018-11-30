921db56ecd0b1e7f28cbaf489c5a4755603af2a6
Igor Minar <igor@angular.io>
2018-11-30 06:10:23 +0000

    2d51579 docs: move Minko to Angular (#27339)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+50.sha-8ceea0f
+ * @license Angular v7.1.0+52.sha-2d51579
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+50.sha-8ceea0f');
+var VERSION = new Version('7.1.0+52.sha-2d51579');
 
 /**
  * @license
