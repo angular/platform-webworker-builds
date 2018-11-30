16cf0aa4951e150bff2346cd2a051e58b8d3eea6
Igor Minar <igor@angular.io>
2018-11-30 06:18:54 +0000

    4effb89 test(ivy): update //packages/compiler tests for Ivy (#27301)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+63.sha-eb99753
+ * @license Angular v7.1.0+62.sha-4effb89
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+63.sha-eb99753');
+var VERSION = new Version('7.1.0+62.sha-4effb89');
 
 /**
  * @license
