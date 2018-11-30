1ac78578dc11566bf0e51947dd8ca59479ed5641
Igor Minar <igor@angular.io>
2018-11-30 22:07:21 +0000

    c8a8dcf build(bazel): update handling of rules_nodejs transitive deps (#27264)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+83.sha-1fa5478
+ * @license Angular v7.1.0+81.sha-c8a8dcf
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+83.sha-1fa5478');
+    var VERSION = new i0.Version('7.1.0+81.sha-c8a8dcf');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
