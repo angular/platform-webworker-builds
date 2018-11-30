1fd54200e8d5d4af4ad4e9306ad7a142a4a906d4
Igor Minar <igor@angular.io>
2018-11-30 06:12:22 +0000

    816ec0b refactor(ivy): treate LView as the primary global state (#27282)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+52.sha-2d51579
+ * @license Angular v7.1.0+55.sha-816ec0b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+52.sha-2d51579');
+var VERSION = new Version('7.1.0+55.sha-816ec0b');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
