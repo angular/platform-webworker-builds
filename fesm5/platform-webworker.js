f39eb461793c4f62d012bdad4db1dcf3f1317a89
Igor Minar <igor@angular.io>
2018-11-30 22:04:49 +0000

    1ddc34c build(bazel): update handling of rules_nodejs transitive deps (#27264)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+14.sha-fc2c23e
+ * @license Angular v7.1.1+13.sha-1ddc34c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+14.sha-fc2c23e');
+var VERSION = new Version('7.1.1+13.sha-1ddc34c');
 
 /**
  * @license
