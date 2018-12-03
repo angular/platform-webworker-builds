e639966e5936ef2c8d48823668991b761fcb4c85
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-03 20:30:33 +0000

    7d89cff fix(ivy): Ensure ComponentFactory uses both injectors to retrieve rendererFactory (#27341)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+99.sha-01fd0cd
+ * @license Angular v7.1.0+109.sha-7d89cff
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+99.sha-01fd0cd');
+var VERSION = new Version('7.1.0+109.sha-7d89cff');
 
 /**
  * @license
