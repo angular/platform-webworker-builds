2527d9ddc1a0f236fd2e21ce7f0e02fb454eefab
Igor Minar <igor@angular.io>
2018-12-04 22:39:16 +0000

    8e644d9 fix(ivy): taking "interpolation" config option into account (FW-723) (#27363)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+121.sha-d3c08e7
+ * @license Angular v7.1.0+128.sha-8e644d9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+121.sha-d3c08e7');
+var VERSION = new Version('7.1.0+128.sha-8e644d9');
 
 /**
  * @license
