407c7581cc01f0c49b332d8b39264c1a432bdb33
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-04 00:04:13 +0000

    6552471 fix(ivy): account for `useValue: undefined` providers in module injector (#27035)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+109.sha-7d89cff
+ * @license Angular v7.1.0+115.sha-6552471
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+109.sha-7d89cff');
+var VERSION = new Version('7.1.0+115.sha-6552471');
 
 /**
  * @license
