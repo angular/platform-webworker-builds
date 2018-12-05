52c75e44d60bf5c677e8a46f9b54f35b5c95e958
Igor Minar <igor@angular.io>
2018-12-05 19:50:40 +0000

    821fecb test(ivy): update root causes for @angular/core TestBed failures (#27479)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+162.sha-2643350
+ * @license Angular v7.1.0+163.sha-821fecb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+162.sha-2643350');
+    var VERSION = new i0.Version('7.1.0+163.sha-821fecb');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
