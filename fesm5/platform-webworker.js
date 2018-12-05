0b75fd48e3f6f18f2c91872b3773287f8dd2b816
Igor Minar <igor@angular.io>
2018-12-05 04:47:10 +0000

    5c57920 fix(docs-infra): log the successful creation of previews (#27436)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+24.sha-9b4d959
+ * @license Angular v7.1.1+25.sha-5c57920
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+24.sha-9b4d959');
+var VERSION = new Version('7.1.1+25.sha-5c57920');
 
 /**
  * @license
