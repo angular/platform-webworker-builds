3e91c6ba8f8ad2155272b8b2372d4613bd1e2116
Igor Minar <igor@angular.io>
2018-12-05 19:49:27 +0000

    821fecb test(ivy): update root causes for @angular/core TestBed failures (#27479)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+162.sha-2643350
+ * @license Angular v7.1.0+163.sha-821fecb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.0+162.sha-2643350');
+var VERSION = new Version('7.1.0+163.sha-821fecb');
 
 /**
  * @license
