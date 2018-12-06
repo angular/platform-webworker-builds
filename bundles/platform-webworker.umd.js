0ec27088fc203e33e54a4bf5935403ae48d7053b
Igor Minar <igor@angular.io>
2018-12-06 05:20:55 +0000

    6740d0d build: remove obsolete gulp build task (#27386)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.0+185.sha-a315ded
+ * @license Angular v7.1.0+194.sha-6740d0d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.1.0+185.sha-a315ded');
+    var VERSION = new i0.Version('7.1.0+194.sha-6740d0d');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
