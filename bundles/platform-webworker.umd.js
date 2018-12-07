bbb093f64d6fdf0d4bc90f79dc6486fedd7d9f50
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-07 20:21:12 +0000

    84ad0cc build: update source-map-support to get rid of Node's DeprecationWarning (#27538)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+15.sha-255e672
+ * @license Angular v7.2.0-beta.1+14.sha-84ad0cc
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.1+15.sha-255e672');
+    var VERSION = new i0.Version('7.2.0-beta.1+14.sha-84ad0cc');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
