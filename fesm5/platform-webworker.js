d0889f56b69bc40679415fddedf400b49956de4f
Igor Minar <igor@angular.io>
2018-12-07 06:06:40 +0000

    11d2a8c release: cut the v7.1.2 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.1+57.sha-ad106ab
+ * @license Angular v7.1.2
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.1+57.sha-ad106ab');
+var VERSION = new Version('7.1.2');
 
 /**
  * @license
