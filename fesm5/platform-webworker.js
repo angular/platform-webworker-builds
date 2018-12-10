8a6a5e0755e1f369ef89854c1a14efe1e7716d8e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-10 18:55:38 +0000

    11996cd docs: remove AngularDart from the version picker (#27550)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.2+7.sha-8fc763d
+ * @license Angular v7.1.2+8.sha-11996cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.2+7.sha-8fc763d');
+var VERSION = new Version('7.1.2+8.sha-11996cd');
 
 /**
  * @license
