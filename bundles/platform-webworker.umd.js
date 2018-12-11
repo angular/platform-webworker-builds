bd778d65fe9abc5ebae77484ad32c20edd2a36b1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 20:00:21 +0000

    3cb6dad docs: update npm scripts to support angular 7 (#27334)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+29.sha-ceb14de
+ * @license Angular v7.2.0-beta.1+28.sha-3cb6dad
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.1+29.sha-ceb14de');
+    var VERSION = new i0.Version('7.2.0-beta.1+28.sha-3cb6dad');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
