9155b44597cc2779a03479b274dcceee4cb1d4bf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-11 22:21:12 +0000

    098a787 docs: release notes for the v7.1.3 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.1+32.sha-053b43d
+ * @license Angular v7.2.0-beta.1+33.sha-098a787
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.1+32.sha-053b43d');
+var VERSION = new Version('7.2.0-beta.1+33.sha-098a787');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
