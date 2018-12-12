767e22a9499fe3c73f269c070370849fe1957355
Miško Hevery <misko@hevery.com>
2018-12-12 21:31:40 +0000

    7fabe44 fix(ivy): add support for optional nullable injection tokens (#27552)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+9.sha-37c05bd
+ * @license Angular v7.2.0-beta.2+10.sha-7fabe44
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+9.sha-37c05bd');
+var VERSION = new Version('7.2.0-beta.2+10.sha-7fabe44');
 
 /**
  * @license
