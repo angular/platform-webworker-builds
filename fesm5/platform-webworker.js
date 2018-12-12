5b178a23fbef76a697c0de6133be2f798b6fa8d3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:25:37 +0000

    fc6dc78 fix(docs-infra): fix top menu item clickable area (#27620)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+3.sha-cabf1c7
+ * @license Angular v7.2.0-beta.2+5.sha-fc6dc78
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+3.sha-cabf1c7');
+var VERSION = new Version('7.2.0-beta.2+5.sha-fc6dc78');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
