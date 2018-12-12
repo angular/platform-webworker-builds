3c971a35912dc333403b095fa12c7dd69fd57a3f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-12-12 19:26:44 +0000

    042463f build(bazel): fix postinstall-patches.js script run during yarn postinstall when running yarn_install (#27610)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+5.sha-fc6dc78
+ * @license Angular v7.2.0-beta.2+4.sha-042463f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+5.sha-fc6dc78');
+var VERSION = new Version('7.2.0-beta.2+4.sha-042463f');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
