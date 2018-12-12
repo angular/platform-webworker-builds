9a0434b5a781a9656f296a5975edad2510e444a0
Miško Hevery <misko@hevery.com>
2018-12-12 21:31:51 +0000

    37c05bd fix(ivy): avoid destroy renderer method invocation for child views (#27592)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+6.sha-28ceca0
+ * @license Angular v7.2.0-beta.2+9.sha-37c05bd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+6.sha-28ceca0');
+var VERSION = new Version('7.2.0-beta.2+9.sha-37c05bd');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
