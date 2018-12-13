232a97a57453f551fb550bf419cf9b0de367858b
Miško Hevery <misko@hevery.com>
2018-12-13 19:39:57 +0000

    a808c7d build: ts-api-guardian tsconfig warning with bazel (#27583)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+16.sha-89ace1a
+ * @license Angular v7.1.3+17.sha-a808c7d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+16.sha-89ace1a');
+var VERSION = new Version('7.1.3+17.sha-a808c7d');
 
 /**
  * @license
