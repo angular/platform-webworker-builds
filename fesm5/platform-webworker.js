874d73e191ccf2daf324e3f089261dba567383bb
Miško Hevery <misko@hevery.com>
2018-12-14 23:56:42 +0000

    cd1e206 fix(animations): do not truncate decimals for delay (#24455)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+25.sha-c220328
+ * @license Angular v7.1.3+28.sha-cd1e206
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+25.sha-c220328');
+var VERSION = new Version('7.1.3+28.sha-cd1e206');
 
 /**
  * @license
