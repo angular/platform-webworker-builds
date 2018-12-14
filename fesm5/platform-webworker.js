839cc3087d70f419b9aaff9908c0685941b8dea1
Miško Hevery <misko@hevery.com>
2018-12-14 23:58:25 +0000

    b224057 docs(common): fix documentation for getLocaleCurrencyName (#27087) PR Close #27087

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+27.sha-453589f
+ * @license Angular v7.1.3+29.sha-b224057
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+27.sha-453589f');
+var VERSION = new Version('7.1.3+29.sha-b224057');
 
 /**
  * @license
