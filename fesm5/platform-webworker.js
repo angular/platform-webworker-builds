78bca710217b5aa4dc208238302627ecc8590504
Miško Hevery <misko@hevery.com>
2018-12-14 23:56:52 +0000

    453589f fix(animations): mark actual descendant node as disabled (#26180)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+28.sha-cd1e206
+ * @license Angular v7.1.3+27.sha-453589f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+28.sha-cd1e206');
+var VERSION = new Version('7.1.3+27.sha-453589f');
 
 /**
  * @license
