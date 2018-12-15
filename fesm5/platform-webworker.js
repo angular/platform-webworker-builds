68910a396221998d9327c5972f5f1ba1e3ba8300
Miško Hevery <misko@hevery.com>
2018-12-15 00:05:34 +0000

    023bd31 fix(ivy): ngcc should not emit TypeScript syntax (#27051)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+46.sha-df123e0
+ * @license Angular v7.2.0-beta.2+45.sha-023bd31
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+46.sha-df123e0');
+var VERSION = new Version('7.2.0-beta.2+45.sha-023bd31');
 
 /**
  * @license
