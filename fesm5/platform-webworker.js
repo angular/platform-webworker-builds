66ed7eeab22821b5733236c27405090052833880
Miško Hevery <misko@hevery.com>
2018-12-15 00:03:58 +0000

    b39efdd fix(common): KeyValuePipe should return empty array for empty objects (#27258)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+42.sha-0b78f54
+ * @license Angular v7.2.0-beta.2+44.sha-b39efdd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+42.sha-0b78f54');
+var VERSION = new Version('7.2.0-beta.2+44.sha-b39efdd');
 
 /**
  * @license
