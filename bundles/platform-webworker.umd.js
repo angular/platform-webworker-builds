749531fcb17679d3c8f3f59cdf541caa7f165f02
Miško Hevery <misko@hevery.com>
2018-12-17 18:17:40 +0000

    94f17e9 docs: remove misplaced code in Universal guide (#26865)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+53.sha-ea10a3a
+ * @license Angular v7.2.0-beta.2+62.sha-94f17e9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+53.sha-ea10a3a');
+    var VERSION = new i0.Version('7.2.0-beta.2+62.sha-94f17e9');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
