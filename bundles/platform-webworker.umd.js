8d2e663a610e6a3ec55bf31037f55f29fd6faf93
Miško Hevery <misko@hevery.com>
2018-12-17 18:17:45 +0000

    a726338 docs: Add Smart UI to UI Components section in resources (#27515)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+62.sha-94f17e9
+ * @license Angular v7.2.0-beta.2+55.sha-a726338
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+62.sha-94f17e9');
+    var VERSION = new i0.Version('7.2.0-beta.2+55.sha-a726338');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
