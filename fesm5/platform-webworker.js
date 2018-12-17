cfb3a6fc79ffb76bf69f284d52aa0f944473d0c8
Miško Hevery <misko@hevery.com>
2018-12-17 18:12:24 +0000

    ea10a3a test(ivy): enable all test for @angular/common (#27700)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+52.sha-f8096d4
+ * @license Angular v7.2.0-beta.2+53.sha-ea10a3a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+52.sha-f8096d4');
+var VERSION = new Version('7.2.0-beta.2+53.sha-ea10a3a');
 
 /**
  * @license
