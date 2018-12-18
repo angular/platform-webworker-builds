3be3129c31837838634910ab2465459798db52ba
Miško Hevery <misko@hevery.com>
2018-12-18 01:13:41 +0000

    be08611 docs(forms): update API reference for validator directives (#26926)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.1.3+33.sha-183f278
+ * @license Angular v7.1.3+35.sha-be08611
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.1.3+33.sha-183f278');
+var VERSION = new Version('7.1.3+35.sha-be08611');
 
 /**
  * @license
