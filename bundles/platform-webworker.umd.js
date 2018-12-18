7088d83848e36acf3d8138ccbcf28c259207bdaf
Miško Hevery <misko@hevery.com>
2018-12-18 18:04:09 +0000

    8d0a3ce build: use https link to editorconfig.org in .editorconfig (#27664)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+76.sha-f57916c
+ * @license Angular v7.2.0-beta.2+80.sha-8d0a3ce
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+76.sha-f57916c');
+    var VERSION = new i0.Version('7.2.0-beta.2+80.sha-8d0a3ce');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
