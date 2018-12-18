6f11ceb3f4eb16f31d05c21cf6e0dd927ac9b5eb
Miško Hevery <misko@hevery.com>
2018-12-18 21:45:44 +0000

    b04bc5d test(ivy): re-enable passing upgrade tests (#27736)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+82.sha-1c93afe
+ * @license Angular v7.2.0-beta.2+83.sha-b04bc5d
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+82.sha-1c93afe');
+    var VERSION = new i0.Version('7.2.0-beta.2+83.sha-b04bc5d');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
