602e478ab887d76565dc5fe30c562afaafc59a2d
Miško Hevery <misko@hevery.com>
2018-12-18 18:03:16 +0000

    f57916c fix(bazel): devserver entry_module should have underscore name (#27719)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+79.sha-8fdd702
+ * @license Angular v7.2.0-beta.2+76.sha-f57916c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-beta.2+79.sha-8fdd702');
+    var VERSION = new i0.Version('7.2.0-beta.2+76.sha-f57916c');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
