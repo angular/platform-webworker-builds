46a57c360c3e4f2a7d64e2b3ed332630129bc34d
Miško Hevery <misko@hevery.com>
2018-12-19 00:33:57 +0000

    f48a00f release: cut the v7.2.0-rc.0 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-beta.2+92.sha-24fa0b7
+ * @license Angular v7.2.0-rc.0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-beta.2+92.sha-24fa0b7');
+var VERSION = new Version('7.2.0-rc.0');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
