4ff878f4540d8b62af380f6072b281d4210bb4c7
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 20:40:52 +0000

    a20b2f7 fix(ivy): process creation mode deeply before running update mode (#27744)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0
+ * @license Angular v7.2.0-rc.0+1.sha-a20b2f7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0');
+var VERSION = new Version('7.2.0-rc.0+1.sha-a20b2f7');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
