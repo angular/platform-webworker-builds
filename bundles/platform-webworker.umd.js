f49e561417163f918c1fae923e2cc31a457d54b8
Matias Niemelä <matias@yearofmoo.com>
2018-12-19 21:35:04 +0000

    3ab25ab refactor: fix broken linting rules due to revert

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+4.sha-0604527
+ * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+4.sha-0604527');
+    var VERSION = new i0.Version('7.2.0-rc.0+5.sha-3ab25ab');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+4.sha-0604527
+ * @license Angular v7.2.0-rc.0+5.sha-3ab25ab
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+4.sha-0604527');
+var VERSION = new Version('7.2.0-rc.0+5.sha-3ab25ab');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
