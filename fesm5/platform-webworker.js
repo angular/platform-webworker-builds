9241545840be23a07dcc42f12f27540c775128f3
Matias Niemelä <matias@yearofmoo.com>
2018-12-20 17:28:12 +0000

    a0585c9 fix(ivy): correct content projection with nested templates (#27755)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+10.sha-4b70a4e
+ * @license Angular v7.2.0-rc.0+12.sha-a0585c9
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+10.sha-4b70a4e');
+var VERSION = new Version('7.2.0-rc.0+12.sha-a0585c9');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
