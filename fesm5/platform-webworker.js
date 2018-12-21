60b07f5b9d2fab17e421bb1b56678a1c29e58f6d
Matias Niemelä <matias@yearofmoo.com>
2018-12-21 20:37:58 +0000

    cdd737e test(ivy): mark tests using getDebugContext as obsolete in ivy (#27790)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+20.sha-5da55d6
+ * @license Angular v7.2.0-rc.0+21.sha-cdd737e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+20.sha-5da55d6');
+var VERSION = new Version('7.2.0-rc.0+21.sha-cdd737e');
 
 /**
  * @license
