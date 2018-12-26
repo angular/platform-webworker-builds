259ac98d7a1221736fa9d59274baa8aafdf09af7
Ben Lesh <ben@benlesh.com>
2018-12-26 19:55:45 +0000

    707c682 docs(elements): add "Firefox" for support of custom elements (#27789)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+32.sha-3f64e87
+ * @license Angular v7.2.0-rc.0+33.sha-707c682
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0-rc.0+32.sha-3f64e87');
+    var VERSION = new i0.Version('7.2.0-rc.0+33.sha-707c682');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
