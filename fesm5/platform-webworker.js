878e56b106d64436ff0a41066a4238bcf0295dcc
Ben Lesh <ben@benlesh.com>
2018-12-26 19:58:18 +0000

    92d3942 build: update to latest rules_nodejs (#27764)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+14.sha-4ad7e21
+ * @license Angular v7.2.0-rc.0+15.sha-92d3942
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+14.sha-4ad7e21');
+var VERSION = new Version('7.2.0-rc.0+15.sha-92d3942');
 
 /**
  * @license
