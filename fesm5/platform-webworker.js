f6dd16f7964fda11cc0cf91a18cb6e13bffe0fb5
Ben Lesh <ben@benlesh.com>
2019-01-03 18:09:42 +0000

    679235a docs: improve description of bundle budgets (#27833)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+21.sha-fdd8b04
+ * @license Angular v7.2.0-rc.0+19.sha-679235a
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+21.sha-fdd8b04');
+var VERSION = new Version('7.2.0-rc.0+19.sha-679235a');
 
 /**
  * @license
