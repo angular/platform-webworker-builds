7845e32b6f141b10ab2fb018f14d465fd8c11dd4
Ben Lesh <ben@benlesh.com>
2019-01-03 18:22:39 +0000

    880b4aa fix(docs-infra): upgrade to latest dgeni-packages to fix linking problem (#27864)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+47.sha-5fee444
+ * @license Angular v7.2.0-rc.0+48.sha-880b4aa
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+47.sha-5fee444');
+var VERSION = new Version('7.2.0-rc.0+48.sha-880b4aa');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
