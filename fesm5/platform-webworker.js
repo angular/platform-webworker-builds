a11049473c48403eede1f3844ccdea0c4377ada8
Ben Lesh <ben@benlesh.com>
2019-01-03 18:21:22 +0000

    5fee444 test(ivy): update test failure root cause (#27855)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+45.sha-a979001
+ * @license Angular v7.2.0-rc.0+47.sha-5fee444
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+45.sha-a979001');
+var VERSION = new Version('7.2.0-rc.0+47.sha-5fee444');
 
 /**
  * @license
