27c013ee20635f7199db9858526307b53cbff769
Kara Erickson <karakara@google.com>
2019-01-04 23:24:19 +0000

    929334b fix(ivy): should not throw when getting VCRef.parentInjector on the root view (#27909)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+63.sha-fb7816f
+ * @license Angular v7.2.0-rc.0+64.sha-929334b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+63.sha-fb7816f');
+var VERSION = new Version('7.2.0-rc.0+64.sha-929334b');
 
 /**
  * @license
