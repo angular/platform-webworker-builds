e21fd2dd63f596b18c0a8deaafff58baf33c7298
Kara Erickson <karakara@google.com>
2019-01-04 20:52:28 +0000

    3f2ebbd fix(ivy): support bindings in ICUs in nested contexts (#27914)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+57.sha-7eb2c41
+ * @license Angular v7.2.0-rc.0+56.sha-3f2ebbd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+57.sha-7eb2c41');
+var VERSION = new Version('7.2.0-rc.0+56.sha-3f2ebbd');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
