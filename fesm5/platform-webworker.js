489a777b22bcd46a4e243c65f593902288886313
Kara Erickson <karakara@google.com>
2019-01-04 20:50:18 +0000

    38b4d15 fix(ivy): implement 'TestBed.overrideTemplateUsingTestingModule' function (#27717)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+53.sha-8a05199
+ * @license Angular v7.2.0-rc.0+55.sha-38b4d15
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+53.sha-8a05199');
+var VERSION = new Version('7.2.0-rc.0+55.sha-38b4d15');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
