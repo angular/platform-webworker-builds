636c188962f6e63e1d1151d0a7d2280491e8459c
Kara Erickson <karakara@google.com>
2019-01-04 22:42:33 +0000

    0bd9deb fix(ivy): pipes should overwrite pipes with later entry in the pipes array (#27910)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+28.sha-eea2b0f
+ * @license Angular v7.2.0-rc.0+61.sha-0bd9deb
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+28.sha-eea2b0f');
+var VERSION = new Version('7.2.0-rc.0+61.sha-0bd9deb');
 
 /**
  * @license
