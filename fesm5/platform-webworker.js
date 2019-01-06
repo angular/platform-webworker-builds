a35ca9fca51dfc7490983fa13d73fa902ad49a26
Filipe Silva <filipematossilva@gmail.com>
2019-01-06 11:08:53 +0000

    a78a590 ci: add linux buildkite tests

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-4890b1d
+ * @license Angular v7.2.0-rc.0+54.sha-a78a590
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+54.sha-4890b1d');
+var VERSION = new Version('7.2.0-rc.0+54.sha-a78a590');
 
 /**
  * @license
