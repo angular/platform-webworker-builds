613d02c1d096aa3ebdfbe28f9399b64afcbec29e
Filipe Silva <filipematossilva@gmail.com>
2019-01-06 15:23:33 +0000

    8d5ab90 ci: add a change

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+54.sha-1dd11c9
+ * @license Angular v7.2.0-rc.0+55.sha-8d5ab90
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+54.sha-1dd11c9');
+var VERSION = new Version('7.2.0-rc.0+55.sha-8d5ab90');
 
 /**
  * @license
