7e251c8cd9be76d6910745892b88b276056a9af8
Kara Erickson <karakara@google.com>
2019-01-07 23:20:42 +0000

    ac5f5ed test(bazel): re-enable ng_package golden testing on ci (#27829)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+16.sha-9de9c8a
+ * @license Angular v7.2.0+14.sha-ac5f5ed
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+16.sha-9de9c8a');
+    var VERSION = new i0.Version('7.2.0+14.sha-ac5f5ed');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
