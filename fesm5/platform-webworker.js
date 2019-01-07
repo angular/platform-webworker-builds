7a0196b579b4d11d9e931a5db46a6414e42eb2bd
Kara Erickson <karakara@google.com>
2019-01-07 23:19:18 +0000

    4613864 test(ivy): add i18n integration tests via TestBed (#27852)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+10.sha-a75c734
+ * @license Angular v7.2.0+15.sha-4613864
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+10.sha-a75c734');
+var VERSION = new Version('7.2.0+15.sha-4613864');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
