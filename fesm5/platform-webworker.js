f062ebfd4965f4bd938ad4aaafa67822f819175b
Kara Erickson <karakara@google.com>
2019-01-08 18:47:26 +0000

    917c09c ci: update recommended buildkite VM (#27728)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+32.sha-f547110
+ * @license Angular v7.2.0+33.sha-917c09c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+32.sha-f547110');
+var VERSION = new Version('7.2.0+33.sha-917c09c');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
