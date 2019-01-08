1c5742530541a4e8fe015b29253606ba95c6318d
Kara Erickson <karakara@google.com>
2019-01-08 04:41:15 +0000

    f547110 test(ivy): root causes update for 'platform-browser-dynamic' and i18n tests (#27972)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+17.sha-c1dacdd
+ * @license Angular v7.2.0+32.sha-f547110
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+17.sha-c1dacdd');
+var VERSION = new Version('7.2.0+32.sha-f547110');
 
 /**
  * @license
