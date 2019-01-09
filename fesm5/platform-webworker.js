d6231d511d8bd187e1a5e1d9f30ce94abcf750b0
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:52:31 +0000

    e1e4887 refactor(ivy): merge canInsertNativeNode and getParentNative (#28011)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+73.sha-91a8a4f
+ * @license Angular v7.2.0+78.sha-e1e4887
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+73.sha-91a8a4f');
+var VERSION = new Version('7.2.0+78.sha-e1e4887');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
