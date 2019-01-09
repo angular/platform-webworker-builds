6582a0b3487ec3b5e1fe818a83262dce109a0008
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:04:04 +0000

    91a8a4f docs(forms): remove duplicated link (#27884) PR Close #27884

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+72.sha-582395b
+ * @license Angular v7.2.0+73.sha-91a8a4f
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+72.sha-582395b');
+    var VERSION = new i0.Version('7.2.0+73.sha-91a8a4f');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
