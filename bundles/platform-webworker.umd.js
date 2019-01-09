a99d4d95f38d7691365a07d78dbc5a4c44855035
Andrew Kushnir <akushnir@google.com>
2019-01-09 21:03:48 +0000

    582395b docs: fix typo PageNotFoundCompponent (#27799) closes: #27795

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+74.sha-9b2b9b3
+ * @license Angular v7.2.0+72.sha-582395b
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+74.sha-9b2b9b3');
+    var VERSION = new i0.Version('7.2.0+72.sha-582395b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
