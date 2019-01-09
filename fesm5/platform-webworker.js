8ed6f54f7e8a0657289b66a1af8c31c3f5659902
Kara Erickson <karakara@google.com>
2019-01-09 01:06:41 +0000

    1c39ad3 feat(ivy): reference external classes by their exported name (#27743)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+47.sha-b621a69
+ * @license Angular v7.2.0+54.sha-1c39ad3
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+47.sha-b621a69');
+var VERSION = new Version('7.2.0+54.sha-1c39ad3');
 
 /**
  * @license
