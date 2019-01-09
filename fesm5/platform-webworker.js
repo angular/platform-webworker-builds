2fae711efe2bff43394a7b32adff66412757a08c
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:55:03 +0000

    d0c3e25 docs: remove deprecated `fromPromise` from RxJS guide (#28015)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+78.sha-e1e4887
+ * @license Angular v7.2.0+79.sha-d0c3e25
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+78.sha-e1e4887');
+var VERSION = new Version('7.2.0+79.sha-d0c3e25');
 
 /**
  * @license
