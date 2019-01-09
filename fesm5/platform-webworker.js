55db6cf596b4ab90312377538109781bcac4caf2
Kara Erickson <karakara@google.com>
2019-01-09 01:44:49 +0000

    c3aa24c fix(ivy): sanitization for Host Bindings (#27939)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+55.sha-1de4031
+ * @license Angular v7.2.0+56.sha-c3aa24c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+55.sha-1de4031');
+var VERSION = new Version('7.2.0+56.sha-c3aa24c');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
