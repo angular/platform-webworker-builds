ba259cbf32202333bb780709bc966c1368acbdfd
Andrew Kushnir <akushnir@google.com>
2019-01-09 19:30:10 +0000

    d6cfe2e build: use cross platform workspace_status_command (#27431)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+56.sha-c3aa24c
+ * @license Angular v7.2.0+67.sha-d6cfe2e
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+56.sha-c3aa24c');
+    var VERSION = new i0.Version('7.2.0+67.sha-d6cfe2e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
