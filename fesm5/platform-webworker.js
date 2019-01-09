bf637129505e169f24d07095ab4b1548c8490aca
Andrew Kushnir <akushnir@google.com>
2019-01-09 23:55:30 +0000

    11325ba test(ivy): mark jit_summaries_integration_spec as obsolete in Ivy (#28027)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+79.sha-d0c3e25
+ * @license Angular v7.2.0+80.sha-11325ba
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+79.sha-d0c3e25');
+var VERSION = new Version('7.2.0+80.sha-11325ba');
 
 /**
  * @license
