97bb179de15a6a4d575e7604c71940f93bfec7c7
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:34:40 +0000

    3bafc00 test(ivy): update root cause for @angular/core (#28046)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+94.sha-ee60c76
+ * @license Angular v7.2.0+97.sha-3bafc00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+94.sha-ee60c76');
+    var VERSION = new i0.Version('7.2.0+97.sha-3bafc00');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+94.sha-ee60c76
+ * @license Angular v7.2.0+97.sha-3bafc00
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+94.sha-ee60c76');
+var VERSION = new Version('7.2.0+97.sha-3bafc00');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
