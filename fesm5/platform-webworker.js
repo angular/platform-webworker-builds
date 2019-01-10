7c70d6d5cbb7c10d1103d0b56a3f1d0e9e09470f
Andrew Kushnir <akushnir@google.com>
2019-01-10 01:59:32 +0000

    69ca7cd fix(bazel): Add @bazel/bazel to dev deps (#28032)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0-rc.0+74.sha-e5f5ad3
+ * @license Angular v7.2.0-rc.0+75.sha-69ca7cd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0-rc.0+74.sha-e5f5ad3');
+var VERSION = new Version('7.2.0-rc.0+75.sha-69ca7cd');
 
 /**
  * @license
