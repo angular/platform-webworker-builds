304f6555d79b98c23349711f092bbe65e2ee48da
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:34:48 +0000

    dffcb9c ci: onlyInIvy tests should not be triggered for FIND_PASSING_TESTS mode (#28036)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+97.sha-3bafc00
+ * @license Angular v7.2.0+95.sha-dffcb9c
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+97.sha-3bafc00');
+    var VERSION = new i0.Version('7.2.0+95.sha-dffcb9c');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
