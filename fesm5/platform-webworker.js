137a6dd399696b4e8fd03b4b16f73118bad754c3
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:33:54 +0000

    ee60c76 test(ivy): turn on passing i18n test in CI (#28035)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+90.sha-7374dfd
+ * @license Angular v7.2.0+94.sha-ee60c76
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+90.sha-7374dfd');
+var VERSION = new Version('7.2.0+94.sha-ee60c76');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
