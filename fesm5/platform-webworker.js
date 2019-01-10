9ae5e654fe77be76714cf75d69f4bc3f33a357bc
Andrew Kushnir <akushnir@google.com>
2019-01-10 19:31:59 +0000

    7374dfd docs: document why not using `compare-url` orb (#28010)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+89.sha-c5ab3e8
+ * @license Angular v7.2.0+90.sha-7374dfd
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+89.sha-c5ab3e8');
+var VERSION = new Version('7.2.0+90.sha-7374dfd');
 
 /**
  * @license
