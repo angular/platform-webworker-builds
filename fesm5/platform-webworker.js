5f13dc54755ae3b4b16414ea7bc3e9040a66532f
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:29:56 +0000

    d505468 docs: re-write interpolation section and add example (#25170)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+112.sha-f854eb7
+ * @license Angular v7.2.0+113.sha-d505468
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+112.sha-f854eb7');
+var VERSION = new Version('7.2.0+113.sha-d505468');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
