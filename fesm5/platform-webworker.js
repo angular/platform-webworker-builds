2046e4042ee68d97d9de6abd28c9280c696aa732
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:45:56 +0000

    3ea9f09 docs(router): remove obsolete sections in README.md (#27880)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,6 +1,6 @@
 /**
- * @license Angular v7.2.0+114.sha-e31afb7
- * (c) 2010-2018 Google, Inc. https://angular.io/
+ * @license Angular v7.2.0+117.sha-3ea9f09
+ * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
 
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+114.sha-e31afb7');
+var VERSION = new Version('7.2.0+117.sha-3ea9f09');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
