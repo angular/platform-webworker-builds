05e2bd8ee6fcff760c2d5cad038492e513ddfc68
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:34:46 +0000

    acb2cac docs: fix typo (#27865) PR Close #27865

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+107.sha-96f4969
+ * @license Angular v7.2.0+108.sha-acb2cac
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+107.sha-96f4969');
+var VERSION = new Version('7.2.0+108.sha-acb2cac');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
