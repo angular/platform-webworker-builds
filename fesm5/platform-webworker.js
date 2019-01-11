f964ed210778855fccffdeceab23d0b0c56c9d0b
Andrew Kushnir <akushnir@google.com>
2019-01-11 18:40:15 +0000

    f854eb7 test(ivy): mark tests where checkNoChanges results are different in the view engine and ivy (#28073)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+111.sha-b0f3c20
+ * @license Angular v7.2.0+112.sha-f854eb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+111.sha-b0f3c20');
+var VERSION = new Version('7.2.0+112.sha-f854eb7');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
