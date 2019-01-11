6e1fd2830a27710ffbe34e1f4b812930d62bb4b4
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:37:55 +0000

    e31afb7 fix(ivy): ngcc - identify all ESM5 decorated classes (#27848)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+113.sha-d505468
+ * @license Angular v7.2.0+114.sha-e31afb7
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+113.sha-d505468');
+var VERSION = new Version('7.2.0+114.sha-e31afb7');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
