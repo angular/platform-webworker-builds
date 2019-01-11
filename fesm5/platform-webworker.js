15b8f4704682f14fe98030881dafeef71a9f9233
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:47:34 +0000

    091a8a6 fix(ivy): add missing @nocollapse annotation to __NG_ELEMENT_ID__ fields (#28050)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+122.sha-8c3f98f
+ * @license Angular v7.2.0+128.sha-091a8a6
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+122.sha-8c3f98f');
+var VERSION = new Version('7.2.0+128.sha-091a8a6');
 
 /**
  * @license
