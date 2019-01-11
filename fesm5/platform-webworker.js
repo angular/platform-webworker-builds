73396ff40c099765a9b998da6c0cfb794162da5e
Andrew Kushnir <akushnir@google.com>
2019-01-11 01:08:33 +0000

    9277142 fix(ivy): support multiple exportAs (#27996)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+104.sha-b78351c
+ * @license Angular v7.2.0+105.sha-9277142
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+104.sha-b78351c');
+var VERSION = new Version('7.2.0+105.sha-9277142');
 
 /**
  * @license
