f40cf31c306e957f7c159ea2cd1cdf39281f56af
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:48:57 +0000

    df292c2 fix(ivy): TestBed should not clobber compilation of global-scope modules (#28033)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+128.sha-091a8a6
+ * @license Angular v7.2.0+126.sha-df292c2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+128.sha-091a8a6');
+var VERSION = new Version('7.2.0+126.sha-df292c2');
 
 /**
  * @license
