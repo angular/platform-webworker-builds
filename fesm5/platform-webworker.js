8eb7be326183a5e5988596c1454e079c3b297d6d
Andrew Kushnir <akushnir@google.com>
2019-01-11 19:47:43 +0000

    8c3f98f docs(service-worker): fix example of manually checking for updates (#28020)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+117.sha-3ea9f09
+ * @license Angular v7.2.0+122.sha-8c3f98f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+117.sha-3ea9f09');
+var VERSION = new Version('7.2.0+122.sha-8c3f98f');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
