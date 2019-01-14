2af2967aa9077ffce9c7aa714f5b701f330474b0
Andrew Kushnir <akushnir@google.com>
2019-01-14 18:31:11 +0000

    78bc21c docs: fix scripts section and some minor issues in universal documentation (#26444)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+147.sha-48a03fc
+ * @license Angular v7.2.0+148.sha-78bc21c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+147.sha-48a03fc');
+var VERSION = new Version('7.2.0+148.sha-78bc21c');
 
 /**
  * @license
