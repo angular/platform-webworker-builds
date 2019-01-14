1c6adce0c6695a7392da503ebecf64787254d453
Andrew Kushnir <akushnir@google.com>
2019-01-14 19:26:50 +0000

    5e6e96d docs: fix GitHub pages deployment command (#26896)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+150.sha-638375b
+ * @license Angular v7.2.0+152.sha-5e6e96d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+150.sha-638375b');
+var VERSION = new Version('7.2.0+152.sha-5e6e96d');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
