d833b1afbdef4965acdce4e1acb8be7c184575ae
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:37:35 +0000

    bc02e31 fix(ivy): normalize summary and factory shim files paths (#28006)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+186.sha-6072ca8
+ * @license Angular v7.2.0+187.sha-bc02e31
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+186.sha-6072ca8');
+var VERSION = new Version('7.2.0+187.sha-bc02e31');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
