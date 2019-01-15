693bcc7cabaabe2cb59cb165b6d764f0b70e0a02
Andrew Kushnir <akushnir@google.com>
2019-01-15 22:39:12 +0000

    f983e99 test(ivy): enable passing animation test (#28156)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+190.sha-29bff0f
+ * @license Angular v7.2.0+192.sha-f983e99
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('7.2.0+190.sha-29bff0f');
+    var VERSION = new i0.Version('7.2.0+192.sha-f983e99');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
