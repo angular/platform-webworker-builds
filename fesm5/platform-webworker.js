3e67e8454963baf52091893539daa1552bba5b29
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:12:01 +0000

    850b867 docs: add api doc for switch directives (#27378)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+183.sha-a241200
+ * @license Angular v7.2.0+184.sha-850b867
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+183.sha-a241200');
+var VERSION = new Version('7.2.0+184.sha-850b867');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
