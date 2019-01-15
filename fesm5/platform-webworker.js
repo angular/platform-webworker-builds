3c4d2f119b326bcc6f434d138748042d64cc8680
Andrew Kushnir <akushnir@google.com>
2019-01-15 18:05:28 +0000

    6930451 refactor(ivy): remove def.attributes in favor of the `elementHostAttrs` instruction (#28089)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+178.sha-e62eeed
+ * @license Angular v7.2.0+179.sha-6930451
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+178.sha-e62eeed');
+var VERSION = new Version('7.2.0+179.sha-6930451');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
