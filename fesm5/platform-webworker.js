1860713ffecde16cf7acd286f7f3f7ff5ee892a3
Andrew Kushnir <akushnir@google.com>
2019-01-15 19:17:48 +0000

    6072ca8 fix(ivy): deps are actually supported (#28076)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+185.sha-9460218
+ * @license Angular v7.2.0+186.sha-6072ca8
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+185.sha-9460218');
+var VERSION = new Version('7.2.0+186.sha-6072ca8');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
