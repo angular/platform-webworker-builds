f461b842c6b167b20f839e814614818a26372cc1
Andrew Kushnir <akushnir@google.com>
2019-01-16 18:13:39 +0000

    9a81f0d fix(ivy): update i18n/i18nStart and i18nAttributes instruction order (#28163)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+193.sha-3b7a571
+ * @license Angular v7.2.0+196.sha-9a81f0d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+193.sha-3b7a571');
+var VERSION = new Version('7.2.0+196.sha-9a81f0d');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
