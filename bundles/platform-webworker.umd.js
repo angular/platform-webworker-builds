b51173d4abd4a3f678ea9bc53fc5de3fc5efe28b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:02:38 +0000

    80967ce build(bazel): Use @angular/cli from root package.json (#28139)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+6.sha-50cf2ac
+ * @license Angular v8.0.0-beta.0+5.sha-80967ce
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+6.sha-50cf2ac');
+    var VERSION = new i0.Version('8.0.0-beta.0+5.sha-80967ce');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
