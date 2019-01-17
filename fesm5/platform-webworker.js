adb8bbb1c3db7bd443bf9fe633cab264a3c439be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 02:37:07 +0000

    8a08ff1 docs: fix comments in services example (#26194)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+5.sha-80967ce
+ * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+5.sha-80967ce');
+var VERSION = new Version('8.0.0-beta.0+7.sha-8a08ff1');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
