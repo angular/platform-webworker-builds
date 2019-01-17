602afb47fd9c6bff3fb102195cc4b3c40253cb38
Andrew Kushnir <akushnir@google.com>
2019-01-17 00:43:56 +0000

    a570fdf fix(bazel): Fix integration test after v8 bump (#28194)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.0+199.sha-1e5e62e
+ * @license Angular v8.0.0-beta.0+1.sha-a570fdf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.0+199.sha-1e5e62e');
+var VERSION = new Version('8.0.0-beta.0+1.sha-a570fdf');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
