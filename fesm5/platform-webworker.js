4ceee730efa6f3fc75a33557f83a2b384e1d401d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 22:07:36 +0000

    d8c75f1 docs: fix comments in services example (#26194)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+4.sha-6d40ef2
+ * @license Angular v7.2.1+5.sha-d8c75f1
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+4.sha-6d40ef2');
+var VERSION = new Version('7.2.1+5.sha-d8c75f1');
 
 /**
  * @license
