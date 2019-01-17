14612876af8a77c29d569f1b7edb85c301b1cd80
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-17 18:18:42 +0000

    1f7d3b9 fix(ivy): TestBed should use annotation for the last match rather than the first (#28195)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+7.sha-8a08ff1
+ * @license Angular v8.0.0-beta.0+8.sha-1f7d3b9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+7.sha-8a08ff1');
+var VERSION = new Version('8.0.0-beta.0+8.sha-1f7d3b9');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
