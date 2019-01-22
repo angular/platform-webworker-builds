c3a12b9a59aca64ecd09d106d2c2460f02387c04
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:53:28 +0000

    cf71668 build: hide vscode settings e.g. debug launch config (#28299)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.1+15.sha-54565ed
+ * @license Angular v7.2.1+16.sha-cf71668
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.1+15.sha-54565ed');
+var VERSION = new Version('7.2.1+16.sha-cf71668');
 
 /**
  * @license
