ad7c44eca21958964ed437b931bb6aeec246a7a1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 19:36:55 +0000

    1df8be5 docs: update link to post on redirects in router guide (#27256)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+36.sha-6324ad4
+ * @license Angular v8.0.0-beta.0+37.sha-1df8be5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+36.sha-6324ad4');
+var VERSION = new Version('8.0.0-beta.0+37.sha-1df8be5');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
