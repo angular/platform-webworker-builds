5dbd99227f03669335ce07c0a1f029f0f9515737
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-22 20:38:25 +0000

    9882434 docs: add method doc (#27377)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+54.sha-37f8263
+ * @license Angular v8.0.0-beta.0+59.sha-9882434
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+54.sha-37f8263');
+var VERSION = new Version('8.0.0-beta.0+59.sha-9882434');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
