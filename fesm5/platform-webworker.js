1adc0f079011fe286aa884902d581c832c9edfb6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 19:21:05 +0000

    cf8770f fix(ivy): don't create TNodes for native projectable nodes (#28275)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1
+ * @license Angular v8.0.0-beta.1+3.sha-cf8770f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1');
+var VERSION = new Version('8.0.0-beta.1+3.sha-cf8770f');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
