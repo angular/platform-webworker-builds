ace2e2c79f20b0eee7cdb30da1c615cace799175
Jason Aden <jasonaden@google.com>
2019-01-23 23:17:07 +0000

    32c61f4 fix(ivy): ngUpgrade should distinguish element and module injectors (#28313)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+14.sha-22a43cf
+ * @license Angular v8.0.0-beta.1+15.sha-32c61f4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+14.sha-22a43cf');
+var VERSION = new Version('8.0.0-beta.1+15.sha-32c61f4');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
