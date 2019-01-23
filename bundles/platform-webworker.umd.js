f6d08fb54211c8ae5553ecf67c626af53fbf1bd8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:08:40 +0000

    fe9b3ea docs: release notes for the v7.2.2 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+65.sha-50df897
+ * @license Angular v8.0.0-beta.0+66.sha-fe9b3ea
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.0+65.sha-50df897');
+    var VERSION = new i0.Version('8.0.0-beta.0+66.sha-fe9b3ea');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
