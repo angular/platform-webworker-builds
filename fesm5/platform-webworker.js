2c3074d95afb7c4ab500bcac86e356c07cba876a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-01-23 00:48:54 +0000

    8f1198f release: cut the v8.0.0-beta.1 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.0+66.sha-fe9b3ea
+ * @license Angular v8.0.0-beta.1
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.0+66.sha-fe9b3ea');
+var VERSION = new Version('8.0.0-beta.1');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
