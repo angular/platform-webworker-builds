3b330adfb8c91b17b50ad59f6ea71e3aa2edfa41
Jason Aden <jasonaden@google.com>
2019-01-24 23:59:09 +0000

    35f7ff0 docs: add api doc for ngif (#27376) (#28344)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+12.sha-bc10328
+ * @license Angular v7.2.2+15.sha-35f7ff0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+12.sha-bc10328');
+var VERSION = new Version('7.2.2+15.sha-35f7ff0');
 
 /**
  * @license
