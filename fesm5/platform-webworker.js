f9b76406c706881d250d556738578d2b1bb8ddb7
Jason Aden <jasonaden@google.com>
2019-01-25 21:28:08 +0000

    fdc2b0b fix(ivy): queries should register matches from top to bottom (#28319)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+43.sha-3d5a919
+ * @license Angular v8.0.0-beta.1+35.sha-fdc2b0b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.1+43.sha-3d5a919');
+var VERSION = new Version('8.0.0-beta.1+35.sha-fdc2b0b');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
