17828df50d3f616399d9d237f56920ad42fa5cf4
Jason Aden <jasonaden@google.com>
2019-01-28 20:44:09 +0000

    fd8dbd5 test(ivy): @Injectable is required on type providers (#28331)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.1+35.sha-fdc2b0b
+ * @license Angular v8.0.0-beta.1+56.sha-fd8dbd5
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.1+35.sha-fdc2b0b');
+    var VERSION = new i0.Version('8.0.0-beta.1+56.sha-fd8dbd5');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
