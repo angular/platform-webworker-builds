ef4fb08773961376d9df1b16fbb62d4fa82643bb
Jason Aden <jasonaden@google.com>
2019-01-30 01:12:10 +0000

    22bc6ef docs: fix typo for Browserslist in build guide (#28328)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.2+45.sha-2c63108
+ * @license Angular v7.2.2+49.sha-22bc6ef
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.2+45.sha-2c63108');
+var VERSION = new Version('7.2.2+49.sha-22bc6ef');
 
 /**
  * @license
