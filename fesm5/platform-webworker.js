978a3755da5a6befae14935e6df3a2e2501c8f34
Matias Niemelä <matias@yearofmoo.com>
2019-02-04 22:07:46 +0000

    fc88a79 fix(ivy): errors not being logged to ErrorHandler (#28447)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.2+23.sha-3d52271
+ * @license Angular v8.0.0-beta.2+26.sha-fc88a79
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.2+23.sha-3d52271');
+var VERSION = new Version('8.0.0-beta.2+26.sha-fc88a79');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
