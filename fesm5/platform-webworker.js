3c8ccd4aadf0b0e86899aded164079d7cf75c805
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:57:23 +0000

    d4add54 test(ivy): root cause analysis for e2e tests in common (#28506)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+9.sha-2ea4f69
+ * @license Angular v8.0.0-beta.3+8.sha-d4add54
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+9.sha-2ea4f69');
+var VERSION = new Version('8.0.0-beta.3+8.sha-d4add54');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
