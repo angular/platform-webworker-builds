7a6f314f6b9e77e73d53d296c107e23dce712ced
Matias Niemelä <matias@yearofmoo.com>
2019-02-06 04:49:26 +0000

    5a2c3ff fix(ivy): proper component resolution in case of inheritance (#28439)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+1.sha-ed0cf7e
+ * @license Angular v8.0.0-beta.3+2.sha-5a2c3ff
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+1.sha-ed0cf7e');
+    var VERSION = new i0.Version('8.0.0-beta.3+2.sha-5a2c3ff');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
