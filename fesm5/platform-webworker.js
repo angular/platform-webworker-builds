09a9c2a4d112b5e0fc4e7ddf171aec6736cca7d2
Misko Hevery <misko@hevery.com>
2019-02-07 05:40:35 +0000

    da81c9c build(docs-infra): use pinned dependencies when possible in `ng-packages-installer` (#28510)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+23.sha-47f5b5f
+ * @license Angular v7.2.4+28.sha-da81c9c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+23.sha-47f5b5f');
+var VERSION = new Version('7.2.4+28.sha-da81c9c');
 
 /**
  * @license
