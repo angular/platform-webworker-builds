da6295556b6952b9b312c0ef0bd53a94d3667692
Miško Hevery <misko@hevery.com>
2019-02-08 18:03:46 +0000

    6e4ef91 docs: update merge label meaning (#28574) PR Close #28574

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+60.sha-1ff3699
+ * @license Angular v8.0.0-beta.3+58.sha-6e4ef91
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+60.sha-1ff3699');
+var VERSION = new Version('8.0.0-beta.3+58.sha-6e4ef91');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
