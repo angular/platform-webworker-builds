f118b85e269517cd17d14b42add4bdd2269210ea
Miško Hevery <misko@hevery.com>
2019-02-08 02:12:45 +0000

    94f042b refactor(ivy): remove unneeded detach property (#28595) PR Close #28595

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+50.sha-4b7264f
+ * @license Angular v8.0.0-beta.3+53.sha-94f042b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+50.sha-4b7264f');
+    var VERSION = new i0.Version('8.0.0-beta.3+53.sha-94f042b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
