4fc4c6e3a0e56dcc283ef2d95dae8d6dcfe91a87
Miško Hevery <misko@hevery.com>
2019-02-08 22:09:38 +0000

    7115e7c build: remove outdated polymer tree benchmark (#28568)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+62.sha-44de68c
+ * @license Angular v8.0.0-beta.3+68.sha-7115e7c
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+62.sha-44de68c');
+    var VERSION = new i0.Version('8.0.0-beta.3+68.sha-7115e7c');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
