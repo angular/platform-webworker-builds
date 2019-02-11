81a8959079f3393af7be0a3a35bc4c9fd07a91e7
Miško Hevery <misko@hevery.com>
2019-02-11 17:30:33 +0000

    aa163be refactor(compiler): allow disabling external symbol factory reexports (#28594)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+44.sha-2f73c55
+ * @license Angular v7.2.4+45.sha-aa163be
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+44.sha-2f73c55');
+var VERSION = new Version('7.2.4+45.sha-aa163be');
 
 /**
  * @license
