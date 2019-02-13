d06712a1bd12e1a9cfe02e4e2d3444f61d86bfae
Miško Hevery <misko@hevery.com>
2019-02-13 18:25:10 +0000

    0c7581d refactor(bazel): use multi_sass_binary rule (#28669)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+129.sha-91b7152
+ * @license Angular v8.0.0-beta.3+131.sha-0c7581d
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.3+129.sha-91b7152');
+    var VERSION = new i0.Version('8.0.0-beta.3+131.sha-0c7581d');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
