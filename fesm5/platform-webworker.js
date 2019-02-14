adec26b706a42ef8471b592a5ec7268c2ca30c23
Misko Hevery <misko@hevery.com>
2019-02-14 03:52:07 +0000

    8cec4b3 build: make api-extractor work in google3 (#28588)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+168.sha-b0afc4c
+ * @license Angular v8.0.0-beta.3+166.sha-8cec4b3
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+168.sha-b0afc4c');
+var VERSION = new Version('8.0.0-beta.3+166.sha-8cec4b3');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
