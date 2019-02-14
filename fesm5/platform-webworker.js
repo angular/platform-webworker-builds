ce3ef662bcec5a10fad10ef1e9f96f7317f6f1a4
Misko Hevery <misko@hevery.com>
2019-02-14 03:34:08 +0000

    d3c6e51 build: update jquery to latest (#28719)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.4+71.sha-ecef0dd
+ * @license Angular v7.2.4+73.sha-d3c6e51
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.4+71.sha-ecef0dd');
+var VERSION = new Version('7.2.4+73.sha-d3c6e51');
 
 /**
  * @license
