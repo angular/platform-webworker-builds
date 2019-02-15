e07c9b5d4d039bcb699da770d66f7b1a5649bca5
Miško Hevery <misko@hevery.com>
2019-02-15 22:53:58 +0000

    6fcf286 release: cut the v8.0.0-beta.4 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.3+182.sha-fcc8c56
+ * @license Angular v8.0.0-beta.4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.3+182.sha-fcc8c56');
+var VERSION = new Version('8.0.0-beta.4');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
