84a9a2e550ea8e21cdea57dee98b3f58a599830b
Misko Hevery <misko@hevery.com>
2019-02-17 05:24:24 +0000

    c1094cf refactor(ivy): remove duplicated getRootView function (#28732)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4
+ * @license Angular v8.0.0-beta.4+2.sha-c1094cf
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4');
+var VERSION = new Version('8.0.0-beta.4+2.sha-c1094cf');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
