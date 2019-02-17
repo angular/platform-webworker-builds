da2049c0085c487f4e78868e4cfeacba0bf8b70d
Misko Hevery <misko@hevery.com>
2019-02-17 05:27:45 +0000

    04a1923 ci: add /packages/* to the @angular/fw-dev-infra CODEOWNERS group (#28745)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+2.sha-c1094cf
+ * @license Angular v8.0.0-beta.4+4.sha-04a1923
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.4+2.sha-c1094cf');
+    var VERSION = new i0.Version('8.0.0-beta.4+4.sha-04a1923');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
