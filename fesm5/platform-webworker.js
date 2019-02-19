eb03b5dbd7044a70b460752e8cd537c376cdfa1d
Igor Minar <igor@angular.io>
2019-02-19 21:19:22 +0000

    3b78f9e build(compiler-cli): upgrade chokidar to latest version (#28797)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+16.sha-f6ca619
+ * @license Angular v7.2.5+17.sha-3b78f9e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.5+16.sha-f6ca619');
+var VERSION = new Version('7.2.5+17.sha-3b78f9e');
 
 /**
  * @license
