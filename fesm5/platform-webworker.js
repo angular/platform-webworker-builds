0dca46f43c40dd6bc28c796bfbb997491eb9a28f
Igor Minar <igor@angular.io>
2019-02-19 21:28:30 +0000

    d8ae899 docs(common): fix a typo in a debounce requests example header (#28829)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+28.sha-19afb79
+ * @license Angular v8.0.0-beta.4+29.sha-d8ae899
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+28.sha-19afb79');
+var VERSION = new Version('8.0.0-beta.4+29.sha-d8ae899');
 
 /**
  * @license
