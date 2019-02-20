8e5072f1a96276e1fa7f387c4bac706921a62bde
Igor Minar <igor@angular.io>
2019-02-20 17:40:02 +0000

    48cabb4 build: use vendored yarn under Bazel (#28839)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.5+28.sha-cfe4732
+ * @license Angular v7.2.5+27.sha-48cabb4
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.5+28.sha-cfe4732');
+var VERSION = new Version('7.2.5+27.sha-48cabb4');
 
 /**
  * @license
