4a7ebefaf20c6e1f8ef849504985f86e22ce6c3e
Igor Minar <igor@angular.io>
2019-02-20 22:12:38 +0000

    cfb2d17 feat(ivy): convert [ngStyle] and [ngClass] to use ivy styling bindings (#28711)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.4+56.sha-a7e1c0c
+ * @license Angular v8.0.0-beta.5+2.sha-cfb2d17
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.4+56.sha-a7e1c0c');
+var VERSION = new Version('8.0.0-beta.5+2.sha-cfb2d17');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
