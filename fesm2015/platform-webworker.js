dc9b9f0d16e229eb94ace0c8a273890e9388ece6
Ben Lesh <ben@benlesh.com>
2019-02-21 21:02:36 +0000

    3f2b51b ci: make yarn globally available and independent of `$BASH_ENV` (#28889)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+19.sha-d8704a3
+ * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1591,7 +1591,7 @@ function spawnWebWorker(uri, instance) {
  * \@publicApi
  * @type {?}
  */
-const VERSION = new Version('8.0.0-beta.5+19.sha-d8704a3');
+const VERSION = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+19.sha-d8704a3
+ * @license Angular v8.0.0-beta.5+20.sha-3f2b51b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+19.sha-d8704a3');
+var VERSION = new Version('8.0.0-beta.5+20.sha-3f2b51b.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
