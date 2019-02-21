bb6c738ff0f1ce595546f52d2e661c2dffefcc45
Igor Minar <igor@angular.io>
2019-02-21 00:53:12 +0000

    623fd3f build: remove legacy-unit-tests-local job (#28703)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+3.sha-05c25cc
+ * @license Angular v8.0.0-beta.5+5.sha-623fd3f
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+3.sha-05c25cc');
+var VERSION = new Version('8.0.0-beta.5+5.sha-623fd3f');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
