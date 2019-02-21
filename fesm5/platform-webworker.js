d8922d8a18ab99292a22cc0e31611dfde0ea5b9f
Igor Minar <igor@angular.io>
2019-02-21 17:55:44 +0000

    e1aaa7e fix(ivy): component destroy hook called twice when configured as provider (#28470)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+15.sha-ebffde7
+ * @license Angular v8.0.0-beta.5+17.sha-e1aaa7e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+15.sha-ebffde7');
+var VERSION = new Version('8.0.0-beta.5+17.sha-e1aaa7e');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
