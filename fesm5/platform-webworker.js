f4fba3a75df0b7cb8d687183b168600f3a656530
Igor Minar <igor@angular.io>
2019-02-21 01:11:09 +0000

    d0b6622 test(ivy): turn on more passing Material tests (#28876)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+6.sha-13b96ac
+ * @license Angular v8.0.0-beta.5+7.sha-d0b6622
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+6.sha-13b96ac');
+var VERSION = new Version('8.0.0-beta.5+7.sha-d0b6622');
 
 /**
  * @license
