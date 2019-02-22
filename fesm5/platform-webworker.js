6100c7beff59ea068f450e1205ba421b0ff13694
Ben Lesh <ben@benlesh.com>
2019-02-22 23:41:06 +0000

    f75acbd fix(compiler): use correct variable in invalid function (#28656)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+59.sha-395fb18.with-local-changes
+ * @license Angular v8.0.0-beta.5+60.sha-f75acbd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+59.sha-395fb18.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+60.sha-f75acbd.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
