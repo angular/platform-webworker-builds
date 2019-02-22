abba8c556289f9ff91eede6c03d2303e40428fde
Ben Lesh <ben@benlesh.com>
2019-02-22 17:16:14 +0000

    cbfc1f2 docs: fix ngSwitch example (#28899)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes
+ * @license Angular v8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+35.sha-c7fe3a9.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+36.sha-cbfc1f2.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
