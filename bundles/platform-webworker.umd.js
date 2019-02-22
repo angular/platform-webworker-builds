b2437c1a1f652954d4ba034757efb0a30583a332
Ben Lesh <ben@benlesh.com>
2019-02-22 20:21:50 +0000

    9dac04f fix(ivy): ngOnChanges hooks should be inherited from grand-superclasses (#28888)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes
+ * @license Angular v8.0.0-beta.5+44.sha-9dac04f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+43.sha-7f3e3a8.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+44.sha-9dac04f.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
