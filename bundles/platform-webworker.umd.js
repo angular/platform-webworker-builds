3d05a0e13eb10826ced5eaaae2fd86b06f83e36d
Ben Lesh <ben@benlesh.com>
2019-02-23 00:44:35 +0000

    10ba91b build: fix saucelabs_test cronjob in CI (#28939)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+60.sha-f75acbd.with-local-changes
+ * @license Angular v8.0.0-beta.5+61.sha-10ba91b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+60.sha-f75acbd.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+61.sha-10ba91b.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
