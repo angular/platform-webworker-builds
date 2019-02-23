bbbbea133a5b67d0aa2e02ab0f897ae71c9f5210
Igor Minar <igor@angular.io>
2019-02-23 01:52:08 +0000

    350802b build: update to latest bazel rules (#28931)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+61.sha-10ba91b.with-local-changes
+ * @license Angular v8.0.0-beta.5+62.sha-350802b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+61.sha-10ba91b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+62.sha-350802b.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
