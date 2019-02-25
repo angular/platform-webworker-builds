07bf0558672ece2d96066a30bd2b6090732f1ede
Ben Lesh <ben@benlesh.com>
2019-02-25 18:53:51 +0000

    65d3dda docs: add Human Coders trainings in resources.json (#28840) PR Close #28840

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes
+ * @license Angular v8.0.0-beta.5+71.sha-65d3dda.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+72.sha-ef1b9e6.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+71.sha-65d3dda.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
