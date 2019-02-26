d22b566d82acb5fbf58f91bfa46fd91b9c818426
Ben Lesh <ben@benlesh.com>
2019-02-26 19:11:54 +0000

    dbd9ecf test(ivy): update sidenav and drawer root causes (#28972)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+80.sha-f01247f.with-local-changes
+ * @license Angular v8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.5+80.sha-f01247f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes');
 
 /**
  * @license
