3d8bc442825b6f17b627fd288f84bdbbd93002a9
Ben Lesh <ben@benlesh.com>
2019-02-26 19:22:53 +0000

    25a2fef fix(ivy): ViewRef.rootNodes not including projected nodes (#28951)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes
+ * @license Angular v8.0.0-beta.5+82.sha-25a2fef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.5+81.sha-dbd9ecf.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.5+82.sha-25a2fef.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
