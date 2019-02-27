5b0e22db38eb44f3bae730df10b393f3b6f89bba
Ben Lesh <ben@benlesh.com>
2019-02-27 18:51:20 +0000

    f16fca4 build(docs-infra): upgrade cli command docs sources to 3b1fe6437 (#29003)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+7.sha-d127d05.with-local-changes
+ * @license Angular v8.0.0-beta.6+9.sha-f16fca4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+7.sha-d127d05.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+9.sha-f16fca4.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
