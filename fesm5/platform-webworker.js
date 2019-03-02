0c97bb2d99d10dbcd56114f4c3e2e9fbf51939cb
Andrew Kushnir <akushnir@google.com>
2019-03-02 00:04:00 +0000

    a06824a fix(ivy): correctly evaluate enum references in template expressions (#29062)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+48.sha-ba602db.with-local-changes
+ * @license Angular v8.0.0-beta.6+49.sha-a06824a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+48.sha-ba602db.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+49.sha-a06824a.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
