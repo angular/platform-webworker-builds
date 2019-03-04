7401853b1fb639f89fe22ede62e1f721a7f2179d
Andrew Kushnir <akushnir@google.com>
2019-03-04 22:59:06 +0000

    04cf4ef test(ivy): remove ngComponentOutlet example with a lazy-loaded NgModule (#29094)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+66.sha-aa57bdb.with-local-changes
+ * @license Angular v8.0.0-beta.6+67.sha-04cf4ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1023,7 +1023,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.6+66.sha-aa57bdb.with-local-changes');
+var VERSION = new Version('8.0.0-beta.6+67.sha-04cf4ef.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
