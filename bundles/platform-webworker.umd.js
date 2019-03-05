2884b8d6bea47369cdfd891a3d48d999813cc433
Andrew Kushnir <akushnir@google.com>
2019-03-05 18:11:09 +0000

    7c57293 refactor(language-service): clean up imports language-service and misspellings in public API (#29097)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.6+74.sha-3063547.with-local-changes
+ * @license Angular v8.0.0-beta.6+75.sha-7c57293.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1076,7 +1076,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.6+74.sha-3063547.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.6+75.sha-7c57293.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
