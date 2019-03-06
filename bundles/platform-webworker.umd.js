eb6fd01e66d542be437b2b85f641e3a4d984b106
George Kalpakas <kalpakas.g@gmail.com>
2019-03-06 00:43:35 +0000

    d8b63d0 build(compiler-cli): upgrade chokidar to latest version

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v5.2.11-a065dc2
+ * @license Angular v5.2.11-d8b63d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -36,7 +36,7 @@ function __extends(d, b) {
 }
 
 /**
- * @license Angular v5.2.11-a065dc2
+ * @license Angular v5.2.11-d8b63d0
  * (c) 2010-2018 Google, Inc. https://angular.io/
  * License: MIT
  */
@@ -1820,7 +1820,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * \@stable
  */
-var VERSION = new _angular_core.Version('5.2.11-a065dc2');
+var VERSION = new _angular_core.Version('5.2.11-d8b63d0');
 
 /**
  * @fileoverview added by tsickle
