3d6fa63ba4417ee59e2fc824d24357e23debd6cd
Kara Erickson <karakara@google.com>
2019-03-11 16:42:53 +0000

    49dccf4 fix(ivy): process separate declarations and exports for summaries (#29193)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.7+41.sha-a68b1a1.with-local-changes
+ * @license Angular v8.0.0-beta.7+53.sha-49dccf4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.7+41.sha-a68b1a1.with-local-changes');
+var VERSION = new Version('8.0.0-beta.7+53.sha-49dccf4.with-local-changes');
 
 /**
  * @license
