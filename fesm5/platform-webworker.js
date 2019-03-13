99c355e6bdd7286c4dee7e793f500fff71f78c88
Kara Erickson <karakara@google.com>
2019-03-13 01:21:48 +0000

    1625d86 docs: de-emphasize monorepo (#28377)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+3.sha-29fae6d.with-local-changes
+ * @license Angular v8.0.0-beta.8+5.sha-1625d86.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+3.sha-29fae6d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+5.sha-1625d86.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
