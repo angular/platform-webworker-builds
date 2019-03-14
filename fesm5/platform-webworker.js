229abea8f4821b3f342c3b0171e437fc0d7d2ea9
Matias Niemelä <matias@yearofmoo.com>
2019-03-14 21:36:58 +0000

    1a9ab27 docs: update developer guide for use with yarn (#29044)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+35.sha-f50928f.with-local-changes
+ * @license Angular v8.0.0-beta.8+38.sha-1a9ab27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+35.sha-f50928f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+38.sha-1a9ab27.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
