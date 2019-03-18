fea5b5943e7389d1adbf5f5faff3784c78f9c086
Jason Aden <jasonaden@google.com>
2019-03-18 14:38:45 +0000

    8e70ca3 refactor(ivy): Add docs to i18n interfaces (#29344)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+55.sha-9d090cb.with-local-changes
+ * @license Angular v8.0.0-beta.8+56.sha-8e70ca3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+55.sha-9d090cb.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+56.sha-8e70ca3.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
