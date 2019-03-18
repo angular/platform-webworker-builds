76cc4be96f3e2130e77b659d8d6e69db86d5f7f0
Matias Niemelä <matias@yearofmoo.com>
2019-03-18 21:55:15 +0000

    d87b035 refactor(ivy): Update query-related comments (#29342)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+61.sha-b759d63.with-local-changes
+ * @license Angular v8.0.0-beta.8+62.sha-d87b035.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+61.sha-b759d63.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+62.sha-d87b035.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
