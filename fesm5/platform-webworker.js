7b3dc8e86ef063030dee682b69960c6b12b6db21
Jason Aden <jasonaden@google.com>
2019-03-18 14:32:42 +0000

    e8df000 refactor(ivy): update TNodeType (#29343)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.8+52.sha-604f37b.with-local-changes
+ * @license Angular v8.0.0-beta.8+53.sha-e8df000.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.8+52.sha-604f37b.with-local-changes');
+var VERSION = new Version('8.0.0-beta.8+53.sha-e8df000.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
