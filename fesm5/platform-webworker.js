5be50e13247780b910d4d5c876680f1d369f67f2
Matias Niemelä <matias@yearofmoo.com>
2019-03-19 17:23:51 +0000

    8e57965 docs: fix typo in section introduction (#29394) PR Close #29394

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.9+25.sha-761bb0a.with-local-changes
+ * @license Angular v7.2.9+26.sha-8e57965.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.9+25.sha-761bb0a.with-local-changes');
+var VERSION = new Version('7.2.9+26.sha-8e57965.with-local-changes');
 
 /**
  * @license
