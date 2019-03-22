0c8626453eee313285623909ecf4a5c26197b171
Miško Hevery <misko@hevery.com>
2019-03-22 20:37:28 +0000

    b5295ad test(bazel): Add router to bazel integration test (#29459)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+79.sha-9bcc1e8.with-local-changes
+ * @license Angular v8.0.0-beta.9+82.sha-b5295ad.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+79.sha-9bcc1e8.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+82.sha-b5295ad.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
