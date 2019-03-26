20a7e52c53e4f7e08dd0d1e9455a162a8af52235
Miško Hevery <misko@hevery.com>
2019-03-26 20:02:39 +0000

    9724247 fix(ivy): generate empty QueryList for root component content queries (#29514)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.9+94.sha-7951c4f.with-local-changes
+ * @license Angular v8.0.0-beta.9+96.sha-9724247.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.9+94.sha-7951c4f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.9+96.sha-9724247.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
