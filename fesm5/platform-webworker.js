57bd7c82ec58ecc7634057b6e132596cec8ac33b
Kara Erickson <karakara@google.com>
2019-03-26 18:00:51 +0000

    7081e39 test(platform-browser): fix shadow dom test not working in firefox 65 (#29518)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v7.2.10+18.sha-dbbfab3.with-local-changes
+ * @license Angular v7.2.10+21.sha-7081e39.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1013,7 +1013,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('7.2.10+18.sha-dbbfab3.with-local-changes');
+var VERSION = new Version('7.2.10+21.sha-7081e39.with-local-changes');
 
 /**
  * @license
