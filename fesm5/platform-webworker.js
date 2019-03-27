609ba9c2a6f52ab77e350d70fc41b9bf6ac15a42
Miško Hevery <misko@hevery.com>
2019-03-27 17:42:26 +0000

    f8c7c3c docs: add schematic details and links to config page (#27272)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes
+ * @license Angular v8.0.0-beta.10+17.sha-f8c7c3c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+12.sha-ac3ce0d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+17.sha-f8c7c3c.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
