3dbcae0a12f7dc56a8339181ffdfdfe926453dc7
Miško Hevery <misko@hevery.com>
2019-03-27 21:09:35 +0000

    e4c1c88 feat(ivy): add property instruction (#29513)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes
+ * @license Angular v8.0.0-beta.10+31.sha-e4c1c88.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1052,7 +1052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+28.sha-aaa8a3a.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+31.sha-e4c1c88.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
