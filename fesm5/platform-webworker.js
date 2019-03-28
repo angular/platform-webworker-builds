133361599cf428ae28c23a7d8140c76953967524
Jason Aden <jasonaden@google.com>
2019-03-28 22:52:50 +0000

    57f7996 docs: add asset config details (#28214)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
+ * @license Angular v8.0.0-beta.10+44.sha-57f7996.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+42.sha-6c76dfc.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+44.sha-57f7996.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
