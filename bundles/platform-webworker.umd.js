816f09d59047fe5f1cbcd9f2b4ca726d71b78ea1
Jason Aden <jasonaden@google.com>
2019-03-28 22:51:55 +0000

    6c76dfc docs: copy-edit ivy opt-in page (#29507)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+41.sha-78ba503.with-local-changes
+ * @license Angular v8.0.0-beta.10+42.sha-6c76dfc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1052,7 +1052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-beta.10+41.sha-78ba503.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-beta.10+42.sha-6c76dfc.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
