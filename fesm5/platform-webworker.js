296874ecf81a8035e9c8ebb2dc99116a01292d04
Jason Aden <jasonaden@google.com>
2019-04-01 18:49:04 +0000

    9cd9053 docs: add missing window key shortcut to language service guide (#29613)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+77.sha-ff2a55f.with-local-changes
+ * @license Angular v8.0.0-beta.10+78.sha-9cd9053.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+77.sha-ff2a55f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+78.sha-9cd9053.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
