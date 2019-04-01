b2ac40c618a65f58ca28f3e28b9239ec66b8ed90
Jason Aden <jasonaden@google.com>
2019-04-01 22:49:21 +0000

    8bfaaf1 docs: add Web Worker guide (#29633)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+91.sha-1f469cd.with-local-changes
+ * @license Angular v8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+91.sha-1f469cd.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+92.sha-8bfaaf1.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
