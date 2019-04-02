b492b1e2886104075b964d860a96a0b929a045a4
Jason Aden <jasonaden@google.com>
2019-04-02 17:58:30 +0000

    d11b0c0 refactor(compiler): update docs (#29599)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+102.sha-1c07061.with-local-changes
+ * @license Angular v8.0.0-beta.10+101.sha-d11b0c0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+102.sha-1c07061.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+101.sha-d11b0c0.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
