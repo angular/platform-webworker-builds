4ebdb7ae5d1fde1a576fb94ed83f94ea7f64c344
Jason Aden <jasonaden@google.com>
2019-04-02 18:05:16 +0000

    9f54d76 refactor(upgrade): use Bazel packages to avoid symlinks in the source (#29466)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.10+106.sha-cbd5d28.with-local-changes
+ * @license Angular v8.0.0-beta.10+105.sha-9f54d76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.10+106.sha-cbd5d28.with-local-changes');
+var VERSION = new Version('8.0.0-beta.10+105.sha-9f54d76.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
