96aef75cad6a24d41d2cd9bcad200db90996f3db
Jason Aden <jasonaden@google.com>
2019-04-03 23:00:48 +0000

    c7ff728 docs: add angular.de to the workshops page (#28856)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+6.sha-b9fead7.with-local-changes
+ * @license Angular v8.0.0-beta.11+8.sha-c7ff728.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+6.sha-b9fead7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+8.sha-c7ff728.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
