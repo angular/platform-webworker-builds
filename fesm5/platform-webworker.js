97c903b4e7c060478fd8297f7b06fabd998c470a
Jason Aden <jasonaden@google.com>
2019-04-03 22:50:13 +0000

    9810c6c fix(bazel): support running ng-add on minimal applications (#29681)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+2.sha-3246399.with-local-changes
+ * @license Angular v8.0.0-beta.11+4.sha-9810c6c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+2.sha-3246399.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+4.sha-9810c6c.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
