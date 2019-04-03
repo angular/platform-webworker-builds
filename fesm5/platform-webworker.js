3295a37dbb2c84148d4c209dfc79ea0c96ba0cb9
Jason Aden <jasonaden@google.com>
2019-04-03 22:58:28 +0000

    b9fead7 docs: update announcements for home page (#29671)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+7.sha-d96f62f.with-local-changes
+ * @license Angular v8.0.0-beta.11+6.sha-b9fead7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+7.sha-d96f62f.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+6.sha-b9fead7.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
