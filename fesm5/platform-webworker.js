4467b337c37e3800c60ae788f832f436846cba10
Igor Minar <igor@angular.io>
2019-04-08 19:22:15 +0000

    66b87ce fix(ivy): add dev mode counter information to styling (#29579)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+38.sha-cf40105.with-local-changes
+ * @license Angular v8.0.0-beta.11+41.sha-66b87ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+38.sha-cf40105.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+41.sha-66b87ce.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
