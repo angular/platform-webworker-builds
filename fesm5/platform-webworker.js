44961c449651cc762b10ec00391d80791e7e065e
Igor Minar <igor@angular.io>
2019-04-08 17:05:52 +0000

    6d35c50 test(core): add test for template variable object property assignment (#29708)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+32.sha-8bbf481.with-local-changes
+ * @license Angular v8.0.0-beta.11+34.sha-6d35c50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+32.sha-8bbf481.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+34.sha-6d35c50.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
