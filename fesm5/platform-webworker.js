69b59f24b9e33f2a834a866e884f8711f3bb3dfe
Igor Minar <igor@angular.io>
2019-04-11 15:14:53 +0000

    712d60e fix(platform-browser): insert APP_ID in styles, contentAttr and hostAttr (#17745)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+72.sha-3ea8d65.with-local-changes
+ * @license Angular v8.0.0-beta.11+73.sha-712d60e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+72.sha-3ea8d65.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+73.sha-712d60e.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
