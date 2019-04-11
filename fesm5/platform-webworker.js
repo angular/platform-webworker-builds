8593827319620d9457aa72e8a8097c7560a70f43
Igor Minar <igor@angular.io>
2019-04-11 18:20:29 +0000

    54058ba fix(core): resolve ts compile issues due to lenient tsconfig (#29843)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+83.sha-91c7b45.with-local-changes
+ * @license Angular v8.0.0-beta.11+84.sha-54058ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+83.sha-91c7b45.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+84.sha-54058ba.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
