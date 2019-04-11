399c0eb272eaf5ad72dda30b5df7649780f0d6f3
Igor Minar <igor@angular.io>
2019-04-11 14:57:56 +0000

    4191344 perf(ivy): coalesce handlers for events with the same name on the same element (#29786)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.11+71.sha-faf8fdd.with-local-changes
+ * @license Angular v8.0.0-beta.11+70.sha-4191344.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.11+71.sha-faf8fdd.with-local-changes');
+var VERSION = new Version('8.0.0-beta.11+70.sha-4191344.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
