034336566fac16b3e6391445034b905b11bf086a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-04-16 20:50:30 +0000

    1794a8e fix(ivy): coalesced listeners should preventDefault if any returns false (#29934)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.12+24.sha-2bfb6a0.with-local-changes
+ * @license Angular v8.0.0-beta.12+26.sha-1794a8e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.12+24.sha-2bfb6a0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.12+26.sha-1794a8e.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
