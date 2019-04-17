c5b66726203a6575c364eb1c6863a5c075135e1c
Ben Lesh <ben@benlesh.com>
2019-04-17 19:31:32 +0000

    a48d288 build(language-service): no need to bootstrap init_node_spec.js (#29937)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+2.sha-d9c39dc.with-local-changes
+ * @license Angular v8.0.0-beta.13+3.sha-a48d288.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+2.sha-d9c39dc.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+3.sha-a48d288.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
