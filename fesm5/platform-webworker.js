2be53738e893cc38a5d8da8cf53546f6079fd106
Ben Lesh <ben@benlesh.com>
2019-04-19 17:01:30 +0000

    f9bb53a fix(ivy): allow TestBed.createComponent to create components in isolation (#29981)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes
+ * @license Angular v8.0.0-beta.13+29.sha-f9bb53a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+28.sha-c1d5fbd.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+29.sha-f9bb53a.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
