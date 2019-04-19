b40ed5b1b94481d3deab04b9ff1c2fca002a8bee
Ben Lesh <ben@benlesh.com>
2019-04-19 17:20:46 +0000

    9b93bd6 fix(ivy): ensure views created in constructors dont break queries (#29983)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+33.sha-5650e38.with-local-changes
+ * @license Angular v8.0.0-beta.13+35.sha-9b93bd6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+33.sha-5650e38.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+35.sha-9b93bd6.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
