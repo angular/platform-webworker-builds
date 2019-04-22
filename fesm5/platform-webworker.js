52330609cbb76637df9e81b037c0fd1f078ce8c2
Ben Lesh <ben@benlesh.com>
2019-04-22 16:03:34 +0000

    8cba4e1 fix(ivy): ngcc - do not copy declaration files into bundle clone (#30020)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+61.sha-3f6bf6d.with-local-changes
+ * @license Angular v8.0.0-beta.13+63.sha-8cba4e1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+61.sha-3f6bf6d.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+63.sha-8cba4e1.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
