3312ec716bef6584db470ab9cd7c3ec7fcef6ae5
Ben Lesh <ben@benlesh.com>
2019-04-22 23:46:06 +0000

    54289ae docs: cleanup code snippets for getting started example app (#29837)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+73.sha-8c80b85.with-local-changes
+ * @license Angular v8.0.0-beta.13+75.sha-54289ae.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+73.sha-8c80b85.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+75.sha-54289ae.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
