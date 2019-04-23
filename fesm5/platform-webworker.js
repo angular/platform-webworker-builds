44f764ce54d0b8e0748666ac09ee361bcea7a82e
Ben Lesh <ben@benlesh.com>
2019-04-23 15:59:32 +0000

    8e8e89a fix(common): prevent repeated application of HttpParams mutations (#29045)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.13+84.sha-364250e.with-local-changes
+ * @license Angular v8.0.0-beta.13+88.sha-8e8e89a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.13+84.sha-364250e.with-local-changes');
+var VERSION = new Version('8.0.0-beta.13+88.sha-8e8e89a.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
