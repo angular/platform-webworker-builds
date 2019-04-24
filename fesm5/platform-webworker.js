982606aff7be3e5d693ad5daa6a64b2a5e0f8d4c
Andrew Kushnir <akushnir@google.com>
2019-04-24 18:19:41 +0000

    d0ca7c7 docs: add Andrew Kushnir to Angular team (#30045)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+13.sha-6c88eb7.with-local-changes
+ * @license Angular v8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.14+13.sha-6c88eb7.with-local-changes');
+var VERSION = new Version('8.0.0-beta.14+14.sha-d0ca7c7.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
