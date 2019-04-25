3d081e88bf5e8cd6ac7ffeb391ed0557004df511
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:27:57 +0000

    d316a18 fix(ivy): don't include query fields in type constructors (#30094)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+40.sha-28fd5ab.with-local-changes
+ * @license Angular v8.0.0-beta.14+42.sha-d316a18.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.14+40.sha-28fd5ab.with-local-changes');
+var VERSION = new Version('8.0.0-beta.14+42.sha-d316a18.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
