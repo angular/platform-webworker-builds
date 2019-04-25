a18d0f27ddff5d43d4c30fbc13c52ca948b40470
Andrew Kushnir <akushnir@google.com>
2019-04-25 00:21:15 +0000

    abcb2cf refactor(upgrade): rename `module` constant to avoid webpack bug (#30058)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+37.sha-a9379e0.with-local-changes
+ * @license Angular v8.0.0-beta.14+38.sha-abcb2cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.14+37.sha-a9379e0.with-local-changes');
+var VERSION = new Version('8.0.0-beta.14+38.sha-abcb2cf.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
