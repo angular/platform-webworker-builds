9100ef04681ff9e270ee74ac99a36b1960f57a46
Andrew Kushnir <akushnir@google.com>
2019-04-25 01:24:59 +0000

    a400429 docs: temporary disable aio_monitoring job due to a version skew (#30102)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-beta.14+43.sha-e720675.with-local-changes
+ * @license Angular v8.0.0-beta.14+44.sha-a400429.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-beta.14+43.sha-e720675.with-local-changes');
+var VERSION = new Version('8.0.0-beta.14+44.sha-a400429.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
