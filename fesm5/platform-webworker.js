aca3be2fe17ace07bbb2d99a382236e071c93e07
Andrew Kushnir <akushnir@google.com>
2019-04-26 19:49:10 +0000

    4ae0ee8 fix(bazel): update peerDep ranges (#30155)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+11.sha-6c86ae7.with-local-changes
+ * @license Angular v8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+11.sha-6c86ae7.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+12.sha-4ae0ee8.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
