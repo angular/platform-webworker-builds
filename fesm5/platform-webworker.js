149acaa72f2842c70ee28d173a7b5ec95ed855f2
Andrew Kushnir <akushnir@google.com>
2019-04-26 18:37:31 +0000

    f3ce8ee fix(ivy): property bindings use correct indices (#30129)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+9.sha-876ceb3.with-local-changes
+ * @license Angular v8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+9.sha-876ceb3.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+10.sha-f3ce8ee.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
