c74c21957a9a0fcf6c731ff57fa50334aee1a591
Andrew Kushnir <akushnir@google.com>
2019-04-29 20:42:09 +0000

    f015dbe docs: add architect terms (#26963)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+57.sha-029a939.with-local-changes
+ * @license Angular v8.0.0-rc.0+58.sha-f015dbe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1052,7 +1052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+57.sha-029a939.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+58.sha-f015dbe.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
