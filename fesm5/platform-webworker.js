673ce6228e7072742fa1f0a21294971c23ab7668
Kara Erickson <karakara@google.com>
2019-05-06 16:44:47 +0000

    4537816 docs: fix `targetEntryPointPath` description (#30237)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+82.sha-bf73fb7.with-local-changes
+ * @license Angular v8.0.0-rc.0+83.sha-4537816.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+82.sha-bf73fb7.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+83.sha-4537816.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
