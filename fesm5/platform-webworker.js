7dd1b612a224a524e146f6f0b49f39804fb3b955
Kara Erickson <karakara@google.com>
2019-05-07 22:58:47 +0000

    392473e fix(core): consistently use ng:/// for sourcemap URLs (#29826)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+101.sha-345a3cd.with-local-changes
+ * @license Angular v8.0.0-rc.0+102.sha-392473e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+101.sha-345a3cd.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+102.sha-392473e.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
