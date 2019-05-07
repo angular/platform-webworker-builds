a9f005702bc50f74dd919f9e043320fa1959b353
Kara Erickson <karakara@google.com>
2019-05-07 20:37:27 +0000

    3b551e0 docs(aio): add awade jigsaw into resources (#30204)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+97.sha-be8fbac.with-local-changes
+ * @license Angular v8.0.0-rc.0+98.sha-3b551e0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+97.sha-be8fbac.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+98.sha-3b551e0.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
