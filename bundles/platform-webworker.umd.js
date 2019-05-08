fb3c308212c0b175d0c44ceeaa9dacd1c957a2a4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-08 00:40:30 +0000

    8ced321 Revert "fix(router): fix a problem with router not responding to back button (#30160)" (#30320)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+103.sha-2a0f497.with-local-changes
+ * @license Angular v8.0.0-rc.0+104.sha-8ced321.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1052,7 +1052,7 @@
     /**
      * @publicApi
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+103.sha-2a0f497.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+104.sha-8ced321.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
