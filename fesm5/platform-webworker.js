7f8d49fdfd565a330a8dd585fe6390eb80e31f8c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 18:56:22 +0000

    0ffdb48 fix(core): static-query migration should handle queries on accessors (#30327)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+124.sha-d70b1ff.with-local-changes
+ * @license Angular v8.0.0-rc.0+126.sha-0ffdb48.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+124.sha-d70b1ff.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+126.sha-0ffdb48.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
