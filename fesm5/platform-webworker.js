993e161636bdf922b02c0f34da3e24e93248ecb1
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-09 19:11:48 +0000

    3aff79c fix(bazel): pass correct arguments to http_server in Windows (#30346)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+132.sha-de996c6.with-local-changes
+ * @license Angular v8.0.0-rc.0+123.sha-3aff79c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+132.sha-de996c6.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+123.sha-3aff79c.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
