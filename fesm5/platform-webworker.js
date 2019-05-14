45d0de58675d6eb31d33a340a4fea91e4fcedbb9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-05-14 22:08:49 +0000

    dbb150a Revert "fix(core): CSS sanitizer now allows parens in file names (#30322)" (#30463)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+176.sha-9e5377a.with-local-changes
+ * @license Angular v8.0.0-rc.0+177.sha-dbb150a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+176.sha-9e5377a.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+177.sha-dbb150a.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
