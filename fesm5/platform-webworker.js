c96b93f711d05361c42a9ba00eac4292b0ac127e
Jason Aden <jasonaden@google.com>
2019-05-15 22:03:32 +0000

    0051ddf docs: correct types in transform method to match specification. (#29812)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+191.sha-79903b1.with-local-changes
+ * @license Angular v8.0.0-rc.0+190.sha-0051ddf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+191.sha-79903b1.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+190.sha-0051ddf.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
