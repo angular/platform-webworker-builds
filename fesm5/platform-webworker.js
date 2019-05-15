7ddd70ae875d18cb194e50f71a152af23013f806
Jason Aden <jasonaden@google.com>
2019-05-15 21:50:44 +0000

    3a8f74e docs: fix example "testing" app compilation (#30427)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+188.sha-8555016.with-local-changes
+ * @license Angular v8.0.0-rc.0+189.sha-3a8f74e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+188.sha-8555016.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+189.sha-3a8f74e.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
