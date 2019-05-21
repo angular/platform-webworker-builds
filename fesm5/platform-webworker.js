5c0427bd07b1361bae6b7789d1508e1e3753c72c
Jason Aden <jasonaden@google.com>
2019-05-21 16:31:46 +0000

    e946594 docs: move instructions to see changes after the error is fixed in the tutorial (#30529)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+266.sha-abe4433.with-local-changes
+ * @license Angular v8.0.0-rc.0+267.sha-e946594.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -999,7 +999,7 @@ function spawnWebWorker(uri, instance) {
 /**
  * @publicApi
  */
-var VERSION = new Version('8.0.0-rc.0+266.sha-abe4433.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+267.sha-e946594.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
