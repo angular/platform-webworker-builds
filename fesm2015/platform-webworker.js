c040b73fabdcfa82fcdabaaa40df226a3650c7bd
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:21:49 +0000

    deb77bd feat(ivy): TestBed support for reusing non-exported components (#30578)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+341.sha-d5f96a8.with-local-changes
+ * @license Angular v8.0.0-rc.0+342.sha-deb77bd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+341.sha-d5f96a8.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+342.sha-deb77bd.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
