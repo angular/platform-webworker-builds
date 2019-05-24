40a518fc3d2dc2b7cf771fab00f35a8ab37946a4
Paul Gschwendtner <paulgschwendtner@gmail.com>
2019-05-24 10:46:16 +0000

    54ee5cd docs(core): static-query migration should use permalink for migration guide

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+336.sha-05479f8.with-local-changes
+ * @license Angular v8.0.0-rc.0+336.sha-54ee5cd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.0-rc.0+336.sha-05479f8.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+336.sha-54ee5cd.with-local-changes');
 
     /**
      * @license
