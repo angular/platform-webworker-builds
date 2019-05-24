39ed40baaa0a9e13e144e1ca90e2feb212909a85
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:24:01 +0000

    661c6e6 fix(ivy): fix PR collision with static: true and new test (#30666)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+343.sha-dc6406e.with-local-changes
+ * @license Angular v8.0.0-rc.0+345.sha-661c6e6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('8.0.0-rc.0+343.sha-dc6406e.with-local-changes');
+    var VERSION = new i0.Version('8.0.0-rc.0+345.sha-661c6e6.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
