4a3cec5e6f2edf7b639344238f4031637fc52383
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 22:38:16 +0000

    573f57a build(docs-infra): upgrade cli command docs sources to 30036352e (#30608)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+346.sha-41cf066.with-local-changes
+ * @license Angular v8.0.0-rc.0+347.sha-573f57a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.0+346.sha-41cf066.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+347.sha-573f57a.with-local-changes');
 
 /**
  * @license
