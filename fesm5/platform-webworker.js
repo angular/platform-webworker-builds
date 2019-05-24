046745d17d7271b0d541c0941ec5a12bc2d3c205
Jason Aden <jasonaden@google.com>
2019-05-24 05:34:47 +0000

    911a816 docs(core): add more info to static query guide (#30646)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+331.sha-2508872.with-local-changes
+ * @license Angular v8.0.0-rc.0+333.sha-911a816.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.0+331.sha-2508872.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+333.sha-911a816.with-local-changes');
 
 /**
  * @license
