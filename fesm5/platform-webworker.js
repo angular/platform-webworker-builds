6390d2c36faf294e512b23284c4ce12e21cf9ac4
Matias Niemelä <matias@yearofmoo.com>
2019-05-24 18:16:59 +0000

    b5756bc docs(bazel): remove out of date known issue with Webstorm (#30650)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.5+4.sha-60f0462.with-local-changes
+ * @license Angular v8.0.0-rc.5+5.sha-b5756bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.5+4.sha-60f0462.with-local-changes');
+var VERSION = new Version('8.0.0-rc.5+5.sha-b5756bc.with-local-changes');
 
 /**
  * @license
