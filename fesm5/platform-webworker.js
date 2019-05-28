3c0228bde61cf1b38fb45159c4073dabdee7350e
Igor Minar <igor@angular.io>
2019-05-28 23:59:51 +0000

    4809382 docs: add note to platform-webworker deprecation (#30705)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+353.sha-51cb592.with-local-changes
+ * @license Angular v8.0.0-rc.0+354.sha-4809382.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.0+353.sha-51cb592.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+354.sha-4809382.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
