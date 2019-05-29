10f4c3d1f908dbe07d4712cbe1629f20dcb9905a
Matias Niemelä <matias@yearofmoo.com>
2019-05-29 18:25:20 +0000

    f8fa2f2 docs(core): template-var-assignment schematic should link to deprecation guide (#30702)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+2.sha-f3ee9a6.with-local-changes
+ * @license Angular v8.0.0+3.sha-f8fa2f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0+2.sha-f3ee9a6.with-local-changes');
+var VERSION = new Version('8.0.0+3.sha-f8fa2f2.with-local-changes');
 
 /**
  * @license
