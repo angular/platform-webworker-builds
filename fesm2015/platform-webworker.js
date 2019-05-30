be6acf31066770acd592fc578213436c7045dd57
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:34:30 +0000

    f5b0c8a docs: remove mention of project settings from analytics (#30701)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+370.sha-6a5cc8f.with-local-changes
+ * @license Angular v8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+370.sha-6a5cc8f.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+377.sha-f5b0c8a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
