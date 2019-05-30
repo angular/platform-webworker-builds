ebfc9ec567fed3738d3f6aaf30bbbdbd76acaab3
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 05:26:19 +0000

    82682bb refactor(ivy): enable sanitization support for the new styling algorithm (#30667)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+356.sha-d72479b.with-local-changes
+ * @license Angular v8.0.0-rc.0+357.sha-82682bb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.0+356.sha-d72479b.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+357.sha-82682bb.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
