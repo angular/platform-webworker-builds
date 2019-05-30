5987371169043c9d1ae68c071d554bada7c634d6
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:37:24 +0000

    31aba96 docs: improve glossary for entry-point. (#29433)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+13.sha-facce2c.with-local-changes
+ * @license Angular v8.0.0+14.sha-31aba96.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.0+13.sha-facce2c.with-local-changes');
+    var VERSION = new core.Version('8.0.0+14.sha-31aba96.with-local-changes');
 
     /**
      * @license
