2958155d308637009c1e7456e7da8539376eceb5
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 20:16:09 +0000

    41f372f ci: fix payload limits value

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes
+ * @license Angular v8.0.0-rc.0+383.sha-41f372f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.0+379.sha-7a0f8ac.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+383.sha-41f372f.with-local-changes');
 
 /**
  * @license
