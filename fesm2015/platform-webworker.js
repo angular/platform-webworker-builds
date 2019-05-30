e883e28614e4479595db87cd4e335c45c72da141
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 21:05:38 +0000

    53c6b78 feat(ivy): add `AttributeMarker.I18n` for i18n attributes (#30402)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+383.sha-41f372f.with-local-changes
+ * @license Angular v8.0.0-rc.0+385.sha-53c6b78.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0-rc.0+383.sha-41f372f.with-local-changes');
+const VERSION = new Version('8.0.0-rc.0+385.sha-53c6b78.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
