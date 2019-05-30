eecd88f0b49a30bb4a623808ddd85def9d311062
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:20:25 +0000

    7757826 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+368.sha-0b23adc.with-local-changes
+ * @license Angular v8.0.0-rc.0+360.sha-7757826.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.0-rc.0+368.sha-0b23adc.with-local-changes');
+    var VERSION = new core.Version('8.0.0-rc.0+360.sha-7757826.with-local-changes');
 
     /**
      * @license
