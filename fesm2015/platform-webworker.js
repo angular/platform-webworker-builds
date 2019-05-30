72ab9b8a87ce2a167f5080736049adf8ff8cf906
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 18:11:17 +0000

    9a9ae60 docs: code sample closing in ivy.md (#30711) PR Close #30711

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+3.sha-f8fa2f2.with-local-changes
+ * @license Angular v8.0.0+5.sha-9a9ae60.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0+3.sha-f8fa2f2.with-local-changes');
+const VERSION = new Version('8.0.0+5.sha-9a9ae60.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
