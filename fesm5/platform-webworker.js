1f2a8afad4f3716e1e936a881dd8a4703bfa21d8
Matias Niemelä <matias@yearofmoo.com>
2019-05-30 21:27:12 +0000

    19bbf4b test(ivy): fix sanitization state leaking issue

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0-rc.0+385.sha-53c6b78.with-local-changes
+ * @license Angular v8.0.0-rc.0+386.sha-19bbf4b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0-rc.0+385.sha-53c6b78.with-local-changes');
+var VERSION = new Version('8.0.0-rc.0+386.sha-19bbf4b.with-local-changes');
 
 /**
  * @license
