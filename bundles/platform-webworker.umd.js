8361c9c23b80aa2801297adff3171363ea0a6925
Misko Hevery <misko@hevery.com>
2019-05-31 03:44:54 +0000

    09c57ec docs: add final release date and update future schedule (#30712)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0.with-local-changes
+ * @license Angular v8.1.0-beta.0+1.sha-09c57ec.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0-beta.0.with-local-changes');
+    var VERSION = new core.Version('8.1.0-beta.0+1.sha-09c57ec.with-local-changes');
 
     /**
      * @license
