44c6b92fea50326428a463aad9d382853fc3fb9b
Misko Hevery <misko@hevery.com>
2019-05-31 04:07:57 +0000

    99c9bca test: move away from deprecated testing method (#30747)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+1.sha-09c57ec.with-local-changes
+ * @license Angular v8.1.0-beta.0+4.sha-99c9bca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-beta.0+1.sha-09c57ec.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+4.sha-99c9bca.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
