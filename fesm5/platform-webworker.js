77015ecbe0397fee346d721bb85c39e5d8803e9c
Misko Hevery <misko@hevery.com>
2019-05-31 04:10:33 +0000

    fa35488 ci: add reviewer group for deprecations guide (#30722) PR Close #30722

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+2.sha-d82adbe.with-local-changes
+ * @license Angular v8.1.0-beta.0+3.sha-fa35488.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-beta.0+2.sha-d82adbe.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+3.sha-fa35488.with-local-changes');
 
 /**
  * @license
