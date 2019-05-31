d2bfb54926a41ec8c22438bae15e5452946cb0ce
Miško Hevery <misko@hevery.com>
2019-05-31 17:13:44 +0000

    f4cd374 fix(docs-infra): update tooltip of events link (#30643)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+7.sha-1f79c82.with-local-changes
+ * @license Angular v8.1.0-beta.0+9.sha-f4cd374.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-beta.0+7.sha-1f79c82.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+9.sha-f4cd374.with-local-changes');
 
 /**
  * @license
