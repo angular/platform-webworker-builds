50209f8c5a21d738f97ee640d55990e43cfaf9d5
Miško Hevery <misko@hevery.com>
2019-05-31 17:20:40 +0000

    aca339e fix(ivy): unable to project into multiple slots with default selector (#30561)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+8.sha-6d861f2.with-local-changes
+ * @license Angular v8.1.0-beta.0+10.sha-aca339e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+8.sha-6d861f2.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+10.sha-aca339e.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
