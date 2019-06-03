14be2600767a05b2b2fb86408b68416614bfafe9
Misko Hevery <misko@hevery.com>
2019-06-03 19:29:25 +0000

    06ffed5 docs: document UrlTree in guard return type in cheatsheet (#30822)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+28.sha-5a15126.with-local-changes
+ * @license Angular v8.0.0+29.sha-06ffed5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.0+28.sha-5a15126.with-local-changes');
+var VERSION = new Version('8.0.0+29.sha-06ffed5.with-local-changes');
 
 /**
  * @license
