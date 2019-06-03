8a7b05c50836ec2814d5051032d450efb53f624f
Misko Hevery <misko@hevery.com>
2019-06-03 16:28:26 +0000

    8bedf50 docs: update example references from "my-app" to "app-root" (#30789)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+16.sha-32886cf.with-local-changes
+ * @license Angular v8.1.0-beta.0+17.sha-8bedf50.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-beta.0+16.sha-32886cf.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+17.sha-8bedf50.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
