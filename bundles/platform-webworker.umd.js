95f55088cdfdfc90944bf12c3f8e5487539f8128
Misko Hevery <misko@hevery.com>
2019-06-03 16:30:23 +0000

    dd8cf19 docs: fix a small typo in injectable decorator description (#30785)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+19.sha-21328f2.with-local-changes
+ * @license Angular v8.1.0-beta.0+18.sha-dd8cf19.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0-beta.0+19.sha-21328f2.with-local-changes');
+    var VERSION = new core.Version('8.1.0-beta.0+18.sha-dd8cf19.with-local-changes');
 
     /**
      * @license
