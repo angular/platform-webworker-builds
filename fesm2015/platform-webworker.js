5cff0d840b2f57ea98280f228a6513955c40c411
Misko Hevery <misko@hevery.com>
2019-06-03 16:34:31 +0000

    393529d docs: update example references from "my-app" to "app-root" (#30789)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+27.sha-9e4b2f1.with-local-changes
+ * @license Angular v8.0.0+26.sha-393529d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0+27.sha-9e4b2f1.with-local-changes');
+const VERSION = new Version('8.0.0+26.sha-393529d.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
