d5e177c21fbc654002780e66d94146eedf361ab6
Misko Hevery <misko@hevery.com>
2019-06-03 16:31:33 +0000

    21328f2 fix(ivy): add back ngDevMode to r3_injector (#30707)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+17.sha-8bedf50.with-local-changes
+ * @license Angular v8.1.0-beta.0+19.sha-21328f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+17.sha-8bedf50.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+19.sha-21328f2.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
