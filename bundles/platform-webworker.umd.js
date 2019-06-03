36759a1528db9087ccc032bc62d408edd444f4d1
Misko Hevery <misko@hevery.com>
2019-06-03 19:14:19 +0000

    cd6fc8b docs: document UrlTree in guard return type in cheatsheet (#30822)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+23.sha-fcdd784.with-local-changes
+ * @license Angular v8.1.0-beta.0+24.sha-cd6fc8b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0-beta.0+23.sha-fcdd784.with-local-changes');
+    var VERSION = new core.Version('8.1.0-beta.0+24.sha-cd6fc8b.with-local-changes');
 
     /**
      * @license
