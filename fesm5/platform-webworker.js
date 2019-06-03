4d8b314af36416808f8edb7615b55da451c56668
Misko Hevery <misko@hevery.com>
2019-06-03 16:37:45 +0000

    b4e6802 refactor(ivy): add ɵɵupdateSyntheticHostBinding command (#30670)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+20.sha-680d385.with-local-changes
+ * @license Angular v8.1.0-beta.0+21.sha-b4e6802.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-beta.0+20.sha-680d385.with-local-changes');
+var VERSION = new Version('8.1.0-beta.0+21.sha-b4e6802.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
