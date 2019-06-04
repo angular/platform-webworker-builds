945ac28834540c05090e45e6770117a5375aa5db
Misko Hevery <misko@hevery.com>
2019-06-04 19:17:22 +0000

    3ed7463 docs: rename component for constructor example (#30845) PR Close #30845

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-beta.0+28.sha-812c231.with-local-changes
+ * @license Angular v8.1.0-beta.0+27.sha-3ed7463.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-beta.0+28.sha-812c231.with-local-changes');
+const VERSION = new Version('8.1.0-beta.0+27.sha-3ed7463.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
