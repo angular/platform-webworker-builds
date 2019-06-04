247f531190b25d7e7230465586a263fa123fb748
Misko Hevery <misko@hevery.com>
2019-06-04 19:37:46 +0000

    ccc1c27 refactor: Move away from index signature based "updateCache". (#30518)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+35.sha-19caace.with-local-changes
+ * @license Angular v8.0.0+34.sha-ccc1c27.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.0+35.sha-19caace.with-local-changes');
+    var VERSION = new core.Version('8.0.0+34.sha-ccc1c27.with-local-changes');
 
     /**
      * @license
