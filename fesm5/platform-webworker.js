192f6989d1a2c8ffd07572567e2e8445d247e319
Miško Hevery <misko@hevery.com>
2019-06-06 21:09:45 +0000

    11a4454 perf(ivy): remove check for function type in renderStringify (#30838)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
+ * @license Angular v8.1.0-next.1+10.sha-11a4454.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+10.sha-11a4454.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
