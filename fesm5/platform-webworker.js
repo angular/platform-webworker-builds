c95f6c825d30c5610cebac27a6f8019e2a3ddbc1
Misko Hevery <misko@hevery.com>
2019-06-06 04:48:27 +0000

    b51d8dd fix(ivy): error for empty bindings on ng-template (#30829)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+2.sha-ea2d453.with-local-changes
+ * @license Angular v8.1.0-next.1+3.sha-b51d8dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.1+2.sha-ea2d453.with-local-changes');
+var VERSION = new Version('8.1.0-next.1+3.sha-b51d8dd.with-local-changes');
 
 /**
  * @license
