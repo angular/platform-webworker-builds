1983f6a637ace014ec25a7b09171d2cd85c91da8
Miško Hevery <misko@hevery.com>
2019-06-06 16:12:48 +0000

    f440bd1 build(docs-infra): fix CLI command github links (#30889)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+6.sha-3859bcc.with-local-changes
+ * @license Angular v8.1.0-next.1+7.sha-f440bd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('8.1.0-next.1+6.sha-3859bcc.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.1+7.sha-f440bd1.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
