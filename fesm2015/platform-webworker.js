3ab1f336f9f62169f8a9f416a4631d9f21140ff7
Misko Hevery <misko@hevery.com>
2019-06-07 16:13:04 +0000

    8aef446 build(docs-infra): upgrade cli command docs sources to d09f130ce (#30909)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+44.sha-f7ee91a.with-local-changes
+ * @license Angular v8.0.0+45.sha-8aef446.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0+44.sha-f7ee91a.with-local-changes');
+const VERSION = new Version('8.0.0+45.sha-8aef446.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
