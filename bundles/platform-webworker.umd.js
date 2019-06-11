0eae920131806963ebde723083a44f126c97afcf
Andrew Kushnir <akushnir@google.com>
2019-06-11 16:59:20 +0000

    8c4bd61 test(core): add missing static flag to view_insertion test view queries (#30967)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.1+17.sha-5c18f23.with-local-changes
+ * @license Angular v8.1.0-next.1+38.sha-8c4bd61.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0-next.1+17.sha-5c18f23.with-local-changes');
+    var VERSION = new core.Version('8.1.0-next.1+38.sha-8c4bd61.with-local-changes');
 
     /**
      * @license
