62a74a25c99e270844068bd0794e7d2f23b81dc8
Igor Minar <igor@angular.io>
2019-06-11 00:36:19 +0000

    dcf9f05 docs: use `Node.js` consistently (#30934) PR Close #30934

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.0+51.sha-6a48485.with-local-changes
+ * @license Angular v8.0.0+55.sha-dcf9f05.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.0+51.sha-6a48485.with-local-changes');
+const VERSION = new Version('8.0.0+55.sha-dcf9f05.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
