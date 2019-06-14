275cad6557321e84ef13d27ecba17dd9cae27480
Andrew Kushnir <akushnir@google.com>
2019-06-14 19:22:43 +0000

    f41242f docs: rewrite inputs/outputs section of Template Syntax (#27685)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+18.sha-e2fd628.with-local-changes
+ * @license Angular v8.1.0-next.2+19.sha-f41242f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('8.1.0-next.2+18.sha-e2fd628.with-local-changes');
+    var VERSION = new i0.Version('8.1.0-next.2+19.sha-f41242f.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
