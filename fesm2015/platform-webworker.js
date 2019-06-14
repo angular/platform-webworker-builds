79b1eb039ae73d3e266c771da6fff46f2ac4502c
Andrew Kushnir <akushnir@google.com>
2019-06-14 18:08:51 +0000

    cc0bd11 build(docs-infra): update `browserlist` configuration file (#31045)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+6.sha-6bc9c78.with-local-changes
+ * @license Angular v8.1.0-next.2+9.sha-cc0bd11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+6.sha-6bc9c78.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+9.sha-cc0bd11.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
