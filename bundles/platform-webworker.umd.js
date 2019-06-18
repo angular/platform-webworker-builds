ab88959f4801580fdeb263e4f48b50fc4698180a
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:19:02 +0000

    a7ff7d8d2 docs: refresh TypeScript configuration guide with updated info and files (#31097)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+27.sha-18d3012.with-local-changes
+ * @license Angular v8.0.1+29.sha-a7ff7d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.1+27.sha-18d3012.with-local-changes');
+    var VERSION = new core.Version('8.0.1+29.sha-a7ff7d8.with-local-changes');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+27.sha-18d3012.with-local-changes
+ * @license Angular v8.0.1+29.sha-a7ff7d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.1+27.sha-18d3012.with-local-changes');
+var VERSION = new Version('8.0.1+29.sha-a7ff7d8.with-local-changes');
 
 /**
  * @license
