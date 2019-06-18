fe0d5ab53195f38fb1654261eff4af403a2440f8
Andrew Kushnir <akushnir@google.com>
2019-06-18 18:13:22 +0000

    48174010a docs: update group() link to point to the animations group API (#30618)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+30.sha-1460e46.with-local-changes
+ * @license Angular v8.0.1+31.sha-4817401.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.0.1+30.sha-1460e46.with-local-changes');
+const VERSION = new Version('8.0.1+31.sha-4817401.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.1+30.sha-1460e46.with-local-changes
+ * @license Angular v8.0.1+31.sha-4817401.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.1+30.sha-1460e46.with-local-changes');
+var VERSION = new Version('8.0.1+31.sha-4817401.with-local-changes');
 
 /**
  * @license
