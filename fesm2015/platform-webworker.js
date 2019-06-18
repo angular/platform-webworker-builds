9726b03c3552cdab5ed50145c97e572ac727252b
Andrew Kushnir <akushnir@google.com>
2019-06-18 17:22:44 +0000

    87168acf3 refactor(ivy): move `bind` instruction into `i18nExp` (#31089)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.2+37.sha-beaab27.with-local-changes
+ * @license Angular v8.1.0-next.2+36.sha-87168ac.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.2+37.sha-beaab27.with-local-changes');
+const VERSION = new Version('8.1.0-next.2+36.sha-87168ac.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
