2e39a3b42813cbeab13080471089acc2024f4e41
Kara Erickson <karakara@google.com>
2019-06-19 22:51:27 +0000

    ab86a58f2 docs: remove inappropriate link (#31091)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+3.sha-4ec5081.with-local-changes
+ * @license Angular v8.1.0-next.3+4.sha-ab86a58.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+3.sha-4ec5081.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+4.sha-ab86a58.with-local-changes');
 
 /**
  * @license
