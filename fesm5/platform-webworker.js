8dbbc3e015c7a771cb7234e4e6969eace14281fb
Kara Erickson <karakara@google.com>
2019-06-19 23:20:27 +0000

    9c222350f ci: re-enable payload size tracking (#31138)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+7.sha-c36edff.with-local-changes
+ * @license Angular v8.0.2+8.sha-9c22235.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.2+7.sha-c36edff.with-local-changes');
+var VERSION = new Version('8.0.2+8.sha-9c22235.with-local-changes');
 
 /**
  * @license
