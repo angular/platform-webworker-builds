a036c7afdb0821dbf4b0196f40f51afdcb32265d
Kara Erickson <karakara@google.com>
2019-06-19 23:07:50 +0000

    f0395836b fix(ivy): evaluate non existing property access as undefined (#30738)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+5.sha-4b16d98.with-local-changes
+ * @license Angular v8.1.0-next.3+6.sha-f039583.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+5.sha-4b16d98.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+6.sha-f039583.with-local-changes');
 
 /**
  * @license
