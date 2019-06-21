a9eab26e76e318db190a46ec3a83f10fa9058f94
Kara Erickson <karakara@google.com>
2019-06-21 17:55:14 +0000

    83f0304cf docs: clarify wording in Template Syntax (#31119)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+33.sha-4bd6fca.with-local-changes
+ * @license Angular v8.1.0-next.3+35.sha-83f0304.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+33.sha-4bd6fca.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+35.sha-83f0304.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
