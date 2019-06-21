76014995edef53667f2bc79758d6de6898b76686
Kara Erickson <karakara@google.com>
2019-06-21 17:45:28 +0000

    1f1cf1a16 docs: add Mosaic library to resources (#31026)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+31.sha-b166442.with-local-changes
+ * @license Angular v8.1.0-next.3+28.sha-1f1cf1a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+31.sha-b166442.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+28.sha-1f1cf1a.with-local-changes');
 
 /**
  * @license
