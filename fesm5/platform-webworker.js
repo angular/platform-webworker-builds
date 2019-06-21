fbd1e0906dd2c7f13825e209c19425e3f1cdede5
Kara Erickson <karakara@google.com>
2019-06-21 02:07:28 +0000

    dc613b336 refactor(ivy): removing unused properties from i18n interfaces (#31171)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+25.sha-7ff628f.with-local-changes
+ * @license Angular v8.1.0-next.3+26.sha-dc613b3.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+25.sha-7ff628f.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+26.sha-dc613b3.with-local-changes');
 
 /**
  * @license
