53148f97e0edaa5f95a0deb97c5594e8b3bdf041
Kara Erickson <karakara@google.com>
2019-06-21 17:45:59 +0000

    b1664425a build(docs-infra): upgrade cli command docs sources to a7d4472eb (#31184)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+27.sha-48def92.with-local-changes
+ * @license Angular v8.1.0-next.3+31.sha-b166442.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1656,7 +1656,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-next.3+27.sha-48def92.with-local-changes');
+const VERSION = new Version('8.1.0-next.3+31.sha-b166442.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
