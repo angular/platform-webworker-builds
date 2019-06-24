6957892f8a982b1d637a89585273561a56e39d46
Kara Erickson <karakara@google.com>
2019-06-24 22:20:11 +0000

    980bcaf17 fix(bazel): remove unsupported Css pre-processors from ng new (#31234)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+25.sha-f470e69.with-local-changes
+ * @license Angular v8.0.2+24.sha-980bcaf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.2+25.sha-f470e69.with-local-changes');
+    var VERSION = new core.Version('8.0.2+24.sha-980bcaf.with-local-changes');
 
     /**
      * @license
