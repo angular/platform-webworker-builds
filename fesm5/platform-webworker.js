98abc4ed64ddd82c83574289179490d41d852e34
Kara Erickson <karakara@google.com>
2019-06-25 17:54:25 +0000

    a1d436e6a docs: fix formatting (#31120)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+61.sha-b11a205.with-local-changes
+ * @license Angular v8.1.0-next.3+68.sha-a1d436e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+61.sha-b11a205.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+68.sha-a1d436e.with-local-changes');
 
 /**
  * @license
