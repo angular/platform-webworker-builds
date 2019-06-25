20e89587f64bc1e4f5d0cc749e0206fa1445b42d
Kara Erickson <karakara@google.com>
2019-06-25 17:59:33 +0000

    0a46b2acf fix(docs-infra): separate vendor-specific CSS selectors (#31252)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+34.sha-1d193df.with-local-changes
+ * @license Angular v8.0.2+33.sha-0a46b2a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.0.2+34.sha-1d193df.with-local-changes');
+    var VERSION = new core.Version('8.0.2+33.sha-0a46b2a.with-local-changes');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.0.2+34.sha-1d193df.with-local-changes
+ * @license Angular v8.0.2+33.sha-0a46b2a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.0.2+34.sha-1d193df.with-local-changes');
+var VERSION = new Version('8.0.2+33.sha-0a46b2a.with-local-changes');
 
 /**
  * @license
