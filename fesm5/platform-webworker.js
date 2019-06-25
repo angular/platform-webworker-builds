0ce6b40de63272e9ef3c6930fde2a887ddeb0d04
Kara Erickson <karakara@google.com>
2019-06-25 22:04:18 +0000

    ae1ac4598 docs(core): update code sample for "outputs" attribute (#31199)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+80.sha-280e856.with-local-changes
+ * @license Angular v8.1.0-next.3+81.sha-ae1ac45.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+80.sha-280e856.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+81.sha-ae1ac45.with-local-changes');
 
 /**
  * @license
