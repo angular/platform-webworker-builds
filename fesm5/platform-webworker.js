7a44b66093fca6b12077b30fe6299bbca13c266d
Kara Erickson <karakara@google.com>
2019-06-26 20:52:05 +0000

    9c87d223e ci: target default pool for linux RBE executions (#31297)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+106.sha-2be061a.with-local-changes
+ * @license Angular v8.1.0-next.3+107.sha-9c87d22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+106.sha-2be061a.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+107.sha-9c87d22.with-local-changes');
 
 /**
  * @license
