01387edfce03bdd5548e66f8c2b159aa061c067d
Kara Erickson <karakara@google.com>
2019-06-26 21:18:39 +0000

    da4f7fbe1 docs: release notes for the v8.0.3 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-next.3+105.sha-3851544.with-local-changes
+ * @license Angular v8.1.0-next.3+108.sha-da4f7fb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-next.3+105.sha-3851544.with-local-changes');
+var VERSION = new Version('8.1.0-next.3+108.sha-da4f7fb.with-local-changes');
 
 /**
  * @license
