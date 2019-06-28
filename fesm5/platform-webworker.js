24375e10e861527bb93ea1dd639f47c7d505577a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:48:40 +0000

    81332150a perf(ivy): chain host binding instructions (#31296)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+20.sha-2b5d52f.with-local-changes
+ * @license Angular v8.1.0-rc.0+19.sha-8133215.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-rc.0+20.sha-2b5d52f.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+19.sha-8133215.with-local-changes');
 
 /**
  * @license
