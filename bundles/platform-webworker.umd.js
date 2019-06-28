e41349a6a92c3564073c3f48b3cec76de05c38c4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-06-28 16:47:12 +0000

    f57e77eeb build(docs-infra): upgrade cli command docs sources to 9edecc522 (#31331)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+18.sha-c6b29f4.with-local-changes
+ * @license Angular v8.1.0-rc.0+21.sha-f57e77e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0-rc.0+18.sha-c6b29f4.with-local-changes');
+    var VERSION = new core.Version('8.1.0-rc.0+21.sha-f57e77e.with-local-changes');
 
     /**
      * @license
