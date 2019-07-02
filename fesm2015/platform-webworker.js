1ca3c1a73c9e27c46b00dc954eeddee0f5ba35e4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 19:04:26 +0000

    376c5fceb docs: add accessibility guide (#30851)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+32.sha-d0c32e0.with-local-changes
+ * @license Angular v8.1.0-rc.0+34.sha-376c5fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.1.0-rc.0+32.sha-d0c32e0.with-local-changes');
+const VERSION = new Version('8.1.0-rc.0+34.sha-376c5fc.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
