a28d341b91fc54f42df7f0fc49f8e63c69a847e5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-07-02 18:51:16 +0000

    a57ea2640 test(zone.js): fix typos (#31358)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0-rc.0+28.sha-ffe7050.with-local-changes
+ * @license Angular v8.1.0-rc.0+31.sha-a57ea26.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0-rc.0+28.sha-ffe7050.with-local-changes');
+var VERSION = new Version('8.1.0-rc.0+31.sha-a57ea26.with-local-changes');
 
 /**
  * @license
