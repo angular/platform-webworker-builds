17e4ab5fa4143740f2b92098096f4e87a5b654ae
Jason Aden <jasonaden@google.com>
2019-07-08 17:46:22 +0000

    9a2d1fab8 docs: add correct default cli ngModule (#31166)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+8.sha-8052de0.with-local-changes
+ * @license Angular v8.2.0-next.0+9.sha-9a2d1fa.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.0+8.sha-8052de0.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+9.sha-9a2d1fa.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
