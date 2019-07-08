d504caae4c8d2651f07d3cf153a4dee516042e8f
Jason Aden <jasonaden@google.com>
2019-07-08 17:02:09 +0000

    e30f494a3 refactor(ivy): remove interpolation instructions (#31395)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+3.sha-1d3e227.with-local-changes
+ * @license Angular v8.2.0-next.0+4.sha-e30f494.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('8.2.0-next.0+3.sha-1d3e227.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.0+4.sha-e30f494.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
