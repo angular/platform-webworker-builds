440dcf0aa6ce4c2b682c51903bf256efb648076f
Jason Aden <jasonaden@google.com>
2019-07-08 17:03:43 +0000

    4b05ebc80 docs: fix formatting in workspace config and add helpful info for universal (#31399)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+4.sha-e30f494.with-local-changes
+ * @license Angular v8.2.0-next.0+5.sha-4b05ebc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.0+4.sha-e30f494.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+5.sha-4b05ebc.with-local-changes');
 
 /**
  * @license
