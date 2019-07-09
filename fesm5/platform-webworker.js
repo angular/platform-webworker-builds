ac63b744155d19a1618867bbc22c02bd32ea20b3
Jason Aden <jasonaden@google.com>
2019-07-09 17:03:03 +0000

    24fd1a18a fix(ivy): handle namespaced imports correctly (#31367)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+7.sha-bdfbb92.with-local-changes
+ * @license Angular v8.1.0+9.sha-24fd1a1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0+7.sha-bdfbb92.with-local-changes');
+var VERSION = new Version('8.1.0+9.sha-24fd1a1.with-local-changes');
 
 /**
  * @license
