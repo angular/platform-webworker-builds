de8127ea0a36ecbf0ef92067cec8292745e3708a
Jason Aden <jasonaden@google.com>
2019-07-09 21:14:33 +0000

    7724f7446 refactor(bazel): remove unused lockfile (#31458)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.0+15.sha-98a68ad.with-local-changes
+ * @license Angular v8.2.0-next.0+20.sha-7724f74.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.0+15.sha-98a68ad.with-local-changes');
+var VERSION = new Version('8.2.0-next.0+20.sha-7724f74.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
