e4643abea37f91c3f02ac2ed105100f5a8d25b7f
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 05:55:28 +0000

    a395cb1a5 docs: add Renderer to deprecations doc (#31419)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+12.sha-1863254.with-local-changes
+ * @license Angular v8.1.0+14.sha-a395cb1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0+12.sha-1863254.with-local-changes');
+    var VERSION = new core.Version('8.1.0+14.sha-a395cb1.with-local-changes');
 
     /**
      * @license
