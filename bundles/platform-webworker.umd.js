e9fd8a33ea4c038f9ee914c6240e7ebed98f9805
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 19:57:10 +0000

    ef44f51d5 refactor(ivy): remove instruction usage from other instructions (#31456)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+22.sha-7c62a8f.with-local-changes
+ * @license Angular v8.2.0-next.1+24.sha-ef44f51.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('8.2.0-next.1+22.sha-7c62a8f.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.1+24.sha-ef44f51.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
