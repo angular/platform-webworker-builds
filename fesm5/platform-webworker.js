d9fc484d420f7065b6aafb3baf0ff449f83a79ec
Matias Niemelä <matias@yearofmoo.com>
2019-07-11 21:38:01 +0000

    3ade93f6f ci(docs-infra): run a11y audits on certain pages on CI (#31414)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+29.sha-4aed480.with-local-changes
+ * @license Angular v8.1.0+34.sha-3ade93f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0+29.sha-4aed480.with-local-changes');
+var VERSION = new Version('8.1.0+34.sha-3ade93f.with-local-changes');
 
 /**
  * @license
