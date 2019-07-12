414f48c07c11e5516c12449518be358e03933a01
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 22:18:07 +0000

    b65e11e3c build(docs-infra): display CLI positional option enum values (#31529)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+45.sha-604d906.with-local-changes
+ * @license Angular v8.2.0-next.1+47.sha-b65e11e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.1+45.sha-604d906.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+47.sha-b65e11e.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
