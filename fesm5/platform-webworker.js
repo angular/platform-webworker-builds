7a46798df7cd3f3d547b393cc89e43249ebfba3e
Matias Niemelä <matias@yearofmoo.com>
2019-07-12 15:57:08 +0000

    63e458dd3 fix(ivy): handle ICUs with placeholders in case other nested ICUs are present (#31516)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+35.sha-d545bbe.with-local-changes
+ * @license Angular v8.2.0-next.1+36.sha-63e458d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.1+35.sha-d545bbe.with-local-changes');
+var VERSION = new Version('8.2.0-next.1+36.sha-63e458d.with-local-changes');
 
 /**
  * @license
