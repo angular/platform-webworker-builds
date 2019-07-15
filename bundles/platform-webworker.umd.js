2faf5bcc2ee2e25f4970823d0558b079c8d2cdd5
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 17:00:01 +0000

    7c7774ff2 docs: remove and update resource list (#31469)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+42.sha-0986595.with-local-changes
+ * @license Angular v8.1.0+46.sha-7c7774f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.0+42.sha-0986595.with-local-changes');
+    var VERSION = new core.Version('8.1.0+46.sha-7c7774f.with-local-changes');
 
     /**
      * @license
