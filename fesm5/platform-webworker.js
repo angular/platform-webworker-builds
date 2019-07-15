d180de18154b1839e040169d3dd50f7b9ecd1e5d
Matias Niemelä <matias@yearofmoo.com>
2019-07-15 21:13:04 +0000

    78a809808 docs: include svg files in stackblitz and download examples (#31559)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+48.sha-781cbf8.with-local-changes
+ * @license Angular v8.1.0+49.sha-78a8098.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0+48.sha-781cbf8.with-local-changes');
+var VERSION = new Version('8.1.0+49.sha-78a8098.with-local-changes');
 
 /**
  * @license
