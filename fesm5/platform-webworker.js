4cc524f40afc915ee4f9bdb496ecdd04bb3577a5
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 04:20:30 +0000

    cf460d853 docs: clarify meaning of injectable decorator (#31573)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.0+51.sha-9ac9c84.with-local-changes
+ * @license Angular v8.1.0+54.sha-cf460d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.1.0+51.sha-9ac9c84.with-local-changes');
+var VERSION = new Version('8.1.0+54.sha-cf460d8.with-local-changes');
 
 /**
  * @license
