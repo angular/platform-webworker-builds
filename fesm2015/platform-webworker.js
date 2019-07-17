c6d60e8e3d2ff84407d40c5fdd18b991582d4e37
Matias Niemelä <matias@yearofmoo.com>
2019-07-17 17:45:26 +0000

    32c07ceca docs(core): fix typo in event-binding code example (#31611)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.1+67.sha-12fd069.with-local-changes
+ * @license Angular v8.2.0-next.1+70.sha-32c07ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1593,7 +1593,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.1+67.sha-12fd069.with-local-changes');
+const VERSION = new Version('8.2.0-next.1+70.sha-32c07ce.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
