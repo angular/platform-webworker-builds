d33609783197d55d8542de071be38919c33986b8
Kara Erickson <karakara@google.com>
2019-07-19 23:15:40 +0000

    f14693b9a refactor(ivy): correct circular dependency which causes g3 failures (#31665)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+25.sha-7151eae.with-local-changes
+ * @license Angular v8.2.0-next.2+32.sha-f14693b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.0-next.2+25.sha-7151eae.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.2+32.sha-f14693b.with-local-changes');
 
     /**
      * @license
