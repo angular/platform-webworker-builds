38e497580cd603b454152ff71265a8ebd162e77a
Miško Hevery <misko@hevery.com>
2019-07-19 17:22:37 +0000

    157456b39 ci: add info about `test_saucelabs_bazel` being limited to master builds (#31651)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.1.2+11.sha-b844f5f.with-local-changes
+ * @license Angular v8.1.2+13.sha-157456b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.1.2+11.sha-b844f5f.with-local-changes');
+    var VERSION = new core.Version('8.1.2+13.sha-157456b.with-local-changes');
 
     /**
      * @license
