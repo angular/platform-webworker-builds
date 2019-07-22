68cdabec2a59c74d68b22cea833b2b2dd81080b1
Miško Hevery <misko@hevery.com>
2019-07-22 17:14:45 +0000

    c845a7b88 fix(docs-infra): fix logo to have branded red color (#31663)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+39.sha-b9a94c6.with-local-changes
+ * @license Angular v8.2.0-next.2+42.sha-c845a7b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+39.sha-b9a94c6.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+42.sha-c845a7b.with-local-changes');
 
 /**
  * @license
