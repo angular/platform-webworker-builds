0c4e42ddd6ddc9b0e421d1b5a140fee3581a8449
Kara Erickson <karakara@google.com>
2019-07-22 19:34:14 +0000

    5a8eb924b fix(bazel): increase memory limit of ngc under bazel from 2 to 4 GB (#31784)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+41.sha-8b26447.with-local-changes
+ * @license Angular v8.2.0-next.2+43.sha-5a8eb92.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+41.sha-8b26447.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+43.sha-5a8eb92.with-local-changes');
 
 /**
  * @license
