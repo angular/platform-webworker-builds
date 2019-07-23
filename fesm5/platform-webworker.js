be02c7d322f53bbd540e01ae469bdc5c52b2adce
Kara Erickson <karakara@google.com>
2019-07-23 17:38:45 +0000

    0e68c7edf fix(ivy): support projecting containers created by query reads (#31790)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+32.sha-f14693b.with-local-changes
+ * @license Angular v8.2.0-next.2+46.sha-0e68c7e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+32.sha-f14693b.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+46.sha-0e68c7e.with-local-changes');
 
 /**
  * @license
