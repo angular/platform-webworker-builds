4e31c7347d2208bc02f6d67bd5038c8eac2db54f
Miško Hevery <misko@hevery.com>
2019-07-24 21:55:10 +0000

    87ce4e997 fix(zone.js): zone-mix should import correct browser module (#31628)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+74.sha-29e1c53.with-local-changes
+ * @license Angular v8.2.0-next.2+73.sha-87ce4e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1086,7 +1086,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.0-next.2+74.sha-29e1c53.with-local-changes');
+    var VERSION = new core.Version('8.2.0-next.2+73.sha-87ce4e9.with-local-changes');
 
     /**
      * @license
