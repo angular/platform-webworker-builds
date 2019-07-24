8b50665060b3f4c5f56d4ed3dbe0f458ff82b55a
Misko Hevery <misko@hevery.com>
2019-07-24 04:35:56 +0000

    82055b2fb feat(bazel): compile targets used for indexing by Kythe with Ivy (#31786)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+68.sha-5ae9b76.with-local-changes
+ * @license Angular v8.2.0-next.2+67.sha-82055b2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1022,7 +1022,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+68.sha-5ae9b76.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+67.sha-82055b2.with-local-changes');
 
 /**
  * @license
