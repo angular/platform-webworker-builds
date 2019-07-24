5ad19f07f7550595901910433443131fc46c6ff1
Miško Hevery <misko@hevery.com>
2019-07-24 21:56:37 +0000

    87ce4e997 fix(zone.js): zone-mix should import correct browser module (#31628)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+74.sha-29e1c53.with-local-changes
+ * @license Angular v8.2.0-next.2+73.sha-87ce4e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+74.sha-29e1c53.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+73.sha-87ce4e9.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
