b5916edf5bec7d6abc54f7ad91143965ce06da83
Miško Hevery <misko@hevery.com>
2019-07-25 20:30:52 +0000

    0cd4c019c build: lock material unit tests job to specific commit (#31569)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+86.sha-85d051f.with-local-changes
+ * @license Angular v8.2.0-next.2+88.sha-0cd4c01.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1008,7 +1008,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+86.sha-85d051f.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+88.sha-0cd4c01.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
