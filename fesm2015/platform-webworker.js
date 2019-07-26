d409a5db9ca1d8ec6b2159f09185288b9a9a77a1
Andrew Kushnir <akushnir@google.com>
2019-07-26 22:23:13 +0000

    5f0d5e9cc build: update to nodejs rules 0.34.0 and bazel 0.28.1 (#31824)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+103.sha-5296c04.with-local-changes
+ * @license Angular v8.2.0-next.2+104.sha-5f0d5e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1596,7 +1596,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.2.0-next.2+103.sha-5296c04.with-local-changes');
+const VERSION = new Version('8.2.0-next.2+104.sha-5f0d5e9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
