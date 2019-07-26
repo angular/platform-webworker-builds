634155f0500f650efc337d57bdd82cc67c8fe7e1
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:34:36 +0000

    2647f708b build(docs-infra): upgrade cli command docs sources to 6b6b8f828 (#31856)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+1.sha-fa15814.with-local-changes
+ * @license Angular v8.2.0-rc.0+2.sha-2647f70.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-rc.0+1.sha-fa15814.with-local-changes');
+var VERSION = new Version('8.2.0-rc.0+2.sha-2647f70.with-local-changes');
 
 /**
  * @license
