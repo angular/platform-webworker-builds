8f16e7ec9b5cef00b1c303ce76a2297d173c3e78
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:23:26 +0000

    859ebdd83 fix(ivy): correctly bind `targetToIdentifier` to the TemplateVisitor (#31861)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+97.sha-3067309.with-local-changes
+ * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+97.sha-3067309.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
 
 /**
  * @license
