84800cdc5d7efd1c66a2dac891dfd6c1763485de
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:42:01 +0000

    8c446b05d test(ivy): fix broken testcase in Windows (#31860)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+2.sha-2647f70.with-local-changes
+ * @license Angular v8.2.0-rc.0+3.sha-8c446b0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.0-rc.0+2.sha-2647f70.with-local-changes');
+    var VERSION = new core.Version('8.2.0-rc.0+3.sha-8c446b0.with-local-changes');
 
     /**
      * @license
