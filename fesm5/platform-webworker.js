856147c61e83ece247f8da6676f4d443cc9d1ac3
Andrew Kushnir <akushnir@google.com>
2019-07-26 21:21:47 +0000

    155f40c17 docs: fix header mistake (#31811)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+3.sha-8c446b0.with-local-changes
+ * @license Angular v8.2.0-rc.0+4.sha-155f40c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-rc.0+3.sha-8c446b0.with-local-changes');
+var VERSION = new Version('8.2.0-rc.0+4.sha-155f40c.with-local-changes');
 
 /**
  * @license
