c26c978289f4d605c6856ed9ce68aee884e68428
Andrew Kushnir <akushnir@google.com>
2019-07-26 19:44:29 +0000

    397d0ba9a test(ivy): fix broken testcase in Windows (#31860)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+98.sha-859ebdd.with-local-changes
+ * @license Angular v8.2.0-next.2+99.sha-397d0ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1064,7 +1064,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('8.2.0-next.2+98.sha-859ebdd.with-local-changes');
+    var VERSION = new i0.Version('8.2.0-next.2+99.sha-397d0ba.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
