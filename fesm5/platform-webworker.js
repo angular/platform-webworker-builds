419e71371f43db4e12c1b46e3aac1e9eca22053a
Andrew Kushnir <akushnir@google.com>
2019-07-30 20:22:33 +0000

    185b3dd08 build(docs-infra): update main bundle size (#31839)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-next.2+113.sha-3479fdd.with-local-changes
+ * @license Angular v8.2.0-next.2+125.sha-185b3dd.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0-next.2+113.sha-3479fdd.with-local-changes');
+var VERSION = new Version('8.2.0-next.2+125.sha-185b3dd.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
