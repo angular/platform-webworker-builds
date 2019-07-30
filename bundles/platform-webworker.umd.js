4c94d1cc907b4da81ca4ce07651a9e5d11447b60
Andrew Kushnir <akushnir@google.com>
2019-07-30 22:27:09 +0000

    03e8a31bf Revert "fix(zone.js): don't wrap uncaught promise error. (#31443)" (#31918)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+12.sha-800e90e.with-local-changes
+ * @license Angular v8.2.0-rc.0+13.sha-03e8a31.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.0-rc.0+12.sha-800e90e.with-local-changes');
+    var VERSION = new core.Version('8.2.0-rc.0+13.sha-03e8a31.with-local-changes');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0-rc.0+12.sha-800e90e.with-local-changes
+ * @license Angular v8.2.0-rc.0+13.sha-03e8a31.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1596,7 +1596,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('8.2.0-rc.0+12.sha-800e90e.with-local-changes');
+const VERSION = new Version('8.2.0-rc.0+13.sha-03e8a31.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
