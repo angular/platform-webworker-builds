e24afe7577ccfbf70e41b367deb86ae672472245
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-01 17:31:30 +0000

    c1ae6124c fix(upgrade): compile downgraded components synchronously (if possible) (#31840)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+11.sha-d0d875a.with-local-changes
+ * @license Angular v9.0.0-next.0+8.sha-c1ae612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.0+11.sha-d0d875a.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+8.sha-c1ae612.with-local-changes');
 
     /**
      * @license
