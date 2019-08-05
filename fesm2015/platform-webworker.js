4e8db29e316215f21588bb5a649df689fc8d2db2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:12:06 +0000

    7db269ba6 fix(ivy): ngcc - correctly detect formats processed in previous runs (#32003)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+25.sha-96cbcd6.with-local-changes
+ * @license Angular v9.0.0-next.0+34.sha-7db269b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1596,7 +1596,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.0+25.sha-96cbcd6.with-local-changes');
+const VERSION = new Version('9.0.0-next.0+34.sha-7db269b.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
