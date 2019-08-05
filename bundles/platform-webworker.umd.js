d647961f92c7e52b393442f43b48fe9e32b4a2ad
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-05 20:12:24 +0000

    382d3ed1d fix(docs-infra): ui polish (#31013)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+34.sha-7db269b.with-local-changes
+ * @license Angular v9.0.0-next.0+30.sha-382d3ed.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1089,7 +1089,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.0+34.sha-7db269b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.0+30.sha-382d3ed.with-local-changes');
 
     /**
      * @license
