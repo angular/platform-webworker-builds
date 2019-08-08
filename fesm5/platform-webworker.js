e47e3ca3f3b12b3442a145e508b2a5364133deef
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:36:31 +0000

    f7eebd022 build(docs-infra): upgrade cli command docs sources to bb4be27da (#32036)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.0+53.sha-e5a89e0.with-local-changes
+ * @license Angular v9.0.0-next.0+55.sha-f7eebd0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.0+53.sha-e5a89e0.with-local-changes');
+var VERSION = new Version('9.0.0-next.0+55.sha-f7eebd0.with-local-changes');
 
 /**
  * @license
