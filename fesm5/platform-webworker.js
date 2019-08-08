2c917f2b3985230fdfaed34dc9f3ccd1e09d1e2f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-08-08 18:43:57 +0000

    5bf912599 docs: remove duplicate sentence in toh-pt5 (#32021)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.0+20.sha-bb46277.with-local-changes
+ * @license Angular v8.2.0+22.sha-5bf9125.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.0+20.sha-bb46277.with-local-changes');
+var VERSION = new Version('8.2.0+22.sha-5bf9125.with-local-changes');
 
 /**
  * @license
