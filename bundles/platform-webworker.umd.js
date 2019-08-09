9e49de47f0f788d639e459736db78d24ecd980b3
Kara Erickson <karakara@google.com>
2019-08-09 19:46:56 +0000

    0ddf0c489 fix(compiler): do not remove whitespace wrapping i18n expansions (#31962)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+9.sha-eb5412d.with-local-changes
+ * @license Angular v9.0.0-next.1+11.sha-0ddf0c4.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1064,7 +1064,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.0.0-next.1+9.sha-eb5412d.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.1+11.sha-0ddf0c4.with-local-changes');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
