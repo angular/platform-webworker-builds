b0934a705487f19cec6bdcd093bcad4c107ce681
Kara Erickson <karakara@google.com>
2019-08-12 17:55:07 +0000

    6eb9c2fab perf(ivy): don't read global state when interpolated values don't change (#32093)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.1+21.sha-7b94145.with-local-changes
+ * @license Angular v9.0.0-next.1+22.sha-6eb9c2f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.1+21.sha-7b94145.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.1+22.sha-6eb9c2f.with-local-changes');
 
     /**
      * @license
