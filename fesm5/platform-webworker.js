d07b60ece4795bc0470c01a4d1a4e28e25d16182
Kara Erickson <karakara@google.com>
2019-08-13 17:16:25 +0000

    dec7e5286 ci: use local strategy for AngularTemplateCompile and TypescriptCompile on CI (#32112)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+2.sha-18bac15.with-local-changes
+ * @license Angular v8.2.2+4.sha-dec7e52.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+2.sha-18bac15.with-local-changes');
+var VERSION = new Version('8.2.2+4.sha-dec7e52.with-local-changes');
 
 /**
  * @license
