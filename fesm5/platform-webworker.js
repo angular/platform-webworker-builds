7cf710729c44c9b31b8cd5f59da63208bc50d33a
Kara Erickson <karakara@google.com>
2019-08-13 18:56:09 +0000

    dc76c14e3 docs: edit location doc (#32042)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+3.sha-2f812f3.with-local-changes
+ * @license Angular v8.2.2+7.sha-dc76c14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+3.sha-2f812f3.with-local-changes');
+var VERSION = new Version('8.2.2+7.sha-dc76c14.with-local-changes');
 
 /**
  * @license
