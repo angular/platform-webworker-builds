3e6c31fed6ffa550d1cffaeab418c4e71ebfd5b7
Kara Erickson <karakara@google.com>
2019-08-13 19:00:38 +0000

    0ffc1d0d2 docs: update marketing resources with Angular UI Toolkit (#31969)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+7.sha-dc76c14.with-local-changes
+ * @license Angular v8.2.2+6.sha-0ffc1d0.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+7.sha-dc76c14.with-local-changes');
+var VERSION = new Version('8.2.2+6.sha-0ffc1d0.with-local-changes');
 
 /**
  * @license
