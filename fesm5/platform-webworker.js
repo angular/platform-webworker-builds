5636a4e019ac0ae25261492fe3c030c5fd561315
Kara Erickson <karakara@google.com>
2019-08-13 17:13:05 +0000

    ebcf79d7f docs(zone.js): update DEVELOPER.md for changelog instruction (#32016)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+2.sha-48487cb.with-local-changes
+ * @license Angular v9.0.0-next.2+4.sha-ebcf79d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.2+2.sha-48487cb.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+4.sha-ebcf79d.with-local-changes');
 
 /**
  * @license
