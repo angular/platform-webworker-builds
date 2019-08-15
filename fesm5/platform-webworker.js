fd015de1bbc6ac950fd7aeaa8eaa8a854af24df2
Andrew Kushnir <akushnir@google.com>
2019-08-15 20:03:41 +0000

    6f2e8afae docs: fix typo of Typescript to TypeScript (#32153)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+17.sha-adc869f.with-local-changes
+ * @license Angular v8.2.2+19.sha-6f2e8af.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+17.sha-adc869f.with-local-changes');
+var VERSION = new Version('8.2.2+19.sha-6f2e8af.with-local-changes');
 
 /**
  * @license
