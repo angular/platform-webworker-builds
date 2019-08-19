8f99a8abce14af1f98fef95765c390122c59a183
Andrew Kushnir <akushnir@google.com>
2019-08-19 17:33:23 +0000

    789dd6a0d docs(service-worker): mention that dataGroups only cache non-mutating requests (#32142)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+21.sha-e5b18e8.with-local-changes
+ * @license Angular v8.2.2+22.sha-789dd6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+21.sha-e5b18e8.with-local-changes');
+var VERSION = new Version('8.2.2+22.sha-789dd6a.with-local-changes');
 
 /**
  * @license
