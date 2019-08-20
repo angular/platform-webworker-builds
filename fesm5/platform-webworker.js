ebcb4837dd8c1db3cbce79a90edb4fae3799ab8e
Andrew Kushnir <akushnir@google.com>
2019-08-20 17:15:56 +0000

    af170d2ae ci: exclude the upstream g3 branch from building on CI (#32202)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.2+24.sha-a01ed0d.with-local-changes
+ * @license Angular v8.2.2+25.sha-af170d2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.2+24.sha-a01ed0d.with-local-changes');
+var VERSION = new Version('8.2.2+25.sha-af170d2.with-local-changes');
 
 /**
  * @license
