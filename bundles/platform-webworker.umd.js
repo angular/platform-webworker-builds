65108afa5361c97d57632a48719b61e83d81fabf
Andrew Kushnir <akushnir@google.com>
2019-08-21 00:09:52 +0000

    10ea3a9f4 docs: change to global approvers (#31940)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+73.sha-ec4381d.with-local-changes
+ * @license Angular v9.0.0-next.2+74.sha-10ea3a9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.2+73.sha-ec4381d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.2+74.sha-10ea3a9.with-local-changes');
 
     /**
      * @license
