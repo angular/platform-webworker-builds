36cb2790fda71f27d5349c53985979742befc397
Andrew Kushnir <akushnir@google.com>
2019-08-21 17:24:06 +0000

    424ab4867 fix(compiler): return enableIvy true when using `readConfiguration` (#32234)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.2+77.sha-62f4140.with-local-changes
+ * @license Angular v9.0.0-next.2+78.sha-424ab48.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.2+77.sha-62f4140.with-local-changes');
+var VERSION = new Version('9.0.0-next.2+78.sha-424ab48.with-local-changes');
 
 /**
  * @license
