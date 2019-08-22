f03340922f2df860f834e755b337efdd8f0b8df6
atscott <atscott@google.com>
2019-08-22 14:02:08 +0000

    21edc6a82 docs: update http guide (#32045)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+8.sha-b6fa929.with-local-changes
+ * @license Angular v9.0.0-next.3+7.sha-21edc6a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2064,7 +2064,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.3+8.sha-b6fa929.with-local-changes');
+const VERSION = new Version('9.0.0-next.3+7.sha-21edc6a.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
