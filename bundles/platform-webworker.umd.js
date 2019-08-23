bbae5f2667830f41b97cc2407f96c70a053d9daf
atscott <atscott@google.com>
2019-08-23 18:32:32 +0000

    5e0890b3a build(docs-infra): upgrade cli command docs sources to 166cd1501 (#32286)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+11.sha-43bac21.with-local-changes
+ * @license Angular v8.2.3+12.sha-5e0890b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.3+11.sha-43bac21.with-local-changes');
+    var VERSION = new core.Version('8.2.3+12.sha-5e0890b.with-local-changes');
 
     /**
      * @license
