83339bf86ff035a03a5e828363f25d9f446312f0
Andrew Scott <atscott01@gmail.com>
2019-08-23 16:29:34 +0000

    43bac2130 docs: fix http example to use correct property (#32288)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+10.sha-4df4150.with-local-changes
+ * @license Angular v8.2.3+11.sha-43bac21.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.3+10.sha-4df4150.with-local-changes');
+var VERSION = new Version('8.2.3+11.sha-43bac21.with-local-changes');
 
 /**
  * @license
