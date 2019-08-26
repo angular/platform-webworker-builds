283c3f42ff629e0ddb6359df3d6cafba1014b790
atscott <atscott@google.com>
2019-08-26 22:23:19 +0000

    3a4839c97 docs: reference articles specifically (#32221)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+43.sha-e63a7b0.with-local-changes
+ * @license Angular v9.0.0-next.3+45.sha-3a4839c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.3+43.sha-e63a7b0.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.3+45.sha-3a4839c.with-local-changes');
 
     /**
      * @license
