82f61a41cdd4a869f2342b961f6d90c722c5bc45
atscott <atscott@google.com>
2019-08-27 16:30:02 +0000

    f57c17de2 build: remove the deprecated `aio` commit message scope (#32341)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+49.sha-c7c7f9f.with-local-changes
+ * @license Angular v9.0.0-next.3+47.sha-f57c17d.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.3+49.sha-c7c7f9f.with-local-changes');
+var VERSION = new Version('9.0.0-next.3+47.sha-f57c17d.with-local-changes');
 
 var MessageBasedPlatformLocation = /** @class */ (function () {
     function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
