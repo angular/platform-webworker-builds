69edc3e6096cbda0f80cb7771dc15520f5061a28
atscott <atscott@google.com>
2019-08-27 16:31:12 +0000

    55eaa5fb6 docs: Use ngOnInit to fetch data from services in the getting started guide (#32273)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.3+50.sha-70cf8ed.with-local-changes
+ * @license Angular v9.0.0-next.3+51.sha-55eaa5f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.3+50.sha-70cf8ed.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.3+51.sha-55eaa5f.with-local-changes');
 
     /**
      * @license
