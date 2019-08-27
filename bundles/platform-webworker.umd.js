85acf93551a72a747e47cd102c17deeec79af948
atscott <atscott@google.com>
2019-08-27 16:25:27 +0000

    73dd43170 build: remove the deprecated `aio` commit message scope (#32341)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+20.sha-514271b.with-local-changes
+ * @license Angular v8.2.3+21.sha-73dd431.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.3+20.sha-514271b.with-local-changes');
+    var VERSION = new core.Version('8.2.3+21.sha-73dd431.with-local-changes');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+20.sha-514271b.with-local-changes
+ * @license Angular v8.2.3+21.sha-73dd431.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.3+20.sha-514271b.with-local-changes');
+var VERSION = new Version('8.2.3+21.sha-73dd431.with-local-changes');
 
 /**
  * @license
