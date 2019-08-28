00672a0ae00bee12ea7fe7c316571f742a08d203
Andrew Scott <atscott01@gmail.com>
2019-08-28 19:26:55 +0000

    261593aab docs: update release notes for 8.2.4 to indicate a docs only release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.3+35.sha-b7f95be.with-local-changes
+ * @license Angular v8.2.4+1.sha-261593a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.3+35.sha-b7f95be.with-local-changes');
+var VERSION = new Version('8.2.4+1.sha-261593a.with-local-changes');
 
 /**
  * @license
