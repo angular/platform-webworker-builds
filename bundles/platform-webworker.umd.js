aba49253a41c88f021754b2b09362c2aa74c6fe6
Miško Hevery <misko@hevery.com>
2019-08-29 00:45:25 +0000

    97fc45f32 fix(language-service): Invalidate Reflector caches when program changes (#32357)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+19.sha-d4703d9.with-local-changes
+ * @license Angular v9.0.0-next.4+17.sha-97fc45f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.4+19.sha-d4703d9.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.4+17.sha-97fc45f.with-local-changes');
 
     /**
      * @license
