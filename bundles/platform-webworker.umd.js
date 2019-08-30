7bce4318b68b20cdd05d0609b3c9afab53337a1d
Misko Hevery <misko@hevery.com>
2019-08-30 20:06:45 +0000

    c8b065524 refactor(ivy): cleanup di tests from render3 (#32165)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+55.sha-d498314.with-local-changes
+ * @license Angular v9.0.0-next.4+60.sha-c8b0655.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.4+55.sha-d498314.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.4+60.sha-c8b0655.with-local-changes');
 
     /**
      * @license
