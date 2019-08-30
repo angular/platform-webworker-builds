c79bf91deaf6ed3146ca7c90e5e68e320db403b8
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:12:44 +0000

    3a45bef0e docs: add clarification regarding development in older browsers in Deployment guide (#32210)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
+ * @license Angular v8.2.4+8.sha-3a45bef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.4+5.sha-9d2348b.with-local-changes');
+    var VERSION = new core.Version('8.2.4+8.sha-3a45bef.with-local-changes');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+5.sha-9d2348b.with-local-changes
+ * @license Angular v8.2.4+8.sha-3a45bef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.4+5.sha-9d2348b.with-local-changes');
+var VERSION = new Version('8.2.4+8.sha-3a45bef.with-local-changes');
 
 /**
  * @license
