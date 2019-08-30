c14b097f2613add9468a68a52a7710966ff18b7c
Matias Niemelä <matias@yearofmoo.com>
2019-08-30 18:59:21 +0000

    43619fc84 revert: docs: create Issue and Pull Request markdown doc, explaining automatic locking policy (#32405)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.4+51.sha-ad13520.with-local-changes
+ * @license Angular v9.0.0-next.4+52.sha-43619fc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.4+51.sha-ad13520.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.4+52.sha-43619fc.with-local-changes');
 
     /**
      * @license
