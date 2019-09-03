1accd6e973104eafc02479682f7610794e95aab3
Miško Hevery <misko@hevery.com>
2019-09-03 19:20:09 +0000

    2cd5d4c64 docs: correct @ngtools/webpack url (#32407)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+15.sha-3248fe8.with-local-changes
+ * @license Angular v8.2.4+16.sha-2cd5d4c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.4+15.sha-3248fe8.with-local-changes');
+var VERSION = new Version('8.2.4+16.sha-2cd5d4c.with-local-changes');
 
 /**
  * @license
