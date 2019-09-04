428ccae525fe6636b6222cb65c86e0b2f91b00e5
Miško Hevery <misko@hevery.com>
2019-09-04 19:08:38 +0000

    5112669d2 refactor(benchpress): remove two mutable exports from largetable/util.ts (#32425)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.4+18.sha-3d453fe.with-local-changes
+ * @license Angular v8.2.4+19.sha-5112669.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.4+18.sha-3d453fe.with-local-changes');
+var VERSION = new Version('8.2.4+19.sha-5112669.with-local-changes');
 
 /**
  * @license
