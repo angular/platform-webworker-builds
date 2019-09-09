bc7270a2849547a2162ec25ffe102378f4d04841
Matias Niemelä <matias@yearofmoo.com>
2019-09-09 16:41:08 +0000

    b5eda603a ci: work around `CIRCLE_COMPARE_URL` not being available wih CircleCI Pipelines (#32537)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+28.sha-a65d3fa.with-local-changes
+ * @license Angular v9.0.0-next.5+27.sha-b5eda60.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.5+28.sha-a65d3fa.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.5+27.sha-b5eda60.with-local-changes');
 
     /**
      * @license
