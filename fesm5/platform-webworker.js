6e21616c4557617192d5f0b651ebb4d1e14f41cc
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 17:30:05 +0000

    6e380cff8 ci: update i18n CODEOWNERS for @angular/localize package (#32570)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+15.sha-76a8470.with-local-changes
+ * @license Angular v8.2.5+16.sha-6e380cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.5+15.sha-76a8470.with-local-changes');
+var VERSION = new Version('8.2.5+16.sha-6e380cf.with-local-changes');
 
 /**
  * @license
