306498925f50af4b438649c7b9b7ebdb8881fcb0
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 21:43:29 +0000

    79b673a17 docs: add mdb angular boilerplate to docs resources (#30448)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.5+68.sha-3b37469.with-local-changes
+ * @license Angular v9.0.0-next.5+69.sha-79b673a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.5+68.sha-3b37469.with-local-changes');
+var VERSION = new Version('9.0.0-next.5+69.sha-79b673a.with-local-changes');
 
 /**
  * @license
