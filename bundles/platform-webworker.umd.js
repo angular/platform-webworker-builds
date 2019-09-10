f48c345c232822079fdfadf1600334dea496080c
Matias Niemelä <matias@yearofmoo.com>
2019-09-10 21:47:26 +0000

    2565f6795 docs: add mdb angular boilerplate to docs resources (#30448)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.5+17.sha-38f4dcd.with-local-changes
+ * @license Angular v8.2.5+18.sha-2565f67.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.5+17.sha-38f4dcd.with-local-changes');
+    var VERSION = new core.Version('8.2.5+18.sha-2565f67.with-local-changes');
 
     /**
      * @license
