e9aed1677587b3871bbcdfd023393b4d98b992df
Matias Niemelä <matias@yearofmoo.com>
2019-09-11 21:03:03 +0000

    bb9e61202 docs: remove duplicate entry in changelog for 8.2.6

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+2.sha-c1ac21d.with-local-changes
+ * @license Angular v9.0.0-next.6+3.sha-bb9e612.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.6+2.sha-c1ac21d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.6+3.sha-bb9e612.with-local-changes');
 
     /**
      * @license
