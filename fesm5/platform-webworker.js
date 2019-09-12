66ad36ad99aa9bcd884ed22b206951d00c381d54
Kara Erickson <karakara@google.com>
2019-09-12 18:57:01 +0000

    88c28ce20 refactor(ivy): migrate debug spec from render3 (#32621)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+32.sha-e013aee.with-local-changes
+ * @license Angular v9.0.0-next.6+33.sha-88c28ce.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.6+32.sha-e013aee.with-local-changes');
+var VERSION = new Version('9.0.0-next.6+33.sha-88c28ce.with-local-changes');
 
 /**
  * @license
