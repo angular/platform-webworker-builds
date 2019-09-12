632b554e6c80dc416536f5b50eb8ef8fb9ae3bee
Kara Erickson <karakara@google.com>
2019-09-12 17:52:00 +0000

    1771d6ff2 fix(language-service): Lazily instantiate MetadataResolver (#32631)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.6+27.sha-527ce3b.with-local-changes
+ * @license Angular v9.0.0-next.6+21.sha-1771d6f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.6+27.sha-527ce3b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.6+21.sha-1771d6f.with-local-changes');
 
     /**
      * @license
