b1474bdd5c8e20d04bfa8b3921df5bcba9e64b27
Andrew Kushnir <akushnir@google.com>
2019-09-25 00:22:04 +0000

    948714c17 revert: refactor(ivy): ensure `StylingDebug` instances provide context debug info (#32753) (#32841)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.7+22.sha-3a96631.with-local-changes
+ * @license Angular v9.0.0-next.7+24.sha-948714c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1011,7 +1011,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.7+22.sha-3a96631.with-local-changes');
+var VERSION = new Version('9.0.0-next.7+24.sha-948714c.with-local-changes');
 
 /**
  * @license
