2539b2cc326fdbdaabae178a34296fa695e00c01
atscott <atscott@google.com>
2019-09-27 00:27:23 +0000

    e81fc14da fix(docs-infra): correctly run on IE11 (#32871)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+2.sha-d5c210a.with-local-changes
+ * @license Angular v8.2.8+3.sha-e81fc14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.8+2.sha-d5c210a.with-local-changes');
+    var VERSION = new core.Version('8.2.8+3.sha-e81fc14.with-local-changes');
 
     /**
      * @license
