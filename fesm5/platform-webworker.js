538de80ac6ed49c76f3a6fed1712721fb7f68739
atscott <atscott@google.com>
2019-09-27 23:19:00 +0000

    bdea243fe docs: add missing space between "Component" and "will" in guide (#32885)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.8+8.sha-2397144.with-local-changes
+ * @license Angular v8.2.8+9.sha-bdea243.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.8+8.sha-2397144.with-local-changes');
+var VERSION = new Version('8.2.8+9.sha-bdea243.with-local-changes');
 
 /**
  * @license
