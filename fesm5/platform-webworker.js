9b7ff2d905e73a4647fee56419a69c00c64b5541
atscott <atscott01@google.com>
2019-10-02 16:58:18 +0000

    25219baeb feat(core): make static query flag optional (#32686)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.8+44.sha-5d5c94d.with-local-changes
+ * @license Angular v9.0.0-next.8+52.sha-25219ba.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.8+44.sha-5d5c94d.with-local-changes');
+var VERSION = new Version('9.0.0-next.8+52.sha-25219ba.with-local-changes');
 
 /**
  * @license
