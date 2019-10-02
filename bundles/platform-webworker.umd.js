84a821266af03ed5510228baa58276d52b2b7004
atscott <atscott01@gmail.com>
2019-10-02 22:38:26 +0000

    6f5f481fd refactor(core): undecorated-classes-with-di migration should ignore referenced resources (#32953)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+17.sha-90dda58.with-local-changes
+ * @license Angular v9.0.0-next.9+18.sha-6f5f481.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1206,7 +1206,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.0.0-next.9+17.sha-90dda58.with-local-changes');
+    var VERSION = new i0.Version('9.0.0-next.9+18.sha-6f5f481.with-local-changes');
 
     /**
      * @license
