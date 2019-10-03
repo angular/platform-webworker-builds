41439245dc5d57d85273c8da523156d233c19c8a
atscott <atscott01@gmail.com>
2019-10-03 16:35:50 +0000

    5332b04f3 build: TypeScript 3.6 compatibility. (#32908)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+18.sha-6f5f481.with-local-changes
+ * @license Angular v9.0.0-next.9+19.sha-5332b04.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1018,7 +1018,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.9+18.sha-6f5f481.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+19.sha-5332b04.with-local-changes');
 
 /**
  * @license
