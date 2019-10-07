2432e404820536372bc1e1f48fc83a4233b69835
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-07 17:45:33 +0000

    bad343433 fix(ivy): avoid exposing `ng` with Closure Compiler enhanced optimizations (#33010)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+52.sha-d18289f.with-local-changes
+ * @license Angular v9.0.0-next.9+51.sha-bad3434.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.9+52.sha-d18289f.with-local-changes');
+var VERSION = new Version('9.0.0-next.9+51.sha-bad3434.with-local-changes');
 
 /**
  * @license
