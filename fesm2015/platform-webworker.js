edb49271fe25e1c89eef58df2f04c732734bdde0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-10-08 20:25:19 +0000

    9f0c549bc perf(ivy): avoid memory allocation in the isAnimationProp check (#32997)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.9+73.sha-ed71141.with-local-changes
+ * @license Angular v9.0.0-next.9+75.sha-9f0c549.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2043,7 +2043,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.9+73.sha-ed71141.with-local-changes');
+const VERSION = new Version('9.0.0-next.9+75.sha-9f0c549.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
