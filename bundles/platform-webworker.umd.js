2f1d7ce56dbe0921ef847f96624538b02bdaf71f
Miško Hevery <misko@hevery.com>
2019-10-10 21:36:38 +0000

    90007e97c feat(ngcc): support version ranges in project/default configurations (#33008)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+25.sha-df78d7c.with-local-changes
+ * @license Angular v9.0.0-next.10+24.sha-90007e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.10+25.sha-df78d7c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.10+24.sha-90007e9.with-local-changes');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+25.sha-df78d7c.with-local-changes
+ * @license Angular v9.0.0-next.10+24.sha-90007e9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1959,7 +1959,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.10+25.sha-df78d7c.with-local-changes');
+const VERSION = new Version('9.0.0-next.10+24.sha-90007e9.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
