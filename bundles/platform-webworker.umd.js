27e33d44afbe104b75d63f963e08aac3e334a30d
Miško Hevery <misko@hevery.com>
2019-10-11 19:25:32 +0000

    0c69ec20c fix: google3 sync which requires type hints (#33108)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+35.sha-0dcff40.with-local-changes
+ * @license Angular v9.0.0-next.10+36.sha-0c69ec2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.10+35.sha-0dcff40.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.10+36.sha-0c69ec2.with-local-changes');
 
     /**
      * @license
