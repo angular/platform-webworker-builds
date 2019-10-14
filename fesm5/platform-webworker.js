eda27adf5c7a1a1b336bc5f75d107a622133a6da
Miško Hevery <misko@hevery.com>
2019-10-14 23:59:45 +0000

    64aae3a9d test(language-service): test project cleanup (#33157)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+79.sha-10dcbcf.with-local-changes
+ * @license Angular v9.0.0-next.10+77.sha-64aae3a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.10+79.sha-10dcbcf.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+77.sha-64aae3a.with-local-changes');
 
 /**
  * @license
