19c58b50bb1452c07a04537d5db8cc6d7e0f2b70
Miško Hevery <misko@hevery.com>
2019-10-14 16:44:37 +0000

    d8249d123 feat(ivy): better error messages for unknown components (#33064)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+42.sha-64fd0d6.with-local-changes
+ * @license Angular v9.0.0-next.10+44.sha-d8249d1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.10+42.sha-64fd0d6.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+44.sha-d8249d1.with-local-changes');
 
 /**
  * @license
