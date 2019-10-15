e58f64cdba917fa6859a984d60a0240f5de4d908
Miško Hevery <misko@hevery.com>
2019-10-15 21:17:20 +0000

    ec6a9f2a0 docs: add postinstall script removal to Ivy guide (#32999)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.10+84.sha-8b0cb2f.with-local-changes
+ * @license Angular v9.0.0-next.10+93.sha-ec6a9f2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.10+84.sha-8b0cb2f.with-local-changes');
+var VERSION = new Version('9.0.0-next.10+93.sha-ec6a9f2.with-local-changes');
 
 /**
  * @license
