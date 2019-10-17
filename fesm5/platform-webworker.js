034d11e0d3549fcd9034e8053330d9c7f5c51569
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:37:23 +0000

    9d54679e6 test: clean up explicit dynamic query usages (#33015)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+42.sha-7e64bbe.with-local-changes
+ * @license Angular v9.0.0-next.11+43.sha-9d54679.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.11+42.sha-7e64bbe.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+43.sha-9d54679.with-local-changes');
 
 /**
  * @license
