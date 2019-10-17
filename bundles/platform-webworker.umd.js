f380a22d1d5df00732de305bb95f4e1b83c5bf75
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 23:03:03 +0000

    422eb14dc build: remove vendored Babel typings (#33226)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+46.sha-7b64680.with-local-changes
+ * @license Angular v9.0.0-next.11+47.sha-422eb14.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.11+46.sha-7b64680.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+47.sha-422eb14.with-local-changes');
 
     /**
      * @license
