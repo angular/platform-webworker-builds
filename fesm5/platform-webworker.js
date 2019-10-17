c0499076fc0eab99c8d4a8968455454bd6e357c6
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 23:10:01 +0000

    fd4fed14d fix(compiler): absolute source span for template attribute expressions (#33189)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+47.sha-422eb14.with-local-changes
+ * @license Angular v9.0.0-next.11+48.sha-fd4fed1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.11+47.sha-422eb14.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+48.sha-fd4fed1.with-local-changes');
 
 /**
  * @license
