f398746070c0d8eb002da6c54cde4e5f1b8db67b
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:32:57 +0000

    0e260d2c3 style: enforce disallowance of String, Number and Boolean constructors (#33210)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+33.sha-d1323b5.with-local-changes
+ * @license Angular v9.0.0-next.11+26.sha-0e260d2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.11+33.sha-d1323b5.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+26.sha-0e260d2.with-local-changes');
 
     /**
      * @license
