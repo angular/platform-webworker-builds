4ff9c4b7e67ce5bb591768e0ba54ef03ecb47a65
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:33:01 +0000

    cc7c2a81d build: update webdriver-manager to support latest Chrome (#33206)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+25.sha-3dc6490.with-local-changes
+ * @license Angular v9.0.0-next.11+30.sha-cc7c2a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.11+25.sha-3dc6490.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+30.sha-cc7c2a8.with-local-changes');
 
 /**
  * @license
