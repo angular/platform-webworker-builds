dd455e935c66748d14ce92aa94263aa14a97f926
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 18:34:53 +0000

    4800fa1c0 refactor(core): tweek micro-benchmarks to make them more consistent (#33207)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+26.sha-0e260d2.with-local-changes
+ * @license Angular v9.0.0-next.11+31.sha-4800fa1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.11+26.sha-0e260d2.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+31.sha-4800fa1.with-local-changes');
 
     /**
      * @license
