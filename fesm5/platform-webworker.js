ad2fd40e6c8c9e27524302a9e769ed577b3a6955
Matias Niemelä <matias@yearofmoo.com>
2019-10-17 20:24:27 +0000

    bb53b6549 refactor(ivy): move all of the instruction state into a singe object (#33093)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+40.sha-78214e7.with-local-changes
+ * @license Angular v9.0.0-next.11+39.sha-bb53b65.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.11+40.sha-78214e7.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+39.sha-bb53b65.with-local-changes');
 
 /**
  * @license
