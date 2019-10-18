322cd67c793869f73222a132115003675115e058
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:09:18 +0000

    e0059c7d51 refactor: format files (#31248)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+65.sha-6958d11.with-local-changes
+ * @license Angular v9.0.0-next.11+74.sha-e0059c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.11+65.sha-6958d11.with-local-changes');
+var VERSION = new Version('9.0.0-next.11+74.sha-e0059c7.with-local-changes');
 
 /**
  * @license
