0888d8e6b33794c2320f6b830eba760daa391154
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 01:02:25 +0000

    9e7668f16 fix(common): remove deprecated support for intl API (#29250)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+54.sha-5dfbcd5.with-local-changes
+ * @license Angular v9.0.0-next.11+55.sha-9e7668f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.11+54.sha-5dfbcd5.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+55.sha-9e7668f.with-local-changes');
 
     /**
      * @license
