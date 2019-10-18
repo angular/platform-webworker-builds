2390671bccc0d042ca7d99551d5fb2c62f8fe74b
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 20:53:36 +0000

    1799f621b7 refactor(core): deprecate entryComponents (#33205)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12.with-local-changes
+ * @license Angular v9.0.0-next.12+1.sha-1799f62.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.12.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+1.sha-1799f62.with-local-changes');
 
 /**
  * @license
