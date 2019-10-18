8c2da76d976aada5011991276de5e564393b9361
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 00:01:04 +0000

    de445709d fix(ivy): use ReflectionHost to check exports when writing an import (#33192)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+48.sha-fd4fed1.with-local-changes
+ * @license Angular v9.0.0-next.11+53.sha-de44570.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1195,7 +1195,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.11+48.sha-fd4fed1.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+53.sha-de44570.with-local-changes');
 
     /**
      * @license
