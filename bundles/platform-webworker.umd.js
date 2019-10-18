417a83a324d48a4bc773735e3dee91d9d5766f57
Matias Niemelä <matias@yearofmoo.com>
2019-10-18 19:15:59 +0000

    0e08ad628a fix(ivy): throw better error for missing generic type in ModuleWithProviders (#33187)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.11+74.sha-e0059c7.with-local-changes
+ * @license Angular v9.0.0-next.11+75.sha-0e08ad6.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.11+74.sha-e0059c7.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.11+75.sha-0e08ad6.with-local-changes');
 
     /**
      * @license
