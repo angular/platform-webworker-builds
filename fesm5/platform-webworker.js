b84064cb603409ed5cb64617506e00f9b33a2f7b
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:52:53 +0000

    d7dc6cbc04 refactor(compiler-cli): remove unused method `FileSystem#mkdir()` (#33237)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+9.sha-755a80c.with-local-changes
+ * @license Angular v9.0.0-next.12+11.sha-d7dc6cb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.12+9.sha-755a80c.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+11.sha-d7dc6cb.with-local-changes');
 
 /**
  * @license
