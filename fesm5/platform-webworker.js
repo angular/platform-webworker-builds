e8b722806bde8aa20b00ec7e357e9203d04667a5
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 20:15:42 +0000

    418e9cf3c4 build: address comments, restructure setup-rbe.sh script (#33109)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+24.sha-9de4b1c.with-local-changes
+ * @license Angular v9.0.0-next.12+31.sha-418e9cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.12+24.sha-9de4b1c.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+31.sha-418e9cf.with-local-changes');
 
 /**
  * @license
