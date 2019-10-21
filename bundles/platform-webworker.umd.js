dba3ab13afb5f7e518a100be0c750f5c21d085a2
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 17:09:16 +0000

    e5081bcf25 build: update the integration/payloads limit

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+11.sha-d7dc6cb.with-local-changes
+ * @license Angular v9.0.0-next.12+17.sha-e5081bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.12+11.sha-d7dc6cb.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.12+17.sha-e5081bc.with-local-changes');
 
     /**
      * @license
