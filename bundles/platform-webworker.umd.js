062dead7b6217feb503b289f44c6f1d778d49878
Matias Niemelä <matias@yearofmoo.com>
2019-10-21 15:46:06 +0000

    755a80c7ec docs: update universal docs for new ivy implementation (#33040)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+7.sha-d4db746.with-local-changes
+ * @license Angular v9.0.0-next.12+9.sha-755a80c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.12+7.sha-d4db746.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.12+9.sha-755a80c.with-local-changes');
 
     /**
      * @license
