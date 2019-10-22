c40be9ff0b796dcb948b608d2184896531212e6e
Matias Niemelä <matias@yearofmoo.com>
2019-10-22 17:26:03 +0000

    a86a179f45 build: use http caching on windows CI runs (#33298)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+34.sha-ec482da.with-local-changes
+ * @license Angular v9.0.0-next.12+35.sha-a86a179.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.12+34.sha-ec482da.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+35.sha-a86a179.with-local-changes');
 
 /**
  * @license
