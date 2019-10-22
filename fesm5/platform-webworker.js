add5bee139450d72ebe7c5936286d8ffbaba7777
Matias Niemelä <matias@yearofmoo.com>
2019-10-22 18:39:01 +0000

    7f7dc7c294 perf(ivy): avoid unnecessary i18n pass while processing a template (#33284)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.12+45.sha-5d86e4a.with-local-changes
+ * @license Angular v9.0.0-next.12+46.sha-7f7dc7c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.12+45.sha-5d86e4a.with-local-changes');
+var VERSION = new Version('9.0.0-next.12+46.sha-7f7dc7c.with-local-changes');
 
 /**
  * @license
