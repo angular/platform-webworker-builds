c9a516699f91fd50c9363254d4428f092981c26b
Andrew Kushnir <akushnir@google.com>
2019-10-24 18:30:53 +0000

    9a5e08f2a7 test(ivy): add styling [class]  benchmarks (#33375)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+16.sha-25ed82d.with-local-changes
+ * @license Angular v9.0.0-next.13+18.sha-9a5e08f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.13+16.sha-25ed82d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.13+18.sha-9a5e08f.with-local-changes');
 
     /**
      * @license
