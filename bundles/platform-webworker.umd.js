dcc6adf5bba5ccbccf3e2352fd078a4cc1a53024
Andrew Kushnir <akushnir@google.com>
2019-10-24 23:34:08 +0000

    0d9be22023 feat(ivy): strictness flags for template type checking (#33365)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+28.sha-4aa51b7.with-local-changes
+ * @license Angular v9.0.0-next.13+31.sha-0d9be22.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-next.13+28.sha-4aa51b7.with-local-changes');
+    var VERSION = new core.Version('9.0.0-next.13+31.sha-0d9be22.with-local-changes');
 
     /**
      * @license
