9cb79b777865c3c35df5662caf47b67c5ca00831
Andrew Kushnir <akushnir@google.com>
2019-10-25 20:18:19 +0000

    3505692f75 docs: add @next to update instructions (#33410)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+52.sha-8d15bfa.with-local-changes
+ * @license Angular v9.0.0-next.13+53.sha-3505692.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.13+52.sha-8d15bfa.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+53.sha-3505692.with-local-changes');
 
 /**
  * @license
