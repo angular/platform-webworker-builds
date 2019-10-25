5ac57a9b73fbb42ebaa38f04d521fefdd5d49588
Andrew Kushnir <akushnir@google.com>
2019-10-25 00:59:53 +0000

    dcdb433b7d perf(ivy): apply [style]/[class] bindings directly to style/className (#33336)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.13+34.sha-ee4fc12.with-local-changes
+ * @license Angular v9.0.0-next.13+35.sha-dcdb433.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.13+34.sha-ee4fc12.with-local-changes');
+var VERSION = new Version('9.0.0-next.13+35.sha-dcdb433.with-local-changes');
 
 /**
  * @license
