51e9cdde8c667e56a2db45f5a8d5e790fc214f41
Andrew Kushnir <akushnir@google.com>
2019-10-28 20:49:30 +0000

    e483acaa17 fix(ivy): ensure overrides for 'multi: true' only appear once in final providers (#33104)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+8.sha-c61f413.with-local-changes
+ * @license Angular v9.0.0-next.14+10.sha-e483aca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1959,7 +1959,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.14+8.sha-c61f413.with-local-changes');
+const VERSION = new Version('9.0.0-next.14+10.sha-e483aca.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
