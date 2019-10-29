d95c28e37ae6a9c73078a95c9cd62f6b0b5a52f2
Andrew Kushnir <akushnir@google.com>
2019-10-29 21:35:27 +0000

    1fc778644a build: remove --nowatchfs flag as --watchfs is a noop on windows now (#33471)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.12+5.sha-f97c464.with-local-changes
+ * @license Angular v8.2.12+7.sha-1fc7786.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1025,7 +1025,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('8.2.12+5.sha-f97c464.with-local-changes');
+var VERSION = new Version('8.2.12+7.sha-1fc7786.with-local-changes');
 
 /**
  * @license
