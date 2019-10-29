9a7de70bf38ecb980c86d6ef66981b34d6d72f5e
Andrew Kushnir <akushnir@google.com>
2019-10-29 19:03:11 +0000

    afa50838a7 docs: update instructions for v9 to reflect cli update (#33473)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+16.sha-27745c5.with-local-changes
+ * @license Angular v9.0.0-next.14+18.sha-afa5083.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2043,7 +2043,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-next.14+16.sha-27745c5.with-local-changes');
+const VERSION = new Version('9.0.0-next.14+18.sha-afa5083.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
