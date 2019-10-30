832a3c23bc7bde067043ec8aa4ac40525aede363
Andrew Kushnir <akushnir@google.com>
2019-10-30 22:08:07 +0000

    a38508122a docs: release notes for the v8.2.13 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+43.sha-c35587b.with-local-changes
+ * @license Angular v9.0.0-next.14+44.sha-a385081.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.14+43.sha-c35587b.with-local-changes');
+var VERSION = new Version('9.0.0-next.14+44.sha-a385081.with-local-changes');
 
 /**
  * @license
