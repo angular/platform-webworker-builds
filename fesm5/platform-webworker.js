679eb1978040be451d5b8016c5222b337d641642
Andrew Kushnir <akushnir@google.com>
2019-10-30 18:51:35 +0000

    f197191a5f refactor(core): better error message for undecorated-classes-with-di migration (#33315)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+34.sha-fba72f0.with-local-changes
+ * @license Angular v9.0.0-next.14+36.sha-f197191.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.14+34.sha-fba72f0.with-local-changes');
+var VERSION = new Version('9.0.0-next.14+36.sha-f197191.with-local-changes');
 
 /**
  * @license
