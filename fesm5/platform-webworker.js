2cd19d71b476eeaa4f5954c81c15b9ab7c4b651a
Andrew Kushnir <akushnir@google.com>
2019-10-30 19:06:46 +0000

    31dccab2da test(language-service): Completions test should reuse existing host and services (#33478)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-next.14+41.sha-300d7ca.with-local-changes
+ * @license Angular v9.0.0-next.14+42.sha-31dccab.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-next.14+41.sha-300d7ca.with-local-changes');
+var VERSION = new Version('9.0.0-next.14+42.sha-31dccab.with-local-changes');
 
 /**
  * @license
