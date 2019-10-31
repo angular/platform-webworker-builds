6805d7e0764cb3754375fe4e228f58b448fcc6cd
atscott <atscott01@gmail.com>
2019-10-31 22:58:47 +0000

    66ab701606 docs: add example of a server in ruby for deployment (#33418) PR Close #33418

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+7.sha-1d141a8.with-local-changes
+ * @license Angular v9.0.0-rc.0+4.sha-66ab701.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+7.sha-1d141a8.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+4.sha-66ab701.with-local-changes');
 
 /**
  * @license
