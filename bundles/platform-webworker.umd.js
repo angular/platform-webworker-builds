f0a5c6d004677b9a836041e4fa11b87e58e60198
Andrew Kushnir <akushnir@google.com>
2019-10-31 17:44:22 +0000

    2174827fe5 build: ensure that version stamping is applied when the npm_package publish action is called (#33512)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v8.2.12+8.sha-38e9959.with-local-changes
+ * @license Angular v8.2.13+1.sha-2174827.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1213,7 +1213,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('8.2.12+8.sha-38e9959.with-local-changes');
+    var VERSION = new core.Version('8.2.13+1.sha-2174827.with-local-changes');
 
     /**
      * @license
