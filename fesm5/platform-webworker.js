f5ccae791e38d6e1c72f3bb8f23346ee7ccee20e
atscott <atscott01@gmail.com>
2019-10-31 22:39:57 +0000

    4099822ba2 docs: fix and refine COMMITTER.md (#33276)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+3.sha-64ac106.with-local-changes
+ * @license Angular v9.0.0-rc.0+2.sha-4099822.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+3.sha-64ac106.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+2.sha-4099822.with-local-changes');
 
 /**
  * @license
