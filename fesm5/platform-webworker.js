3ed53f491335c08ebd9ffd1046faaa725978d2f0
atscott <atscott01@gmail.com>
2019-11-01 18:11:51 +0000

    fc8eecad3f fix(compiler-cli): remove unused CLI private exports (#33242)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+25.sha-f02213a.with-local-changes
+ * @license Angular v9.0.0-rc.0+23.sha-fc8eeca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+25.sha-f02213a.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+23.sha-fc8eeca.with-local-changes');
 
 /**
  * @license
