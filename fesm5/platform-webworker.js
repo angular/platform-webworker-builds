41e62d86fce713e348dac927de852b4631046061
atscott <atscott01@gmail.com>
2019-11-01 00:03:38 +0000

    38758d856a fix(ivy): don't crash on unknown pipe (#33454)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+14.sha-72eba77.with-local-changes
+ * @license Angular v9.0.0-rc.0+16.sha-38758d8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+14.sha-72eba77.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+16.sha-38758d8.with-local-changes');
 
 /**
  * @license
