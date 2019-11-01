ecb22502d446a7b36a6456a4abc8e123dbd1a6bf
atscott <atscott01@gmail.com>
2019-11-01 22:44:09 +0000

    67aedd8366 docs: remove erroneous brack  in animations guide (#33525)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+29.sha-d77453d.with-local-changes
+ * @license Angular v9.0.0-rc.0+31.sha-67aedd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.0+29.sha-d77453d.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+31.sha-67aedd8.with-local-changes');
 
     /**
      * @license
