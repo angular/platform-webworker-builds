7047d0894383bb103a8ac5cb1e1cfd87005ac20d
atscott <atscott01@gmail.com>
2019-11-01 18:00:03 +0000

    9ebac71521 fix(language-service): Should not crash if expr ends unexpectedly (#33524)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+22.sha-feb5f27.with-local-changes
+ * @license Angular v9.0.0-rc.0+20.sha-9ebac71.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.0+22.sha-feb5f27.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+20.sha-9ebac71.with-local-changes');
 
     /**
      * @license
