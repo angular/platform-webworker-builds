ca0ce784dd3640042b937d0bbba07f798c49f78d
atscott <atscott01@gmail.com>
2019-11-04 16:28:43 +0000

    5977a72fa6 refactor(core): typo in undecorated-classes-with-di AOT failure message (#33018)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+32.sha-a925c2d.with-local-changes
+ * @license Angular v9.0.0-rc.0+34.sha-5977a72.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+32.sha-a925c2d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+34.sha-5977a72.with-local-changes');
 
 /**
  * @license
