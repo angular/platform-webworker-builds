fc3b85a699b49b0f9081b4db0d7d772a618fde5e
atscott <atscott01@gmail.com>
2019-11-05 01:12:42 +0000

    25aaff2ee1 docs: Adding Doug Parker to contributors list. (#33516)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+40.sha-4abf15c.with-local-changes
+ * @license Angular v9.0.0-rc.0+42.sha-25aaff2.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.0+40.sha-4abf15c.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+42.sha-25aaff2.with-local-changes');
 
     /**
      * @license
