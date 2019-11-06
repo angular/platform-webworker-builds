9079a99d844da61360c14723a938f1418eebe6e8
atscott <atscott01@gmail.com>
2019-11-06 01:12:42 +0000

    88eefd86b9 build: reference zone.js from source directly instead of npm. (#33046)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+62.sha-d8e6501.with-local-changes
+ * @license Angular v9.0.0-rc.0+63.sha-88eefd8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.0+62.sha-d8e6501.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.0+63.sha-88eefd8.with-local-changes');
 
     /**
      * @license
