3667dc563724cb8f81d4ede8b10222c6ac169b97
atscott <atscott01@gmail.com>
2019-11-06 20:17:30 +0000

    7b87392f47 docs: added value param wrt. method signature (#32968)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+71.sha-d749dd3.with-local-changes
+ * @license Angular v9.0.0-rc.0+73.sha-7b87392.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+71.sha-d749dd3.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+73.sha-7b87392.with-local-changes');
 
 /**
  * @license
