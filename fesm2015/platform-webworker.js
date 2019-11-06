b65522a9dde8c1e025074b3df021935b4f899a7a
atscott <atscott01@gmail.com>
2019-11-06 16:48:19 +0000

    3297a76195 refactor(ivy): remove TStylingContext locking in favor of firstUpdatePass flag (#33521)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+66.sha-5453c4c.with-local-changes
+ * @license Angular v9.0.0-rc.0+67.sha-3297a76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1959,7 +1959,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.0+66.sha-5453c4c.with-local-changes');
+const VERSION = new Version('9.0.0-rc.0+67.sha-3297a76.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.0+66.sha-5453c4c.with-local-changes
+ * @license Angular v9.0.0-rc.0+67.sha-3297a76.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.0+66.sha-5453c4c.with-local-changes');
+var VERSION = new Version('9.0.0-rc.0+67.sha-3297a76.with-local-changes');
 
 /**
  * @license
