de95da2d1efd7aa71739c644eb41f43fddd36df8
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:16:21 +0000

    8912b11f56 fix(ivy): recompile component when template changes in ngc watch mode (#33551)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+11.sha-8d72a37.with-local-changes
+ * @license Angular v9.0.0-rc.1+12.sha-8912b11.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+11.sha-8d72a37.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+12.sha-8912b11.with-local-changes');
 
 /**
  * @license
