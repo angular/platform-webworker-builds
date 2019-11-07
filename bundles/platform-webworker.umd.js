0e2b354383706ec8d34e89b704facbeb02a88e84
Andrew Scott <atscott01@gmail.com>
2019-11-07 18:16:35 +0000

    cd8333cf0d fix(ivy): recompile component when template changes in ngc watch mode (#33551)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+7.sha-99ead47.with-local-changes
+ * @license Angular v9.0.0-rc.1+13.sha-cd8333c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1205,7 +1205,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+7.sha-99ead47.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+13.sha-cd8333c.with-local-changes');
 
     /**
      * @license
