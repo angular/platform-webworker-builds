093b21ab702b93640b10d2250c1f36e8b337e3db
Kara Erickson <karakara@google.com>
2019-11-08 19:15:14 +0000

    570936e39f docs: add ivy ssr opt-out (#33616)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+27.sha-1db8650.with-local-changes
+ * @license Angular v9.0.0-rc.1+26.sha-570936e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1007,7 +1007,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+27.sha-1db8650.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+26.sha-570936e.with-local-changes');
 
 /**
  * @license
