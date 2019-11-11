61e696a0fcb56492f6d64d39fbceeda1a07cd0b8
Kara Erickson <karakara@google.com>
2019-11-11 21:07:29 +0000

    c6ebcd1eb9 docs: fix numbering (#33736)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+47.sha-eece138.with-local-changes
+ * @license Angular v9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+47.sha-eece138.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+48.sha-c6ebcd1.with-local-changes');
 
 /**
  * @license
