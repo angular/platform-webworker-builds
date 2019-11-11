96b270309eaa94631d0005cba6173d490e9cfc36
Kara Erickson <karakara@google.com>
2019-11-11 19:26:05 +0000

    ca1ff3ea11 docs: update update guide to use `ng update` instead of `npm i` (#33726)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes
+ * @license Angular v9.0.0-rc.1+46.sha-ca1ff3e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1000,7 +1000,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+45.sha-a8f9ff5.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+46.sha-ca1ff3e.with-local-changes');
 
 /**
  * @license
