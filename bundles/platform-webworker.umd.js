9a648cf9c8d3b2a1b3fbefe815bfd637828b8f4a
Kara Erickson <karakara@google.com>
2019-11-12 18:25:26 +0000

    f1b0547f0a revert: fix(ivy): R3TestBed should clean up registered modules after each test (#32872) (#33663)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+58.sha-fcde671.with-local-changes
+ * @license Angular v9.0.0-rc.1+61.sha-f1b0547.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1207,7 +1207,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+58.sha-fcde671.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+61.sha-f1b0547.with-local-changes');
 
     /**
      * @license
