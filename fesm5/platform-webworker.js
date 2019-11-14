a238e084a69230163095c6d5dff53a502c46abab
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-14 17:58:35 +0000

    e25b256aa4 build: minor improvements to build script output (#33823)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+100.sha-b30bb8d.with-local-changes
+ * @license Angular v9.0.0-rc.1+106.sha-e25b256.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1002,7 +1002,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+100.sha-b30bb8d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+106.sha-e25b256.with-local-changes');
 
 /**
  * @license
