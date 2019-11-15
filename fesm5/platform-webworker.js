6c16bf6cb7ee60e45179503101a4df71d1f9facf
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-15 21:34:26 +0000

    3074cdfea9 Revert "fix(router): make routerLinkActive work with query params which contain arrays (#22666)" (#33861)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.2+36.sha-c182dea.with-local-changes
+ * @license Angular v9.0.0-rc.2+37.sha-3074cdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1009,7 +1009,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.2+36.sha-c182dea.with-local-changes');
+var VERSION = new Version('9.0.0-rc.2+37.sha-3074cdf.with-local-changes');
 
 /**
  * @license
