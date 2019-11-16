85d459e99ede55307b2276cdb17fbf80ab1ae046
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-16 00:17:57 +0000

    c89eed56b6 build: add ngJitMode to ng_rollup_bundle terser config (#33773)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+126.sha-b90aac0.with-local-changes
+ * @license Angular v9.0.0-rc.1+127.sha-c89eed5.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1002,7 +1002,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+126.sha-b90aac0.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+127.sha-c89eed5.with-local-changes');
 
 /**
  * @license
