6ae17653346d4fb9c3e2c5eaa25b181cf77cc5c5
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-20 22:47:34 +0000

    d78d29ff8c docs: release notes for the v9.0.0-rc.3 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+188.sha-b54ed98.with-local-changes
+ * @license Angular v9.0.0-rc.1+190.sha-d78d29f.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+188.sha-b54ed98.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+190.sha-d78d29f.with-local-changes');
 
 /**
  * @license
