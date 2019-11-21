23d98b9be288af9d10c94da993971d25b7601848
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-11-21 19:16:31 +0000

    bf1bcd1e08 fix(ngcc): render localized strings when in ES5 format (#33857)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+209.sha-346e1c1.with-local-changes
+ * @license Angular v9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1966,7 +1966,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+209.sha-346e1c1.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+210.sha-bf1bcd1.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
