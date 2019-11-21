97259fab98dfa8a2f38192334caf7a0754aee9fb
Matias Niemelä <matias@yearofmoo.com>
2019-11-21 22:19:15 +0000

    fd5f101bab docs: add CSRF to XSRF section of http guide (#32933)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+215.sha-2303dc4.with-local-changes
+ * @license Angular v9.0.0-rc.1+216.sha-fd5f101.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2050,7 +2050,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+215.sha-2303dc4.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+216.sha-fd5f101.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
