3a8464b2a8f2dd7f35c79a1461eecd5094b8242a
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:01:12 +0000

    e24453b9f8 docs: Add Joey Perrott to Angular team in contributors (#33962)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+241.sha-4cf1979.with-local-changes
+ * @license Angular v9.0.0-rc.1+242.sha-e24453b.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+241.sha-4cf1979.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+242.sha-e24453b.with-local-changes');
 
 /**
  * @license
