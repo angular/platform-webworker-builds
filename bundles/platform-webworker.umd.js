ddb88c9bdbccf0198ca7e2d127c70f21fb7513ea
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 22:09:26 +0000

    85a4a1ac60 refactor(bazel): use getattr instead of hasattr in ng_module.bzl (#33765)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+42.sha-99e68d0.with-local-changes
+ * @license Angular v9.0.0-rc.3+47.sha-85a4a1a.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.3+42.sha-99e68d0.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.3+47.sha-85a4a1a.with-local-changes');
 
     /**
      * @license
