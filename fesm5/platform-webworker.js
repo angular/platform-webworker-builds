386e6b5850e7f1ed3e4e1237951a9080b1c135ea
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 23:00:24 +0000

    4cf197998a fix(ivy): track changes across failed builds (#33971)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+239.sha-17857a3.with-local-changes
+ * @license Angular v9.0.0-rc.1+241.sha-4cf1979.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+239.sha-17857a3.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+241.sha-4cf1979.with-local-changes');
 
 /**
  * @license
