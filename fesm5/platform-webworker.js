dd381bfa65663d3a7cf5b4339c16e64fee5f34ef
Matias Niemelä <matias@yearofmoo.com>
2019-11-22 01:24:21 +0000

    828e5c1f79 fix(router): make routerLinkActive work with query params which contain arrays (#22666)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+218.sha-7d579b9.with-local-changes
+ * @license Angular v9.0.0-rc.1+220.sha-828e5c1.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+218.sha-7d579b9.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+220.sha-828e5c1.with-local-changes');
 
 /**
  * @license
