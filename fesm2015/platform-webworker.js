48c9367ea7dfaf0b5b2266c264dc110f80623cbe
Matias Niemelä <matias@yearofmoo.com>
2019-11-23 00:01:06 +0000

    7b77ead88f ci: add JoostK to @angular/fw-compiler (#33994)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.3+52.sha-9248dba.with-local-changes
+ * @license Angular v9.0.0-rc.3+55.sha-7b77ead.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.3+52.sha-9248dba.with-local-changes');
+const VERSION = new Version('9.0.0-rc.3+55.sha-7b77ead.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
