fd8254a5679a54c5ff497db0685e2c9c9fad0cb8
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 19:32:21 +0000

    56f33bc266 docs: initial Ivy i18n guide updates (#33762)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+254.sha-31e2bdf.with-local-changes
+ * @license Angular v9.0.0-rc.1+255.sha-56f33bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+254.sha-31e2bdf.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+255.sha-56f33bc.with-local-changes');
 
     /**
      * @license
