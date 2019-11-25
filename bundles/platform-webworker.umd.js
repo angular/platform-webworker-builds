f35d752082a1c44cfa7193ffc8a0238aade4d110
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:56:13 +0000

    51a56bc4c6 docs: fix typos and highlight booleans to keep consistency (#34013)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+246.sha-1217c6b.with-local-changes
+ * @license Angular v9.0.0-rc.1+247.sha-51a56bc.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+246.sha-1217c6b.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+247.sha-51a56bc.with-local-changes');
 
     /**
      * @license
