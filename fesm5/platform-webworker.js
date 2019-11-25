4df891f41d238c37cf7959391da5f15dbb011e41
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 18:07:49 +0000

    31e2bdffa8 perf(ivy): fix creation time micro-benchmarks (#34031)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+253.sha-d80eeb6.with-local-changes
+ * @license Angular v9.0.0-rc.1+254.sha-31e2bdf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+253.sha-d80eeb6.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+254.sha-31e2bdf.with-local-changes');
 
 /**
  * @license
