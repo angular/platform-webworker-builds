45b62637ca48f66fc7bde07378d40d26bd6212f8
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 16:59:51 +0000

    f2800b90b2 docs(docs-infra): fix typos and spelling in the stackblitz-builder/README.md (#34009)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+248.sha-44225e4.with-local-changes
+ * @license Angular v9.0.0-rc.1+249.sha-f2800b9.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+248.sha-44225e4.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+249.sha-f2800b9.with-local-changes');
 
 /**
  * @license
