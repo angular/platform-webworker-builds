918b3c268a1c32ce09213fe4a4ea7c3b59501a6e
Matias Niemelä <matias@yearofmoo.com>
2019-11-25 21:54:15 +0000

    ec495c807f ci: do not check/upload `packages-dist/` UMD bundle sizes multiple times (#33987)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+255.sha-56f33bc.with-local-changes
+ * @license Angular v9.0.0-rc.1+258.sha-ec495c8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+255.sha-56f33bc.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+258.sha-ec495c8.with-local-changes');
 
     /**
      * @license
