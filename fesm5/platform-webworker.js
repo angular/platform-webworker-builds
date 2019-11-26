ddf03b6f75124a035d4a80868be606bb2ced225f
Matias Niemelä <matias@yearofmoo.com>
2019-11-26 03:59:15 +0000

    d655093123 docs: add Animations import in Router doc (#33740)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+266.sha-953365d.with-local-changes
+ * @license Angular v9.0.0-rc.1+267.sha-d655093.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+266.sha-953365d.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+267.sha-d655093.with-local-changes');
 
 /**
  * @license
