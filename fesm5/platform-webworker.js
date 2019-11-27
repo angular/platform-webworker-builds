dd789cdc8503f62d640d555cc5789129d770eb9b
Miško Hevery <misko@hevery.com>
2019-11-27 00:30:02 +0000

    3f68377c3d ci: check versions of non-local integration project dependencies (#33968)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+270.sha-fadb2d9.with-local-changes
+ * @license Angular v9.0.0-rc.1+286.sha-3f68377.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+270.sha-fadb2d9.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+286.sha-3f68377.with-local-changes');
 
 /**
  * @license
