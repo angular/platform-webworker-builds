3e8b8baee8dd2c09a9deda30adf5f48b530d1e39
Miško Hevery <misko@hevery.com>
2019-12-02 19:50:34 +0000

    1425e63029 fix(language-service): determine correct type for ngFor exported values (#34089)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+310.sha-bbce2ad.with-local-changes
+ * @license Angular v9.0.0-rc.1+312.sha-1425e63.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+310.sha-bbce2ad.with-local-changes');
+const VERSION = new Version('9.0.0-rc.1+312.sha-1425e63.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
