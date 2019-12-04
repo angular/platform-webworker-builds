ceda1bdd58f695c89395336f6fad2947dc7bb79d
Miško Hevery <misko@hevery.com>
2019-12-04 00:33:30 +0000

    627814cc4a docs(compiler): fix typos and spelling (#34008)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+355.sha-8f102fe.with-local-changes
+ * @license Angular v9.0.0-rc.1+356.sha-627814c.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+355.sha-8f102fe.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+356.sha-627814c.with-local-changes');
 
 /**
  * @license
