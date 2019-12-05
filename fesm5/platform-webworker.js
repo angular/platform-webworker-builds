245fd057c17a481fe1497f862fb7d6c430d761e6
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:53:33 +0000

    b4600cadbf ci: update saucelabs to use angular-framework account (#34233)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+2.sha-1183075.with-local-changes
+ * @license Angular v9.0.0-rc.5+11.sha-b4600ca.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.5+2.sha-1183075.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5+11.sha-b4600ca.with-local-changes');
 
 /**
  * @license
