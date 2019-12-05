06cf77b6c7c51a111eeaa00c4a999e132c14c737
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:33:43 +0000

    542f236dc4 docs: add documentation for server and Ivy without bundleDependencies (#34225)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+376.sha-a188312.with-local-changes
+ * @license Angular v9.0.0-rc.1+377.sha-542f236.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+376.sha-a188312.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+377.sha-542f236.with-local-changes');
 
     /**
      * @license
