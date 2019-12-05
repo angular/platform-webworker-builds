b0be3a83d0aa3b7483ff35838a26fcb766f01b07
Andrew Kushnir <akushnir@google.com>
2019-12-05 21:29:47 +0000

    d6f278867f build: Enable ivy in google3 if ng_module attr is True (#34238)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+383.sha-de62d8e.with-local-changes
+ * @license Angular v9.0.0-rc.1+386.sha-d6f2788.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+383.sha-de62d8e.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+386.sha-d6f2788.with-local-changes');
 
     /**
      * @license
