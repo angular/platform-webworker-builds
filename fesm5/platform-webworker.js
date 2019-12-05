b49654de270c44dc388183e753e4a835fe343ec4
Andrew Kushnir <akushnir@google.com>
2019-12-05 18:45:38 +0000

    ecb29de82f build: update to rules_nodejs 0.42.2 (#34243)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+381.sha-023c9be.with-local-changes
+ * @license Angular v9.0.0-rc.1+380.sha-ecb29de.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+381.sha-023c9be.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+380.sha-ecb29de.with-local-changes');
 
 /**
  * @license
