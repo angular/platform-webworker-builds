aacaa361a2f541f996566733dc8d14e402070f17
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:37:00 +0000

    10a33efc3e fix(ivy): align TestBed.overrideProvider with what happens with providers in TestBed providers array (#33769)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+28.sha-6e944ac.with-local-changes
+ * @license Angular v9.0.0-rc.5+32.sha-10a33ef.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.5+28.sha-6e944ac.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.5+32.sha-10a33ef.with-local-changes');
 
     /**
      * @license
