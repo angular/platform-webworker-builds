3b914cf06a464db5ca683ec6cb9ea7d152174833
Andrew Kushnir <akushnir@google.com>
2019-12-06 21:20:17 +0000

    718d7fe5fe fix(ivy): properly parenthesize ternary expressions when emitted (#34221)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+398.sha-f72de51.with-local-changes
+ * @license Angular v9.0.0-rc.1+399.sha-718d7fe.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+398.sha-f72de51.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.1+399.sha-718d7fe.with-local-changes');
 
     /**
      * @license
