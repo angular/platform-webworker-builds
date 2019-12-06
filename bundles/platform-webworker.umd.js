e89aa7a0e879d74a67a62a80b0c5fe79ee61f86e
Andrew Kushnir <akushnir@google.com>
2019-12-06 18:45:13 +0000

    7dfd327099 fix(language-service): Simplify resolution logic in banner (#34262)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+22.sha-0e911f8.with-local-changes
+ * @license Angular v9.0.0-rc.5+25.sha-7dfd327.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.5+22.sha-0e911f8.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.5+25.sha-7dfd327.with-local-changes');
 
     /**
      * @license
