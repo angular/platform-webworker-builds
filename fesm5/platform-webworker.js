2ec9c1713292208d6136408759a1177a0dc541f2
Andrew Kushnir <akushnir@google.com>
2019-12-09 20:35:45 +0000

    60051ebb59 fix(compiler): switch to modern diagnostic formatting (#34234)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+35.sha-1347064.with-local-changes
+ * @license Angular v9.0.0-rc.5+37.sha-60051eb.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.5+35.sha-1347064.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5+37.sha-60051eb.with-local-changes');
 
 /**
  * @license
