d70f4b6833188124f50fbc34543da8ac6fb4bcd5
Andrew Kushnir <akushnir@google.com>
2019-12-10 18:54:27 +0000

    46f9201e6f refactor(ivy): pull property read into a constant (#34290)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+44.sha-7c2bb37.with-local-changes
+ * @license Angular v9.0.0-rc.5+47.sha-46f9201.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.5+44.sha-7c2bb37.with-local-changes');
+    var VERSION = new core.Version('9.0.0-rc.5+47.sha-46f9201.with-local-changes');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+44.sha-7c2bb37.with-local-changes
+ * @license Angular v9.0.0-rc.5+47.sha-46f9201.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.5+44.sha-7c2bb37.with-local-changes');
+const VERSION = new Version('9.0.0-rc.5+47.sha-46f9201.with-local-changes');
 
 /**
  * @fileoverview added by tsickle
