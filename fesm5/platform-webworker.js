553ffbe830f74659daf313f9f75173af8676bb8d
Andrew Kushnir <akushnir@google.com>
2019-12-10 00:35:35 +0000

    24d1f9e87c fix(ngcc): fix undecorated child migration when `exportAs` is present (#34014)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.5+37.sha-60051eb.with-local-changes
+ * @license Angular v9.0.0-rc.5+40.sha-24d1f9e.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.5+37.sha-60051eb.with-local-changes');
+var VERSION = new Version('9.0.0-rc.5+40.sha-24d1f9e.with-local-changes');
 
 /**
  * @license
