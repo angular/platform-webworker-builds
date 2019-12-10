d2c307534235ddb65bd659e38930369f04474a3f
Andrew Kushnir <akushnir@google.com>
2019-12-10 17:37:04 +0000

    f95e2c7e7e docs: fix broken links (#34311)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+411.sha-ead169a.with-local-changes
+ * @license Angular v9.0.0-rc.1+413.sha-f95e2c7.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+411.sha-ead169a.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+413.sha-f95e2c7.with-local-changes');
 
 /**
  * @license
