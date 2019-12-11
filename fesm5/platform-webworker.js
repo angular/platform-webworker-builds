0faa2ccdbea41177f9c02cf30e0293c43c206809
Kara Erickson <karakara@google.com>
2019-12-11 21:43:50 +0000

    81c75cf560 fix(ngcc): handle imports in dts files when processing UMD (#34356)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6.with-local-changes
+ * @license Angular v9.0.0-rc.6+8.sha-81c75cf.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.6.with-local-changes');
+var VERSION = new Version('9.0.0-rc.6+8.sha-81c75cf.with-local-changes');
 
 /**
  * @license
