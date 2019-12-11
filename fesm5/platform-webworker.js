d5610802ef80233d273862614874f3334f7e110b
Andrew Kushnir <akushnir@google.com>
2019-12-11 01:00:52 +0000

    b72c7a89a9 refactor(ivy): include generic type for `ModuleWithProviders` in .d.ts files (#34235)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+423.sha-0984fbc.with-local-changes
+ * @license Angular v9.0.0-rc.1+425.sha-b72c7a8.with-local-changes
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+423.sha-0984fbc.with-local-changes');
+var VERSION = new Version('9.0.0-rc.1+425.sha-b72c7a8.with-local-changes');
 
 /**
  * @license
