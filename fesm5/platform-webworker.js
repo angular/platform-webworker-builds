c3a3314b843c480b9107254bea6ea06bdae3091d
Kara Erickson <karakara@google.com>
2019-12-16 16:08:46 +0000

    04ab03664d ci: ensure saucelabs test output is human readable (#34277)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+476.sha-3bbd12d
+ * @license Angular v9.0.0-rc.1+478.sha-04ab036
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+476.sha-3bbd12d');
+var VERSION = new Version('9.0.0-rc.1+478.sha-04ab036');
 
 /**
  * @license
