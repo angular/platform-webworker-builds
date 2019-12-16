3e042dd699a1144098ea24681521a33f95f34aa4
Kara Erickson <karakara@google.com>
2019-12-16 23:55:45 +0000

    5cecd97493 feat(forms): expand NgModel disabled type to work with strict template type checking (#34438)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+489.sha-5df8a3b
+ * @license Angular v9.0.0-rc.1+490.sha-5cecd97
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+489.sha-5df8a3b');
+var VERSION = new Version('9.0.0-rc.1+490.sha-5cecd97');
 
 /**
  * @license
