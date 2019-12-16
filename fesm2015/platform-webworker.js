914d6f441cf91f12a9342d520c643614169a3cd9
Kara Erickson <karakara@google.com>
2019-12-16 22:34:26 +0000

    5df8a3ba95 fix(language-service): HTML path should include last node before cursor (#34440)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+488.sha-28b4f4a
+ * @license Angular v9.0.0-rc.1+489.sha-5df8a3b
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+488.sha-28b4f4a');
+const VERSION = new Version('9.0.0-rc.1+489.sha-5df8a3b');
 
 /**
  * @fileoverview added by tsickle
