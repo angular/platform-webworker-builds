b5852dc423407ac6de72979440e50004f49f1d2b
Kara Erickson <karakara@google.com>
2019-12-18 18:43:23 +0000

    dfecca29da refactor: TypeScript 3.7 fixes. (#34372)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+499.sha-9d1175e
+ * @license Angular v9.0.0-rc.1+502.sha-dfecca2
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+499.sha-9d1175e');
+var VERSION = new Version('9.0.0-rc.1+502.sha-dfecca2');
 
 /**
  * @license
