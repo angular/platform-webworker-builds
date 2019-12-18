7eadb9fe96dd2b9cc03dc5424ce93491d781dc84
Kara Erickson <karakara@google.com>
2019-12-18 17:38:31 +0000

    1ebb405ef0 build(docs-infra): upgrade cli command docs sources to 30d7b1353 (#34465)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+68.sha-82dce68
+ * @license Angular v9.0.0-rc.6+72.sha-1ebb405
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.6+68.sha-82dce68');
+    var VERSION = new core.Version('9.0.0-rc.6+72.sha-1ebb405');
 
     /**
      * @license
