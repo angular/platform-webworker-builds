c057fcd398fcf55d3f1abec4abb84abf165808f6
Kara Erickson <karakara@google.com>
2019-12-18 17:34:17 +0000

    a04f7c0d5f fix(language-service): Proper completions for properties and events (#34445)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+501.sha-1eae7c8
+ * @license Angular v9.0.0-rc.1+500.sha-a04f7c0
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+501.sha-1eae7c8');
+const VERSION = new Version('9.0.0-rc.1+500.sha-a04f7c0');
 
 /**
  * @fileoverview added by tsickle
