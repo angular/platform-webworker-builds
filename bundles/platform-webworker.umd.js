ab9233b30819c8846800e0bd5a74d7d499e3c99f
Kara Erickson <karakara@google.com>
2019-12-18 17:38:49 +0000

    9d1175e2b2 fix(ivy): improve ExpressionChangedAfterChecked error (#34381)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+500.sha-a04f7c0
+ * @license Angular v9.0.0-rc.1+499.sha-9d1175e
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1203,7 +1203,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+500.sha-a04f7c0');
+    var VERSION = new i0.Version('9.0.0-rc.1+499.sha-9d1175e');
 
     /**
      * @license
