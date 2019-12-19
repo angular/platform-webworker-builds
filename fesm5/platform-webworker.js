e5639dac9da6895c00aa899f45246f0c09fee2b8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2019-12-19 19:57:58 +0000

    2dffe65cfd fix(language-service): completions after "let x of |" in ngFor (#34473)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+519.sha-260a061
+ * @license Angular v9.0.0-rc.1+520.sha-2dffe65
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+519.sha-260a061');
+var VERSION = new Version('9.0.0-rc.1+520.sha-2dffe65');
 
 /**
  * @license
