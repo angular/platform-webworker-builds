39327674c3232ebc6a50f2509bc66356ac8529d6
Kara Erickson <karakara@google.com>
2019-12-19 00:10:29 +0000

    6c33b7040d release: cut the v9.0.0-rc.7 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.6+89.sha-f563c7c
+ * @license Angular v9.0.0-rc.7
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.6+89.sha-f563c7c');
+var VERSION = new Version('9.0.0-rc.7');
 
 /**
  * @license
