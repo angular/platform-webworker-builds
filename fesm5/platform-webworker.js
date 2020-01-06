704ebaf7158cf714faa33b2587543156b7bbd599
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:38:54 +0000

    1de49ba369 refactor(ivy): consistently translate types to `ts.TypeNode` (#34021)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+523.sha-19944c2
+ * @license Angular v9.0.0-rc.1+525.sha-1de49ba
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+523.sha-19944c2');
+var VERSION = new Version('9.0.0-rc.1+525.sha-1de49ba');
 
 /**
  * @license
