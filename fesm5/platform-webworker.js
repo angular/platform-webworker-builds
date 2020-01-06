bfc25fb7802fe34d5401c4b90a3d0c79095c3708
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-06 19:40:38 +0000

    e116816131 refactor(ivy): let `strictTemplates` imply `fullTemplateTypeCheck` (#34195)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+525.sha-1de49ba
+ * @license Angular v9.0.0-rc.1+527.sha-e116816
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+525.sha-1de49ba');
+var VERSION = new Version('9.0.0-rc.1+527.sha-e116816');
 
 /**
  * @license
