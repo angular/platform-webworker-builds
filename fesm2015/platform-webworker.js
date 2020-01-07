ef80dade79d9ad56eeecbe7c357fc10e8034f293
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-07 19:46:16 +0000

    18d89c9c89 docs(forms): remove redundant word (#34584)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+538.sha-3053e02
+ * @license Angular v9.0.0-rc.1+554.sha-18d89c9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+538.sha-3053e02');
+const VERSION = new Version('9.0.0-rc.1+554.sha-18d89c9');
 
 /**
  * @fileoverview added by tsickle
