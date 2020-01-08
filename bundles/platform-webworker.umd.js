cf543b35016a4e41290112f4ac3d5415d69aa13d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-08 00:14:47 +0000

    4f42de9704 fix(ngcc): capture entry-point dependencies from typings as well as source (#34494)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+554.sha-18d89c9
+ * @license Angular v9.0.0-rc.1+541.sha-4f42de9
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+554.sha-18d89c9');
+    var VERSION = new core.Version('9.0.0-rc.1+541.sha-4f42de9');
 
     /**
      * @license
