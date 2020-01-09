51c995ee29cd2abfd90b89b7c82599e968d416bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-01-09 00:07:38 +0000

    8815ace418 fix(ngcc): insert definitions after statement (#34677)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+557.sha-02e4921
+ * @license Angular v9.0.0-rc.1+574.sha-8815ace
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+557.sha-02e4921');
+const VERSION = new Version('9.0.0-rc.1+574.sha-8815ace');
 
 /**
  * @fileoverview added by tsickle
