077001acf2c3eb2bd24886cfead68a1c797a2dfe
atscott <atscott@google.com>
2020-01-11 00:24:50 +0000

    08c25818d8 fix(ivy): don't run decorator handlers against declaration files (#34557)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+75.sha-a7d8dcd
+ * @license Angular v9.0.0-rc.8+76.sha-08c2581
  * (c) 2010-2019 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.8+75.sha-a7d8dcd');
+    var VERSION = new core.Version('9.0.0-rc.8+76.sha-08c2581');
 
     /**
      * @license
