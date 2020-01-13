bbbce565d8dcbaf503a0ef8f2e8b118629b9388c
atscott <atscott@google.com>
2020-01-13 23:35:17 +0000

    7305b02023 refactor(language-service): adds Symbol#typeArguments and does cleanup (#34571)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+89.sha-f1cdb8f
+ * @license Angular v9.0.0-rc.8+91.sha-7305b02
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.8+89.sha-f1cdb8f');
+var VERSION = new Version('9.0.0-rc.8+91.sha-7305b02');
 
 /**
  * @license
