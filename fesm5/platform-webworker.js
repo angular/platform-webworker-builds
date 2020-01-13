87599da104ba251c41df185b62771ec4057b69a9
atscott <atscott@google.com>
2020-01-13 18:04:14 +0000

    8de3c20e50 fix(ivy): do not reset view dirty state in check no changes mode (#34495)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+641.sha-ebcd59a
+ * @license Angular v9.0.0-rc.1+642.sha-8de3c20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+641.sha-ebcd59a');
+var VERSION = new Version('9.0.0-rc.1+642.sha-8de3c20');
 
 /**
  * @license
