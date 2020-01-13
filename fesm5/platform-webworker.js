78c69d45f9fafecd22e0a2db018f7a97644bbe2a
atscott <atscott@google.com>
2020-01-13 18:22:21 +0000

    58f10026c4 docs: add deprecation info about default currency code (#32584)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+644.sha-cfbb1a1
+ * @license Angular v9.0.0-rc.1+649.sha-58f1002
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+644.sha-cfbb1a1');
+var VERSION = new Version('9.0.0-rc.1+649.sha-58f1002');
 
 /**
  * @license
