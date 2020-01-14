b35af8e73b89f441fb7cac0d6cabe11340b8a29f
atscott <atscott@google.com>
2020-01-14 23:50:25 +0000

    cb142b6df9 refactor(compiler): refactor and export Bazel host creation. (#34686)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
+ * @license Angular v9.0.0-rc.1+674.sha-cb142b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+673.sha-c3c72f6');
+const VERSION = new Version('9.0.0-rc.1+674.sha-cb142b6');
 
 /**
  * @fileoverview added by tsickle
