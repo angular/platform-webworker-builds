921c89724a08305f3de8a7f14368c79b734935c8
atscott <atscott@google.com>
2020-01-14 18:05:49 +0000

    277681096d fix(ivy): properly bootstrap components with attribute selectors (#34450)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+657.sha-1a45387
+ * @license Angular v9.0.0-rc.1+658.sha-2776810
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+657.sha-1a45387');
+    var VERSION = new core.Version('9.0.0-rc.1+658.sha-2776810');
 
     /**
      * @license
