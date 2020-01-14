2f0517f841164bd30a0819c70a5c6c92849f84c7
atscott <atscott@google.com>
2020-01-14 23:39:29 +0000

    c3c72f689a fix(ivy): handle overloaded constructors in ngtsc (#34590)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+672.sha-9ceee07
+ * @license Angular v9.0.0-rc.1+673.sha-c3c72f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+672.sha-9ceee07');
+const VERSION = new Version('9.0.0-rc.1+673.sha-c3c72f6');
 
 /**
  * @fileoverview added by tsickle
