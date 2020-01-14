02330acdf7f2b60964187f9e311bf688754b7dc9
atscott <atscott@google.com>
2020-01-14 22:35:03 +0000

    d863526ca9 docs: replace non-breaking with regular spaces in `resources.json` (#34772)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+669.sha-23cbfa7
+ * @license Angular v9.0.0-rc.1+671.sha-d863526
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+669.sha-23cbfa7');
+const VERSION = new Version('9.0.0-rc.1+671.sha-d863526');
 
 /**
  * @fileoverview added by tsickle
