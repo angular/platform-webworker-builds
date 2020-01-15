5876ea622c94b4eb1743008dabdf85dffe5cd024
atscott <atscott@google.com>
2020-01-15 00:12:50 +0000

    fa39a8c026 feat(language-service): provide completion for $event variable (#34566)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+118.sha-c4d035e
+ * @license Angular v9.0.0-rc.8+119.sha-fa39a8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.8+118.sha-c4d035e');
+    var VERSION = new core.Version('9.0.0-rc.8+119.sha-fa39a8c');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.8+118.sha-c4d035e
+ * @license Angular v9.0.0-rc.8+119.sha-fa39a8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.8+118.sha-c4d035e');
+var VERSION = new Version('9.0.0-rc.8+119.sha-fa39a8c');
 
 /**
  * @license
