ea9ca25d4e80b9d8b35baca8fb6305595cef6076
Matias Niemelä <matias@yearofmoo.com>
2020-01-15 20:21:35 +0000

    762c531e30 build: update to rules_nodejs 1.1.0 (#34736)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.9+3.sha-fb7a6ce
+ * @license Angular v9.0.0-rc.9+21.sha-762c531
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.9+3.sha-fb7a6ce');
+    var VERSION = new core.Version('0.0.0');
 
     /**
      * @license
