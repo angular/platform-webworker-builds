fd68443c108b09bb5f684047764f0174693c370a
GitHub <noreply@github.com>
2020-01-15 21:35:57 +0000

    1fd4d4f0d4 fixup! docs: fix public api tool name

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-966cb35
+ * @license Angular v9.0.0-rc.1+693.sha-1fd4d4f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+692.sha-966cb35');
+    var VERSION = new core.Version('9.0.0-rc.1+693.sha-1fd4d4f');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+692.sha-966cb35
+ * @license Angular v9.0.0-rc.1+693.sha-1fd4d4f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+692.sha-966cb35');
+var VERSION = new Version('9.0.0-rc.1+693.sha-1fd4d4f');
 
 /**
  * @license
