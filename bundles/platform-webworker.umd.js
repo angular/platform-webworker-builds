ce6c75977fba9322284e9a44263cd5a749e51698
atscott <atscott@google.com>
2020-01-15 18:46:05 +0000

    3d5bcd5883 test(ngcc): update dependency host test description (#34695)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+689.sha-b7385a7
+ * @license Angular v9.0.0-rc.1+692.sha-3d5bcd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+689.sha-b7385a7');
+    var VERSION = new core.Version('9.0.0-rc.1+692.sha-3d5bcd5');
 
     /**
      * @license
