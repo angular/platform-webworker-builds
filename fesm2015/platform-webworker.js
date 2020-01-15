bccd471a0c0ac88a7210a21bccd0d1b5f8ae83d3
atscott <atscott@google.com>
2020-01-15 18:04:05 +0000

    b7385a77ad docs: release notes for the v9.0.0-rc.9 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+688.sha-bf8ba89
+ * @license Angular v9.0.0-rc.1+689.sha-b7385a7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+688.sha-bf8ba89');
+const VERSION = new Version('9.0.0-rc.1+689.sha-b7385a7');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+688.sha-bf8ba89
+ * @license Angular v9.0.0-rc.1+689.sha-b7385a7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+688.sha-bf8ba89');
+var VERSION = new Version('9.0.0-rc.1+689.sha-b7385a7');
 
 /**
  * @license
