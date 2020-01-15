506953315eada4c1c4e42355a23f11e4155ad3cb
atscott <atscott@google.com>
2020-01-15 18:44:53 +0000

    fb7a6ce586 test(ngcc): update dependency host test description (#34695)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.9
+ * @license Angular v9.0.0-rc.9+3.sha-fb7a6ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.9');
+const VERSION = new Version('9.0.0-rc.9+3.sha-fb7a6ce');
 
 /**
  * @fileoverview added by tsickle
