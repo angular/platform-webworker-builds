7a762fea7896e759f33d2ca9f6e90b66ef7c190f
GitHub <noreply@github.com>
2020-01-16 11:57:44 +0000

    32e1dc7284 fixup! build: disable link to PUBLIC_API docs when running ts-api-guardian in angular repo

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+695.sha-9d66345
+ * @license Angular v9.0.0-rc.1+696.sha-32e1dc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+695.sha-9d66345');
+const VERSION = new Version('9.0.0-rc.1+696.sha-32e1dc7');
 
 /**
  * @fileoverview added by tsickle
