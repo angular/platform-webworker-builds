ed38018d47e1feb2a08dac7633b25b4bad0e19b8
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:52:04 +0000

    a4cbb86360 docs: fix typo on router guide (#34911)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+14.sha-34c30ae
+ * @license Angular v9.0.0-rc.10+15.sha-a4cbb86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.10+14.sha-34c30ae');
+    var VERSION = new core.Version('9.0.0-rc.10+15.sha-a4cbb86');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+14.sha-34c30ae
+ * @license Angular v9.0.0-rc.10+15.sha-a4cbb86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.10+14.sha-34c30ae');
+var VERSION = new Version('9.0.0-rc.10+15.sha-a4cbb86');
 
 /**
  * @license
