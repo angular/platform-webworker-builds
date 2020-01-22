4c85309606641e26b5ca72a1c7d853c23471bb02
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 19:49:30 +0000

    0d8309509b release: cut the v9.0.0-rc.10 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+738.sha-11a4370
+ * @license Angular v9.0.0-rc.1+740.sha-0d83095
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('0.0.0');
+var VERSION = new Version('9.0.0-rc.1+740.sha-0d83095');
 
 /**
  * @license
