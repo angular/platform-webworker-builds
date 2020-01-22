5403257ef14f5ca45186b0ba7871e07b3f1eccca
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:27:02 +0000

    34c30ae0aa build(docs-infra): always specify a landing file for StackBlitz projects (#34553)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+9.sha-20dc436
+ * @license Angular v9.0.0-rc.10+14.sha-34c30ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.10+9.sha-20dc436');
+    var VERSION = new core.Version('9.0.0-rc.10+14.sha-34c30ae');
 
     /**
      * @license
