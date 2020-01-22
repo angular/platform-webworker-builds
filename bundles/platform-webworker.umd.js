c4ec3e7251c11685bf751680645974ac0f178542
Andrew Kushnir <akushnir@google.com>
2020-01-22 23:59:05 +0000

    d15cf60c49 feat(compiler-cli): require node 10 as runtime engine (#34722)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+755.sha-92c411f
+ * @license Angular v9.0.0-rc.1+760.sha-d15cf60
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+755.sha-92c411f');
+    var VERSION = new core.Version('9.0.0-rc.1+760.sha-d15cf60');
 
     /**
      * @license
