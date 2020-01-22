ff48d200189498e4abf4adc3e1a36e48af5f338f
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 20:19:43 +0000

    61ad50d40a docs: add message on click of hero in toh-4 (#34496)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+746.sha-e7cf37d
+ * @license Angular v9.0.0-rc.1+747.sha-61ad50d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+746.sha-e7cf37d');
+var VERSION = new Version('9.0.0-rc.1+747.sha-61ad50d');
 
 /**
  * @license
