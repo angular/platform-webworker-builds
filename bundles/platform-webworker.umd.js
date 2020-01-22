b74bb7fce9b1192c056195939269c0ee6c2cef92
Andrew Kushnir <akushnir@google.com>
2020-01-22 22:10:29 +0000

    e1fc44f5d6 build: remove @angular/http from modules (#34892)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+749.sha-13ce375
+ * @license Angular v9.0.0-rc.1+750.sha-e1fc44f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+749.sha-13ce375');
+    var VERSION = new core.Version('9.0.0-rc.1+750.sha-e1fc44f');
 
     /**
      * @license
