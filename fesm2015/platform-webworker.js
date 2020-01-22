7713f1ce6a7de775870d1caf745c91092e4f0bd2
Matias Niemelä <matias@yearofmoo.com>
2020-01-22 21:18:28 +0000

    32489c7426 revert: refactor(ivy): remove styleSanitizer instruction in favor of an inline param (#34480) (#34910)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+747.sha-61ad50d
+ * @license Angular v9.0.0-rc.1+748.sha-32489c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+747.sha-61ad50d');
+const VERSION = new Version('9.0.0-rc.1+748.sha-32489c7');
 
 /**
  * @fileoverview added by tsickle
