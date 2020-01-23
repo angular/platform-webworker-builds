839e90dc74accc608fab854564e59ec3123b03c6
Andrew Kushnir <akushnir@google.com>
2020-01-23 18:34:24 +0000

    68025ce09a test(language-service): recognize inputs/ouputs declared in decorator (#34875)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
+ * @license Angular v9.0.0-rc.1+764.sha-68025ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+763.sha-1ea04ff');
+const VERSION = new Version('9.0.0-rc.1+764.sha-68025ce');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+763.sha-1ea04ff
+ * @license Angular v9.0.0-rc.1+764.sha-68025ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+763.sha-1ea04ff');
+var VERSION = new Version('9.0.0-rc.1+764.sha-68025ce');
 
 /**
  * @license
