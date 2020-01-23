ceb0965f908ca0b5a7b48ee7e48745ea0c3108e5
Andrew Kushnir <akushnir@google.com>
2020-01-23 00:49:55 +0000

    3999d7adf0 docs: update payload size limits for angular.io application (#34916)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+15.sha-a4cbb86
+ * @license Angular v9.0.0-rc.10+22.sha-3999d7a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.10+15.sha-a4cbb86');
+var VERSION = new Version('9.0.0-rc.10+22.sha-3999d7a');
 
 /**
  * @license
