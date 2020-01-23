7f3d63246c4dee328ff0aa5cf6d04351b39dc597
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:47:37 +0000

    3049878aa9 ci: bump release version of ts-api-guardian (#34933)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+778.sha-055533b
+ * @license Angular v9.0.0-rc.1+779.sha-3049878
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+778.sha-055533b');
+const VERSION = new Version('9.0.0-rc.1+779.sha-3049878');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+778.sha-055533b
+ * @license Angular v9.0.0-rc.1+779.sha-3049878
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+778.sha-055533b');
+var VERSION = new Version('9.0.0-rc.1+779.sha-3049878');
 
 /**
  * @license
