8b8f7ce248578ccf595cb1efb269c084916f1264
Andrew Kushnir <akushnir@google.com>
2020-01-23 19:05:32 +0000

    e69d02c69f fix(ivy): use any for generic context checks when !strictTemplates (#34649)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+22.sha-3999d7a
+ * @license Angular v9.0.0-rc.10+34.sha-e69d02c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.10+22.sha-3999d7a');
+var VERSION = new Version('9.0.0-rc.10+34.sha-e69d02c');
 
 /**
  * @license
