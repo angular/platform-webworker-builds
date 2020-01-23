05e851f4568332fe67cbade411adcdd63ab05f20
Andrew Kushnir <akushnir@google.com>
2020-01-23 21:45:52 +0000

    3049878aa9 ci: bump release version of ts-api-guardian (#34933)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+778.sha-055533b
+ * @license Angular v9.0.0-rc.1+779.sha-3049878
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+778.sha-055533b');
+    var VERSION = new core.Version('9.0.0-rc.1+779.sha-3049878');
 
     /**
      * @license
