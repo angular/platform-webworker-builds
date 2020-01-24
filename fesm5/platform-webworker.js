e9dfc093e7714bb946d382e7f5a6870457a3ebc4
Andrew Kushnir <akushnir@google.com>
2020-01-24 00:18:19 +0000

    b50ed5c22c fix(language-service): Make metadata in Declaration non-optional (#34936)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+787.sha-7659f2e
+ * @license Angular v9.0.0-rc.1+788.sha-b50ed5c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+787.sha-7659f2e');
+var VERSION = new Version('9.0.0-rc.1+788.sha-b50ed5c');
 
 /**
  * @license
