b2f0c2c3aafae956eb5d99c1cdd05330d7bac0f5
Andrew Kushnir <akushnir@google.com>
2020-01-24 20:46:35 +0000

    ef95da6d3b fix(ivy): don't detect changes on detached child embedded views (#34846)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+792.sha-ed24100
+ * @license Angular v9.0.0-rc.1+793.sha-ef95da6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+792.sha-ed24100');
+const VERSION = new Version('9.0.0-rc.1+793.sha-ef95da6');
 
 /**
  * @fileoverview added by tsickle
