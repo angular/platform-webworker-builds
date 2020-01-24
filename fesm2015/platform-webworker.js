bebb77eb1f41b535e6350e3b8ba4e42bbba3b688
Andrew Kushnir <akushnir@google.com>
2020-01-24 20:42:48 +0000

    622737cee3 feat(common): expose getLocaleCurrencyCode publicly (#34946)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+52.sha-65354fb
+ * @license Angular v9.0.0-rc.10+54.sha-622737c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+52.sha-65354fb');
+const VERSION = new Version('9.0.0-rc.10+54.sha-622737c');
 
 /**
  * @fileoverview added by tsickle
