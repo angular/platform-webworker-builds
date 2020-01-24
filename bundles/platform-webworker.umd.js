205edecbeff1669365437d5ac8310c019942d6b2
Miško Hevery <misko@hevery.com>
2020-01-24 22:22:40 +0000

    9bd9590767 refactor(ivy): change styling to use programmatic API on updates (#34804)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+793.sha-ef95da6
+ * @license Angular v9.0.0-rc.1+806.sha-9bd9590
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+793.sha-ef95da6');
+    var VERSION = new core.Version('9.0.0-rc.1+806.sha-9bd9590');
 
     /**
      * @license
