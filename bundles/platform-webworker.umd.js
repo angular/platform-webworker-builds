4e03036160461125d768f79eb600700543c54afa
Igor Minar <igor@angular.io>
2020-01-25 06:48:33 +0000

    bdd3865314 build(docs-infra): update CLI to 9.0.0-rc.10

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+4.sha-8fca9b6
+ * @license Angular v9.0.0-rc.11+5.sha-bdd3865
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.11+4.sha-8fca9b6');
+    var VERSION = new core.Version('9.0.0-rc.11+5.sha-bdd3865');
 
     /**
      * @license
