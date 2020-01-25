a4cf4f1033e8f25e36792f5871b58bedb3c88e16
Andrew Kushnir <akushnir@google.com>
2020-01-25 00:15:16 +0000

    8fca9b6530 refactor(platform-browser): avoid mutable exports. (#34207) (#34960)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+67.sha-2e1a16b
+ * @license Angular v9.0.0-rc.11+4.sha-8fca9b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.10+67.sha-2e1a16b');
+const VERSION = new Version('9.0.0-rc.11+4.sha-8fca9b6');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.10+67.sha-2e1a16b
+ * @license Angular v9.0.0-rc.11+4.sha-8fca9b6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.10+67.sha-2e1a16b');
+var VERSION = new Version('9.0.0-rc.11+4.sha-8fca9b6');
 
 /**
  * @license
