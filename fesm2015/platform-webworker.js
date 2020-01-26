13a9aef956338060fef69c194aa038533ece536a
Igor Minar <igor@angular.io>
2020-01-26 23:21:23 +0000

    3d4b1431ec test: add 'localize' as an allowed commit message scope (#34974)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+809.sha-be8d06c
+ * @license Angular v9.0.0-rc.1+814.sha-3d4b143
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+809.sha-be8d06c');
+const VERSION = new Version('9.0.0-rc.1+814.sha-3d4b143');
 
 /**
  * @fileoverview added by tsickle
