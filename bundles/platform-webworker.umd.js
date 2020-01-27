be211ac04b1fba718e9f0771d9481dd288bfad84
Andrew Kushnir <akushnir@google.com>
2020-01-27 18:00:55 +0000

    b3bd6ca925 build: update API Golden files after node 12 update (#34955)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+815.sha-c349cb2
+ * @license Angular v9.0.0-rc.1+819.sha-b3bd6ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+815.sha-c349cb2');
+    var VERSION = new core.Version('9.0.0-rc.1+819.sha-b3bd6ca');
 
     /**
      * @license
