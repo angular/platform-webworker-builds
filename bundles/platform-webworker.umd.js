524b4ad42d50ae2aed9d554decccaa611adda872
Andrew Kushnir <akushnir@google.com>
2020-01-27 17:53:49 +0000

    c349cb2d16 build: update CLI to 9.0.0-rc.10 (#34965)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+817.sha-724ce8c
+ * @license Angular v9.0.0-rc.1+815.sha-c349cb2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1203,7 +1203,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.0.0-rc.1+817.sha-724ce8c');
+    var VERSION = new i0.Version('9.0.0-rc.1+815.sha-c349cb2');
 
     /**
      * @license
