506718cd48790fba07acd86e581cb1b118b99784
Andrew Kushnir <akushnir@google.com>
2020-01-27 19:27:05 +0000

    6e5cfd2cd2 fix(ivy): catch FatalDiagnosticError thrown from preanalysis phase (#34801)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+824.sha-549ab2e
+ * @license Angular v9.0.0-rc.1+827.sha-6e5cfd2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+824.sha-549ab2e');
+const VERSION = new Version('9.0.0-rc.1+827.sha-6e5cfd2');
 
 /**
  * @fileoverview added by tsickle
