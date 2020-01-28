877aa6fbd338923c2a63dc7c439d806bc21ba7af
Andrew Kushnir <akushnir@google.com>
2020-01-28 17:31:03 +0000

    e7c74cbd69 feat(language-service): completions for output $event properties in (#34570)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+830.sha-90b303f
+ * @license Angular v9.0.0-rc.1+832.sha-e7c74cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+830.sha-90b303f');
+const VERSION = new Version('9.0.0-rc.1+832.sha-e7c74cb');
 
 /**
  * @fileoverview added by tsickle
