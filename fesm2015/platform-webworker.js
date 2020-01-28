4db614d21d5a2858a6e34ae0fbdd0f485fdefec2
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 18:11:19 +0000

    04c73e55c4 ci: remove draft condition from pullapprove (#35012)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+26.sha-bd79a16
+ * @license Angular v9.0.0-rc.11+32.sha-04c73e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+26.sha-bd79a16');
+const VERSION = new Version('9.0.0-rc.11+32.sha-04c73e5');
 
 /**
  * @fileoverview added by tsickle
