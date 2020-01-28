34feae326df77e7fcac46c7ab0347b4e4d4107a6
Joey Perrott <josephperrott.github@gmail.com>
2020-01-28 17:41:45 +0000

    296dc0622f ci: Migrate from CODEOWNERS to PullApprove for PR review management (#34814)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+832.sha-e7c74cb
+ * @license Angular v9.0.0-rc.1+834.sha-296dc06
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+832.sha-e7c74cb');
+var VERSION = new Version('9.0.0-rc.1+834.sha-296dc06');
 
 /**
  * @license
