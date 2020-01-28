5ed8f14d3d95c1a58e6a4f381b84b5662265e950
Andrew Kushnir <akushnir@google.com>
2020-01-28 22:09:17 +0000

    dff4e1e19c refactor: simplify bazel saucelabs targets using karma pre-test wrapper and shared saucelabs connection between tests (#34769)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+837.sha-a5a5981
+ * @license Angular v9.0.0-rc.1+838.sha-dff4e1e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+837.sha-a5a5981');
+const VERSION = new Version('9.0.0-rc.1+838.sha-dff4e1e');
 
 /**
  * @fileoverview added by tsickle
