049d678d98db643a23a5c1a66d90a326fa541288
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:10:38 +0000

    bd61cd0267 ci: filter out bad commit from the commit message check to green up master (#35017)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+838.sha-dff4e1e
+ * @license Angular v9.0.0-rc.1+840.sha-bd61cd0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+838.sha-dff4e1e');
+var VERSION = new Version('9.0.0-rc.1+840.sha-bd61cd0');
 
 /**
  * @license
