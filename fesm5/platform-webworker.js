ede020b61cd80b5cbb3bd67debbaa95f1dbaf998
Andrew Kushnir <akushnir@google.com>
2020-01-29 00:18:43 +0000

    669df70da5 fix(ivy): ensure `DebugNode`/`DebugElement` are tree-shakeable in Ivy (#35003)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+840.sha-bd61cd0
+ * @license Angular v9.0.0-rc.1+844.sha-669df70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+840.sha-bd61cd0');
+var VERSION = new Version('9.0.0-rc.1+844.sha-669df70');
 
 /**
  * @license
