81829be2b939df612031f1109e3bc1fd3537d94c
Andrew Kushnir <akushnir@google.com>
2020-01-29 21:31:31 +0000

    a892054448 docs: remove named node version (#35014)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+855.sha-519e9e1
+ * @license Angular v9.0.0-rc.1+857.sha-a892054
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+855.sha-519e9e1');
+var VERSION = new Version('9.0.0-rc.1+857.sha-a892054');
 
 /**
  * @license
