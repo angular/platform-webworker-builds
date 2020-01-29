f004bf2da9229a90f7e07a9f3dadadf87e337dc3
Andrew Kushnir <akushnir@google.com>
2020-01-29 23:37:16 +0000

    0390d20d60 build: remove unneeded style code for ngClass and ngStyle (#34998)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+58.sha-9b7d703
+ * @license Angular v9.0.0-rc.11+59.sha-0390d20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+58.sha-9b7d703');
+const VERSION = new Version('9.0.0-rc.11+59.sha-0390d20');
 
 /**
  * @fileoverview added by tsickle
