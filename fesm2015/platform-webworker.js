8f3f0fdbde3b222d89363de3e3107ad0d0e502a7
Andrew Kushnir <akushnir@google.com>
2020-01-30 01:01:49 +0000

    36b8c0320b refactor(ivy): clean of  #34804 from previous merge (#35022)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+61.sha-19c4895
+ * @license Angular v9.0.0-rc.11+62.sha-36b8c03
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+61.sha-19c4895');
+const VERSION = new Version('9.0.0-rc.11+62.sha-36b8c03');
 
 /**
  * @fileoverview added by tsickle
