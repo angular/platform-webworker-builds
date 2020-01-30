d92bd76ca65989475fcfa2dced93b37073f38e69
Andrew Kushnir <akushnir@google.com>
2020-01-30 00:02:56 +0000

    19c489524f feat(ivy): Change static priority resolution to be same level as directive it belongs to (#34938)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+59.sha-0390d20
+ * @license Angular v9.0.0-rc.11+61.sha-19c4895
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.11+59.sha-0390d20');
+    var VERSION = new core.Version('9.0.0-rc.11+61.sha-19c4895');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+59.sha-0390d20
+ * @license Angular v9.0.0-rc.11+61.sha-19c4895
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.11+59.sha-0390d20');
+const VERSION = new Version('9.0.0-rc.11+61.sha-19c4895');
 
 /**
  * @fileoverview added by tsickle
