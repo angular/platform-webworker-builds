0c34d4bdd5a13119afca74259952c423cac3153b
Andrew Kushnir <akushnir@google.com>
2020-01-30 17:04:59 +0000

    37f39d6db5 build(compiler-cli): update to chokidar 3.x (#35047)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+865.sha-c1cf46c
+ * @license Angular v9.0.0-rc.1+866.sha-37f39d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+865.sha-c1cf46c');
+var VERSION = new Version('9.0.0-rc.1+866.sha-37f39d6');
 
 /**
  * @license
