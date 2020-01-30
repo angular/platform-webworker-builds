91421e8b38749833bd9d4971cd8702ff95044158
Andrew Kushnir <akushnir@google.com>
2020-01-30 22:21:49 +0000

    3bdf1164ce ci: fix StephenFluin's casing in entries in PullApprove (#35062)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.11+67.sha-36dfca9
+ * @license Angular v9.0.0-rc.12+1.sha-3bdf116
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.11+67.sha-36dfca9');
+    var VERSION = new core.Version('9.0.0-rc.12+1.sha-3bdf116');
 
     /**
      * @license
