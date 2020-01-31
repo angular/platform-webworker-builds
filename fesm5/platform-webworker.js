53f2dd369cceddcdcd8ab1a3abd862780463be12
Misko Hevery <misko@hevery.com>
2020-01-31 21:42:14 +0000

    58b29f1503 fix: should also allow subclass Promise without Symbol.species (#34533)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+880.sha-e672b1f
+ * @license Angular v9.0.0-rc.1+887.sha-58b29f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+880.sha-e672b1f');
+var VERSION = new Version('9.0.0-rc.1+887.sha-58b29f1');
 
 /**
  * @license
