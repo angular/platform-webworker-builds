d6c362b8705c8b0a2b9db69ea0a2ab1ff861c728
Misko Hevery <misko@hevery.com>
2020-01-31 21:10:51 +0000

    6b710f7ae1 docs: update awade jigsaw's web site url of the resources (#35025)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+878.sha-fcfce99
+ * @license Angular v9.0.0-rc.1+877.sha-6b710f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+878.sha-fcfce99');
+var VERSION = new Version('9.0.0-rc.1+877.sha-6b710f7');
 
 /**
  * @license
