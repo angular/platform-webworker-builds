c3d4186ac2eec4833d6291acad953c8e48c26b9a
Miško Hevery <misko@hevery.com>
2020-02-03 17:06:45 +0000

    3d4067a464 fix(ngcc): do not lock if the target is not compiled by Angular (#35057)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+897.sha-a3c7ab9
+ * @license Angular v9.0.0-rc.1+898.sha-3d4067a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+897.sha-a3c7ab9');
+var VERSION = new Version('9.0.0-rc.1+898.sha-3d4067a');
 
 /**
  * @license
