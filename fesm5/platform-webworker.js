03ab7fc3ca2463c9f36ef8b3b2c807e64d9cf28a
Miško Hevery <misko@hevery.com>
2020-02-03 22:45:47 +0000

    523c785e8f fix(ngcc): correctly invalidate cache when moving/removing files/directories (#35106)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+905.sha-3c30474
+ * @license Angular v9.0.0-rc.1+906.sha-523c785
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+905.sha-3c30474');
+var VERSION = new Version('9.0.0-rc.1+906.sha-523c785');
 
 /**
  * @license
