a28dd887ea157688835f5b432c413e30a5cc69b9
Miško Hevery <misko@hevery.com>
2020-02-04 18:44:31 +0000

    e6cb60ee22 docs: update typescript version reference (#35120)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+908.sha-0e3eeb4
+ * @license Angular v9.0.0-rc.1+910.sha-e6cb60e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+908.sha-0e3eeb4');
+const VERSION = new Version('9.0.0-rc.1+910.sha-e6cb60e');
 
 /**
  * @fileoverview added by tsickle
