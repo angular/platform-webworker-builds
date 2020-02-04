6ff8a431042e70a3b786162cd98c85015f26228b
Miško Hevery <misko@hevery.com>
2020-02-04 19:08:28 +0000

    1a23c79835 docs: changes AoT to AOT for consistency (#35112)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+2.sha-a320dc9
+ * @license Angular v9.0.0-rc.14+12.sha-1a23c79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.14+2.sha-a320dc9');
+    var VERSION = new core.Version('9.0.0-rc.14+12.sha-1a23c79');
 
     /**
      * @license
