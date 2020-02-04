5d73f43ccecccda3fbe7fdb3a034b8c41cd0fec2
Miško Hevery <misko@hevery.com>
2020-02-04 18:47:07 +0000

    f7d4cc9cab ci: properly validate commit messages locally (#35035)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+910.sha-e6cb60e
+ * @license Angular v9.0.0-rc.1+914.sha-f7d4cc9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+910.sha-e6cb60e');
+var VERSION = new Version('9.0.0-rc.1+914.sha-f7d4cc9');
 
 /**
  * @license
