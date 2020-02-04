4b76f247d8094fc6dcc8e4346e34bdc6980fffdd
Miško Hevery <misko@hevery.com>
2020-02-04 19:58:32 +0000

    7fb66eca25 docs: changes AoT to AOT for consistency (#35112)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+916.sha-6ddf550
+ * @license Angular v9.0.0-rc.1+919.sha-7fb66ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+916.sha-6ddf550');
+const VERSION = new Version('9.0.0-rc.1+919.sha-7fb66ec');
 
 /**
  * @fileoverview added by tsickle
