539d239a6c904c1876e4d7b9865d21cdfaf9cccf
Miško Hevery <misko@hevery.com>
2020-02-04 17:21:05 +0000

    0e3eeb4e71 docs: restructure nav for beginner concepts (#34681)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+909.sha-7369dd6
+ * @license Angular v9.0.0-rc.1+908.sha-0e3eeb4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+909.sha-7369dd6');
+const VERSION = new Version('9.0.0-rc.1+908.sha-0e3eeb4');
 
 /**
  * @fileoverview added by tsickle
