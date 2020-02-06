b0cd040fa6eadfebe4f306862767e0440ddf4bff
Misko Hevery <misko@hevery.com>
2020-02-06 01:13:35 +0000

    7de87728e2 ci: temporary disable the Android 7 SauceLab due to timeout (#35174)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+20.sha-cf3071f
+ * @license Angular v9.0.0-rc.14+21.sha-7de8772
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.14+20.sha-cf3071f');
+    var VERSION = new core.Version('9.0.0-rc.14+21.sha-7de8772');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.14+20.sha-cf3071f
+ * @license Angular v9.0.0-rc.14+21.sha-7de8772
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.14+20.sha-cf3071f');
+var VERSION = new Version('9.0.0-rc.14+21.sha-7de8772');
 
 /**
  * @license
