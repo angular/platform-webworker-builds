7959ad1de94a8110690a684f53c3f43113879aa9
Misko Hevery <misko@hevery.com>
2020-02-06 17:57:05 +0000

    722251da40 build: update scripts/release/post-check with the latest versions (#35151)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+934.sha-c286fbb
+ * @license Angular v9.0.0-rc.1+935.sha-722251d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
+const VERSION = new Version('9.0.0-rc.1+935.sha-722251d');
 
 /**
  * @fileoverview added by tsickle
