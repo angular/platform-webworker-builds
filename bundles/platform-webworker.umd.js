15c7c31c0bc8ac9ddfcd1cedee5c5a486ae2f7d9
Misko Hevery <misko@hevery.com>
2020-02-06 22:13:14 +0000

    f4b9d664f2 docs(ivy): add a not about compile-time constants to the ivy compatibility guide (#35194)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+5.sha-becab76
+ * @license Angular v9.0.0+7.sha-f4b9d66
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0+5.sha-becab76');
+    var VERSION = new core.Version('9.0.0+7.sha-f4b9d66');
 
     /**
      * @license
