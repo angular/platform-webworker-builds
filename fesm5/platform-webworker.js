d23c083a440ae319765f14c095bcfa343ad32d4a
Misko Hevery <misko@hevery.com>
2020-02-06 17:55:50 +0000

    c286fbb9dc docs(ivy): add size debugging section (#35178)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+933.sha-7526ef7
+ * @license Angular v9.0.0-rc.1+934.sha-c286fbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+933.sha-7526ef7');
+var VERSION = new Version('9.0.0-rc.1+934.sha-c286fbb');
 
 /**
  * @license
