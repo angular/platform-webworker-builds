0d92b46d8c680c769f66ff97627156b1c2a4d954
Misko Hevery <misko@hevery.com>
2020-02-06 22:06:44 +0000

    053dabb7ed docs: consolidate and remove rc/next notes from the v9.0.0 release (#35195)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+937.sha-22acdc2
+ * @license Angular v9.0.0-rc.1+938.sha-053dabb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+937.sha-22acdc2');
+var VERSION = new Version('9.0.0-rc.1+938.sha-053dabb');
 
 /**
  * @license
