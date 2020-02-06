f830b44747b45a19560abae12584149c5f4ac4d3
Kara Erickson <karakara@google.com>
2020-02-06 22:51:32 +0000

    ba0cdd3010 feat(docs-infra): add v8 to the version picker in the navbar (#35196)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+939.sha-86db592
+ * @license Angular v9.0.0-rc.1+940.sha-ba0cdd3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+939.sha-86db592');
+    var VERSION = new core.Version('9.0.0-rc.1+940.sha-ba0cdd3');
 
     /**
      * @license
