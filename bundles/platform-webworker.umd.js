507291c22c508364f89de2127c88639bf1581094
Misko Hevery <misko@hevery.com>
2020-02-06 23:19:10 +0000

    60b887090c docs: consilidate release notes for the v8.0.0 release (#35197)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+9.sha-bcb539f
+ * @license Angular v9.0.0+10.sha-60b8870
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0+9.sha-bcb539f');
+    var VERSION = new core.Version('9.0.0+10.sha-60b8870');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+9.sha-bcb539f
+ * @license Angular v9.0.0+10.sha-60b8870
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0+9.sha-bcb539f');
+const VERSION = new Version('9.0.0+10.sha-60b8870');
 
 /**
  * @fileoverview added by tsickle
