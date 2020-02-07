f96b074428bb5111393786c86127ae7838f67f1a
Misko Hevery <misko@hevery.com>
2020-02-07 18:23:22 +0000

    79742a397f fix(docs-infra): fix parameters with @Optional() decorator do not match declared, optional type (#35150)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+956.sha-53193c9
+ * @license Angular v9.0.0-rc.1+958.sha-79742a3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+956.sha-53193c9');
+var VERSION = new Version('9.0.0-rc.1+958.sha-79742a3');
 
 /**
  * @license
