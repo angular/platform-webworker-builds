4c0699b17c94223f532f0859e989531f3d48a04c
Misko Hevery <misko@hevery.com>
2020-02-07 18:23:14 +0000

    53193c9779 docs: fix typo in glossary (#35108)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+955.sha-16eb691
+ * @license Angular v9.0.0-rc.1+956.sha-53193c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+955.sha-16eb691');
+const VERSION = new Version('9.0.0-rc.1+956.sha-53193c9');
 
 /**
  * @fileoverview added by tsickle
