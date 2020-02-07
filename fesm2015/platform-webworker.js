1e815e407fa59e8b13e391a17fabafb5bab196a1
Misko Hevery <misko@hevery.com>
2020-02-07 18:25:18 +0000

    0cbdd54fd0 fix(ivy): ensure module imports are instantiated before the module being declared (#35172)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+958.sha-79742a3
+ * @license Angular v9.0.0-rc.1+959.sha-0cbdd54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+958.sha-79742a3');
+const VERSION = new Version('9.0.0-rc.1+959.sha-0cbdd54');
 
 /**
  * @fileoverview added by tsickle
