92d4953db13df1511ad50d3f5f87b3467980f5b0
Misko Hevery <misko@hevery.com>
2020-02-07 18:19:03 +0000

    16eb691e7f docs(ivy): add anchor-node insert before/after difference (#35207)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+954.sha-d3650ce
+ * @license Angular v9.0.0-rc.1+955.sha-16eb691
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+954.sha-d3650ce');
+var VERSION = new Version('9.0.0-rc.1+955.sha-16eb691');
 
 /**
  * @license
