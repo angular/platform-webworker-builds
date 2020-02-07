42d0b457d61fccb093d9019565a0c8e4863a461d
Misko Hevery <misko@hevery.com>
2020-02-07 00:39:40 +0000

    14fc1812f3 ci: re-enable android 7 saucelabs tests (#35171)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+10.sha-60b8870
+ * @license Angular v9.0.0+19.sha-14fc181
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0+10.sha-60b8870');
+    var VERSION = new core.Version('9.0.0+19.sha-14fc181');
 
     /**
      * @license
