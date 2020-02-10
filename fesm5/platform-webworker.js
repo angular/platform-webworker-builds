813cc741ec28380c442b69a99af3e219781594a0
Kara Erickson <karakara@google.com>
2020-02-10 19:11:43 +0000

    76b77ec3c2 docs(forms): update the ngForm deprecation notice (#35263)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+976.sha-2d5bb26
+ * @license Angular v9.0.0-rc.1+977.sha-76b77ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0-rc.1+976.sha-2d5bb26');
+var VERSION = new Version('9.0.0-rc.1+977.sha-76b77ec');
 
 /**
  * @license
