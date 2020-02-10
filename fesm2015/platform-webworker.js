05fc1bd9f8b998282157634a30f5f56e9c5ae798
Kara Erickson <karakara@google.com>
2020-02-10 17:44:25 +0000

    13be065ed9 ci: re-enable disabled components-repo-unit-tests job (#35123)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+973.sha-ae0253f
+ * @license Angular v9.0.0-rc.1+975.sha-13be065
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+973.sha-ae0253f');
+const VERSION = new Version('9.0.0-rc.1+975.sha-13be065');
 
 /**
  * @fileoverview added by tsickle
