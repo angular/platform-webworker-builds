d408bc40c411bb0fd03995f19f56c4a42dfbcaa1
Kara Erickson <karakara@google.com>
2020-02-11 20:12:22 +0000

    7c9735a995 fix(elements): schematics fails with schema.json not found error (#35211)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+984.sha-332937e
+ * @license Angular v9.0.0-rc.1+985.sha-7c9735a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+984.sha-332937e');
+const VERSION = new Version('9.0.0-rc.1+985.sha-7c9735a');
 
 /**
  * @fileoverview added by tsickle
