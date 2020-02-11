0718887ad968f8f78c3e343fb06c447aeb826835
Kara Erickson <karakara@google.com>
2020-02-11 01:03:19 +0000

    81241af7ac fix(language-service): Suggest ? and ! operator on nullable receiver (#35200)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+982.sha-a92d97c
+ * @license Angular v9.0.0-rc.1+983.sha-81241af
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+982.sha-a92d97c');
+const VERSION = new Version('9.0.0-rc.1+983.sha-81241af');
 
 /**
  * @fileoverview added by tsickle
