41a230559923a3cea1d8ab52d5b2d69759c8a694
Kara Erickson <karakara@google.com>
2020-02-11 20:03:45 +0000

    332937ef24 feat: make mocha a zone module. (#34719)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+983.sha-81241af
+ * @license Angular v9.0.0-rc.1+984.sha-332937e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0-rc.1+983.sha-81241af');
+const VERSION = new Version('9.0.0-rc.1+984.sha-332937e');
 
 /**
  * @fileoverview added by tsickle
