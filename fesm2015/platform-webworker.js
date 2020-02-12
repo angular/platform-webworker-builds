baa7f0e385f7ec88f161698092834dcd33b3592b
Kara Erickson <karakara@google.com>
2020-02-12 01:25:02 +0000

    554c2cbd5c fix(forms): change Array.reduce usage to Array.forEach (#35349)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+47.sha-94d002b
+ * @license Angular v9.0.0+53.sha-554c2cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.0+47.sha-94d002b');
+const VERSION = new Version('9.0.0+53.sha-554c2cb');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0+47.sha-94d002b
+ * @license Angular v9.0.0+53.sha-554c2cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.0+47.sha-94d002b');
+var VERSION = new Version('9.0.0+53.sha-554c2cb');
 
 /**
  * @license
