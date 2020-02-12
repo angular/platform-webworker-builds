f161854d549cc946f05851c90639eefe550afcb7
Kara Erickson <karakara@google.com>
2020-02-12 01:29:40 +0000

    eef047bc5f fix(forms): change Array.reduce usage to Array.forEach (#35349)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.0-rc.1+989.sha-b9b38f0
+ * @license Angular v9.0.0-rc.1+990.sha-eef047b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.0-rc.1+989.sha-b9b38f0');
+    var VERSION = new core.Version('9.0.0-rc.1+990.sha-eef047b');
 
     /**
      * @license
