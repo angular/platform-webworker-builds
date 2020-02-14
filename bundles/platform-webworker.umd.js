f76689bb5da16acfd95c9af67e1418aa2dc7f462
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 19:46:12 +0000

    be5e75c804 docs: change `item` to `items` to match the code (#35433)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+23.sha-2ca7984
+ * @license Angular v9.1.0-next.0+24.sha-be5e75c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1203,7 +1203,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.1.0-next.0+23.sha-2ca7984');
+    var VERSION = new i0.Version('9.1.0-next.0+24.sha-be5e75c');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+23.sha-2ca7984
+ * @license Angular v9.1.0-next.0+24.sha-be5e75c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+23.sha-2ca7984');
+const VERSION = new Version('9.1.0-next.0+24.sha-be5e75c');
 
 /**
  * @fileoverview added by tsickle
