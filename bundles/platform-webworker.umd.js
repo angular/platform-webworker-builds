f528fcd2d278028d933a6bf5c5a7bf42504499c6
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-14 00:58:11 +0000

    27cd01f606 build: update to bazel_toolchains 2.1.0 (#35430)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+17.sha-9ca1faf
+ * @license Angular v9.1.0-next.0+20.sha-27cd01f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.0+17.sha-9ca1faf');
+    var VERSION = new core.Version('9.1.0-next.0+20.sha-27cd01f');
 
     /**
      * @license
