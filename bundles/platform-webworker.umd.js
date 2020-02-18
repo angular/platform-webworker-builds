e769414cf5b4c86260765cd5b2969f5e3892f002
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:18:27 +0000

    c013dd4ca6 fix(core): better handing of ICUs outside of i18n blocks (#35347)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+33.sha-7b56daf
+ * @license Angular v9.1.0-next.0+34.sha-c013dd4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.0+33.sha-7b56daf');
+    var VERSION = new core.Version('9.1.0-next.0+34.sha-c013dd4');
 
     /**
      * @license
