30df82ede8bf6383a8c9e63a07de9cd5f385e94d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:21:37 +0000

    2f18e8565e style(docs-infra): properly format files (#35318)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+34.sha-c013dd4
+ * @license Angular v9.1.0-next.0+36.sha-2f18e85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+34.sha-c013dd4');
+const VERSION = new Version('9.1.0-next.0+36.sha-2f18e85');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+34.sha-c013dd4
+ * @license Angular v9.1.0-next.0+36.sha-2f18e85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.0+34.sha-c013dd4');
+var VERSION = new Version('9.1.0-next.0+36.sha-2f18e85');
 
 /**
  * @license
