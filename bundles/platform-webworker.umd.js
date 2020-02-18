425384c25b583ecd74c93268a18c42ec3e6f87bc
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:10:40 +0000

    d7c4f40171 docs(upgrade): separate AngularJS Material typings to its own block (#35514)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+26.sha-d05629b
+ * @license Angular v9.1.0-next.0+29.sha-d7c4f40
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.0+26.sha-d05629b');
+    var VERSION = new core.Version('9.1.0-next.0+29.sha-d7c4f40');
 
     /**
      * @license
