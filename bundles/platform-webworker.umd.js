04197c8f8a260c1024d4964e5a015dd0ce3929e3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-18 21:10:50 +0000

    7b56daffe6 fix(docs-infra): add fallback to web fonts so downloading does not block rendering (#35352)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+29.sha-d7c4f40
+ * @license Angular v9.1.0-next.0+33.sha-7b56daf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.0+29.sha-d7c4f40');
+    var VERSION = new core.Version('9.1.0-next.0+33.sha-7b56daf');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+29.sha-d7c4f40
+ * @license Angular v9.1.0-next.0+33.sha-7b56daf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.0+29.sha-d7c4f40');
+const VERSION = new Version('9.1.0-next.0+33.sha-7b56daf');
 
 /**
  * @fileoverview added by tsickle
