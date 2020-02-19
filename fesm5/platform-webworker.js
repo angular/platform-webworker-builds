99c964a0ae17411d8ddde771754f20ef660f6cb4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 01:47:30 +0000

    5fbfe6996a fix(ivy): wrong context passed to ngOnDestroy when resolved multiple times (#35249)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.0+37.sha-183a862
+ * @license Angular v9.1.0-next.0+38.sha-5fbfe69
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.0+37.sha-183a862');
+var VERSION = new Version('9.1.0-next.0+38.sha-5fbfe69');
 
 /**
  * @license
