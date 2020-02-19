5b26fc2dc79990c64b83f0fb9bcc668de9f66971
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-02-19 21:24:34 +0000

    3a97972e58 fix(docs-infra): improve focus styles in topnav and footer (#33255)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2
+ * @license Angular v9.0.2+3.sha-3a97972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.2');
+    var VERSION = new core.Version('9.0.2+3.sha-3a97972');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2
+ * @license Angular v9.0.2+3.sha-3a97972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.2');
+var VERSION = new Version('9.0.2+3.sha-3a97972');
 
 /**
  * @license
