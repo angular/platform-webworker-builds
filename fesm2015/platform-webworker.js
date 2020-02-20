767e881feb741ac96594fd23a32061a075c96ff0
Miško Hevery <misko@hevery.com>
2020-02-20 21:04:12 +0000

    646655d09a fix(compiler): use FatalDiagnosticError to generate better error messages (#35244)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+4.sha-4c693ea
+ * @license Angular v9.1.0-next.1+11.sha-646655d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+4.sha-4c693ea');
+const VERSION = new Version('9.1.0-next.1+11.sha-646655d');
 
 /**
  * @fileoverview added by tsickle
