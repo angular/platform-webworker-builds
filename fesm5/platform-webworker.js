420e775b8ca0e4a570c6c105f8b06bb3b65a1cbd
Miško Hevery <misko@hevery.com>
2020-02-20 19:15:12 +0000

    1b72fc10eb docs: edit displaying-data page to introduce section (#35507)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+1.sha-8e12707
+ * @license Angular v9.1.0-next.1+5.sha-1b72fc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+1.sha-8e12707');
+var VERSION = new Version('9.1.0-next.1+5.sha-1b72fc1');
 
 /**
  * @license
