286991ef025c455e77b22a1ddac487e160c593a8
Miško Hevery <misko@hevery.com>
2020-02-21 17:01:46 +0000

    824d9a8cbf build: update cli packages to latest versions (#35608)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+10.sha-72664ca
+ * @license Angular v9.0.2+19.sha-824d9a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.2+10.sha-72664ca');
+    var VERSION = new core.Version('9.0.2+19.sha-824d9a8');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.2+10.sha-72664ca
+ * @license Angular v9.0.2+19.sha-824d9a8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.2+10.sha-72664ca');
+const VERSION = new Version('9.0.2+19.sha-824d9a8');
 
 /**
  * @fileoverview added by tsickle
