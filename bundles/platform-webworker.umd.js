64831f5cdc578603ed2bebb19643d7caad9c950e
Miško Hevery <misko@hevery.com>
2020-02-21 21:01:28 +0000

    049f118c1b fix(language-service): provide hover for interpolation in attribute value (#35494)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+27.sha-54fd33f
+ * @license Angular v9.1.0-next.1+28.sha-049f118
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.1+27.sha-54fd33f');
+    var VERSION = new core.Version('9.1.0-next.1+28.sha-049f118');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+27.sha-54fd33f
+ * @license Angular v9.1.0-next.1+28.sha-049f118
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+27.sha-54fd33f');
+var VERSION = new Version('9.1.0-next.1+28.sha-049f118');
 
 /**
  * @license
