2951fdf2df58b12e62019e406e9791a3897e7537
Miško Hevery <misko@hevery.com>
2020-02-21 22:48:22 +0000

    e084835fb1 Revert "feat: support passive event options by defining global variables in zone.js config file (#34503)"

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+31.sha-e17bde9
+ * @license Angular v9.1.0-next.1+32.sha-e084835
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.1+31.sha-e17bde9');
+    var VERSION = new core.Version('9.1.0-next.1+32.sha-e084835');
 
     /**
      * @license
