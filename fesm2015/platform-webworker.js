c8d2aff915b4f0afb859c54ceecd645df0f15c1a
Miško Hevery <misko@hevery.com>
2020-02-21 17:38:56 +0000

    54fd33fb80 fix(language-service): infer context type of structural directives (#35537) (#35561)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+22.sha-8cb1f65
+ * @license Angular v9.1.0-next.1+27.sha-54fd33f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+22.sha-8cb1f65');
+const VERSION = new Version('9.1.0-next.1+27.sha-54fd33f');
 
 /**
  * @fileoverview added by tsickle
