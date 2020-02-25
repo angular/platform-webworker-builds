ebd7e8a0407374745541f55fd4f2fa181d3bdf57
Miško Hevery <misko@hevery.com>
2020-02-25 01:54:19 +0000

    f1ffa7a367 ci: move saucelabs_ivy & saucelabs_view_engine to the daily monitoring CircleCI workflow (#35516)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+54.sha-f9d483e
+ * @license Angular v9.1.0-next.1+52.sha-f1ffa7a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+54.sha-f9d483e');
+var VERSION = new Version('9.1.0-next.1+52.sha-f1ffa7a');
 
 /**
  * @license
