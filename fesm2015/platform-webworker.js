bec23501518fea5d63552b96314a2066b47ba382
Miško Hevery <misko@hevery.com>
2020-02-25 21:41:15 +0000

    8e354dae00 fix(language-service): get the right 'ElementAst' in the nested HTML tag (#35317)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+61.sha-1e20b2c
+ * @license Angular v9.1.0-next.1+59.sha-8e354da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.1+61.sha-1e20b2c');
+const VERSION = new Version('9.1.0-next.1+59.sha-8e354da');
 
 /**
  * @fileoverview added by tsickle
