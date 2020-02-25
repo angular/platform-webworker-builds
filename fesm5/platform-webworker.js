bfdda3713b35af8969b47632a669e54e636dc051
Miško Hevery <misko@hevery.com>
2020-02-25 01:51:32 +0000

    f9d483e76e feat: add a temp solution to support passive event listeners. (#34503)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+53.sha-af76651
+ * @license Angular v9.1.0-next.1+54.sha-f9d483e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+53.sha-af76651');
+var VERSION = new Version('9.1.0-next.1+54.sha-f9d483e');
 
 /**
  * @license
