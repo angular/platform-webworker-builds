2381d8a525c716cae9ea73c5222711d9d056ae1d
Miško Hevery <misko@hevery.com>
2020-02-26 21:04:49 +0000

    6f68eae9dd docs: more precise docs for template statement syntax (#34348)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.1+63.sha-bde4cd7
+ * @license Angular v9.1.0-next.1+64.sha-6f68eae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.1+63.sha-bde4cd7');
+var VERSION = new Version('9.1.0-next.1+64.sha-6f68eae');
 
 /**
  * @license
