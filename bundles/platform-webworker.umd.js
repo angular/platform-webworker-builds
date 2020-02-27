f553a30c2d106f49b24b60aaa04090c2d2ee1609
atscott <atscott@google.com>
2020-02-27 19:27:31 +0000

    bf6fbf5a74 feat(docs-infra): add useful links if landed on 404 page and no search results found (#34978)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3
+ * @license Angular v9.0.3+6.sha-bf6fbf5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.3');
+    var VERSION = new core.Version('9.0.3+6.sha-bf6fbf5');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3
+ * @license Angular v9.0.3+6.sha-bf6fbf5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.3');
+var VERSION = new Version('9.0.3+6.sha-bf6fbf5');
 
 /**
  * @license
