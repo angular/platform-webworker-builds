c86271849ba4676161f238cf4f6099aab4231ed6
Matias Niemelä <matias@yearofmoo.com>
2020-02-27 21:31:30 +0000

    9b53054ea8 refactor(ngcc): guard against a crash if source-map flattening fails (#35718)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3+6.sha-bf6fbf5
+ * @license Angular v9.0.3+9.sha-9b53054
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.0.3+6.sha-bf6fbf5');
+    var VERSION = new core.Version('9.0.3+9.sha-9b53054');
 
     /**
      * @license
