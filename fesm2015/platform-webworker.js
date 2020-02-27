ec3473abff5edb814f73d15e3a9e296e9a97a545
atscott <atscott@google.com>
2020-02-27 19:31:34 +0000

    01ab168774 feat(docs-infra): add useful links if landed on 404 page and no search results found (#34978)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+3.sha-20b0c80
+ * @license Angular v9.1.0-next.2+6.sha-01ab168
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+3.sha-20b0c80');
+const VERSION = new Version('9.1.0-next.2+6.sha-01ab168');
 
 /**
  * @fileoverview added by tsickle
