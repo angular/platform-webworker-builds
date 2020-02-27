7f131a9751b4178620cc6f27fe9485706636369c
atscott <atscott@google.com>
2020-02-27 22:29:23 +0000

    287d841486 build: allow auto-discover all typings files in npm package by ts-api-guardian (#35691)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.3+9.sha-9b53054
+ * @license Angular v9.0.4+3.sha-287d841
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.3+9.sha-9b53054');
+var VERSION = new Version('9.0.4+3.sha-287d841');
 
 /**
  * @license
