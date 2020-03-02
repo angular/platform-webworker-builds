67897d82caa719851a37801a20f572d2644b3064
atscott <atscott@google.com>
2020-03-02 16:45:59 +0000

    40da51f641 fix(compiler): support i18n attributes on `<ng-template>` tags (#35681)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+32.sha-35c9f0d
+ * @license Angular v9.1.0-next.2+33.sha-40da51f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.2+32.sha-35c9f0d');
+    var VERSION = new core.Version('9.1.0-next.2+33.sha-40da51f');
 
     /**
      * @license
