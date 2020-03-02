92e2fe4fd1f20f479d18b4b0265488efd307cf8d
atscott <atscott@google.com>
2020-03-02 17:52:26 +0000

    44e47da4cf docs: reword explanation for using singleton services decorative to be more understandable (#35002)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+35.sha-a153b61
+ * @license Angular v9.1.0-next.2+36.sha-44e47da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.2+35.sha-a153b61');
+    var VERSION = new core.Version('9.1.0-next.2+36.sha-44e47da');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+35.sha-a153b61
+ * @license Angular v9.1.0-next.2+36.sha-44e47da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+35.sha-a153b61');
+const VERSION = new Version('9.1.0-next.2+36.sha-44e47da');
 
 /**
  * @fileoverview added by tsickle
