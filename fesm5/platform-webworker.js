509eef73dd4c898a2af22931f49e3d3ecd78e8a8
atscott <atscott@google.com>
2020-03-02 16:53:30 +0000

    a153b61098 fix(core): treat `[class]` and `[className]` as unrelated bindings (#35668)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+33.sha-40da51f
+ * @license Angular v9.1.0-next.2+35.sha-a153b61
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+33.sha-40da51f');
+var VERSION = new Version('9.1.0-next.2+35.sha-a153b61');
 
 /**
  * @license
