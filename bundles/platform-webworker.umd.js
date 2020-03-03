58c710959ce9c5cd1ec2e4f15b3a09515a01047f
atscott <atscott@google.com>
2020-03-03 17:30:04 +0000

    f13ee3e692 build: update to rules_nodejs 1.4.0 (#35813)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+51.sha-89b5b97
+ * @license Angular v9.1.0-next.2+49.sha-f13ee3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.2+51.sha-89b5b97');
+    var VERSION = new core.Version('9.1.0-next.2+49.sha-f13ee3e');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+51.sha-89b5b97
+ * @license Angular v9.1.0-next.2+49.sha-f13ee3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+51.sha-89b5b97');
+const VERSION = new Version('9.1.0-next.2+49.sha-f13ee3e');
 
 /**
  * @fileoverview added by tsickle
