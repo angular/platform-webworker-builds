68dd6a82b9ff726c97a25bb829e697be469d820e
atscott <atscott@google.com>
2020-03-03 22:16:07 +0000

    1f8a243b67 fix(core): unable to NgModuleRef.injector in module constructor (#35731)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+56.sha-17cf04e
+ * @license Angular v9.1.0-next.2+57.sha-1f8a243
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+56.sha-17cf04e');
+const VERSION = new Version('9.1.0-next.2+57.sha-1f8a243');
 
 /**
  * @fileoverview added by tsickle
