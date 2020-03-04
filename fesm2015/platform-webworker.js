e8a7bf2ae10f270fa9e8d8329b2689c70570ab94
atscott <atscott@google.com>
2020-03-04 00:30:33 +0000

    d2d4225ce6 Revert "fix(core): log error instead of warning for unknown properties and elements (#35798)" (#35845)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.4+44.sha-218e82e
+ * @license Angular v9.0.4+45.sha-d2d4225
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.4+44.sha-218e82e');
+const VERSION = new Version('9.0.4+45.sha-d2d4225');
 
 /**
  * @fileoverview added by tsickle
