462f44148225b5d89f87e0b350f498557f2e9408
Matias Niemelä <matias@yearofmoo.com>
2020-03-04 22:16:27 +0000

    773d7b86aa fix(router): state data missing in routerLink (#33203)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5
+ * @license Angular v9.0.5+1.sha-773d7b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.5');
+const VERSION = new Version('9.0.5+1.sha-773d7b8');
 
 /**
  * @fileoverview added by tsickle
