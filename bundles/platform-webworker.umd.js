ff6860b9d7986265e5eabb76f39fa17e242fdef6
atscott <atscott@google.com>
2020-03-04 17:04:19 +0000

    2e728f7fff docs: remove `ivy` and mention `ve` label in docs (#35809)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+61.sha-d543b13
+ * @license Angular v9.1.0-next.2+62.sha-2e728f7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1203,7 +1203,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.1.0-next.2+61.sha-d543b13');
+    var VERSION = new i0.Version('9.1.0-next.2+62.sha-2e728f7');
 
     /**
      * @license
