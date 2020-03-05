37a7e14e32c5d67abcac889fdf16d1dd8ed4a3dd
Matias Niemelä <matias@yearofmoo.com>
2020-03-05 21:29:32 +0000

    716d50aa21 refactor(compiler): Break up parseTemplateBindings() for microsyntax (#35812)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+76.sha-8ef29b6
+ * @license Angular v9.1.0-next.2+79.sha-716d50a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+76.sha-8ef29b6');
+var VERSION = new Version('9.1.0-next.2+79.sha-716d50a');
 
 /**
  * @license
