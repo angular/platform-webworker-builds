807a88ab8ed6140b428808ba790eb3564acf2e6d
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 23:00:48 +0000

    958165888c fix(bazel): do not use manifest paths for generated imports within compilation unit (#35841)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+96.sha-82a6fc5
+ * @license Angular v9.1.0-next.2+98.sha-9581658
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+96.sha-82a6fc5');
+var VERSION = new Version('9.1.0-next.2+98.sha-9581658');
 
 /**
  * @license
