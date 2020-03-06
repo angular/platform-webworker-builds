b5d538b17f1d42fe49f59df63c4350d22dab2ba4
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 22:09:50 +0000

    d912d4f34c docs: markup mini fix (#35883) PR Close #35883

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+90.sha-bef14cf
+ * @license Angular v9.1.0-next.2+92.sha-d912d4f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+90.sha-bef14cf');
+var VERSION = new Version('9.1.0-next.2+92.sha-d912d4f');
 
 /**
  * @license
