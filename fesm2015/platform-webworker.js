061bbbe3d167626a6a213a5d6d59a3dd58a61fac
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:02:39 +0000

    44f9fa6ce5 docs: add missing closing bracket (#35890)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+85.sha-b42e2e0
+ * @license Angular v9.1.0-next.2+87.sha-44f9fa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.2+85.sha-b42e2e0');
+const VERSION = new Version('9.1.0-next.2+87.sha-44f9fa6');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+85.sha-b42e2e0
+ * @license Angular v9.1.0-next.2+87.sha-44f9fa6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.2+85.sha-b42e2e0');
+var VERSION = new Version('9.1.0-next.2+87.sha-44f9fa6');
 
 /**
  * @license
