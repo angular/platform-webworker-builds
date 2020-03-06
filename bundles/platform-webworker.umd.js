d84bb3e49d35cdef4a192604bbc39bb21e2b3b1d
Matias Niemelä <matias@yearofmoo.com>
2020-03-06 18:03:55 +0000

    59607dc495 fix(core): undecorated-classes-with-di migration should handle libraries generated with CLI versions past v6.2.0 (#35824)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+86.sha-a1b9061
+ * @license Angular v9.1.0-next.2+91.sha-59607dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.2+86.sha-a1b9061');
+    var VERSION = new core.Version('9.1.0-next.2+91.sha-59607dc');
 
     /**
      * @license
