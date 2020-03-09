6cb561c6c0e78f29f33db5022b554fdf6ea2d091
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:43:04 +0000

    4cc519eb10 refactor(benchpress): remove benchmarks_external from ..pullapprove.yml (#35670)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+22.sha-9e23a69
+ * @license Angular v9.0.5+29.sha-4cc519e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.5+22.sha-9e23a69');
+const VERSION = new Version('9.0.5+29.sha-4cc519e');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+22.sha-9e23a69
+ * @license Angular v9.0.5+29.sha-4cc519e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.5+22.sha-9e23a69');
+var VERSION = new Version('9.0.5+29.sha-4cc519e');
 
 /**
  * @license
