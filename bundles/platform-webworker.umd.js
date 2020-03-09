c5703d846a1b49fa6f68da25ffc643d96ab4fac9
Matias Niemelä <matias@yearofmoo.com>
2020-03-09 16:48:14 +0000

    2b8edbbb1d refactor(benchpress): remove benchmarks_external from ..pullapprove.yml (#35670)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.2+106.sha-11f7e27
+ * @license Angular v9.1.0-next.2+108.sha-2b8edbb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.2+106.sha-11f7e27');
+    var VERSION = new core.Version('9.1.0-next.2+108.sha-2b8edbb');
 
     /**
      * @license
