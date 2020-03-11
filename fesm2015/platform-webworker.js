fefc3088679585edb213df3d2690012274c6f496
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:27:42 +0000

    e262962b12 fix(docs-infra): add boilerplate path and project type for elements example project (#33484)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+6.sha-3112b1b
+ * @license Angular v9.0.6+17.sha-e262962
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.6+6.sha-3112b1b');
+const VERSION = new Version('9.0.6+17.sha-e262962');
 
 /**
  * @fileoverview added by tsickle
