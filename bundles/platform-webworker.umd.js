ac4a9fa2a5e571097888d86a44ad47f1af9cbbc7
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:15:40 +0000

    9a1240360b build: add more info to SauceLabs README (#35704)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+12.sha-10f1e94
+ * @license Angular v9.1.0-next.4+18.sha-9a12403
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.4+12.sha-10f1e94');
+    var VERSION = new core.Version('9.1.0-next.4+18.sha-9a12403');
 
     /**
      * @license
