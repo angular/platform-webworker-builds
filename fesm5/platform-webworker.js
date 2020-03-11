d8942482bedebe1a4e829b73e787cc859fb060b2
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 19:13:19 +0000

    5f7d06668e fix(compiler-cli): TypeScript peer dependency range (#36008)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+14.sha-5e3a898
+ * @license Angular v9.1.0-next.4+15.sha-5f7d066
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.4+14.sha-5e3a898');
+var VERSION = new Version('9.1.0-next.4+15.sha-5f7d066');
 
 /**
  * @license
