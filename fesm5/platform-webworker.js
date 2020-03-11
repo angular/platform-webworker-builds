ea727ca2c6fcf9743c14cc8280e3f8e58bbbc851
Matias Niemelä <matias@yearofmoo.com>
2020-03-11 01:35:04 +0000

    8bf26c67c9 docs(dev-infra): update triage and contributing docs for dev-infra (#35995)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.5+44.sha-ce728dd
+ * @license Angular v9.0.6+5.sha-8bf26c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.5+44.sha-ce728dd');
+var VERSION = new Version('9.0.6+5.sha-8bf26c6');
 
 /**
  * @license
