0f8035d0341c03b317e830d0373982388ab3e41e
Andrew Kushnir <akushnir@google.com>
2020-03-11 23:06:51 +0000

    2b6028b643 build: remove legacy integration test runner (#35985)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+34.sha-99ea5d7
+ * @license Angular v9.1.0-next.4+35.sha-2b6028b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-next.4+34.sha-99ea5d7');
+    var VERSION = new core.Version('9.1.0-next.4+35.sha-2b6028b');
 
     /**
      * @license
