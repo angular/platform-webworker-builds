4b815717dde49acfb2fa442eb242cd11c23c145c
Andrew Kushnir <akushnir@google.com>
2020-03-12 19:51:15 +0000

    f699c962bb test(core): Add benchpress test for transplanted view change detection (#36001)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+27.sha-dec80b0
+ * @license Angular v9.0.6+28.sha-f699c96
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.0.6+27.sha-dec80b0');
+var VERSION = new Version('9.0.6+28.sha-f699c96');
 
 /**
  * @license
