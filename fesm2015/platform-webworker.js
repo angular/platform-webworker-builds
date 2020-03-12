6bb302ef5516c9f572ff32289d91f972886e8ca8
Andrew Kushnir <akushnir@google.com>
2020-03-12 17:12:34 +0000

    37a48391f2 refactor(ngcc): remove unused `LockFileWithSignalHandlers` (#35938)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+36.sha-0d806c8
+ * @license Angular v9.1.0-next.4+37.sha-37a4839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.4+36.sha-0d806c8');
+const VERSION = new Version('9.1.0-next.4+37.sha-37a4839');
 
 /**
  * @fileoverview added by tsickle
