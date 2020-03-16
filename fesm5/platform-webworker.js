769636126b4e2731eccab332bbc0398bdd407279
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:25:53 +0000

    3fa895298d fix(zone.js): zone.js patches rxjs should check null for unsubscribe (#35990)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+53.sha-0f8e710
+ * @license Angular v9.1.0-next.4+56.sha-3fa8952
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.4+53.sha-0f8e710');
+var VERSION = new Version('9.1.0-next.4+56.sha-3fa8952');
 
 /**
  * @license
