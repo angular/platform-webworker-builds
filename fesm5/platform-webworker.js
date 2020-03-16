e735c0cd11a96fcf7b577fe2e994f70906bf299a
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:19:48 +0000

    31bec8ce61 feat(compiler): Propagate source span and value span to Variable AST (#36047)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+58.sha-e179c58
+ * @license Angular v9.1.0-next.4+59.sha-31bec8c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.4+58.sha-e179c58');
+var VERSION = new Version('9.1.0-next.4+59.sha-31bec8c');
 
 /**
  * @license
