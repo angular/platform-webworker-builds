9575d7ce1651fd4afa21515a7548215573b087e3
Andrew Kushnir <akushnir@google.com>
2020-03-16 18:18:58 +0000

    a52b5680cb fix(compiler): do not recurse to find static symbols of same module (#35262)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.0.6+39.sha-1342668
+ * @license Angular v9.0.6+41.sha-a52b5680
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.0.6+39.sha-1342668');
+const VERSION = new Version('9.0.6+41.sha-a52b5680');
 
 /**
  * @fileoverview added by tsickle
