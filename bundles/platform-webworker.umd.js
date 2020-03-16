f07d7db7e64b2bfad5615658156ec119ad91b7f6
Andrew Kushnir <akushnir@google.com>
2020-03-16 16:24:16 +0000

    0f8e710c7c feat(zone.js): Monkey patch MessagePort.prototype onproperties (#34610)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+54.sha-8456c5e
+ * @license Angular v9.1.0-next.4+53.sha-0f8e710
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1203,7 +1203,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.1.0-next.4+54.sha-8456c5e');
+    var VERSION = new i0.Version('9.1.0-next.4+53.sha-0f8e710');
 
     /**
      * @license
