c73ed8e8bd258147acbbd01d001774d89bbdb074
Andrew Kushnir <akushnir@google.com>
2020-03-17 18:03:48 +0000

    afc9839f43 build(zone.js): Update `zone.js` release process to use `google-wombot` (#36110)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+67.sha-e3ecdc6
+ * @license Angular v9.1.0-next.4+68.sha-afc9839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1203,7 +1203,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('9.1.0-next.4+67.sha-e3ecdc6');
+    var VERSION = new i0.Version('9.1.0-next.4+68.sha-afc9839');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+67.sha-e3ecdc6
+ * @license Angular v9.1.0-next.4+68.sha-afc9839
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.4+67.sha-e3ecdc6');
+var VERSION = new Version('9.1.0-next.4+68.sha-afc9839');
 
 /**
  * @license
