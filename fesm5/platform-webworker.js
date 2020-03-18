476fc53093e16230cc50fa0970c258f69da369ea
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:00:00 +0000

    c1a0f0617c release: cut the v9.1.0-next.5 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.4+104.sha-13f7631
+ * @license Angular v9.1.0-next.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-next.4+104.sha-13f7631');
+var VERSION = new Version('9.1.0-next.5');
 
 /**
  * @license
