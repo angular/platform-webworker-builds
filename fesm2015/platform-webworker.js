b1e8fe68ac5065ed0e007080cdc1653927d83ba5
Andrew Kushnir <akushnir@google.com>
2020-03-18 23:18:55 +0000

    d50881a86e docs: move asset management section (#35665)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-next.5+6.sha-c9f554c
+ * @license Angular v9.1.0-next.5+1.sha-d50881a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-next.5+6.sha-c9f554c');
+const VERSION = new Version('9.1.0-next.5+1.sha-d50881a');
 
 /**
  * @fileoverview added by tsickle
