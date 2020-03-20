05dc09d2fcdfde866a0047e4113dae50ed777a21
Misko Hevery <misko@hevery.com>
2020-03-20 21:20:39 +0000

    d25012e864 docs(zone.js): Typos on zone.md file and fixes on code examples. (#36138)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+10.sha-c21c46a
+ * @license Angular v9.1.0-rc.0+11.sha-d25012e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+10.sha-c21c46a');
+var VERSION = new Version('9.1.0-rc.0+11.sha-d25012e');
 
 /**
  * @license
