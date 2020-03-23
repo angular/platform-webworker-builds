bb35cf50c38a6daeb85aa7f39b2b6de87e4a5700
Misko Hevery <misko@hevery.com>
2020-03-23 19:00:58 +0000

    1cb7b88505 fix(dev-infra): change circular deps positional params to camelCase (#36165)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+19.sha-fced8ee
+ * @license Angular v9.1.0-rc.0+18.sha-1cb7b88
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-rc.0+19.sha-fced8ee');
+    var VERSION = new core.Version('9.1.0-rc.0+18.sha-1cb7b88');
 
     /**
      * @license
