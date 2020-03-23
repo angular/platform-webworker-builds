5d1a61960089e5e70b77a82efbb845d7c500d0c8
Misko Hevery <misko@hevery.com>
2020-03-23 18:56:54 +0000

    e81ad3a1bc docs: Add asterisk info in template syntax guide (#36176)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+13.sha-0ce8ad3
+ * @license Angular v9.1.0-rc.0+16.sha-e81ad3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-rc.0+13.sha-0ce8ad3');
+    var VERSION = new core.Version('9.1.0-rc.0+16.sha-e81ad3a');
 
     /**
      * @license
