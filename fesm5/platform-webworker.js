660d04ec50b10946a28e648cea51d1c1dbed0926
Misko Hevery <misko@hevery.com>
2020-03-24 23:42:50 +0000

    4f66250618 docs: release notes for the v9.1.0-rc.2 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+34.sha-d783519
+ * @license Angular v9.1.0-rc.0+35.sha-4f66250
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+34.sha-d783519');
+var VERSION = new Version('9.1.0-rc.0+35.sha-4f66250');
 
 /**
  * @license
