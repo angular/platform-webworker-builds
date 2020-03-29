664b50e7670b8c9558f799df7106888f5e05a87c
Igor Minar <igor@angular.io>
2020-03-29 06:44:35 +0000

    c94a33c525 refactor(dev-infra): fix lint warnings for Pullapprove-related scripts (#36287)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+54.sha-5cee709
+ * @license Angular v9.1.0-rc.0+55.sha-c94a33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+54.sha-5cee709');
+const VERSION = new Version('9.1.0-rc.0+55.sha-c94a33c');
 
 /**
  * @fileoverview added by tsickle
