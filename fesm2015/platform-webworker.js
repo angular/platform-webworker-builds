e8414442387afa8246074e130dd52dc70ad6f59d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:53:47 +0000

    2510e7dad6 docs: fix typo in Schematics guide (#36328)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+60.sha-5b6ced5
+ * @license Angular v9.1.0-rc.0+62.sha-2510e7d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+60.sha-5b6ced5');
+const VERSION = new Version('9.1.0-rc.0+62.sha-2510e7d');
 
 /**
  * @fileoverview added by tsickle
