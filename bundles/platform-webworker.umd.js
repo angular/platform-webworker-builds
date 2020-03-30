66afc09c1d9fc8e8552efac3d29e42fc7e6ea09e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 22:54:46 +0000

    80c68583d1 refactor(core): use more narrow `QueryList` import to avoid circular deps issue (#36286)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+62.sha-2510e7d
+ * @license Angular v9.1.0-rc.0+61.sha-80c6858
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1210,7 +1210,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-rc.0+62.sha-2510e7d');
+    var VERSION = new core.Version('9.1.0-rc.0+61.sha-80c6858');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+62.sha-2510e7d
+ * @license Angular v9.1.0-rc.0+61.sha-80c6858
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+62.sha-2510e7d');
+var VERSION = new Version('9.1.0-rc.0+61.sha-80c6858');
 
 /**
  * @license
