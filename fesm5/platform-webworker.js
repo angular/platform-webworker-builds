5cde638ca058b3101ac3764c0cb6125400aabbc2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-03-30 19:21:26 +0000

    15cffa210c build: update to rules_nodejs 1.5.0 (#36307)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+16.sha-c3b297a
+ * @license Angular v9.1.0+20.sha-15cffa2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0+16.sha-c3b297a');
+var VERSION = new Version('9.1.0+20.sha-15cffa2');
 
 /**
  * @license
