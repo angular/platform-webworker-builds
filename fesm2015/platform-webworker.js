5b06ed40a78e3c0f81c3e9c7f51be7874a49aff3
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:51:11 +0000

    5516802142 fix(compiler): avoid undefined expressions in holey array (#36343)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+73.sha-9d8bb63
+ * @license Angular v9.1.0-rc.0+82.sha-5516802
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+73.sha-9d8bb63');
+const VERSION = new Version('9.1.0-rc.0+82.sha-5516802');
 
 /**
  * @fileoverview added by tsickle
