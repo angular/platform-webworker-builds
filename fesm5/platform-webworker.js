0fca71d405ae95b671c9021b3fd3a2e892c99c1f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:56:54 +0000

    cb0a2a055d fix(ngcc): allow ngcc configuration to match pre-release versions of packages (#36370)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+27.sha-beb3f19
+ * @license Angular v9.1.0+43.sha-cb0a2a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0+27.sha-beb3f19');
+var VERSION = new Version('9.1.0+43.sha-cb0a2a0');
 
 /**
  * @license
