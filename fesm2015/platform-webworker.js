ec22d658879a70526d9e6dc87aad1370ce5639a1
Kara Erickson <karakara@google.com>
2020-04-01 20:34:32 +0000

    4d1d0fa03b docs: update ng-conf announcement to remove livestream link (#36382)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+67.sha-ec8bae1
+ * @license Angular v9.1.0-rc.0+68.sha-4d1d0fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+67.sha-ec8bae1');
+const VERSION = new Version('9.1.0-rc.0+68.sha-4d1d0fa');
 
 /**
  * @fileoverview added by tsickle
