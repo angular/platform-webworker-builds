54d3e288fb7979fa07d1b773a038bd3f53bdb49b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-04-01 20:53:01 +0000

    cc4b813e75 fix(ngcc): handle bad path mappings when finding entry-points (#36331)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+82.sha-5516802
+ * @license Angular v9.1.0-rc.0+81.sha-cc4b813
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+82.sha-5516802');
+var VERSION = new Version('9.1.0-rc.0+81.sha-cc4b813');
 
 /**
  * @license
