c6789557d069e5cdfcc66eabc02fde82919b42c1
Kara Erickson <karakara@google.com>
2020-04-01 22:45:50 +0000

    4d458db1b5 docs: update and edit web-worker page (#36383)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+84.sha-326240e
+ * @license Angular v9.1.0-rc.0+87.sha-4d458db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+84.sha-326240e');
+const VERSION = new Version('9.1.0-rc.0+87.sha-4d458db');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+84.sha-326240e
+ * @license Angular v9.1.0-rc.0+87.sha-4d458db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+84.sha-326240e');
+var VERSION = new Version('9.1.0-rc.0+87.sha-4d458db');
 
 /**
  * @license
