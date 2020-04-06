9a3ab095e33456a6ffbc40196bc7d71277f75d44
Kara Erickson <karakara@google.com>
2020-04-06 22:57:34 +0000

    aeb6d0d0f7 fix(zone.js): should not try to patch fetch if it is not writable (#36311)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+81.sha-068e283
+ * @license Angular v9.1.0+88.sha-aeb6d0d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0+81.sha-068e283');
+var VERSION = new Version('9.1.0+88.sha-aeb6d0d');
 
 /**
  * @license
