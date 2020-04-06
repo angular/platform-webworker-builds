c717db39a84cd0a2c58a5b942e9e7d234dd88bd2
Kara Erickson <karakara@google.com>
2020-04-06 16:57:11 +0000

    1b4df6484e docs: add missing command in `WebWorker` guide (#36397)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+115.sha-1140bbc
+ * @license Angular v9.1.0-rc.0+114.sha-1b4df64
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-rc.0+115.sha-1140bbc');
+    var VERSION = new core.Version('9.1.0-rc.0+114.sha-1b4df64');
 
     /**
      * @license
