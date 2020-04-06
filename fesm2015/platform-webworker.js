99084f611d15e93a1c3e080484f04203ba81cd8f
Kara Erickson <karakara@google.com>
2020-04-06 22:55:41 +0000

    eb8c6c7eff test(language-service): Move pipe tests to TEST_TEMPLATE (#36407)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+123.sha-09c8416
+ * @license Angular v9.1.0-rc.0+128.sha-eb8c6c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+123.sha-09c8416');
+const VERSION = new Version('9.1.0-rc.0+128.sha-eb8c6c7');
 
 /**
  * @fileoverview added by tsickle
