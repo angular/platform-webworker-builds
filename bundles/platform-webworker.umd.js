96c8ff73651d7a0babd6f04aa32b68260f550583
Kara Erickson <karakara@google.com>
2020-04-06 18:57:11 +0000

    f9fb8338f5 fix(ngcc): support ignoring deep-imports via package config (#36423)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+114.sha-1b4df64
+ * @license Angular v9.1.0-rc.0+118.sha-f9fb833
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.0-rc.0+114.sha-1b4df64');
+    var VERSION = new core.Version('9.1.0-rc.0+118.sha-f9fb833');
 
     /**
      * @license
