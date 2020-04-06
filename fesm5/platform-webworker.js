adc49c6cdd7f1b719ca9014eb6cb64abe3faef02
Kara Erickson <karakara@google.com>
2020-04-06 21:22:11 +0000

    09c84169d5 docs: fix broken guide link (tutorial/index --> tutorial) (#36453)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+122.sha-d43c306
+ * @license Angular v9.1.0-rc.0+123.sha-09c8416
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+122.sha-d43c306');
+var VERSION = new Version('9.1.0-rc.0+123.sha-09c8416');
 
 /**
  * @license
