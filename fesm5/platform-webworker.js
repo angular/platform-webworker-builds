15f83db714c0350cb652bd70e25477e16b611852
Kara Erickson <karakara@google.com>
2020-04-07 22:32:34 +0000

    81195a238b fix(language-service): use the `HtmlAst` to get the span of HTML tag (#36371)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+141.sha-4a18428
+ * @license Angular v9.1.0-rc.0+144.sha-81195a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+141.sha-4a18428');
+var VERSION = new Version('9.1.0-rc.0+144.sha-81195a2');
 
 /**
  * @license
