dd3b8eb570e4e98029b613303e049529f99f176d
Kara Erickson <karakara@google.com>
2020-04-07 22:46:39 +0000

    fd2ef74a31 docs: update reactive forms page (#35969)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0+99.sha-b02c950
+ * @license Angular v9.1.0+106.sha-fd2ef74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0+99.sha-b02c950');
+const VERSION = new Version('9.1.0+106.sha-fd2ef74');
 
 /**
  * @fileoverview added by tsickle
