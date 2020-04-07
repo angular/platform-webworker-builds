3f6c6f0a27e63deae0fc40b5043d99f19d51ec38
Kara Erickson <karakara@google.com>
2020-04-07 18:57:25 +0000

    7f28845f58 test(language-service): remove ng-for-cases.ts (#36470)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+138.sha-e0415db
+ * @license Angular v9.1.0-rc.0+140.sha-7f28845
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+138.sha-e0415db');
+var VERSION = new Version('9.1.0-rc.0+140.sha-7f28845');
 
 /**
  * @license
