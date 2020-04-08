b912132f9404ec89f34994435e9f0fda3ccad99c
atscott <atscott@google.com>
2020-04-08 21:55:16 +0000

    717df13207 fix(ngcc): do not warn if `paths` mapping does not exist (#36525)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1
+ * @license Angular v10.0.0-next.1+1.sha-717df13
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1220,7 +1220,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('10.0.0-next.1');
+    var VERSION = new i0.Version('10.0.0-next.1+1.sha-717df13');
 
     /**
      * @license
