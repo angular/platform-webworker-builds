a33748b75a7886707e3b5d8650abe677139ba8bf
atscott <atscott@google.com>
2020-04-08 22:49:13 +0000

    5da621d3dd fix(language-service): remove circular dependency instance (#36463)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+11.sha-cbed582
+ * @license Angular v9.1.1+12.sha-5da621d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.1+11.sha-cbed582');
+    var VERSION = new core.Version('9.1.1+12.sha-5da621d');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+11.sha-cbed582
+ * @license Angular v9.1.1+12.sha-5da621d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.1+11.sha-cbed582');
+const VERSION = new Version('9.1.1+12.sha-5da621d');
 
 /**
  * @fileoverview added by tsickle
