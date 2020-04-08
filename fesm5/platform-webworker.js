2a1a4d8546f16528a49608bd810fcc848f5f0f4c
Kara Erickson <karakara@google.com>
2020-04-08 18:44:30 +0000

    db4a448439 release: cut the v10.0.0-next.0 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+151.sha-7549c65
+ * @license Angular v10.0.0-next.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.0-rc.0+151.sha-7549c65');
+var VERSION = new Version('10.0.0-next.0');
 
 /**
  * @license
