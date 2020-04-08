335c3cca0ced9607a568ed48561bac1973eb3455
atscott <atscott@google.com>
2020-04-08 21:50:58 +0000

    33eee43263 fix(ngcc): do not warn if `paths` mapping does not exist (#36525)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+8.sha-72053b0
+ * @license Angular v9.1.1+9.sha-33eee43
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.1+8.sha-72053b0');
+    var VERSION = new core.Version('9.1.1+9.sha-33eee43');
 
     /**
      * @license
