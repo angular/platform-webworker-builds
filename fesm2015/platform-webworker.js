1a798a5520f2c2723020ba6524e62f6fd929bac4
Kara Erickson <karakara@google.com>
2020-04-08 00:01:58 +0000

    1beccc1a6c docs: release notes for the v9.1.1 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.0-rc.0+149.sha-0075017
+ * @license Angular v9.1.0-rc.0+150.sha-1beccc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.0-rc.0+149.sha-0075017');
+const VERSION = new Version('9.1.0-rc.0+150.sha-1beccc1');
 
 /**
  * @fileoverview added by tsickle
