ea6240935a78e206f95823be7d73f3900824c4ce
Kara Erickson <karakara@google.com>
2020-04-08 19:34:09 +0000

    03f2f1ae47 build(bazel): fix runfiles resolve in karma-saucelabs.js after $location => $rootpath cleanup (#36511)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.0
+ * @license Angular v10.0.0-next.0+8.sha-03f2f1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.0');
+const VERSION = new Version('10.0.0-next.0+8.sha-03f2f1a');
 
 /**
  * @fileoverview added by tsickle
