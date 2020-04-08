a2704d230bffb903f780abebdf46786f75819235
atscott <atscott@google.com>
2020-04-08 23:21:42 +0000

    333b8679ad refactor(bazel): use runfiles helper in ts-api-guardian (#36471)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+12.sha-5da621d
+ * @license Angular v9.1.1+13.sha-333b867
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.1+12.sha-5da621d');
+    var VERSION = new core.Version('9.1.1+13.sha-333b867');
 
     /**
      * @license
