c2d8895800cc442265a2331b1346b3163aacb4f0
atscott <atscott@google.com>
2020-04-08 22:12:49 +0000

    cbed582a1a style(compiler): reformat of codebase with new clang-format version (#36520)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+9.sha-33eee43
+ * @license Angular v9.1.1+11.sha-cbed582
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.1+9.sha-33eee43');
+    var VERSION = new core.Version('9.1.1+11.sha-cbed582');
 
     /**
      * @license
