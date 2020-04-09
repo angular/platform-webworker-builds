2b7136e196e55022901aa2eba5d11b3502824a6b
atscott <atscott@google.com>
2020-04-09 18:55:36 +0000

    13ae2d7271 style: format forms validators to fix lint error (#36546)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+13.sha-078b0be
+ * @license Angular v10.0.0-next.1+12.sha-13ae2d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+13.sha-078b0be');
+const VERSION = new Version('10.0.0-next.1+12.sha-13ae2d7');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+13.sha-078b0be
+ * @license Angular v10.0.0-next.1+12.sha-13ae2d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.1+13.sha-078b0be');
+var VERSION = new Version('10.0.0-next.1+12.sha-13ae2d7');
 
 /**
  * @license
