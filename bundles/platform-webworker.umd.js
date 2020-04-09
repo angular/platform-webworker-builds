774cbf88f3674413926471012a079014d41a0d87
atscott <atscott01@gmail.com>
2020-04-09 01:24:35 +0000

    1bfa908ab3 style: typescript lint fix (#36531)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+13.sha-333b867
+ * @license Angular v9.1.1+15.sha-1bfa908
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.1+13.sha-333b867');
+    var VERSION = new core.Version('9.1.1+15.sha-1bfa908');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+13.sha-333b867
+ * @license Angular v9.1.1+15.sha-1bfa908
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.1+13.sha-333b867');
+var VERSION = new Version('9.1.1+15.sha-1bfa908');
 
 /**
  * @license
