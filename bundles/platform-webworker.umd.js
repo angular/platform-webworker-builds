60a62cffbc93713efbbac008a7b9a1fe9aa362fd
atscott <atscott@google.com>
2020-04-09 17:07:41 +0000

    e526f74dfd refactor(compiler): create a new root `BindingScope` for each template (#36362)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+7.sha-9e78f55
+ * @license Angular v10.0.0-next.1+8.sha-e526f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.1+7.sha-9e78f55');
+    var VERSION = new core.Version('10.0.0-next.1+8.sha-e526f74');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+7.sha-9e78f55
+ * @license Angular v10.0.0-next.1+8.sha-e526f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.1+7.sha-9e78f55');
+var VERSION = new Version('10.0.0-next.1+8.sha-e526f74');
 
 /**
  * @license
