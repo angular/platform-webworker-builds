580394bc26e192fdc3946e16536463cd42f2bec6
atscott <atscott@google.com>
2020-04-09 20:58:09 +0000

    58d028178f fix(docs-infra): fix `elements` example when used with ES5 (#36536)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+25.sha-e06512b
+ * @license Angular v9.1.1+26.sha-58d0281
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1012,7 +1012,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.1+25.sha-e06512b');
+var VERSION = new Version('9.1.1+26.sha-58d0281');
 
 /**
  * @license
