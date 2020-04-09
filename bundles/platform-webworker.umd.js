0d729fcc4fa5e60d98aad8af9beb5ea5e374ad58
atscott <atscott@google.com>
2020-04-09 18:53:46 +0000

    13ae2d7271 style: format forms validators to fix lint error (#36546)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+13.sha-078b0be
+ * @license Angular v10.0.0-next.1+12.sha-13ae2d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.1+13.sha-078b0be');
+    var VERSION = new core.Version('10.0.0-next.1+12.sha-13ae2d7');
 
     /**
      * @license
