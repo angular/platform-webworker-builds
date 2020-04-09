2fea5f69ca37bd36d06e0e5d4f449a0448124239
atscott <atscott@google.com>
2020-04-09 19:07:30 +0000

    4aa4e6fd03 fix(compiler): handle type references to namespaced symbols correctly (#36106)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.1+17.sha-3bedfda');
+    var VERSION = new core.Version('10.0.0-next.1+14.sha-4aa4e6f');
 
     /**
      * @license
