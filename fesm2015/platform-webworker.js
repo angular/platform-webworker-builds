8912cbd26fe03970054141acdb48c141b8ff387c
atscott <atscott@google.com>
2020-04-09 17:09:37 +0000

    e526f74dfd refactor(compiler): create a new root `BindingScope` for each template (#36362)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+7.sha-9e78f55
+ * @license Angular v10.0.0-next.1+8.sha-e526f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+7.sha-9e78f55');
+const VERSION = new Version('10.0.0-next.1+8.sha-e526f74');
 
 /**
  * @fileoverview added by tsickle
