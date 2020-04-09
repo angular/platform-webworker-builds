0f06b9a340f7b27567c58e7a6ec08b7715480380
atscott <atscott@google.com>
2020-04-09 17:52:10 +0000

    66724fd159 docs(forms): clarify the description of `minLength` and `maxLength` (#36297)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+17.sha-c8f9092
+ * @license Angular v9.1.1+19.sha-66724fd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.1+17.sha-c8f9092');
+    var VERSION = new core.Version('9.1.1+19.sha-66724fd');
 
     /**
      * @license
