711fab89edcf90ea82506aea56e8c937ba13b9f9
atscott <atscott@google.com>
2020-04-09 19:09:21 +0000

    4aa4e6fd03 fix(compiler): handle type references to namespaced symbols correctly (#36106)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1220,7 +1220,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('10.0.0-next.1+17.sha-3bedfda');
+    var VERSION = new i0.Version('10.0.0-next.1+14.sha-4aa4e6f');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+17.sha-3bedfda
+ * @license Angular v10.0.0-next.1+14.sha-4aa4e6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2063,7 +2063,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+17.sha-3bedfda');
+const VERSION = new Version('10.0.0-next.1+14.sha-4aa4e6f');
 
 /**
  * @fileoverview added by tsickle
