8ec8a3a6b00d8595970b822ac76d99612cf6b62b
atscott <atscott@google.com>
2020-04-09 17:06:51 +0000

    c8f9092364 fix(dev-infra): fix commit message validation in git worktrees (#36507)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.1+15.sha-1bfa908
+ * @license Angular v9.1.1+17.sha-c8f9092
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.1+15.sha-1bfa908');
+const VERSION = new Version('9.1.1+17.sha-c8f9092');
 
 /**
  * @fileoverview added by tsickle
