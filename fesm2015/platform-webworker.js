543fc490f85467a764b97cb474da377a1b1ed3e0
atscott <atscott@google.com>
2020-04-10 16:30:32 +0000

    6ab43d7335 fix(ngcc): correctly detect external files from nested `node_modules/` (#36559)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+19.sha-fee3161
+ * @license Angular v10.0.0-next.1+20.sha-6ab43d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+19.sha-fee3161');
+const VERSION = new Version('10.0.0-next.1+20.sha-6ab43d7');
 
 /**
  * @fileoverview added by tsickle
