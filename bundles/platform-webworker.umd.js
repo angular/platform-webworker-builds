e7dff389759648694bda4c3c643e0fd0742f40ae
atscott <atscott@google.com>
2020-04-10 21:16:34 +0000

    ca677481a2 fix(core): undecorated-classes-with-decorated-fields migration should avoid error if base class has no value declaration (#36543)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+20.sha-6ab43d7
+ * @license Angular v10.0.0-next.1+21.sha-ca67748
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.1+20.sha-6ab43d7');
+    var VERSION = new core.Version('10.0.0-next.1+21.sha-ca67748');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+20.sha-6ab43d7
+ * @license Angular v10.0.0-next.1+21.sha-ca67748
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+20.sha-6ab43d7');
+const VERSION = new Version('10.0.0-next.1+21.sha-ca67748');
 
 /**
  * @fileoverview added by tsickle
