caeebbee91cd553850c5474149bd79388ae15223
atscott <atscott@google.com>
2020-04-13 15:42:12 +0000

    713bce19f4 docs: move ng-conf 2020 to the already presented section (#36413)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+22.sha-5737e65
+ * @license Angular v10.0.0-next.1+26.sha-713bce1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.1+22.sha-5737e65');
+    var VERSION = new core.Version('10.0.0-next.1+26.sha-713bce1');
 
     /**
      * @license
