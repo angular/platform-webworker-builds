aeb6984a22e95895859567d3c7ba030ea4cbdac6
atscott <atscott@google.com>
2020-04-13 15:41:25 +0000

    9181b73e5a docs: fix typo in Tests guide (#36592)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+21.sha-ca67748
+ * @license Angular v10.0.0-next.1+23.sha-9181b73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1220,7 +1220,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new i0.Version('10.0.0-next.1+21.sha-ca67748');
+    var VERSION = new i0.Version('10.0.0-next.1+23.sha-9181b73');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+21.sha-ca67748
+ * @license Angular v10.0.0-next.1+23.sha-9181b73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.1+21.sha-ca67748');
+var VERSION = new Version('10.0.0-next.1+23.sha-9181b73');
 
 /**
  * @license
