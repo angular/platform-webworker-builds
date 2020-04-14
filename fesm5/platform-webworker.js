0a6468eab742b06dbceb80a10ba454d768b76525
atscott <atscott@google.com>
2020-04-14 17:37:25 +0000

    29122c518b style: lint (#36580)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1005,7 +1005,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.1+27.sha-0cc53fb');
+var VERSION = new Version('10.0.0-next.1+30.sha-29122c5');
 
 /**
  * @license
