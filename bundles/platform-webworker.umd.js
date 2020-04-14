d01efc2141effcf8412df2804f8cd4ace78d71cd
atscott <atscott@google.com>
2020-04-14 17:35:41 +0000

    29122c518b style: lint (#36580)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1227,7 +1227,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.1+27.sha-0cc53fb');
+    var VERSION = new core.Version('10.0.0-next.1+30.sha-29122c5');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+27.sha-0cc53fb
+ * @license Angular v10.0.0-next.1+30.sha-29122c5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+27.sha-0cc53fb');
+const VERSION = new Version('10.0.0-next.1+30.sha-29122c5');
 
 /**
  * @fileoverview added by tsickle
