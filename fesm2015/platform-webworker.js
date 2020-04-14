7e6df0c19cc4f5b6bec18685e013a7655012713e
atscott <atscott@google.com>
2020-04-14 17:37:15 +0000

    d50cb30443 test: disable failing saucelabs tests (#36620)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+30.sha-29122c5
+ * @license Angular v10.0.0-next.1+31.sha-d50cb30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1979,7 +1979,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+30.sha-29122c5');
+const VERSION = new Version('10.0.0-next.1+31.sha-d50cb30');
 
 /**
  * @fileoverview added by tsickle
