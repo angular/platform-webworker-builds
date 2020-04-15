95b26c0532734f0200512f6f51644a4988376f84
atscott <atscott@google.com>
2020-04-15 16:50:32 +0000

    e041ac6f0d fix(ngcc): display output from the unlocker process on Windows (#36569)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+36.sha-663b768
+ * @license Angular v10.0.0-next.1+38.sha-e041ac6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.1+36.sha-663b768');
+var VERSION = new Version('10.0.0-next.1+38.sha-e041ac6');
 
 /**
  * @license
