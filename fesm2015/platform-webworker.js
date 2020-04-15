dde7be374807a9032e40973e85616ddca03e49dc
atscott <atscott@google.com>
2020-04-15 16:43:59 +0000

    663b768780 fix(ngcc): force ngcc to exit on error (#36622)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+35.sha-0147de9
+ * @license Angular v10.0.0-next.1+36.sha-663b768
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.1+35.sha-0147de9');
+const VERSION = new Version('10.0.0-next.1+36.sha-663b768');
 
 /**
  * @fileoverview added by tsickle
