ed43a2894fb9beb0e051a5fb393a08ab97254adc
atscott <atscott@google.com>
2020-04-15 23:39:18 +0000

    074266b896 release: cut the v10.0.0-next.2 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.1+39.sha-c4dc642
+ * @license Angular v10.0.0-next.2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.1+39.sha-c4dc642');
+var VERSION = new Version('10.0.0-next.2');
 
 /**
  * @license
