df2cbabdbc2456ff20765e7be15ddeb230d89957
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:34:05 +0000

    1fa1dd5aaa docs: getting started guide use pipe before introduction (#36584)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+28.sha-78840e5
+ * @license Angular v10.0.0-next.2+30.sha-1fa1dd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+28.sha-78840e5');
+const VERSION = new Version('10.0.0-next.2+30.sha-1fa1dd5');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+28.sha-78840e5
+ * @license Angular v10.0.0-next.2+30.sha-1fa1dd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2+28.sha-78840e5');
+var VERSION = new Version('10.0.0-next.2+30.sha-1fa1dd5');
 
 /**
  * @license
