33f9a529cf44bd483f52294263293c0f6004efa1
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 21:35:23 +0000

    4480ba3e29 ci: remove reliance on Github API for CI setup (#36500)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+31.sha-5e79799
+ * @license Angular v10.0.0-next.2+32.sha-4480ba3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.2+31.sha-5e79799');
+    var VERSION = new core.Version('10.0.0-next.2+32.sha-4480ba3');
 
     /**
      * @license
