996c6c3705bc737f01ac5ea5d41ef35ed16a4bd0
atscott <atscott@google.com>
2020-04-16 17:15:02 +0000

    797c306306 docs: add string-union type note (#35859)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2
+ * @license Angular v9.1.2+17.sha-797c306
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.2');
+const VERSION = new Version('9.1.2+17.sha-797c306');
 
 /**
  * @fileoverview added by tsickle
