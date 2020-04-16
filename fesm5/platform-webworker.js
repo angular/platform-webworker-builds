e916c102f7bc3216873d637ed6e71715571f33d0
atscott <atscott@google.com>
2020-04-16 17:09:12 +0000

    4a9f0bebc3 fix(core): prevent unknown property check for AOT-compiled components (#36072)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+3.sha-b1f1d3f
+ * @license Angular v10.0.0-next.2+14.sha-4a9f0be
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2+3.sha-b1f1d3f');
+var VERSION = new Version('10.0.0-next.2+14.sha-4a9f0be');
 
 /**
  * @license
