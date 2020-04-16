66e3dfe99a20c57d5b9e74af3fec88fcfccefee5
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:30:31 +0000

    8be0972836 fix(language-service): properly evaluate types in comparable expressions (#36529)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+27.sha-c332d4d
+ * @license Angular v10.0.0-next.2+29.sha-8be0972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+27.sha-c332d4d');
+const VERSION = new Version('10.0.0-next.2+29.sha-8be0972');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+27.sha-c332d4d
+ * @license Angular v10.0.0-next.2+29.sha-8be0972
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2+27.sha-c332d4d');
+var VERSION = new Version('10.0.0-next.2+29.sha-8be0972');
 
 /**
  * @license
