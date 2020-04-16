83a3f2dc47f806f9636edfb6ba976f7983f91787
Matias Niemelä <matias@yearofmoo.com>
2020-04-16 20:28:57 +0000

    c332d4d916 refactor(ngcc): moved shared setup into a single function (#36637)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+19.sha-bfda7ca
+ * @license Angular v10.0.0-next.2+27.sha-c332d4d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+19.sha-bfda7ca');
+const VERSION = new Version('10.0.0-next.2+27.sha-c332d4d');
 
 /**
  * @fileoverview added by tsickle
