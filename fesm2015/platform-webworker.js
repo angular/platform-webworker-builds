df520d6e3704c5cc9beb270df2d86b68b87f1f83
atscott <atscott@google.com>
2020-04-16 17:03:26 +0000

    c6e5fc4fbe fix(common): format day-periods that cross midnight (#36611)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2
+ * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2');
+const VERSION = new Version('10.0.0-next.2+1.sha-c6e5fc4');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2
+ * @license Angular v10.0.0-next.2+1.sha-c6e5fc4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2');
+var VERSION = new Version('10.0.0-next.2+1.sha-c6e5fc4');
 
 /**
  * @license
