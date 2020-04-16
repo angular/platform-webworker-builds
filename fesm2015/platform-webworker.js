6db32286e832e3c277a5933e9474d74f990267a1
atscott <atscott@google.com>
2020-04-16 18:30:52 +0000

    bfda7ca5bd docs(changelog): Add new line between changes (#36653)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+18.sha-305b059
+ * @license Angular v10.0.0-next.2+19.sha-bfda7ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+18.sha-305b059');
+const VERSION = new Version('10.0.0-next.2+19.sha-bfda7ca');
 
 /**
  * @fileoverview added by tsickle
