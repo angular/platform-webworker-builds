ff177a2381e129377030b8913735ebebb1423247
atscott <atscott@google.com>
2020-04-16 17:04:22 +0000

    1e3b8a1354 docs: remove unneeded code from `universal` example/guide (#36483)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+2.sha-03ff380
+ * @license Angular v10.0.0-next.2+12.sha-1e3b8a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2+2.sha-03ff380');
+var VERSION = new Version('10.0.0-next.2+12.sha-1e3b8a1');
 
 /**
  * @license
