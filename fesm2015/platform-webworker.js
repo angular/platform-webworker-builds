4e0e72643b2e3c6121417628f40df8bde14752c5
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 16:54:35 +0000

    4da901f8c0 docs: add tony bove to about page (#36335)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+32.sha-4480ba3
+ * @license Angular v10.0.0-next.2+34.sha-4da901f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+32.sha-4480ba3');
+const VERSION = new Version('10.0.0-next.2+34.sha-4da901f');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+32.sha-4480ba3
+ * @license Angular v10.0.0-next.2+34.sha-4da901f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2+32.sha-4480ba3');
+var VERSION = new Version('10.0.0-next.2+34.sha-4da901f');
 
 /**
  * @license
