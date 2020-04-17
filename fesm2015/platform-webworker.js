4e2ff21127464498bf15e072e8304af582139dde
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 22:40:33 +0000

    d7e9d8746a build: list feat commits in patch branch in relase review script (#36651)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+44.sha-3517599
+ * @license Angular v10.0.0-next.2+45.sha-d7e9d87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+44.sha-3517599');
+const VERSION = new Version('10.0.0-next.2+45.sha-d7e9d87');
 
 /**
  * @fileoverview added by tsickle
