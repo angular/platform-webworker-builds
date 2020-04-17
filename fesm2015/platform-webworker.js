9504e08c614ba29f719455329032206f0ee06093
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 20:59:57 +0000

    351759906b refactor(compiler): remove unused CachedFileSystem (#36687)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+41.sha-81d23b3
+ * @license Angular v10.0.0-next.2+44.sha-3517599
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+41.sha-81d23b3');
+const VERSION = new Version('10.0.0-next.2+44.sha-3517599');
 
 /**
  * @fileoverview added by tsickle
