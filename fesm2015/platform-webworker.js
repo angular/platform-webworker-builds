72a133267a96d5082139aa9a8e37da3cc57e8b88
Matias Niemelä <matias@yearofmoo.com>
2020-04-17 16:56:55 +0000

    bb150c2704 fixup!: build(docs-infra): Ensures that only member docs are linked (#36316)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+34.sha-4da901f
+ * @license Angular v10.0.0-next.2+40.sha-bb150c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+34.sha-4da901f');
+const VERSION = new Version('10.0.0-next.2+40.sha-bb150c2');
 
 /**
  * @fileoverview added by tsickle
