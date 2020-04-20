dad42ff7132e2ee7f1e0e51a0adecf55e73f1223
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:53:05 +0000

    2a53f47159 fix(dev-infra): exit non-zero if commit message validation failed (#36723)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+45.sha-722d939
+ * @license Angular v9.1.2+46.sha-2a53f47
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.2+45.sha-722d939');
+const VERSION = new Version('9.1.2+46.sha-2a53f47');
 
 /**
  * @fileoverview added by tsickle
