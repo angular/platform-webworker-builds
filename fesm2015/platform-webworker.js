bbca95876dc07b961314de8569e9e126fba6e4ec
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 17:57:56 +0000

    28ff02b116 docs: remove version ^7.0.0 from LTS support (#36708)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+46.sha-f4ef8a9
+ * @license Angular v10.0.0-next.2+47.sha-28ff02b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+46.sha-f4ef8a9');
+const VERSION = new Version('10.0.0-next.2+47.sha-28ff02b');
 
 /**
  * @fileoverview added by tsickle
