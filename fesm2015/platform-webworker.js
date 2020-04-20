80f266f8668a2ec933283afdb7483a8bb80c831a
Matias Niemelä <matias@yearofmoo.com>
2020-04-20 18:05:30 +0000

    b5e92b9a5d Revert "fix(router): pass correct component to canDeactivate checks when using two or more sibling router-outlets (#36302)" (#36697)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+47.sha-28ff02b
+ * @license Angular v10.0.0-next.2+48.sha-b5e92b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+47.sha-28ff02b');
+const VERSION = new Version('10.0.0-next.2+48.sha-b5e92b9');
 
 /**
  * @fileoverview added by tsickle
