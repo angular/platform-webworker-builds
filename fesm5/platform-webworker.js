d15ca2209fa141949e726164c7eaed4998033b99
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:11:26 +0000

    f186c32245 docs: place download section in architecture to the top (#36565)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+50.sha-458dc10
+ * @license Angular v10.0.0-next.2+53.sha-f186c32
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.2+50.sha-458dc10');
+var VERSION = new Version('10.0.0-next.2+53.sha-f186c32');
 
 /**
  * @license
