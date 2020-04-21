6001cd2eefbeeaed8acfd992af835423c68c3ebd
Matias Niemelä <matias@yearofmoo.com>
2020-04-21 17:24:23 +0000

    0bd50e2e50 fix(core): missing-injectable migration should not migrate `@NgModule` classes (#36369)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+47.sha-254b9ea
+ * @license Angular v9.1.2+51.sha-0bd50e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.2+47.sha-254b9ea');
+const VERSION = new Version('9.1.2+51.sha-0bd50e2');
 
 /**
  * @fileoverview added by tsickle
