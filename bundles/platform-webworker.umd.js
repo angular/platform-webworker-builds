27c99625d57b936ca9f4434ad4db56531d481d78
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:17:49 +0000

    a6e10ef869 build: update `REQUIRED_BASE_SHA` in merge script to `commit-message` script update commit (#36750)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+52.sha-c0ed57d
+ * @license Angular v9.1.2+54.sha-a6e10ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('9.1.2+52.sha-c0ed57d');
+    var VERSION = new core.Version('9.1.2+54.sha-a6e10ef');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+52.sha-c0ed57d
+ * @license Angular v9.1.2+54.sha-a6e10ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.2+52.sha-c0ed57d');
+const VERSION = new Version('9.1.2+54.sha-a6e10ef');
 
 /**
  * @fileoverview added by tsickle
