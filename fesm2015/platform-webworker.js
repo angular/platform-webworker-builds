2b0b9904978d14836df8dcf0c83af88d42ed2c41
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 04:49:29 +0000

    5db8d93fe5 build: fix the compare master and patch script output (#36749)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+57.sha-535c249
+ * @license Angular v10.0.0-next.2+58.sha-5db8d93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+57.sha-535c249');
+const VERSION = new Version('10.0.0-next.2+58.sha-5db8d93');
 
 /**
  * @fileoverview added by tsickle
