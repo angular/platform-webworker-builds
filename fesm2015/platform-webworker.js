c0cf09f957b1b72e8f5533e1c0c23a116ed4bd40
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:32:57 +0000

    1b3ee17888 build: move circular deps golden to a subfolder (#36630)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+3.sha-fc1b557
+ * @license Angular v10.0.0-next.3+6.sha-1b3ee17
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+3.sha-fc1b557');
+const VERSION = new Version('10.0.0-next.3+6.sha-1b3ee17');
 
 /**
  * @fileoverview added by tsickle
