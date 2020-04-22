60cfc72e58153097f802760b7365014c4660dc8c
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:30:27 +0000

    8306da5ce1 release(benchpress): bump version of benchpress to 0.2.0 (#36457)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+2.sha-47f9867
+ * @license Angular v10.0.0-next.3+5.sha-8306da5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.3+2.sha-47f9867');
+    var VERSION = new core.Version('10.0.0-next.3+5.sha-8306da5');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+2.sha-47f9867
+ * @license Angular v10.0.0-next.3+5.sha-8306da5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+2.sha-47f9867');
+const VERSION = new Version('10.0.0-next.3+5.sha-8306da5');
 
 /**
  * @fileoverview added by tsickle
