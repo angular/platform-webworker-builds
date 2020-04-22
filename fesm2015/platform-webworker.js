46cd3aee2c80abd814d40a884bd596a122e4eaf6
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 02:17:36 +0000

    942b986ef3 fix(core): properly identify modules affected by overrides in TestBed (#36649)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.2+55.sha-acf6075
+ * @license Angular v10.0.0-next.2+56.sha-942b986
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.2+55.sha-acf6075');
+const VERSION = new Version('10.0.0-next.2+56.sha-942b986');
 
 /**
  * @fileoverview added by tsickle
