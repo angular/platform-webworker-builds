fddd04e8d88d2e19575d5d638edea0607374416f
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 19:17:15 +0000

    e1071615c6 release: cut the v9.1.3 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+56.sha-8bd5374
+ * @license Angular v9.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.2+56.sha-8bd5374');
+const VERSION = new Version('9.1.3');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.2+56.sha-8bd5374
+ * @license Angular v9.1.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('9.1.2+56.sha-8bd5374');
+var VERSION = new Version('9.1.3');
 
 /**
  * @license
