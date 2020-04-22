4bcf602c6248860502e432ac885aa9f1f60a41e9
Matias Niemelä <matias@yearofmoo.com>
2020-04-22 21:38:42 +0000

    4275b3412f build: add AndrewKushnir to the `fw-testing` group (#36744)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+6.sha-1b3ee17
+ * @license Angular v10.0.0-next.3+7.sha-4275b34
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+6.sha-1b3ee17');
+const VERSION = new Version('10.0.0-next.3+7.sha-4275b34');
 
 /**
  * @fileoverview added by tsickle
