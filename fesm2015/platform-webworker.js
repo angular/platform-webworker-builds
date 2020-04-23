583a75b981b7bcaa0e18d5fd580d1a73eeeb0014
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:40:09 +0000

    f898c9ab57 build: require a commit body in commit messages (#36632)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+9.sha-7743c43
+ * @license Angular v9.1.3+15.sha-f898c9a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.3+9.sha-7743c43');
+const VERSION = new Version('9.1.3+15.sha-f898c9a');
 
 /**
  * @fileoverview added by tsickle
