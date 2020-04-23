24f978867cc7c7a5e682adb3955e70172bff08ac
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:39:12 +0000

    f0ec4ae941 ci: fix pullapprove incorrectly skipping fw-compiler as owner (#36661)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+16.sha-e5d2853
+ * @license Angular v10.0.0-next.3+19.sha-f0ec4ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
+const VERSION = new Version('10.0.0-next.3+19.sha-f0ec4ae');
 
 /**
  * @fileoverview added by tsickle
