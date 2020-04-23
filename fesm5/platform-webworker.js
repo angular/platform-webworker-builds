4b0a39ec60b6b2c1ddfdc680bf7c6157e469f54c
Andrew Kushnir <akushnir@google.com>
2020-04-23 19:37:54 +0000

    e5d2853070 docs: correct ngSwitch definition (#35489)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+13.sha-dd806b0
+ * @license Angular v10.0.0-next.3+16.sha-e5d2853
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.3+13.sha-dd806b0');
+var VERSION = new Version('10.0.0-next.3+16.sha-e5d2853');
 
 /**
  * @license
