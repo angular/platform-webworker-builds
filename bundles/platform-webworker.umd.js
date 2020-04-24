3e36a5493a6352f33973fed81dc4bf3cd9680bb3
Andrew Kushnir <akushnir@google.com>
2020-04-24 16:25:52 +0000

    dd049caf0a fix(language-service): disable update the `@angular/core` module (#36783)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+24.sha-e6161ca
+ * @license Angular v10.0.0-next.3+26.sha-dd049ca
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.3+24.sha-e6161ca');
+    var VERSION = new core.Version('10.0.0-next.3+26.sha-dd049ca');
 
     /**
      * @license
