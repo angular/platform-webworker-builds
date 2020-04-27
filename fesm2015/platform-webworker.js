2beeea7f5bab3890bb7c8aedf89cfe6a4e775856
Andrew Kushnir <akushnir@google.com>
2020-04-27 22:19:35 +0000

    0f389fa5ae fix(core): handle synthetic props in Directive host bindings correctly (#35568)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.3+27.sha-2a27f69
+ * @license Angular v9.1.3+28.sha-0f389fa
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('9.1.3+27.sha-2a27f69');
+const VERSION = new Version('9.1.3+28.sha-0f389fa');
 
 /**
  * @fileoverview added by tsickle
