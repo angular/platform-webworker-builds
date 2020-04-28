0f7df712240178f7edb61855a27e31c779429ef3
Andrew Kushnir <akushnir@google.com>
2020-04-28 19:49:10 +0000

    70dd27ffd8 fix(compiler): normalize line endings in ICU expansions (#36741)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+38.sha-7c8c413
+ * @license Angular v10.0.0-next.3+43.sha-70dd27f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.3+38.sha-7c8c413');
+const VERSION = new Version('10.0.0-next.3+43.sha-70dd27f');
 
 /**
  * @fileoverview added by tsickle
