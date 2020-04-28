0257480201968d11acc94c8c3f9399aae23673cc
Andrew Kushnir <akushnir@google.com>
2020-04-28 23:25:13 +0000

    89c589085d fix(ngcc): recognize enum declarations emitted in JavaScript (#36550)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+44.sha-a6a7e1b
+ * @license Angular v10.0.0-next.3+45.sha-89c5890
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.3+44.sha-a6a7e1b');
+    var VERSION = new core.Version('10.0.0-next.3+45.sha-89c5890');
 
     /**
      * @license
