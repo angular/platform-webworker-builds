d9dd52b13c9d8ef46b326453edee1e5a6a4e60a9
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:20:50 +0000

    c89421bfa8 docs: release notes for the v9.1.4 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+47.sha-cebd4fa
+ * @license Angular v10.0.0-next.3+48.sha-c89421b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.3+47.sha-cebd4fa');
+    var VERSION = new core.Version('10.0.0-next.3+48.sha-c89421b');
 
     /**
      * @license
