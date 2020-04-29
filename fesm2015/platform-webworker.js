58d89596277ab600ad9c1f6cfe4cee9dc5f61ced
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:52:56 +0000

    45c09416ed refactor(ngcc): move `PathMappings` to separate file to avoid circular dependency (#36626)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+1.sha-62ba0ac
+ * @license Angular v10.0.0-next.4+13.sha-45c0941
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
+const VERSION = new Version('10.0.0-next.4+13.sha-45c0941');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+1.sha-62ba0ac
+ * @license Angular v10.0.0-next.4+13.sha-45c0941
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
+var VERSION = new Version('10.0.0-next.4+13.sha-45c0941');
 
 /**
  * @license
