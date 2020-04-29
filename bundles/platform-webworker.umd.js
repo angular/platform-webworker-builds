953dd77221e007d6bb3e0189d96e25fff49336ce
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:39:41 +0000

    52d2e46700 release: cut the v10.0.0-next.4 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.3+48.sha-c89421b
+ * @license Angular v10.0.0-next.4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.3+48.sha-c89421b');
+    var VERSION = new core.Version('10.0.0-next.4');
 
     /**
      * @license
