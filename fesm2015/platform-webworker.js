7dd64aba7534a0f962b86148ca2ec623f7b744c4
Andrew Kushnir <akushnir@google.com>
2020-04-29 21:50:59 +0000

    62ba0acfb5 test(language-service): do not invalidate @angular/core (#36845)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4
+ * @license Angular v10.0.0-next.4+1.sha-62ba0ac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4');
+const VERSION = new Version('10.0.0-next.4+1.sha-62ba0ac');
 
 /**
  * @fileoverview added by tsickle
