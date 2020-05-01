ebd38c33d98af26c9e85e871a7751c949800e45c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:25:57 +0000

    e9300c979e build: use @angular/dev-infra-private for bazel stamping (#36844)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+30.sha-95a407d
+ * @license Angular v10.0.0-next.4+29.sha-e9300c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.4+30.sha-95a407d');
+    var VERSION = new core.Version('10.0.0-next.4+29.sha-e9300c9');
 
     /**
      * @license
