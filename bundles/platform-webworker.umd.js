3255eb7a06fea870ed645b77b113fff034153462
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:25:09 +0000

    95a407d9dd build: Add entry point for Ivy language service (#36864)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+31.sha-12fcc7c
+ * @license Angular v10.0.0-next.4+30.sha-95a407d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.4+31.sha-12fcc7c');
+    var VERSION = new core.Version('10.0.0-next.4+30.sha-95a407d');
 
     /**
      * @license
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+31.sha-12fcc7c
+ * @license Angular v10.0.0-next.4+30.sha-95a407d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.4+31.sha-12fcc7c');
+var VERSION = new Version('10.0.0-next.4+30.sha-95a407d');
 
 /**
  * @license
