9e4f187a2488510655aebe8d5cc5f7813baeead4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-01 17:26:41 +0000

    12fcc7cafe build(language-service): make test project a filegroup (#36865)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+15.sha-49be32c
+ * @license Angular v10.0.0-next.4+31.sha-12fcc7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+15.sha-49be32c');
+const VERSION = new Version('10.0.0-next.4+31.sha-12fcc7c');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+15.sha-49be32c
+ * @license Angular v10.0.0-next.4+31.sha-12fcc7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.4+15.sha-49be32c');
+var VERSION = new Version('10.0.0-next.4+31.sha-12fcc7c');
 
 /**
  * @license
