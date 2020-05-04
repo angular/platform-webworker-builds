26ac0a10797b4546233a83029aa8ef2f4f145d73
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:22:32 +0000

    b3713a112f test(language-service): Add method to override inline template (#36890)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+37.sha-6046e86
+ * @license Angular v10.0.0-next.4+42.sha-b3713a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+37.sha-6046e86');
+const VERSION = new Version('10.0.0-next.4+42.sha-b3713a1');
 
 /**
  * @fileoverview added by tsickle
