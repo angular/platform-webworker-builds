2c79bf1ad152d15a1dce3088712e589bd4627756
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:10:13 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
+const VERSION = new Version('10.0.0-next.4+36.sha-297dab8');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1049,7 +1049,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.4+34.sha-0f3831b');
+var VERSION = new Version('10.0.0-next.4+36.sha-297dab8');
 
 /**
  * @license
