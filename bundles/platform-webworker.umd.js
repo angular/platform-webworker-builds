c8ecb333ba567396d6121b5fce73e178d454fc88
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-04 20:08:19 +0000

    297dab845d build: migrate bazel related formatting/linting to ng-dev format (#36842)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.4+34.sha-0f3831b
+ * @license Angular v10.0.0-next.4+36.sha-297dab8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.4+34.sha-0f3831b');
+    var VERSION = new core.Version('10.0.0-next.4+36.sha-297dab8');
 
     /**
      * @license
