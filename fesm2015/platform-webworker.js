80830a49f45f46114227396c52bdd5e4c49c47a0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:38:52 +0000

    e30e1325f3 fix(core): properly get root nodes from embedded views with <ng-content> (#36051)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+20.sha-fc6c3ae
+ * @license Angular v10.0.0-next.5+21.sha-e30e132
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+20.sha-fc6c3ae');
+const VERSION = new Version('10.0.0-next.5+21.sha-e30e132');
 
 /**
  * @fileoverview added by tsickle
