ac15ab5527369d1bf3caee69b2f44071bfef400c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:27:04 +0000

    b95a336f12 build: fix and re-enable elements tests on saucelabs (#36929)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+7.sha-306f46c
+ * @license Angular v10.0.0-next.5+18.sha-b95a336
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+7.sha-306f46c');
+const VERSION = new Version('10.0.0-next.5+18.sha-b95a336');
 
 /**
  * @fileoverview added by tsickle
