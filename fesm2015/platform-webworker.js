65570d54454fe4aca6f55fc7f354f41857a89442
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:29:02 +0000

    fc6c3ae97d ci: removing CI environment variable caching setup (#36936)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+18.sha-b95a336
+ * @license Angular v10.0.0-next.5+20.sha-fc6c3ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+18.sha-b95a336');
+const VERSION = new Version('10.0.0-next.5+20.sha-fc6c3ae');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+18.sha-b95a336
+ * @license Angular v10.0.0-next.5+20.sha-fc6c3ae
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.5+18.sha-b95a336');
+var VERSION = new Version('10.0.0-next.5+20.sha-fc6c3ae');
 
 /**
  * @license
