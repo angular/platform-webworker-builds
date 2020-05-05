2f3de04de48fbae9d31864244ec2ea5e3153095c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-05 19:44:52 +0000

    35d61c11fd test(core): Add benchmark for transplanted views when insertion is dirty (#36722)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+21.sha-e30e132
+ * @license Angular v10.0.0-next.5+22.sha-35d61c1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+21.sha-e30e132');
+const VERSION = new Version('10.0.0-next.5+22.sha-35d61c1');
 
 /**
  * @fileoverview added by tsickle
