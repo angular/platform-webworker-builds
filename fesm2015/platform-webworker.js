a91600f5e60260dc3b56991e65e59fa005622320
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 02:03:10 +0000

    ecffc3557f perf(compiler-cli): perform template type-checking incrementally (#36211)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+25.sha-388dc93
+ * @license Angular v10.0.0-next.5+42.sha-ecffc35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+25.sha-388dc93');
+const VERSION = new Version('10.0.0-next.5+42.sha-ecffc35');
 
 /**
  * @fileoverview added by tsickle
