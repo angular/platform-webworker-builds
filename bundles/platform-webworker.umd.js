980d9f3b175607d14241bb8d61f7b664fac8d078
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:48:59 +0000

    f7815cf96d test(compiler-cli): ensure reflection tests are not brittle to case-sensitivity (#36859)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+61.sha-f930e75
+ * @license Angular v10.0.0-next.5+71.sha-f7815cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.5+61.sha-f930e75');
+    var VERSION = new core.Version('10.0.0-next.5+71.sha-f7815cf');
 
     /**
      * @license
