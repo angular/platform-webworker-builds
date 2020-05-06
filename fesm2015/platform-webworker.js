732f3d398b527eff6727c48350605833835b316e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 22:28:12 +0000

    20cc3ab37e refactor(core): make generic mandatory for ModuleWithProviders (#36892)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+54.sha-420b9be
+ * @license Angular v10.0.0-next.5+55.sha-20cc3ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+54.sha-420b9be');
+const VERSION = new Version('10.0.0-next.5+55.sha-20cc3ab');
 
 /**
  * @fileoverview added by tsickle
