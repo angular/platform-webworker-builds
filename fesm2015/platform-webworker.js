f8a74b32a453e60f34b24343413893651c85df48
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 21:16:49 +0000

    3c6c00d1d4 docs: remove the note about @angular/http from the deprecations guide (#36944)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+45.sha-fafa50d
+ * @license Angular v10.0.0-next.5+53.sha-3c6c00d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+45.sha-fafa50d');
+const VERSION = new Version('10.0.0-next.5+53.sha-3c6c00d');
 
 /**
  * @fileoverview added by tsickle
