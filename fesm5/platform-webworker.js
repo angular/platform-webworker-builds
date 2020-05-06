ff9bb81cda6c299a7b0ff572ece8307b9426a3ec
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-06 00:27:44 +0000

    fbd281c26e build: remove typescript 3.6 and 3.7 support (#36329)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+22.sha-35d61c1
+ * @license Angular v10.0.0-next.5+24.sha-fbd281c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1056,7 +1056,7 @@ function spawnWebWorker(uri, instance) {
  * @publicApi
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  */
-var VERSION = new Version('10.0.0-next.5+22.sha-35d61c1');
+var VERSION = new Version('10.0.0-next.5+24.sha-fbd281c');
 
 /**
  * @license
