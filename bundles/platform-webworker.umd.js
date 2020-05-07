ed5d44a089858e7dbe46926a0e5493919c3c71cb
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:05:18 +0000

    d0280a0335 fix(compiler): remove outdated and invalid warning for unresolved DI parameters (#36985)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+72.sha-97d6d90
+ * @license Angular v10.0.0-next.5+73.sha-d0280a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.5+72.sha-97d6d90');
+    var VERSION = new core.Version('10.0.0-next.5+73.sha-d0280a0');
 
     /**
      * @license
