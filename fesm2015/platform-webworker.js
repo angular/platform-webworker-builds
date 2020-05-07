fea86233cb15d4f5a2acc4525ab0cfed7e65cc47
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 22:07:15 +0000

    d0280a0335 fix(compiler): remove outdated and invalid warning for unresolved DI parameters (#36985)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+72.sha-97d6d90
+ * @license Angular v10.0.0-next.5+73.sha-d0280a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.5+72.sha-97d6d90');
+const VERSION = new Version('10.0.0-next.5+73.sha-d0280a0');
 
 /**
  * @fileoverview added by tsickle
