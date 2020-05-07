41653e650924c2dd53b901bf7e887bda413c64d0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-07 17:43:30 +0000

    97d6d909e7 fix(forms): number input fires valueChanges twice. (#36087)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.5+71.sha-f7815cf
+ * @license Angular v10.0.0-next.5+72.sha-97d6d90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.5+71.sha-f7815cf');
+    var VERSION = new core.Version('10.0.0-next.5+72.sha-97d6d90');
 
     /**
      * @license
