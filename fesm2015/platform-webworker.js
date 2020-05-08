10c83a5ca396e4d8f452b7c03f89912a26ca0f4d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:06:25 +0000

    14be55c9fa docs(common): remove mention of the deprecated (#36953) web worker platform. Minor grammar/stylistic changes.

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+7.sha-39e066a
+ * @license Angular v10.0.0-next.6+5.sha-14be55c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+7.sha-39e066a');
+const VERSION = new Version('10.0.0-next.6+5.sha-14be55c');
 
 /**
  * @fileoverview added by tsickle
