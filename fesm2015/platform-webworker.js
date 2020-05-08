463cdbc34040f793340cf46ab401c47cb80367c8
Misko Hevery <misko@hevery.com>
2020-05-08 22:05:01 +0000

    eb34aa551a feat(compiler): add name spans for property reads and method calls (#36826)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+11.sha-df71c7c
+ * @license Angular v10.0.0-next.6+16.sha-eb34aa5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+11.sha-df71c7c');
+const VERSION = new Version('10.0.0-next.6+16.sha-eb34aa5');
 
 /**
  * @fileoverview added by tsickle
