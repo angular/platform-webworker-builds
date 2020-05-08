5ca7ea5d81df28c010e293357fa7239896fd42ce
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:00:35 +0000

    7cc332efe6 docs: update aot compiler docs (#35487)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6
+ * @license Angular v10.0.0-next.6+3.sha-7cc332ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.6');
+    var VERSION = new core.Version('10.0.0-next.6+3.sha-7cc332ef');
 
     /**
      * @license
