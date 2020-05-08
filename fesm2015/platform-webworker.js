1e71d9eaa899f9eacee9661d434f0feba035c596
Misko Hevery <misko@hevery.com>
2020-05-08 22:00:29 +0000

    82a3bd5e8b ci: disable sauce labs safari and ios tests (#36973)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+10.sha-c184de1
+ * @license Angular v10.0.0-next.6+14.sha-82a3bd5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+10.sha-c184de1');
+const VERSION = new Version('10.0.0-next.6+14.sha-82a3bd5');
 
 /**
  * @fileoverview added by tsickle
