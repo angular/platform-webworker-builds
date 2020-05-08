a322609e12306da3a13c3db46e8c5269de3d9f06
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-05-08 17:03:31 +0000

    fdd61152c5 docs: replace into with in (#36778)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+3.sha-7cc332ef
+ * @license Angular v10.0.0-next.6+4.sha-fdd6115
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+3.sha-7cc332ef');
+const VERSION = new Version('10.0.0-next.6+4.sha-fdd6115');
 
 /**
  * @fileoverview added by tsickle
