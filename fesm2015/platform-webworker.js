cba1235cb653b62747602bbe08728ce8865d180b
Misko Hevery <misko@hevery.com>
2020-05-11 18:21:32 +0000

    7eb1a58b26 docs: refactor form validation doc to be task-oriented (#36374)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+20.sha-28f3c1c
+ * @license Angular v10.0.0-next.6+21.sha-7eb1a58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+20.sha-28f3c1c');
+const VERSION = new Version('10.0.0-next.6+21.sha-7eb1a58');
 
 /**
  * @fileoverview added by tsickle
