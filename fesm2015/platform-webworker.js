8c2fc83daad1d35e6cde3a821a62f14ea6145647
Misko Hevery <misko@hevery.com>
2020-05-11 18:19:56 +0000

    28f3c1c96a refactor(core): rename `instructions/container.ts` -> `instructions/template.ts` (#34715)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+18.sha-ed1b4a8
+ * @license Angular v10.0.0-next.6+20.sha-28f3c1c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+18.sha-ed1b4a8');
+const VERSION = new Version('10.0.0-next.6+20.sha-28f3c1c');
 
 /**
  * @fileoverview added by tsickle
