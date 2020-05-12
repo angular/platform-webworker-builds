276ecc8ca1d234e12af0b4c099cb60a0d299cae0
Misko Hevery <misko@hevery.com>
2020-05-12 18:13:26 +0000

    198fc6f108 docs(forms): remove rxjs of alias import in form async validator example (#36856)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+27.sha-8fe532e
+ * @license Angular v10.0.0-next.6+26.sha-198fc6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.6+27.sha-8fe532e');
+    var VERSION = new core.Version('10.0.0-next.6+26.sha-198fc6f');
 
     /**
      * @license
