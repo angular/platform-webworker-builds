0c30f55d30f89be85385a0b655e0f54931c8c2b7
Misko Hevery <misko@hevery.com>
2020-05-12 21:59:40 +0000

    7addc9f1a9 docs: add doc for template type guards to stuctural-directives.md (#34549)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
+ * @license Angular v10.0.0-next.6+30.sha-7addc9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.6+29.sha-cb6ddfc');
+    var VERSION = new core.Version('10.0.0-next.6+30.sha-7addc9f');
 
     /**
      * @license
