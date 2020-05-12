7dbdcecda2afd44956a946f3e3a47f2610f9c9e0
Misko Hevery <misko@hevery.com>
2020-05-12 18:11:07 +0000

    681741cd13 docs: minor typo fix in the bazel documentation (#37043)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+24.sha-1e208e8
+ * @license Angular v10.0.0-next.6+28.sha-681741c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.6+24.sha-1e208e8');
+    var VERSION = new core.Version('10.0.0-next.6+28.sha-681741c');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+24.sha-1e208e8
+ * @license Angular v10.0.0-next.6+28.sha-681741c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+24.sha-1e208e8');
+const VERSION = new Version('10.0.0-next.6+28.sha-681741c');
 
 /**
  * @fileoverview added by tsickle
