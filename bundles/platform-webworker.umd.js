67ef0b17486e143247b4f58b8a4f1d564f56665f
Misko Hevery <misko@hevery.com>
2020-05-12 20:37:45 +0000

    cb6ddfc215 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#37074)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+26.sha-198fc6f
+ * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1271,7 +1271,7 @@
      * @publicApi
      * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
      */
-    var VERSION = new core.Version('10.0.0-next.6+26.sha-198fc6f');
+    var VERSION = new core.Version('10.0.0-next.6+29.sha-cb6ddfc');
 
     /**
      * @license
