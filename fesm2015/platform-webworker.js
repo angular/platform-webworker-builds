2f59ef62b62174bb7a3c17ae0d693db125468bcc
Misko Hevery <misko@hevery.com>
2020-05-12 20:39:44 +0000

    cb6ddfc215 Revert "fix(common): `locales/global/*.js` are not ES5 compliant (#36342)" (#37074)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+26.sha-198fc6f
+ * @license Angular v10.0.0-next.6+29.sha-cb6ddfc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2107,7 +2107,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+26.sha-198fc6f');
+const VERSION = new Version('10.0.0-next.6+29.sha-cb6ddfc');
 
 /**
  * @fileoverview added by tsickle
