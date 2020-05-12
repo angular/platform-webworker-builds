454b2e4957a555f717508390dc3960b47b400ab7
Misko Hevery <misko@hevery.com>
2020-05-12 18:11:19 +0000

    8fe532e593 build(dev-infra): cleanup package dependencies for shared package (#36980)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.6+28.sha-681741c
+ * @license Angular v10.0.0-next.6+27.sha-8fe532e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2023,7 +2023,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in version 10
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.6+28.sha-681741c');
+const VERSION = new Version('10.0.0-next.6+27.sha-8fe532e');
 
 /**
  * @fileoverview added by tsickle
