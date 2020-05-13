70460cb426750297cee6f2ffffb71b4f64433a87
Misko Hevery <misko@hevery.com>
2020-05-13 23:24:24 +0000

    57a868627f test(docs-infra): re-enable upgrade-phonecat-2-hybrid example e2e tests (#36969)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+7.sha-45f4a47
+ * @license Angular v10.0.0-next.7+15.sha-57a8686
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1283,7 +1283,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.7+7.sha-45f4a47');
+    var VERSION = new core.Version('10.0.0-next.7+15.sha-57a8686');
 
     /**
      * @license
