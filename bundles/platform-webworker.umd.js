2f8fc3e0da8dd520d9024f64c7a01179c3ce7b3a
Misko Hevery <misko@hevery.com>
2020-05-13 23:18:41 +0000

    14c0ec97d8 feat(dev-infra): introduce validators for ng-dev config loading (#37049)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+9.sha-7d3f504
+ * @license Angular v10.0.0-next.7+8.sha-14c0ec9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1283,7 +1283,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.7+9.sha-7d3f504');
+    var VERSION = new core.Version('10.0.0-next.7+8.sha-14c0ec9');
 
     /**
      * @license
