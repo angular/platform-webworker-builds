8ba6545c8f193202f07ac0d236226c6d963aeb19
Misko Hevery <misko@hevery.com>
2020-05-13 23:17:14 +0000

    352b9c78e0 docs: replaces on with of (#36773)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+6.sha-141fcb9
+ * @license Angular v10.0.0-next.7+3.sha-352b9c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1276,7 +1276,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-next.7+6.sha-141fcb9');
+    var VERSION = new i0.Version('10.0.0-next.7+3.sha-352b9c7');
 
     /**
      * @license
