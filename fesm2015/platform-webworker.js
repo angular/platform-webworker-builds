eb8760a52805641424cd7ac9202eb1a59648cc39
Kara Erickson <karakara@google.com>
2020-05-14 22:42:22 +0000

    4de546eef5 test(core): verify that Ivy i18n works correctly with HTML namespaces (#36943)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+36.sha-719ca52
+ * @license Angular v9.1.6+37.sha-4de546e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('9.1.6+36.sha-719ca52');
+const VERSION = new Version('9.1.6+37.sha-4de546e');
 
 /**
  * @fileoverview added by tsickle
