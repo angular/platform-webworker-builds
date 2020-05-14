78397ad61c5bebccf7e4e08ad37679b1231525b5
Kara Erickson <karakara@google.com>
2020-05-14 20:25:45 +0000

    719ca5283b fix(dev-infra): do not require a commit body for release commits (#37110)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+35.sha-3813f54
+ * @license Angular v9.1.6+36.sha-719ca52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('9.1.6+35.sha-3813f54');
+const VERSION = new Version('9.1.6+36.sha-719ca52');
 
 /**
  * @fileoverview added by tsickle
--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+35.sha-3813f54
+ * @license Angular v9.1.6+36.sha-719ca52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1068,7 +1068,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-var VERSION = new Version('9.1.6+35.sha-3813f54');
+var VERSION = new Version('9.1.6+36.sha-719ca52');
 
 /**
  * @license
