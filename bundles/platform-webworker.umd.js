bfc03168f26108d81bb911403aaaaca8dd52fa7f
Kara Erickson <karakara@google.com>
2020-05-14 19:22:02 +0000

    5f64e52ed4 docs: clarifies the service limitation (#36349)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+44.sha-95f429c
+ * @license Angular v10.0.0-next.7+46.sha-5f64e52
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1276,7 +1276,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-next.7+44.sha-95f429c');
+    var VERSION = new i0.Version('10.0.0-next.7+46.sha-5f64e52');
 
     /**
      * @license
