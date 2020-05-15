ba1f794e85b884e725a6d73490e3351ea7e8400e
Kara Erickson <karakara@google.com>
2020-05-15 18:08:20 +0000

    9548c7cb94 release: cut the v9.1.7 release (#37116)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+47.sha-5ffa4f1
+ * @license Angular v9.1.6+49.sha-9548c7c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1283,7 +1283,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('9.1.6+47.sha-5ffa4f1');
+    var VERSION = new core.Version('9.1.6+49.sha-9548c7c');
 
     /**
      * @license
