f5701aad726dfd5a6f985987d8006b007a901626
Kara Erickson <karakara@google.com>
2020-05-15 20:44:50 +0000

    7f58c579f3 docs(changelog): add breaking change note for ModuleWithProviders (#37121)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+63.sha-3d7c85b2
+ * @license Angular v10.0.0-next.7+64.sha-7f58c57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+63.sha-3d7c85b2');
+const VERSION = new Version('10.0.0-next.7+64.sha-7f58c57');
 
 /**
  * @fileoverview added by tsickle
