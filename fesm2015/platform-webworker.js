041f828a6356cac5a04dcb71253d9adb3c51ba2f
Kara Erickson <karakara@google.com>
2020-05-16 00:19:29 +0000

    3f9b3d7666 build: add kara to public-api approvers (#37143)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.7+64.sha-7f58c57
+ * @license Angular v10.0.0-next.7+66.sha-3f9b3d7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2081,7 +2081,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.7+64.sha-7f58c57');
+const VERSION = new Version('10.0.0-next.7+66.sha-3f9b3d7');
 
 /**
  * @fileoverview added by tsickle
