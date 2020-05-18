2979a7f8f74aefdb322596ff01d6f72a71b4c9f8
Kara Erickson <karakara@google.com>
2020-05-18 17:52:09 +0000

    22d80a6780 refactor(docs-infra): refactors `createOverviewDump` (#37141)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+9.sha-593e07b
+ * @license Angular v10.0.0-next.8+10.sha-22d80a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1276,7 +1276,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-next.8+9.sha-593e07b');
+    var VERSION = new i0.Version('10.0.0-next.8+10.sha-22d80a6');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+9.sha-593e07b
+ * @license Angular v10.0.0-next.8+10.sha-22d80a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2165,7 +2165,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('10.0.0-next.8+9.sha-593e07b');
+const VERSION = new Version('10.0.0-next.8+10.sha-22d80a6');
 
 /**
  * @fileoverview added by tsickle
