7e808e9ead012f5a86821c1fa5ad63dff16e15f8
Kara Erickson <karakara@google.com>
2020-05-18 22:17:48 +0000

    178a78f9ed docs(router): add docs for RouterLink inputs (#37018)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+67.sha-ba9e63a
+ * @license Angular v9.1.6+70.sha-178a78f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1283,7 +1283,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('9.1.6+67.sha-ba9e63a');
+    var VERSION = new core.Version('9.1.6+70.sha-178a78f');
 
     /**
      * @license
