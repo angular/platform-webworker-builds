797bf6178f764d3284a758df9a56d533414e3949
Kara Erickson <karakara@google.com>
2020-05-18 18:50:08 +0000

    6db8e7eb8f ci: fix a typo in payload size limit config (#37151)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+50.sha-0912be4
+ * @license Angular v9.1.6+62.sha-6db8e7e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1068,7 +1068,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-var VERSION = new Version('9.1.6+50.sha-0912be4');
+var VERSION = new Version('9.1.6+62.sha-6db8e7e');
 
 /**
  * @license
