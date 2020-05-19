2dcc3a2d32fafdceaf321978df98a6fd1d990095
Kara Erickson <karakara@google.com>
2020-05-19 22:58:46 +0000

    362fba4bde fix(dev-infra): make commit-message-filter script executable for merge (#37209)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+30.sha-211d33a
+ * @license Angular v10.0.0-next.8+31.sha-362fba4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-next.8+30.sha-211d33a');
+    var VERSION = new i0.Version('10.0.0-next.8+31.sha-362fba4');
 
     /**
      * @license
