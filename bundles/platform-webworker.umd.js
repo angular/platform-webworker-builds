6cad4a9577ec96667c6910bce9706a4abc643e8a
Kara Erickson <karakara@google.com>
2020-05-19 22:44:57 +0000

    383f04b96d feat(dev-infra): register ts-node when reading configuration (#37196)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+27.sha-772c5b8
+ * @license Angular v10.0.0-next.8+29.sha-383f04b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-next.8+27.sha-772c5b8');
+    var VERSION = new i0.Version('10.0.0-next.8+29.sha-383f04b');
 
     /**
      * @license
