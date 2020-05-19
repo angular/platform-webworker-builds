62611743a323302ee124d3d78ca83149424ffa98
Kara Erickson <karakara@google.com>
2020-05-19 22:29:19 +0000

    772c5b8f64 refactor: update to tslib 2.0 and move to direct dependencies (#37198)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+28.sha-4e96cdc
+ * @license Angular v10.0.0-next.8+27.sha-772c5b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-next.8+28.sha-4e96cdc');
+    var VERSION = new i0.Version('10.0.0-next.8+27.sha-772c5b8');
 
     /**
      * @license
