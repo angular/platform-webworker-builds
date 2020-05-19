6febbc88553191dfa80a37fb26112fe606b84b4d
Kara Erickson <karakara@google.com>
2020-05-19 17:42:21 +0000

    deac994517 refactor(docs-infra): remove `rev` property from `Resource` interface and data (#37181)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.8+24.sha-b754ab2
+ * @license Angular v10.0.0-next.8+25.sha-deac994
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1283,7 +1283,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.8+24.sha-b754ab2');
+    var VERSION = new core.Version('10.0.0-next.8+25.sha-deac994');
 
     /**
      * @license
