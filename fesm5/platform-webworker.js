4998aa835f6575dd8eca16f68d2bf4e4c4cd4ea2
Kara Erickson <karakara@google.com>
2020-05-19 18:08:53 +0000

    a63f634592 docs: remove https://angular.io from internal links (#37157)

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.6+74.sha-c423457
+ * @license Angular v9.1.6+76.sha-a63f634
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1068,7 +1068,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-var VERSION = new Version('9.1.6+74.sha-c423457');
+var VERSION = new Version('9.1.6+76.sha-a63f634');
 
 /**
  * @license
