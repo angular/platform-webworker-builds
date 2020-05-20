b6ff977ef90543d0b9c5b8f6c6718a56746175cd
Kara Erickson <karakara@google.com>
2020-05-20 22:12:28 +0000

    1465372a0e fix(elements): do not break when the constructor of an Angular Element is not called (#36114) (#37226)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.8+12.sha-3eee536
+ * @license Angular v9.1.8+19.sha-1465372
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('9.1.8+12.sha-3eee536');
+const VERSION = new Version('9.1.8+19.sha-1465372');
 
 /**
  * @fileoverview added by tsickle
