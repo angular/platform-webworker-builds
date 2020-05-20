7a03cb1fefc17b4ccf7b1f729bb84c987977ed73
Kara Erickson <karakara@google.com>
2020-05-20 17:00:25 +0000

    4c30aa8343 docs(dev-infra): update .circleci/README.md (#37212)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+1.sha-1faf9bb
+ * @license Angular v10.0.0-next.9+4.sha-4c30aa8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.9+1.sha-1faf9bb');
+    var VERSION = new core.Version('10.0.0-next.9+4.sha-4c30aa8');
 
     /**
      * @license
