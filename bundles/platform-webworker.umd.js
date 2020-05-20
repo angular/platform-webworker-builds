a08a52a56c3fd54a48459afc24383771bff4fbc6
Kara Erickson <karakara@google.com>
2020-05-20 21:34:25 +0000

    267f844811 docs: update docs for microbenchmarks (#37140)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+20.sha-59e7203
+ * @license Angular v10.0.0-next.9+21.sha-267f844
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.9+20.sha-59e7203');
+    var VERSION = new core.Version('10.0.0-next.9+21.sha-267f844');
 
     /**
      * @license
