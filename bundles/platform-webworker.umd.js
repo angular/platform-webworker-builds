c5a874c166fb50c1966233763b1ac3b91a16f28c
Kara Erickson <karakara@google.com>
2020-05-20 23:07:16 +0000

    6ca3fb19fd docs: release notes for the v${releaseVersion} release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+21.sha-267f844
+ * @license Angular v10.0.0-next.9+22.sha-6ca3fb1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.9+21.sha-267f844');
+    var VERSION = new core.Version('10.0.0-next.9+22.sha-6ca3fb1');
 
     /**
      * @license
