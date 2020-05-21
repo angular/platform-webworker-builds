976183277927fedb08915f5e0af85d8fb18e09c0
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:42:13 +0000

    87b1aeac0f build(docs-infra): upgrade cli command docs sources to 89dbc2caa (#37243)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+2.sha-cdb5d07
+ * @license Angular v10.0.0-rc.0+4.sha-87b1aea
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+2.sha-cdb5d07');
+    var VERSION = new i0.Version('10.0.0-rc.0+4.sha-87b1aea');
 
     /**
      * @license
