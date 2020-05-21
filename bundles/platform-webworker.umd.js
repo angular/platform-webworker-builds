256935a46dca1d21f645c89a2b116a2a6db9f401
Kara Erickson <karakara@google.com>
2020-05-21 17:58:38 +0000

    cdb5d07606 feat(dev-infra): expose script for determining merge branches (#37217)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0
+ * @license Angular v10.0.0-rc.0+2.sha-cdb5d07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0');
+    var VERSION = new i0.Version('10.0.0-rc.0+2.sha-cdb5d07');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0
+ * @license Angular v10.0.0-rc.0+2.sha-cdb5d07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0');
+const VERSION = new Version('10.0.0-rc.0+2.sha-cdb5d07');
 
 /**
  * @license
