42998636979324e1088a1a86bed697ec7db0ec62
Kara Erickson <karakara@google.com>
2020-05-21 17:45:51 +0000

    cfb37b8994 release: cut the v10.0.0-rc.0 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-next.9+23.sha-a1001f2
+ * @license Angular v10.0.0-rc.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-next.9+23.sha-a1001f2');
+    var VERSION = new core.Version('10.0.0-rc.0');
 
     /**
      * @license
