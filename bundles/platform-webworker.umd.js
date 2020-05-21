ff776ca129a712a09c47222b6a417f25615dd720
Matias Niemelä <matias@yearofmoo.com>
2020-05-21 21:40:33 +0000

    93be1f1543 build: use static patch value for targetting branches in merge config (#37245)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+4.sha-87b1aea
+ * @license Angular v10.0.0-rc.0+3.sha-93be1f1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+4.sha-87b1aea');
+    var VERSION = new core.Version('10.0.0-rc.0+3.sha-93be1f1');
 
     /**
      * @license
