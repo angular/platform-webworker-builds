176e0cf91046bd85af9dfc320c4f4023c13b0ea3
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 20:29:36 +0000

    98d820030c docs(platform-server): fix renderModule usage guidance with Ivy (#37296)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+20.sha-64fc8b8
+ * @license Angular v10.0.0-rc.0+22.sha-98d8200
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+20.sha-64fc8b8');
+const VERSION = new Version('10.0.0-rc.0+22.sha-98d8200');
 
 /**
  * @license
