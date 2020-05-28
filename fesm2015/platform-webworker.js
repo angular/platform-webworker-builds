72a918777a223df5c5b20ba8eb0d3bcb55f04011
Matias Niemelä <matias@yearofmoo.com>
2020-05-28 23:10:32 +0000

    df082307b9 docs: fix typo in committer.md (#37171)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+21.sha-f001aef
+ * @license Angular v10.0.0-rc.0+23.sha-df08230
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+21.sha-f001aef');
+const VERSION = new Version('10.0.0-rc.0+23.sha-df08230');
 
 /**
  * @license
