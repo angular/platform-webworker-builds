30d79c382f7becef020b64a97061440a8a315713
Matias Niemelä <matias@yearofmoo.com>
2020-05-29 01:53:57 +0000

    82761ec50e docs: Mention Bazel builder and schematics in Deprecations section (#37190)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+11.sha-270da1f
+ * @license Angular v10.0.0-rc.0+22.sha-82761ec
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1297,7 +1297,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+11.sha-270da1f');
+    var VERSION = new core.Version('10.0.0-rc.0+22.sha-82761ec');
 
     /**
      * @license
