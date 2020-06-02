524cd031348d8e8ec264e4e70508349ffa85162d
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:52:01 +0000

    4414be77c4 docs: refactor forms overview (#36919)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+47.sha-17e9819
+ * @license Angular v10.0.0-rc.0+45.sha-4414be7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+47.sha-17e9819');
+    var VERSION = new core.Version('10.0.0-rc.0+45.sha-4414be7');
 
     /**
      * @license
