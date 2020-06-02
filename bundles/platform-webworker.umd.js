530b985fe5ccdab3423c94377a5842625ade3719
Matias Niemelä <matias@yearofmoo.com>
2020-06-02 21:55:10 +0000

    044a19966d ci: temporarily disable Android 10 browser unit tests on Saucelabs (#37399)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+45.sha-4414be7
+ * @license Angular v10.0.0-rc.0+48.sha-044a199
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+45.sha-4414be7');
+    var VERSION = new core.Version('10.0.0-rc.0+48.sha-044a199');
 
     /**
      * @license
