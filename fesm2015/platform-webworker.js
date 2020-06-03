2683c82a293ffcc7d19d6a77dd352b4506b491fe
atscott <atscott@google.com>
2020-06-03 19:57:54 +0000

    06a8ad28a7 refactor(forms): use a type guard to get rid of casts (#32541)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+49.sha-382aa50
+ * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+49.sha-382aa50');
+const VERSION = new Version('10.0.0-rc.0+50.sha-06a8ad2');
 
 /**
  * @license
