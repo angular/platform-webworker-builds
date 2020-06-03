2576e91170a25098bb1f6f968fb58a27659d8c09
atscott <atscott@google.com>
2020-06-03 20:57:26 +0000

    60a03b7ef7 refactor(compiler-cli): extract NgCompilerAdapter interface (#37118)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+14.sha-c7c0c1f
+ * @license Angular v10.0.0-rc.2+21.sha-60a03b7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.2+14.sha-c7c0c1f');
+    var VERSION = new core.Version('10.0.0-rc.2+21.sha-60a03b7');
 
     /**
      * @license
