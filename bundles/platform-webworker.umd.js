f267baf51ef3a95a35dfa7cea0c1d8f65b19c7f6
atscott <atscott@google.com>
2020-06-03 20:41:32 +0000

    40f3bb5638 refactor(dev-infra): small changes and fixes (#36800)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+50.sha-06a8ad2
+ * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+50.sha-06a8ad2');
+    var VERSION = new i0.Version('10.0.0-rc.0+54.sha-40f3bb5');
 
     /**
      * @license
