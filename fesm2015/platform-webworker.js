53e3e639219a1ca1e43c6ae7edc4475cc9541b3e
atscott <atscott@google.com>
2020-06-03 19:48:05 +0000

    382aa50c54 docs: refactor template-driven forms doc as a tutorial (#36732)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+48.sha-044a199
+ * @license Angular v10.0.0-rc.0+49.sha-382aa50
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+48.sha-044a199');
+const VERSION = new Version('10.0.0-rc.0+49.sha-382aa50');
 
 /**
  * @license
