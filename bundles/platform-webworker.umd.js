61e88523f3aa0053122451b8e49c351bf0627693
atscott <atscott@google.com>
2020-06-03 20:44:05 +0000

    ab3f4c9fe1 fix(core): infinite loop if injectable using inheritance has a custom decorator (#37022)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
+ * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+54.sha-40f3bb5');
+    var VERSION = new i0.Version('10.0.0-rc.0+55.sha-ab3f4c9');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+54.sha-40f3bb5
+ * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+54.sha-40f3bb5');
+const VERSION = new Version('10.0.0-rc.0+55.sha-ab3f4c9');
 
 /**
  * @license
