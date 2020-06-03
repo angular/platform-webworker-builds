3f655f8eab824b9f7d20208518252829b011852f
atscott <atscott@google.com>
2020-06-03 23:16:00 +0000

    34827559b9 docs: update the stackblitz in the GitHub Issue template (#37219)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+58.sha-65c3888
+ * @license Angular v10.0.0-rc.0+59.sha-3482755
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+58.sha-65c3888');
+const VERSION = new Version('10.0.0-rc.0+59.sha-3482755');
 
 /**
  * @license
