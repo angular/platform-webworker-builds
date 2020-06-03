f982ff19194c4d1adec0eae6046eaf27d3b2f151
atscott <atscott@google.com>
2020-06-03 20:48:42 +0000

    965a688c97 fix(compiler-cli): use ModuleWithProviders type if static eval fails (#37126)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+55.sha-ab3f4c9
+ * @license Angular v10.0.0-rc.0+56.sha-965a688
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+55.sha-ab3f4c9');
+const VERSION = new Version('10.0.0-rc.0+56.sha-965a688');
 
 /**
  * @license
