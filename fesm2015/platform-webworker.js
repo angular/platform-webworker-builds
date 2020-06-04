6b1c386fac27bb512f772216678f34d4401eb07b
atscott <atscott@google.com>
2020-06-04 18:08:50 +0000

    c8f7fc22c7 refactor(dev-infra): change required base commit sha (#37424)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+68.sha-c025357
+ * @license Angular v10.0.0-rc.0+70.sha-c8f7fc2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+68.sha-c025357');
+const VERSION = new Version('10.0.0-rc.0+70.sha-c8f7fc2');
 
 /**
  * @license
