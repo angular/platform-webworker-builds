4ca20952d0b97985c64f67da8704d92d25832707
atscott <atscott@google.com>
2020-06-04 20:04:08 +0000

    d713e33cc4 style(dev-infra): correct tslint failures in dev-infra directory (#37233)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+45.sha-9ec25ea
+ * @license Angular v10.0.0-rc.2+48.sha-d713e33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.2+45.sha-9ec25ea');
+    var VERSION = new core.Version('10.0.0-rc.2+48.sha-d713e33');
 
     /**
      * @license
