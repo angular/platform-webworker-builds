a1b63d9bc3ae49ec4a411e8fcdf52909345ee728
atscott <atscott@google.com>
2020-06-04 16:40:37 +0000

    6e7bd939f6 perf(ngcc): cache parsed tsconfig between runs (#37417)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+60.sha-c4f4675
+ * @license Angular v10.0.0-rc.0+62.sha-6e7bd93
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+60.sha-c4f4675');
+const VERSION = new Version('10.0.0-rc.0+62.sha-6e7bd93');
 
 /**
  * @license
