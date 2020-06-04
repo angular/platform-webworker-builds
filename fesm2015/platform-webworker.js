578469291ac4d35b7ca1acc09f22e2b7e91a5b6c
atscott <atscott@google.com>
2020-06-04 16:46:33 +0000

    c61decf7e5 docs: add blank line before header (#37391)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+61.sha-2021ad1
+ * @license Angular v10.0.0-rc.0+63.sha-c61decf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+61.sha-2021ad1');
+const VERSION = new Version('10.0.0-rc.0+63.sha-c61decf');
 
 /**
  * @license
