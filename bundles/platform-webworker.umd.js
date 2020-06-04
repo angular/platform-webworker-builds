325065ac42c5ffe093b72a509411247cd57a6e96
atscott <atscott@google.com>
2020-06-04 16:47:58 +0000

    57411c85b9 feat(ngcc): implement a program-based entry-point finder (#37075)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+63.sha-c61decf
+ * @license Angular v10.0.0-rc.0+67.sha-57411c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+63.sha-c61decf');
+    var VERSION = new i0.Version('10.0.0-rc.0+67.sha-57411c8');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+63.sha-c61decf
+ * @license Angular v10.0.0-rc.0+67.sha-57411c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+63.sha-c61decf');
+const VERSION = new Version('10.0.0-rc.0+67.sha-57411c8');
 
 /**
  * @license
