3d7b3b9aa47884f126013857f78d9a02841dd1cb
atscott <atscott@google.com>
2020-06-04 17:03:05 +0000

    c025357fb8 feat(dev-infra): Add oauth scope check to ensure necessary permissions for merge tooling (#37421)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+67.sha-57411c8
+ * @license Angular v10.0.0-rc.0+68.sha-c025357
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+67.sha-57411c8');
+const VERSION = new Version('10.0.0-rc.0+68.sha-c025357');
 
 /**
  * @license
