07c383a90e5bab147521cfe35f731b6e17f6e3ed
atscott <atscott@google.com>
2020-06-05 00:08:50 +0000

    3569fdf451 fix(common): prevent duplicate URL change notifications (#37404)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+74.sha-eb6ba9a
+ * @license Angular v10.0.0-rc.0+75.sha-3569fdf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+74.sha-eb6ba9a');
+const VERSION = new Version('10.0.0-rc.0+75.sha-3569fdf');
 
 /**
  * @license
