e6a30b8263f3496d103207816de17c656af5a4b2
atscott <atscott@google.com>
2020-06-05 17:10:23 +0000

    85b6c94cc6 refactor(dev-infra): move GitClient to common util (#37318)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+77.sha-d4c0962
+ * @license Angular v10.0.0-rc.0+79.sha-85b6c94
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+77.sha-d4c0962');
+    var VERSION = new core.Version('10.0.0-rc.0+79.sha-85b6c94');
 
     /**
      * @license
