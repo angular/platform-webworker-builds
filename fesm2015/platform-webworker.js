e09aba87e13035b35f36858e11332f84b9d7589e
atscott <atscott@google.com>
2020-06-05 16:42:33 +0000

    e31208beb1 docs: fix minor error in the "Structural directives" guide (#37452)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+75.sha-3569fdf
+ * @license Angular v10.0.0-rc.0+76.sha-e31208b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+75.sha-3569fdf');
+const VERSION = new Version('10.0.0-rc.0+76.sha-e31208b');
 
 /**
  * @license
