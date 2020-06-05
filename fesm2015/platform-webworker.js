05bad30148f44c82600b4494e07b013d6de55a85
atscott <atscott@google.com>
2020-06-05 18:27:02 +0000

    2349143477 fix(dev-infra): properly determine oauth scopes for git client token (#37462)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+56.sha-e9bff5f
+ * @license Angular v10.0.0-rc.2+57.sha-2349143
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+56.sha-e9bff5f');
+const VERSION = new Version('10.0.0-rc.2+57.sha-2349143');
 
 /**
  * @license
