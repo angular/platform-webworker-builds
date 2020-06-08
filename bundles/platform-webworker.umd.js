452acdc94f83abf06469af42d2b8aa42430598f9
atscott <atscott@google.com>
2020-06-08 17:32:13 +0000

    ed3c549063 ci: remove IgorMinar from reviewers list for pullapprove fallback group (#36456)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+90.sha-3a11617
+ * @license Angular v10.0.0-rc.0+91.sha-ed3c549
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+90.sha-3a11617');
+    var VERSION = new core.Version('10.0.0-rc.0+91.sha-ed3c549');
 
     /**
      * @license
