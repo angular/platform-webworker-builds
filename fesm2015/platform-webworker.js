6340ce93516325e8e6409c131b149d12deab95cc
atscott <atscott@google.com>
2020-06-08 22:26:26 +0000

    a1616ce181 docs: add note on publishing libraries in ivy (#36556)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+70.sha-1c22dff
+ * @license Angular v10.0.0-rc.2+71.sha-a1616ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
+const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
 
 /**
  * @license
