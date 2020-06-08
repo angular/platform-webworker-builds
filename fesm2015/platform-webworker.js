468caba5da048097775bb3659f136fd15b2f05d4
atscott <atscott@google.com>
2020-06-08 22:03:19 +0000

    1c22dff714 docs(docs-infra): fix small typo (#37258)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+69.sha-8d1d6e8
+ * @license Angular v10.0.0-rc.2+70.sha-1c22dff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+69.sha-8d1d6e8');
+const VERSION = new Version('10.0.0-rc.2+70.sha-1c22dff');
 
 /**
  * @license
