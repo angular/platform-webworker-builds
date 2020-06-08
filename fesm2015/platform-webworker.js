f90c0cac2ed3da8f55d6b75ac32b87404ebbdc16
atscott <atscott@google.com>
2020-06-08 16:54:39 +0000

    d63ecf4c5f fix(service-worker): Don't stay locked in EXISTING_CLIENTS_ONLY if corrupted data (#37453)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
+ * @license Angular v10.0.0-rc.0+88.sha-d63ecf4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+89.sha-5e64c2b');
+const VERSION = new Version('10.0.0-rc.0+88.sha-d63ecf4');
 
 /**
  * @license
