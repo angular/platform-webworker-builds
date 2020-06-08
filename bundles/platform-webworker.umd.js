cbfcc488d5c9666a1709966dbe14a233f4767831
atscott <atscott@google.com>
2020-06-08 16:47:06 +0000

    d887ba85ad build: update to latest stable Chromium 83.0.4103 in both rules_webtesting and puppeteer (#37427)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+87.sha-77efddc
+ * @license Angular v10.0.0-rc.0+86.sha-d887ba8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+87.sha-77efddc');
+    var VERSION = new i0.Version('10.0.0-rc.0+86.sha-d887ba8');
 
     /**
      * @license
