b7dab1652eb6a0aec930355598077f5d27aec121
atscott <atscott@google.com>
2020-06-08 17:47:36 +0000

    650974e1b3 test(language-service): Remove all markers from test project (#37475)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+91.sha-ed3c549
+ * @license Angular v10.0.0-rc.0+92.sha-650974e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+91.sha-ed3c549');
+    var VERSION = new core.Version('10.0.0-rc.0+92.sha-650974e');
 
     /**
      * @license
