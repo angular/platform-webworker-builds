60949af26bea3d6ccf77c0167867f6cb92abe99f
atscott <atscott@google.com>
2020-06-08 16:54:06 +0000

    5e64c2b1df style(ngcc): post-merge review tidy up (#37461)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+86.sha-d887ba8
+ * @license Angular v10.0.0-rc.0+89.sha-5e64c2b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+86.sha-d887ba8');
+const VERSION = new Version('10.0.0-rc.0+89.sha-5e64c2b');
 
 /**
  * @license
