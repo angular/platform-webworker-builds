3d008ed90e7f95dec9a03ee75e39558f1397b024
atscott <atscott@google.com>
2020-06-09 00:37:47 +0000

    8d817daf78 fix(router): Fix relative link generation from empty path components (#37446)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+97.sha-87fdd23
+ * @license Angular v10.0.0-rc.0+99.sha-8d817da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+97.sha-87fdd23');
+const VERSION = new Version('10.0.0-rc.0+99.sha-8d817da');
 
 /**
  * @license
