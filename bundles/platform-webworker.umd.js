00487ce14ac1fe9f630780f0fe0b2528a0d99fc1
atscott <atscott@google.com>
2020-06-09 20:50:04 +0000

    eaa38a5adc docs(dev-infra): add comment about what the requiredBaseCommit is (#37509)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+109.sha-cb13731
+ * @license Angular v10.0.0-rc.0+110.sha-eaa38a5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+109.sha-cb13731');
+    var VERSION = new core.Version('10.0.0-rc.0+110.sha-eaa38a5');
 
     /**
      * @license
