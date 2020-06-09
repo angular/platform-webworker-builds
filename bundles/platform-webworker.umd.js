82541ce860f0fa797b8c96716280db463e1f53f4
atscott <atscott@google.com>
2020-06-09 00:44:48 +0000

    bf2cb6fa48 feat(language-service): TS references from template items (#37437)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+71.sha-a1616ce
+ * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.2+71.sha-a1616ce');
+    var VERSION = new core.Version('10.0.0-rc.3+3.sha-bf2cb6f');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.2+71.sha-a1616ce
+ * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.2+71.sha-a1616ce');
+const VERSION = new Version('10.0.0-rc.3+3.sha-bf2cb6f');
 
 /**
  * @license
