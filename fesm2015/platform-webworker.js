0d44c227b9cf2bab00b43504aaf982fadf8a5bb9
atscott <atscott@google.com>
2020-06-09 15:50:46 +0000

    7301e70ddd fix(platform-server): correctly handle absolute relative URLs (#37341)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+101.sha-55979fe
+ * @license Angular v10.0.0-rc.0+104.sha-7301e70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+101.sha-55979fe');
+const VERSION = new Version('10.0.0-rc.0+104.sha-7301e70');
 
 /**
  * @license
