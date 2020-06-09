987575776820f5715dd8c3a64dbc750640357a4e
atscott <atscott@google.com>
2020-06-09 15:48:06 +0000

    7301e70ddd fix(platform-server): correctly handle absolute relative URLs (#37341)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+101.sha-55979fe
+ * @license Angular v10.0.0-rc.0+104.sha-7301e70
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+101.sha-55979fe');
+    var VERSION = new core.Version('10.0.0-rc.0+104.sha-7301e70');
 
     /**
      * @license
