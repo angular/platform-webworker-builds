5c74b083425dff3a0c2beeea0e081cb402ee37c1
atscott <atscott@google.com>
2020-06-09 22:44:12 +0000

    99c41d6db0 release: cut the v9.1.10 release

--- fesm5/platform-webworker.js
+++ fesm5/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.9+18.sha-1aae94a
+ * @license Angular v9.1.10
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1068,7 +1068,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-var VERSION = new Version('9.1.9+18.sha-1aae94a');
+var VERSION = new Version('9.1.10');
 
 /**
  * @license
