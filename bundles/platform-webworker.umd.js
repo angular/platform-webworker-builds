7ef3ec01e85139b54f500c31003ddec7276dc157
atscott <atscott@google.com>
2020-06-09 18:44:21 +0000

    ec6a7ab721 docs: wrong links in lifecycle hooks api documentaion (#36557)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+10.sha-ad6d2b4
+ * @license Angular v10.0.0-rc.3+11.sha-ec6a7ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.3+10.sha-ad6d2b4');
+    var VERSION = new core.Version('10.0.0-rc.3+11.sha-ec6a7ab');
 
     /**
      * @license
