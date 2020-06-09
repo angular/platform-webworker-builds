c05773cdfd2804cff263812a880e74a343a74510
atscott <atscott@google.com>
2020-06-09 15:53:43 +0000

    ad6d2b4619 docs(core): fix path referenced in comments of both compiler facade interface files (#37370)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
+ * @license Angular v10.0.0-rc.3+10.sha-ad6d2b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1317,7 +1317,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.3+3.sha-bf2cb6f');
+    var VERSION = new core.Version('10.0.0-rc.3+10.sha-ad6d2b4');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+3.sha-bf2cb6f
+ * @license Angular v10.0.0-rc.3+10.sha-ad6d2b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1066,7 +1066,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.3+3.sha-bf2cb6f');
+const VERSION = new Version('10.0.0-rc.3+10.sha-ad6d2b4');
 
 /**
  * @license
