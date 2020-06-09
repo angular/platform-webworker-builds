95b5f9df304f79c43e009e361b7b3015f2b6613b
atscott <atscott@google.com>
2020-06-09 00:41:24 +0000

    946f1179e9 docs: release notes for the v10.0.0-rc.3 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+99.sha-8d817da
+ * @license Angular v10.0.0-rc.0+98.sha-946f117
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+99.sha-8d817da');
+    var VERSION = new i0.Version('10.0.0-rc.0+98.sha-946f117');
 
     /**
      * @license
