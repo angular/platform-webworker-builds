d40b3759ef03b1191fa7135cc04fa822f3524e8e
atscott <atscott01@gmail.com>
2020-06-10 17:52:45 +0000

    7b005bb9cf Revert "fix(elements): fire custom element output events during component initialization (#36161)" (#37524)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+114.sha-401ef71
+ * @license Angular v10.0.0-rc.0+115.sha-7b005bb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+114.sha-401ef71');
+const VERSION = new Version('10.0.0-rc.0+115.sha-7b005bb');
 
 /**
  * @license
