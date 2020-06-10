d48c5d211b0d071646e0c021e8d40fcabfc87335
atscott <atscott@google.com>
2020-06-10 20:22:41 +0000

    c2f4a9bf71 build: remove ngcontainer Docker (#36421)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+5.sha-231095f
+ * @license Angular v10.0.0-rc.4+6.sha-c2f4a9b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.4+5.sha-231095f');
+    var VERSION = new core.Version('10.0.0-rc.4+6.sha-c2f4a9b');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+5.sha-231095f
+ * @license Angular v10.0.0-rc.4+6.sha-c2f4a9b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.4+5.sha-231095f');
+const VERSION = new Version('10.0.0-rc.4+6.sha-c2f4a9b');
 
 /**
  * @license
