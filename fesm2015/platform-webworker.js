a72dd68acf6dbe485974f31fb4f053c4d5b94343
atscott <atscott@google.com>
2020-06-10 19:25:46 +0000

    231095fe8a build: add commit message scope for migration changes (#36390)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.3+16.sha-742f3d6
+ * @license Angular v10.0.0-rc.4+5.sha-231095f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1061,7 +1061,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.3+16.sha-742f3d6');
+const VERSION = new Version('10.0.0-rc.4+5.sha-231095f');
 
 /**
  * @license
