ebb589679b8b6be95a3f4d1b0ebb6e9762f43744
atscott <atscott@google.com>
2020-06-10 19:18:19 +0000

    e836eaf448 fix(docs-infra): preconnect to analytics origins (#37400)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+121.sha-a398d6a
+ * @license Angular v10.0.0-rc.0+120.sha-e836eaf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1053,7 +1053,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+121.sha-a398d6a');
+const VERSION = new Version('10.0.0-rc.0+120.sha-e836eaf');
 
 /**
  * @license
