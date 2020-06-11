f17000d19b530a2af548ada0d642a4cf17791e15
Misko Hevery <misko@hevery.com>
2020-06-11 20:31:38 +0000

    a937889c3b refactor(elements): add accessor workaround for build-optimizer (#37456)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+130.sha-8c682c5
+ * @license Angular v10.0.0-rc.0+134.sha-a937889
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+130.sha-8c682c5');
+    var VERSION = new core.Version('10.0.0-rc.0+134.sha-a937889');
 
     /**
      * @license
