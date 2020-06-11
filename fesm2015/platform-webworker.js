f5af4b1210e9bef37732bc7f8e53f1ef42744777
Misko Hevery <misko@hevery.com>
2020-06-11 20:57:30 +0000

    97dc85ba5e feat(core): support injection token as predicate in queries (#37506)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+134.sha-a937889
+ * @license Angular v10.0.0-rc.0+135.sha-97dc85b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+134.sha-a937889');
+const VERSION = new Version('10.0.0-rc.0+135.sha-97dc85b');
 
 /**
  * @license
