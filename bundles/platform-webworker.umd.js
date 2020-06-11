9f75bec26e00344933a2de8b5c035424b411c9ae
Misko Hevery <misko@hevery.com>
2020-06-11 18:24:34 +0000

    39f973430a docs: remove out of date GDEs (#36467)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+124.sha-20346ff
+ * @license Angular v10.0.0-rc.0+125.sha-39f9734
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+124.sha-20346ff');
+    var VERSION = new core.Version('10.0.0-rc.0+125.sha-39f9734');
 
     /**
      * @license
