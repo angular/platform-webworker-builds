d6bfbfe900837c01eda3bb4fe21be13bb64eb358
Misko Hevery <misko@hevery.com>
2020-06-11 23:26:31 +0000

    3a43cdefe8 release: cut the v10.0.0-rc.5 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+14.sha-38c48be
+ * @license Angular v10.0.0-rc.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.4+14.sha-38c48be');
+    var VERSION = new core.Version('10.0.0-rc.5');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.4+14.sha-38c48be
+ * @license Angular v10.0.0-rc.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.4+14.sha-38c48be');
+const VERSION = new Version('10.0.0-rc.5');
 
 /**
  * @license
