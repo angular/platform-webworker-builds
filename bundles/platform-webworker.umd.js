f0dbf22280d721042e6da20ebfbc81dbf81c770e
Misko Hevery <misko@hevery.com>
2020-06-12 22:27:00 +0000

    1bc807c81a style(common): enforce format on newly included files (#36940)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+161.sha-8dcf618
+ * @license Angular v10.0.0-rc.0+164.sha-1bc807c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+161.sha-8dcf618');
+    var VERSION = new core.Version('10.0.0-rc.0+164.sha-1bc807c');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+161.sha-8dcf618
+ * @license Angular v10.0.0-rc.0+164.sha-1bc807c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+161.sha-8dcf618');
+const VERSION = new Version('10.0.0-rc.0+164.sha-1bc807c');
 
 /**
  * @license
