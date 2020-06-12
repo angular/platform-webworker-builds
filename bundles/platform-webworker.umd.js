9b14afa5008a2c32777ca8a6bac8ca9a0411d100
Misko Hevery <misko@hevery.com>
2020-06-12 22:39:24 +0000

    1091ddbb8e build(bazel): check for renderer flag in `ng_module()` (#37529)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+168.sha-62e4acc
+ * @license Angular v10.0.0-rc.0+167.sha-1091ddb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+168.sha-62e4acc');
+    var VERSION = new i0.Version('10.0.0-rc.0+167.sha-1091ddb');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+168.sha-62e4acc
+ * @license Angular v10.0.0-rc.0+167.sha-1091ddb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+168.sha-62e4acc');
+const VERSION = new Version('10.0.0-rc.0+167.sha-1091ddb');
 
 /**
  * @license
