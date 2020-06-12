3022c6d63386186d2387c2ba7063a36410c6b84e
Misko Hevery <misko@hevery.com>
2020-06-12 02:19:53 +0000

    284123c6ba fix(core): should fake a top event task when coalescing events to prevent draining microTaskQueue too early. (#36841)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+136.sha-7e5f6c2
+ * @license Angular v10.0.0-rc.0+138.sha-284123c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+136.sha-7e5f6c2');
+const VERSION = new Version('10.0.0-rc.0+138.sha-284123c');
 
 /**
  * @license
