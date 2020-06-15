178fd57afa9bc69014df9523543003156c171813
Misko Hevery <misko@hevery.com>
2020-06-15 21:59:52 +0000

    ceca5dded5 feat(bazel): expose explicit mapping from closure to devmode files (#36262)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+182.sha-87a679b
+ * @license Angular v10.0.0-rc.0+186.sha-ceca5dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1290,7 +1290,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+182.sha-87a679b');
+    var VERSION = new i0.Version('10.0.0-rc.0+186.sha-ceca5dd');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+182.sha-87a679b
+ * @license Angular v10.0.0-rc.0+186.sha-ceca5dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+182.sha-87a679b');
+const VERSION = new Version('10.0.0-rc.0+186.sha-ceca5dd');
 
 /**
  * @license
