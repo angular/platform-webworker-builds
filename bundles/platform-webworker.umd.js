fb8220b460b0b81f186a4101e3039c2203df8c5f
Misko Hevery <misko@hevery.com>
2020-06-15 21:55:01 +0000

    87a679b210 build: import in-memory-web-api project (#37182)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+185.sha-2a145f2
+ * @license Angular v10.0.0-rc.0+182.sha-87a679b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+185.sha-2a145f2');
+    var VERSION = new core.Version('10.0.0-rc.0+182.sha-87a679b');
 
     /**
      * @license
