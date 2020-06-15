f8e38ad8c3129ad207f2e24af78ac437fa194d22
Misko Hevery <misko@hevery.com>
2020-06-15 21:57:04 +0000

    ceca5dded5 feat(bazel): expose explicit mapping from closure to devmode files (#36262)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+182.sha-87a679b
+ * @license Angular v10.0.0-rc.0+186.sha-ceca5dd
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+182.sha-87a679b');
+    var VERSION = new core.Version('10.0.0-rc.0+186.sha-ceca5dd');
 
     /**
      * @license
