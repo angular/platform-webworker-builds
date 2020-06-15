e842c61c7c7b811fc4dea28cdd129e6d7485dc32
Misko Hevery <misko@hevery.com>
2020-06-15 21:46:51 +0000

    a7359d494a docs: release notes for the v10.0.0-rc.6 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+180.sha-66e6b93
+ * @license Angular v10.0.0-rc.0+181.sha-a7359d4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+180.sha-66e6b93');
+    var VERSION = new core.Version('10.0.0-rc.0+181.sha-a7359d4');
 
     /**
      * @license
