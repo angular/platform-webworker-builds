9344b38a9b1f4ffc003d478038bb90e301182e71
Misko Hevery <misko@hevery.com>
2020-06-15 21:57:40 +0000

    6c7467a58b perf(forms): optimize internal method _anyControls in FormGroup (#32534)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.5
+ * @license Angular v10.0.0-rc.6+3.sha-6c7467a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.5');
+const VERSION = new Version('10.0.0-rc.6+3.sha-6c7467a');
 
 /**
  * @license
