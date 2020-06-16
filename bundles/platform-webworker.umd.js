874359f1ec36037914f06e095b0c0e6fd8b1ccdd
Misko Hevery <misko@hevery.com>
2020-06-16 19:26:11 +0000

    26b0f3dc96 docs: add side effect `package.json` in app structure (#37521)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+4.sha-97bb88f
+ * @license Angular v10.0.0-rc.6+7.sha-26b0f3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.6+4.sha-97bb88f');
+    var VERSION = new core.Version('10.0.0-rc.6+7.sha-26b0f3d');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+4.sha-97bb88f
+ * @license Angular v10.0.0-rc.6+7.sha-26b0f3d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.6+4.sha-97bb88f');
+const VERSION = new Version('10.0.0-rc.6+7.sha-26b0f3d');
 
 /**
  * @license
