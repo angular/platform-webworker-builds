99d8f4f9a929e9d4d2e2ffb3a8ea1f497d51d989
Misko Hevery <misko@hevery.com>
2020-06-16 17:06:33 +0000

    1185aa07a3 docs: wrong example in routerLink (#37590)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+186.sha-ceca5dd
+ * @license Angular v10.0.0-rc.0+187.sha-1185aa0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+186.sha-ceca5dd');
+    var VERSION = new core.Version('10.0.0-rc.0+187.sha-1185aa0');
 
     /**
      * @license
