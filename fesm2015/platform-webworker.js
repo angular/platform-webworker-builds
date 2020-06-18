c8f26ea81eb85504fc7b2396217b20550e0cd2a2
Misko Hevery <misko@hevery.com>
2020-06-18 23:25:45 +0000

    bd7f440357 perf(ngcc): shortcircuit tokenizing in ESM dependency host (#37639)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+193.sha-71f008f
+ * @license Angular v10.0.0-rc.0+198.sha-bd7f440
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+193.sha-71f008f');
+const VERSION = new Version('10.0.0-rc.0+198.sha-bd7f440');
 
 /**
  * @license
