953cd7db76c916ad722766a079e211ea30a4cac6
Misko Hevery <misko@hevery.com>
2020-06-22 18:11:11 +0000

    2a970a0af8 Revert "fix(router): fix navigation ignoring logic to compare to the browser url (#37408)" (#37650)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+196.sha-1d844b9
+ * @license Angular v10.0.0-rc.0+199.sha-2a970a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+196.sha-1d844b9');
+    var VERSION = new core.Version('10.0.0-rc.0+199.sha-2a970a0');
 
     /**
      * @license
