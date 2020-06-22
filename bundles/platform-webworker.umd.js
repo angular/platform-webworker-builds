507e4db7c7688488bbcd7a1ad1e1fe1ebadf9295
Misko Hevery <misko@hevery.com>
2020-06-22 18:14:02 +0000

    7e0eccc4ef docs: Refactor-i18n (#36924)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+199.sha-2a970a0
+ * @license Angular v10.0.0-rc.0+200.sha-7e0eccc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+199.sha-2a970a0');
+    var VERSION = new core.Version('10.0.0-rc.0+200.sha-7e0eccc');
 
     /**
      * @license
