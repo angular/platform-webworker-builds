ea3159562c3139c078e5ba424b4c0749699abd47
Misko Hevery <misko@hevery.com>
2020-06-22 21:01:38 +0000

    5c40fd65fa refactor(compiler-cli): tidy up file_system BUILD.bazel file_system (#37114)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+212.sha-c8c0063
+ * @license Angular v10.0.0-rc.0+216.sha-5c40fd6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+212.sha-c8c0063');
+const VERSION = new Version('10.0.0-rc.0+216.sha-5c40fd6');
 
 /**
  * @license
