924f04924e53dce31d5d9abebc13efd6be27f2e4
Misko Hevery <misko@hevery.com>
2020-06-23 23:56:25 +0000

    27aa00b15f docs: add lightweight token page to library section of docs (#36144)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+20.sha-36a00a2
+ * @license Angular v10.0.0-rc.6+21.sha-27aa00b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.6+20.sha-36a00a2');
+    var VERSION = new core.Version('10.0.0-rc.6+21.sha-27aa00b');
 
     /**
      * @license
