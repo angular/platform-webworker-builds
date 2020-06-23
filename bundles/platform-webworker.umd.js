a49d2920e9e84c1e0f91ab454e20dd6356a3cabb
Misko Hevery <misko@hevery.com>
2020-06-23 20:40:36 +0000

    71c0363bb5 docs: add tslib update migration docs (#37402)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+222.sha-e03adaf
+ * @license Angular v10.0.0-rc.0+223.sha-71c0363
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+222.sha-e03adaf');
+    var VERSION = new i0.Version('10.0.0-rc.0+223.sha-71c0363');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
