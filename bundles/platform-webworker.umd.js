d27f99dc62f1052b36c0730be8609cd0e7a4116a
Andrew Kushnir <akushnir@google.com>
2020-06-23 19:16:39 +0000

    82c8b44db7 docs: Initial commit of update guide for v10 release. (#37152)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+14.sha-4c1edd5
+ * @license Angular v10.0.0-rc.6+17.sha-82c8b44
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.6+14.sha-4c1edd5');
+    var VERSION = new core.Version('10.0.0-rc.6+17.sha-82c8b44');
 
     /**
      * @license
