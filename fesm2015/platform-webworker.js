f7dd7a064b601654cc4f4b7481ed38a0ebf91992
Andrew Kushnir <akushnir@google.com>
2020-06-23 19:23:00 +0000

    5229809adb docs: Initial commit of update guide for v10 release. (#37152)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+216.sha-5c40fd6
+ * @license Angular v10.0.0-rc.0+220.sha-5229809
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+216.sha-5c40fd6');
+const VERSION = new Version('10.0.0-rc.0+220.sha-5229809');
 
 /**
  * @license
