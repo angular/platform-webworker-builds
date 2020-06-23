7120afdedce7af7f3e1538a2f959ee1ccc68c858
Misko Hevery <misko@hevery.com>
2020-06-23 20:35:46 +0000

    36a00a255b ci(docs-infra): disable flaky tests (#37673)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+17.sha-82c8b44
+ * @license Angular v10.0.0-rc.6+20.sha-36a00a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.6+17.sha-82c8b44');
+    var VERSION = new core.Version('10.0.0-rc.6+20.sha-36a00a2');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+17.sha-82c8b44
+ * @license Angular v10.0.0-rc.6+20.sha-36a00a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.6+17.sha-82c8b44');
+const VERSION = new Version('10.0.0-rc.6+20.sha-36a00a2');
 
 /**
  * @license
