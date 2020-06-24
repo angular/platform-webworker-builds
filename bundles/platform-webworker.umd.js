08f1ee61283690bc00677ff99b5532965ce02d50
Misko Hevery <misko@hevery.com>
2020-06-24 17:05:21 +0000

    243824ef74 docs: updating guides and docs related to the v10 release (#37705)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+224.sha-c95802f
+ * @license Angular v10.0.0-rc.0+225.sha-243824e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+224.sha-c95802f');
+    var VERSION = new i0.Version('10.0.0-rc.0+225.sha-243824e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
