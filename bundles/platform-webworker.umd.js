08d46ee7134df84559ebc7385fbe06651904cda3
Misko Hevery <misko@hevery.com>
2020-06-24 23:24:52 +0000

    96891a076f release: sort the v10.0.0 release in `CHANGELOG.md`

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.6+23.sha-c49507b
+ * @license Angular v10.0.0+2.sha-96891a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.6+23.sha-c49507b');
+    var VERSION = new core.Version('10.0.0+2.sha-96891a0');
 
     /**
      * @license
