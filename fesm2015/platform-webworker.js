086905d5bbfeb05369bfcc161c8a53e0a0c9e630
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:03:12 +0000

    8924ec1474 test(docs-infra): add end to end tests for api reference (#37612)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+231.sha-bedc045
+ * @license Angular v10.0.0-rc.0+229.sha-8924ec1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+231.sha-bedc045');
+const VERSION = new Version('10.0.0-rc.0+229.sha-8924ec1');
 
 /**
  * @license
