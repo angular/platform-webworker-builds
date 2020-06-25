9de893fc35fb81bbcbbefc20fecf0323ba6293db
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:03:46 +0000

    a0595f4b4f fix(forms): change error message (#37643)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+229.sha-8924ec1
+ * @license Angular v10.0.0-rc.0+230.sha-a0595f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+229.sha-8924ec1');
+    var VERSION = new i0.Version('10.0.0-rc.0+230.sha-a0595f4');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
