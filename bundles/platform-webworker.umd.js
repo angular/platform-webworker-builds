ff65289dd1b10c9c4e8ecd38fc4af372017bbc31
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:12:00 +0000

    1c1eb3045c docs: Add support schedule for v10 (#37745)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+235.sha-666e6d1
+ * @license Angular v10.0.0-rc.0+237.sha-1c1eb30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+235.sha-666e6d1');
+    var VERSION = new core.Version('10.0.0-rc.0+237.sha-1c1eb30');
 
     /**
      * @license
