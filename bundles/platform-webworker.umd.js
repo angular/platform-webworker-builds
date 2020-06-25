3c8b12bee8e8e72123bc2694828e6df525024951
Misko Hevery <misko@hevery.com>
2020-06-25 03:20:25 +0000

    4481bebb38 docs: add v9.angular.io to the angular.io version picker (#37719)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+227.sha-f5bbfd7
+ * @license Angular v10.0.0-rc.0+228.sha-4481beb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+227.sha-f5bbfd7');
+    var VERSION = new i0.Version('10.0.0-rc.0+228.sha-4481beb');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
