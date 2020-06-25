d205561cd16e7cc7a11797edfd0d62ce991a8b2f
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:36:23 +0000

    e36d5b201a fix(forms): correct usage of `selectedOptions` (#37620)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+245.sha-8a74508
+ * @license Angular v10.0.0-rc.0+243.sha-e36d5b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+245.sha-8a74508');
+const VERSION = new Version('10.0.0-rc.0+243.sha-e36d5b2');
 
 /**
  * @license
