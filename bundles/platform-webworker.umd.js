2a4160ea0f7199cb27c7a62f4fdfd52e957c3142
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:58:21 +0000

    9118f49a63 fix(core): cleanup DOM elements when root view is removed (#37600)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+260.sha-d12cdb5
+ * @license Angular v10.0.0-rc.0+262.sha-9118f49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+260.sha-d12cdb5');
+    var VERSION = new i0.Version('10.0.0-rc.0+262.sha-9118f49');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
