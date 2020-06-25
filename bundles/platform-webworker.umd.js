db48a9546e900f628222f040e15065828370eb26
Andrew Kushnir <akushnir@google.com>
2020-06-25 21:43:17 +0000

    ce879fc416 refactor(compiler-cli): more accurate reporting of complex function call (#37587)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+256.sha-5103d90
+ * @license Angular v10.0.0-rc.0+259.sha-ce879fc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+256.sha-5103d90');
+    var VERSION = new i0.Version('10.0.0-rc.0+259.sha-ce879fc');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
