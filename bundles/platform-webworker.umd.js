49dce56a9fbb958420d26bf66bfe9f71868472a6
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:39:03 +0000

    e36d5b201a fix(forms): correct usage of `selectedOptions` (#37620)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+245.sha-8a74508
+ * @license Angular v10.0.0-rc.0+243.sha-e36d5b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+245.sha-8a74508');
+    var VERSION = new i0.Version('10.0.0-rc.0+243.sha-e36d5b2');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
