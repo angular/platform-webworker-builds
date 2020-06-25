89073012885d3f758059dc28fcc4efd5f4dc3acd
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:38:29 +0000

    a5d5f67be7 fix(http): avoid abort a request when fetch operation is completed (#37367)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+4.sha-0d2cdf6
+ * @license Angular v10.0.0+15.sha-a5d5f67
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0+4.sha-0d2cdf6');
+const VERSION = new Version('10.0.0+15.sha-a5d5f67');
 
 /**
  * @license
