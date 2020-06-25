1db1cab656687e2001128b0498f087d23b9e4cd0
Andrew Kushnir <akushnir@google.com>
2020-06-25 19:05:58 +0000

    f3ec37ebbf release(benchpress): bump version to v0.2.1 and update `@angular/core` peer dependency (#37676)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+232.sha-29490cc
+ * @license Angular v10.0.0-rc.0+234.sha-f3ec37e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+232.sha-29490cc');
+    var VERSION = new i0.Version('10.0.0-rc.0+234.sha-f3ec37e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
