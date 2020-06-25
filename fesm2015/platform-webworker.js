1aa64931b35663ea75045661e83d1765728c1b92
Igor Minar <igor@angular.io>
2020-06-25 21:08:33 +0000

    0feb560136 fixup! feat(docs-infra): update deploy-to-firebase.sh script to support v9 multisite setup

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+1.sha-6c1ab47
+ * @license Angular v9.1.11+3.sha-0feb560
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('9.1.11+1.sha-6c1ab47');
+const VERSION = new Version('9.1.11+3.sha-0feb560');
 
 /**
  * @fileoverview added by tsickle
