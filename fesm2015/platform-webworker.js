dd0a36e3207e94523bcd8c364ea4aece3eef180a
Andrew Kushnir <akushnir@google.com>
2020-06-25 22:46:15 +0000

    a9a4edebe2 fix(docs-infra): fix typo in the deploy-to-firebase.sh script (#37754)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0+20.sha-64f2ffa
+ * @license Angular v10.0.0+21.sha-a9a4ede
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0+20.sha-64f2ffa');
+const VERSION = new Version('10.0.0+21.sha-a9a4ede');
 
 /**
  * @license
