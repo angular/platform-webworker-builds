58103f04b82904ee39cf4f8e9b39ccd5a0a220ac
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:29:02 +0000

    9c668e091f docs(elements): fixed command that adds the package @angular/elements (#37681)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+265.sha-4c7f32f
+ * @license Angular v10.0.0-rc.0+268.sha-9c668e0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+265.sha-4c7f32f');
+const VERSION = new Version('10.0.0-rc.0+268.sha-9c668e0');
 
 /**
  * @license
