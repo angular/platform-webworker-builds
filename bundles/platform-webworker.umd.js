c4b1838835e653ee48667862bb2811ad29da7460
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:31:55 +0000

    c00f4ab2ae fix(core): don't consider inherited NG_ELEMENT_ID during DI (#37574)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+287.sha-2038568
+ * @license Angular v10.0.0-rc.0+288.sha-c00f4ab
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+287.sha-2038568');
+    var VERSION = new i0.Version('10.0.0-rc.0+288.sha-c00f4ab');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
