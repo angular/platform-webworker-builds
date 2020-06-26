84355597864dbfbff47f271437f43a9f5d2de98e
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:20:00 +0000

    f954ab6f10 docs: add note about the month being zero-based in the Date constructor (#37770)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+272.sha-3ee6665
+ * @license Angular v10.0.0-rc.0+273.sha-f954ab6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+272.sha-3ee6665');
+const VERSION = new Version('10.0.0-rc.0+273.sha-f954ab6');
 
 /**
  * @license
