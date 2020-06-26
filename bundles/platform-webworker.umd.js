a85a19cd5f7d89a6e88ce2a1b3aa0900ea91efc3
Andrew Kushnir <akushnir@google.com>
2020-06-26 17:28:24 +0000

    6341a837c1 docs: correct outdated dev instructions for public api golds (#37026)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+275.sha-c942662
+ * @license Angular v10.0.0-rc.0+274.sha-6341a83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+275.sha-c942662');
+    var VERSION = new i0.Version('10.0.0-rc.0+274.sha-6341a83');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
