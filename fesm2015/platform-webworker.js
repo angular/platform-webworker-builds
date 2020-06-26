01fe433095308b37b24c1c692092d7b077cebdcd
Andrew Kushnir <akushnir@google.com>
2020-06-26 00:28:55 +0000

    b2816a1536 docs: typo fixes for schematics-for-libraries.md (#37753)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+264.sha-8572e88
+ * @license Angular v10.0.0-rc.0+267.sha-b2816a1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+264.sha-8572e88');
+const VERSION = new Version('10.0.0-rc.0+267.sha-b2816a1');
 
 /**
  * @license
