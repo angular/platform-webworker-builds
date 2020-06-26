29f295a3f8f749f71f9ffb8be097477f4dddd8f1
Andrew Kushnir <akushnir@google.com>
2020-06-26 22:21:54 +0000

    ae5257cda6 fix(language-service): incorrect autocomplete results on unknown symbol (#37518)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+282.sha-8fd8143
+ * @license Angular v10.0.0-rc.0+283.sha-ae5257c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+282.sha-8fd8143');
+const VERSION = new Version('10.0.0-rc.0+283.sha-ae5257c');
 
 /**
  * @license
