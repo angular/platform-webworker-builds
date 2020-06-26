2741299993a241377eb6e06bd3106eaa0b3be2f5
Andrew Kushnir <akushnir@google.com>
2020-06-26 18:41:11 +0000

    acf3cff9ee ci: exclude "docs" commit type from minBodyLength commit message validation (#37764)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+277.sha-83fe963
+ * @license Angular v10.0.0-rc.0+281.sha-acf3cff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+277.sha-83fe963');
+    var VERSION = new i0.Version('10.0.0-rc.0+281.sha-acf3cff');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
