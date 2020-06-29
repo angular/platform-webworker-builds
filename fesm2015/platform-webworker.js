3a06d1a24a38e91728a0316e95c08bf2f52a009a
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:28:03 +0000

    a177b1b7b1 docs: change definition of providedIn any (#35292)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+303.sha-e84539f
+ * @license Angular v10.0.0-rc.0+301.sha-a177b1b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+303.sha-e84539f');
+const VERSION = new Version('10.0.0-rc.0+301.sha-a177b1b');
 
 /**
  * @license
