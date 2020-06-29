671c5d92a15f2e86341de67c191b04200a7477c5
Andrew Kushnir <akushnir@google.com>
2020-06-29 19:44:57 +0000

    7a91b23cb5 fix(core): fake_async_fallback should have the same logic with fake-async (#37680)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.1+10.sha-89e16ed');
+    var VERSION = new core.Version('10.0.1+16.sha-7a91b23');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+10.sha-89e16ed
+ * @license Angular v10.0.1+16.sha-7a91b23
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.1+10.sha-89e16ed');
+const VERSION = new Version('10.0.1+16.sha-7a91b23');
 
 /**
  * @license
