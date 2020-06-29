0ef541dccc1f7d30d59f548fc16bf5bfafa45170
Andrew Kushnir <akushnir@google.com>
2020-06-29 23:59:53 +0000

    57a518a36d perf(compiler-cli): fix memory leak in retained incremental state (#37835)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+20.sha-29b8318
+ * @license Angular v10.0.1+21.sha-57a518a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.1+20.sha-29b8318');
+    var VERSION = new core.Version('10.0.1+21.sha-57a518a');
 
     /**
      * @license
