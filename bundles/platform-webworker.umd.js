eca9c64ce1f9bcbffc66d9ec6496cbaf763ad623
Andrew Kushnir <akushnir@google.com>
2020-06-29 22:27:43 +0000

    e84539f809 build(docs-infra): update to latest dgeni-packages (#37793)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+302.sha-873330b
+ * @license Angular v10.0.0-rc.0+303.sha-e84539f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+302.sha-873330b');
+    var VERSION = new i0.Version('10.0.0-rc.0+303.sha-e84539f');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
