365a73d9019a51762d80ab3b9718414321bcb869
Andrew Kushnir <akushnir@google.com>
2020-06-29 18:02:00 +0000

    89e16ed6a5 fix(elements): fire custom element output events during component initialization (#37570)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.1+4.sha-78460c1
+ * @license Angular v10.0.1+10.sha-89e16ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1312,7 +1312,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.1+4.sha-78460c1');
+    var VERSION = new core.Version('10.0.1+10.sha-89e16ed');
 
     /**
      * @license
