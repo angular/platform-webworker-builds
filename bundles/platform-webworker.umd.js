95fffbd3c63f7642308bd6ed4e951561a8559931
Andrew Kushnir <akushnir@google.com>
2020-06-30 18:09:06 +0000

    79dc14199b docs: update `/config/app-package-json` redirect (#37774)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+304.sha-7195625
+ * @license Angular v10.0.0-rc.0+305.sha-79dc141
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+304.sha-7195625');
+    var VERSION = new i0.Version('10.0.0-rc.0+305.sha-79dc141');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
