df6c656f5a56566aa7d8398f338b5f7d080e507c
Andrew Kushnir <akushnir@google.com>
2020-06-30 19:23:47 +0000

    6fc80690ae docs: release notes for the v10.0.2 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+306.sha-f6d2705
+ * @license Angular v10.0.0-rc.0+307.sha-6fc8069
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+306.sha-f6d2705');
+    var VERSION = new i0.Version('10.0.0-rc.0+307.sha-6fc8069');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
