3586633d1ca6cf8fc15830d6eb5cff0c4f4af19f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-01 19:19:12 +0000

    196bfa8fae docs: restore link to lightweight token page in left nav (#37855)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+311.sha-1d5ca55
+ * @license Angular v10.0.0-rc.0+312.sha-196bfa8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+311.sha-1d5ca55');
+const VERSION = new Version('10.0.0-rc.0+312.sha-196bfa8');
 
 /**
  * @license
