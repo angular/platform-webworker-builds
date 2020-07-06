f84da66fe4167cec87c47d70e792a251d629a037
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:19:19 +0000

    596fbb3f06 build(docs-infra): improve applying post-install patches (#37896)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+316.sha-1801d0c
+ * @license Angular v10.0.0-rc.0+319.sha-596fbb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+316.sha-1801d0c');
+const VERSION = new Version('10.0.0-rc.0+319.sha-596fbb3');
 
 /**
  * @license
