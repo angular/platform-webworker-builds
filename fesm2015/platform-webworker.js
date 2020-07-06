d75b6fe2b275105d3e8a10cf749c740414dcbb56
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 16:59:39 +0000

    a5ffca0576 fix(router): fix navigation ignoring logic to compare to the browser url (#37716)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+312.sha-196bfa8
+ * @license Angular v10.0.0-rc.0+313.sha-a5ffca0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+312.sha-196bfa8');
+const VERSION = new Version('10.0.0-rc.0+313.sha-a5ffca0');
 
 /**
  * @license
