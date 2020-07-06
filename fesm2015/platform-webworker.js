929864429c8697a3b47d3d9f918d9555fa4644f0
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:09:10 +0000

    d7dd2959c8 perf(router): apply prioritizedGuardValue operator to optimize CanLoad guards (#37523)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+315.sha-a5c3073
+ * @license Angular v10.0.0-rc.0+314.sha-d7dd295
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+315.sha-a5c3073');
+const VERSION = new Version('10.0.0-rc.0+314.sha-d7dd295');
 
 /**
  * @license
