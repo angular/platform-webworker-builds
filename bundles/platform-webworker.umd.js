1e8d6dfa15e1466c97a5bb66225244d5b6740a48
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:19:49 +0000

    d148fdccf2 fix(dev-infra): fix typo in ng-dev config (#37862)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+319.sha-596fbb3
+ * @license Angular v10.0.0-rc.0+317.sha-d148fdc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+319.sha-596fbb3');
+    var VERSION = new core.Version('10.0.0-rc.0+317.sha-d148fdc');
 
     /**
      * @license
