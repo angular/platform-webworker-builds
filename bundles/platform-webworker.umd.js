091318400c82666d72cd81798cb8676399fca9bd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:21:55 +0000

    596fbb3f06 build(docs-infra): improve applying post-install patches (#37896)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+316.sha-1801d0c
+ * @license Angular v10.0.0-rc.0+319.sha-596fbb3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+316.sha-1801d0c');
+    var VERSION = new i0.Version('10.0.0-rc.0+319.sha-596fbb3');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
