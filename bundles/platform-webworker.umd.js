34cbe9d43d14095806fdca958896bbc8b55e59cd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-06 21:21:49 +0000

    ec32eba02c feat(localize): expose `canParse()` diagnostics (#37909)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+318.sha-aed6b13
+ * @license Angular v10.0.0-rc.0+320.sha-ec32eba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+318.sha-aed6b13');
+    var VERSION = new core.Version('10.0.0-rc.0+320.sha-ec32eba');
 
     /**
      * @license
