f57acf8fdcdf98e9f87b4277adb75ebe0a03ecd8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:43:36 +0000

    cb2ab26296 feat(dev-infra): merge script should link to original commit when cherry-picking with API strategy (#37889)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+320.sha-ec32eba
+ * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+320.sha-ec32eba');
+    var VERSION = new i0.Version('10.0.0-rc.0+324.sha-cb2ab26');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
