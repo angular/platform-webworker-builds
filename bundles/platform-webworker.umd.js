9605fd5724079a13ae6f2ebf8e2decf985680ba4
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-07 19:44:30 +0000

    289a96b078 docs: minor fixes to docs related to updating to v10 (#37897)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+324.sha-cb2ab26
+ * @license Angular v10.0.0-rc.0+325.sha-289a96b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.0.0-rc.0+324.sha-cb2ab26');
+    var VERSION = new i0.Version('10.0.0-rc.0+325.sha-289a96b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
