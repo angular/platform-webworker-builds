2d9e245ea925052d4d5b5a41bbea7a40e1aaf0b2
atscott <atscott@google.com>
2020-07-08 23:08:32 +0000

    d0c0f25a48 ci: decrease the minBodyLength commit message limit to 20 chars (#37949)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0
+ * @license Angular v10.1.0-next.0+1.sha-d0c0f25
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0');
+    var VERSION = new core.Version('10.1.0-next.0+1.sha-d0c0f25');
 
     /**
      * @license
