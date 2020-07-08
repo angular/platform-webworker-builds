5532d39fae9359cc96e3116fb8c8f6b87e55a904
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 21:25:28 +0000

    1f0c1f3ff2 release: cut the v9.1.12 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.11+5.sha-a86a412
+ * @license Angular v9.1.12
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1283,7 +1283,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('9.1.11+5.sha-a86a412');
+    var VERSION = new core.Version('9.1.12');
 
     /**
      * @license
