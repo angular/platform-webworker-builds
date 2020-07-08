68bb53d3693c1796d8633d45b88849c76f587503
atscott <atscott@google.com>
2020-07-08 23:29:42 +0000

    f81d3f6199 docs(elements): fix typo (you custom element --> your custom element) (#37966)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+1.sha-d0c0f25
+ * @license Angular v10.1.0-next.0+2.sha-f81d3f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+1.sha-d0c0f25');
+const VERSION = new Version('10.1.0-next.0+2.sha-f81d3f6');
 
 /**
  * @license
