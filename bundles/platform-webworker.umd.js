206c9af6925650e802deb30faf074aab84a5f956
atscott <atscott@google.com>
2020-07-08 23:35:56 +0000

    0a3dbc1e8a build(docs-infra): update @angular/cli to 10.0.1 (#37898)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+4.sha-bbe3543
+ * @license Angular v10.1.0-next.0+8.sha-0a3dbc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+4.sha-bbe3543');
+    var VERSION = new i0.Version('10.1.0-next.0+8.sha-0a3dbc1');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
