aef5881be771a6222f29b59387aedc2d58315701
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-08 19:30:20 +0000

    82a2207f4a build: filter out duplicate cherry-picked commits in changelog (#37956)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+325.sha-289a96b
+ * @license Angular v10.0.0-rc.0+326.sha-82a2207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.0-rc.0+325.sha-289a96b');
+    var VERSION = new core.Version('10.0.0-rc.0+326.sha-82a2207');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.0-rc.0+325.sha-289a96b
+ * @license Angular v10.0.0-rc.0+326.sha-82a2207
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.0-rc.0+325.sha-289a96b');
+const VERSION = new Version('10.0.0-rc.0+326.sha-82a2207');
 
 /**
  * @license
