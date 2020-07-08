00c38b5456f3710e3aed32368200f75bf6aa1b2d
atscott <atscott@google.com>
2020-07-08 23:32:11 +0000

    e50c7dcc4c docs(router): get rid of unnecessary line in wildcard route example and fixing wildcard docregions (#37127)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+3.sha-bdbbff6
+ * @license Angular v10.1.0-next.0+10.sha-e50c7dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+3.sha-bdbbff6');
+    var VERSION = new core.Version('10.1.0-next.0+10.sha-e50c7dc');
 
     /**
      * @license
