b49082a97373fdd72438785f35332a34be8a37bd
atscott <atscott@google.com>
2020-07-08 23:36:44 +0000

    6b731a067a test: fix test failure in saucelabs ivy ie10 (#37892)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+8.sha-0a3dbc1
+ * @license Angular v10.1.0-next.0+9.sha-6b731a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+8.sha-0a3dbc1');
+    var VERSION = new i0.Version('10.1.0-next.0+9.sha-6b731a0');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
