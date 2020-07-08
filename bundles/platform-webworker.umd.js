1f640faa26984454a3308dff052f054380d9b0f1
atscott <atscott@google.com>
2020-07-08 23:33:19 +0000

    0a3dbc1e8a build(docs-infra): update @angular/cli to 10.0.1 (#37898)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+4.sha-bbe3543
+ * @license Angular v10.1.0-next.0+8.sha-0a3dbc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+4.sha-bbe3543');
+    var VERSION = new core.Version('10.1.0-next.0+8.sha-0a3dbc1');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+4.sha-bbe3543
+ * @license Angular v10.1.0-next.0+8.sha-0a3dbc1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+4.sha-bbe3543');
+const VERSION = new Version('10.1.0-next.0+8.sha-0a3dbc1');
 
 /**
  * @license
