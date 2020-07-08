ffef6bfa1666ff5fce2bc0ca44e9352c35c890d2
atscott <atscott@google.com>
2020-07-08 23:34:54 +0000

    e50c7dcc4c docs(router): get rid of unnecessary line in wildcard route example and fixing wildcard docregions (#37127)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+3.sha-bdbbff6
+ * @license Angular v10.1.0-next.0+10.sha-e50c7dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+3.sha-bdbbff6');
+    var VERSION = new i0.Version('10.1.0-next.0+10.sha-e50c7dc');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+3.sha-bdbbff6
+ * @license Angular v10.1.0-next.0+10.sha-e50c7dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+3.sha-bdbbff6');
+const VERSION = new Version('10.1.0-next.0+10.sha-e50c7dc');
 
 /**
  * @license
