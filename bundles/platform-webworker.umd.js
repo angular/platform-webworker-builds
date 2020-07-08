b7d2623595317e44a8783386ec329c277294ad53
atscott <atscott@google.com>
2020-07-08 23:32:54 +0000

    bbe3543c69 refactor(core): remove duplicated WrappedValue class (#37940)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+10.sha-e50c7dc
+ * @license Angular v10.1.0-next.0+4.sha-bbe3543
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+10.sha-e50c7dc');
+    var VERSION = new core.Version('10.1.0-next.0+4.sha-bbe3543');
 
     /**
      * @license
