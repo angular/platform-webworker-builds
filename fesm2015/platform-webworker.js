f61519706ca07003da550db15c3243a4bb020b27
atscott <atscott@google.com>
2020-07-09 17:11:21 +0000

    fb735d625b refactor(service-worker): use nominal type for normalized URLs (#37922)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+11.sha-b98314e
+ * @license Angular v10.1.0-next.0+16.sha-fb735d6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+11.sha-b98314e');
+const VERSION = new Version('10.1.0-next.0+16.sha-fb735d6');
 
 /**
  * @license
