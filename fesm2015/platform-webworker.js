c33c799672b138c71f81e209b11a85afa9c0aac2
atscott <atscott@google.com>
2020-07-09 16:44:31 +0000

    b98314e306 docs: add Formly library to the list of resources (#37257)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+9.sha-6b731a0
+ * @license Angular v10.1.0-next.0+11.sha-b98314e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+9.sha-6b731a0');
+const VERSION = new Version('10.1.0-next.0+11.sha-b98314e');
 
 /**
  * @license
