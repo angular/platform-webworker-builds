25befe9dccfb5d4954fee62e8db53decf4ae99b8
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:25:45 +0000

    a94383f168 fix(compiler): check more cases for pipe usage inside host bindings (#37883)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+33.sha-c776825
+ * @license Angular v10.0.3+36.sha-a94383f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+33.sha-c776825');
+const VERSION = new Version('10.0.3+36.sha-a94383f');
 
 /**
  * @license
