d51f9c3ec0838de6c01970421a1dd4cec55cc4ba
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:23:07 +0000

    9bd4b74b06 feat(dev-infra): commit message validation should skip lines consisting of URLs (#37890)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+20.sha-4b4b745
+ * @license Angular v10.1.0-next.0+22.sha-9bd4b74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+20.sha-4b4b745');
+    var VERSION = new i0.Version('10.1.0-next.0+22.sha-9bd4b74');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
