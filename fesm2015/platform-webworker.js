7c76215738fa90321bb45e7a2e46694f7a9f5e31
Andrew Scott <atscott01@gmail.com>
2020-07-10 18:53:51 +0000

    2e9b953e9d docs(router): fix routerLink docs (#37997) The current content for the routerLink commands input does not make it to aio. fixes #35414

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+36.sha-a94383f
+ * @license Angular v10.0.3+37.sha-2e9b953
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+36.sha-a94383f');
+const VERSION = new Version('10.0.3+37.sha-2e9b953');
 
 /**
  * @license
