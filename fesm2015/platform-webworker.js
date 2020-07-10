a258ce5646629672b0956529f156093c19a81e30
Andrew Scott <atscott01@gmail.com>
2020-07-10 17:31:25 +0000

    c776825fdd fix(dev-infra): include `bazel` utility files in npm package (#37891)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+20.sha-b76a2dc
+ * @license Angular v10.0.3+33.sha-c776825
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+20.sha-b76a2dc');
+const VERSION = new Version('10.0.3+33.sha-c776825');
 
 /**
  * @license
