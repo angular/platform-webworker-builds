343126c9a8dfb611e769143aeb8e457dd69edef0
Andrew Scott <atscott01@gmail.com>
2020-07-10 22:31:29 +0000

    0d38288078 docs: Move router tutorial (toh) from router.md to new file (#37979)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+37.sha-2e9b953
+ * @license Angular v10.0.3+38.sha-0d38288
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+37.sha-2e9b953');
+const VERSION = new Version('10.0.3+38.sha-0d38288');
 
 /**
  * @license
