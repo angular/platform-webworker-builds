ffe35b166be3f77d78cd3cc238ad7d5fa10ad0bb
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:50:40 +0000

    d1aa4678be build(docs-infra): ensure the correct files are in the i18n example (#37947)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+31.sha-a259a84
+ * @license Angular v10.1.0-next.0+30.sha-d1aa467
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+31.sha-a259a84');
+    var VERSION = new core.Version('10.1.0-next.0+30.sha-d1aa467');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+31.sha-a259a84
+ * @license Angular v10.1.0-next.0+30.sha-d1aa467
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+31.sha-a259a84');
+const VERSION = new Version('10.1.0-next.0+30.sha-d1aa467');
 
 /**
  * @license
