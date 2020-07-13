727e514307ff4e6ea1fe52bbdbb75f548e702135
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:55:49 +0000

    9cf78d5701 docs(core): Fixed typo in Type JSdoc (#37930)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+45.sha-53e4ff7
+ * @license Angular v10.0.3+52.sha-9cf78d5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.3+45.sha-53e4ff7');
+    var VERSION = new core.Version('10.0.3+52.sha-9cf78d5');
 
     /**
      * @license
