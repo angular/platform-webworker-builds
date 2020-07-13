9899ee3826f65a16738ca15e754837c61874f4f4
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:30:40 +0000

    36dd817913 docs: fixed typo in https://angular.io/guide/glossary (#36220)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+33.sha-6b31155
+ * @license Angular v10.1.0-next.0+37.sha-36dd817
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+33.sha-6b31155');
+    var VERSION = new core.Version('10.1.0-next.0+37.sha-36dd817');
 
     /**
      * @license
