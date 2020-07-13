214c82798e4a2ccde564b34bd2858837a551b141
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:57:20 +0000

    81542b3b72 docs(core): Fixed typo in Type JSdoc (#37930)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+40.sha-9185c6e
+ * @license Angular v10.1.0-next.0+41.sha-81542b3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+40.sha-9185c6e');
+    var VERSION = new i0.Version('10.1.0-next.0+41.sha-81542b3');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
