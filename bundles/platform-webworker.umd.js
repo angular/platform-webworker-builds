30fa93035250224369d5760c714ae4df0032795c
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:32:38 +0000

    95669df4c3 docs: remove Markus Padourek from angular collaborators (#37962)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+37.sha-36dd817
+ * @license Angular v10.1.0-next.0+34.sha-95669df
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+37.sha-36dd817');
+    var VERSION = new core.Version('10.1.0-next.0+34.sha-95669df');
 
     /**
      * @license
