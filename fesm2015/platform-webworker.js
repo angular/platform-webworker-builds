16d69395c032fa76c3a25b8d8622a068ee8cd918
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:51:38 +0000

    53e4ff72d2 docs: fix typo in router.md (#37227)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.3+38.sha-0d38288
+ * @license Angular v10.0.3+45.sha-53e4ff7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.3+38.sha-0d38288');
+const VERSION = new Version('10.0.3+45.sha-53e4ff7');
 
 /**
  * @license
