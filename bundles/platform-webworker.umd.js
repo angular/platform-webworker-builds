81c35a0625972e56a40d76c76004aac4897b531f
Andrew Scott <atscott01@gmail.com>
2020-07-13 21:39:40 +0000

    10aba15154 fix(language-service): remove completion for string (#37983)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+36.sha-0eaa084
+ * @license Angular v10.1.0-next.0+38.sha-10aba15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+36.sha-0eaa084');
+    var VERSION = new core.Version('10.1.0-next.0+38.sha-10aba15');
 
     /**
      * @license
