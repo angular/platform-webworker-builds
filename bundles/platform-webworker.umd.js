9b9133a9197260871f1c3e1f29d65262e2ad856a
Andrew Scott <atscott01@gmail.com>
2020-07-13 16:54:47 +0000

    66947cf9fa docs: fix typo in router.md (#37227)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+30.sha-d1aa467
+ * @license Angular v10.1.0-next.0+32.sha-66947cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+30.sha-d1aa467');
+    var VERSION = new i0.Version('10.1.0-next.0+32.sha-66947cf');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
