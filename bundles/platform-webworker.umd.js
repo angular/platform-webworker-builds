1d66ecc032fd8441663e223e4a36c6aefbae3c5b
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:46:17 +0000

    f01b0337d2 docs(router): fix typo in "spotlight on pathmatch" (#38039)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+43.sha-4ec3026
+ * @license Angular v10.1.0-next.0+44.sha-f01b033
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+43.sha-4ec3026');
+    var VERSION = new core.Version('10.1.0-next.0+44.sha-f01b033');
 
     /**
      * @license
