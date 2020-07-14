02e64fc0483009a103eac85d11f2f47f2286e546
Andrew Scott <atscott01@gmail.com>
2020-07-14 20:50:36 +0000

    b358495a6c fix(ngcc): report a warning if ngcc tries to use a solution-style tsconfig (#38003)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+48.sha-f4fac40
+ * @license Angular v10.1.0-next.0+49.sha-b358495
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.0+48.sha-f4fac40');
+    var VERSION = new i0.Version('10.1.0-next.0+49.sha-b358495');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
