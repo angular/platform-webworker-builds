079d9f08b269dab71b64977f004710e287610f89
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:32:15 +0000

    9021d3f5e1 docs: fix live examples in testing guides (#38038)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+41.sha-81542b3
+ * @license Angular v10.1.0-next.0+42.sha-9021d3f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+41.sha-81542b3');
+    var VERSION = new core.Version('10.1.0-next.0+42.sha-9021d3f');
 
     /**
      * @license
