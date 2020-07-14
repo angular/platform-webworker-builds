34671cc8fe5820b49177a0a079fc8848dd80791f
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:37:39 +0000

    4ec3026a9a docs(router): fix typo in https://angular.io/guide/router#activated-route (#38034)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-9021d3f
+ * @license Angular v10.1.0-next.0+43.sha-4ec3026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+42.sha-9021d3f');
+const VERSION = new Version('10.1.0-next.0+43.sha-4ec3026');
 
 /**
  * @license
