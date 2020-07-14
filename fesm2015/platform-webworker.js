a731036ebc8088a44e4b3e2b3e2ed4433fe7b687
Sonu Kapoor <sonukapoor@gmail.com>
2020-07-14 18:41:38 +0000

    e52ac490c8 docs(core): fixes minor typo in initNgDevMode function docs

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-2dd22b7
+ * @license Angular v10.1.0-next.0+42.sha-e52ac49
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.0+42.sha-2dd22b7');
+const VERSION = new Version('10.1.0-next.0+42.sha-e52ac49');
 
 /**
  * @license
