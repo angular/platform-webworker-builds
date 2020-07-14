6710098ed5d81a2a48c6433cc5757eacf2670579
Andrew Scott <atscott01@gmail.com>
2020-07-14 16:34:52 +0000

    4ec3026a9a docs(router): fix typo in https://angular.io/guide/router#activated-route (#38034)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.0+42.sha-9021d3f
+ * @license Angular v10.1.0-next.0+43.sha-4ec3026
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.0+42.sha-9021d3f');
+    var VERSION = new core.Version('10.1.0-next.0+43.sha-4ec3026');
 
     /**
      * @license
