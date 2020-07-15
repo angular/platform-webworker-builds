bdd8e31e7523566ef1b351089132f0b275823afb
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:08:11 +0000

    c8e77d8536 ci(docs-infra): increase minimum a11y scores for various pages (#37899)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+10.sha-bf641e1
+ * @license Angular v10.1.0-next.1+8.sha-c8e77d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+10.sha-bf641e1');
+const VERSION = new Version('10.1.0-next.1+8.sha-c8e77d8');
 
 /**
  * @license
