55853a3fd71b3e3b3985315ade45e28c162a55bc
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:35:03 +0000

    24b420527a docs(service-worker): fix typos in `SwRegistrationOptions` API docs (#38047)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+9.sha-406f801
+ * @license Angular v10.1.0-next.1+11.sha-24b4205
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+9.sha-406f801');
+    var VERSION = new i0.Version('10.1.0-next.1+11.sha-24b4205');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
