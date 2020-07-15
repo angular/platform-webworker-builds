4b878fccd4ead4238f506be7f0b1a6fe0dccba1a
Andrew Kushnir <akushnir@google.com>
2020-07-15 19:59:40 +0000

    b00121c9a6 docs: update reference doc for router guards and resolvers (#38079)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+1.sha-1d9e56e
+ * @license Angular v10.1.0-next.1+3.sha-b00121c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+1.sha-1d9e56e');
+const VERSION = new Version('10.1.0-next.1+3.sha-b00121c');
 
 /**
  * @license
