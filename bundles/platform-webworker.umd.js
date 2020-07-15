22b1db1fadb1a019f7b6353267a47a3bb3d2f7d9
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:33:26 +0000

    d8c47b971c docs: fix typo in "Creating libraries" guide (`by publishing...ensures` --> `publishing...ensures`) (#38032)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+11.sha-24b4205
+ * @license Angular v10.1.0-next.1+12.sha-d8c47b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+11.sha-24b4205');
+    var VERSION = new core.Version('10.1.0-next.1+12.sha-d8c47b9');
 
     /**
      * @license
