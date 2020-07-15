d0c389c5b395f97c602abd8809c2bc0f0583c238
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:11:51 +0000

    17ddab98fb fix(core): incorrectly validating properties on ng-content and ng-container (#37773)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4
+ * @license Angular v10.0.4+9.sha-17ddab9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4');
+const VERSION = new Version('10.0.4+9.sha-17ddab9');
 
 /**
  * @license
