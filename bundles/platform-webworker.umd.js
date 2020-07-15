1a888843f6fa4f2cc3818800a4e4ffc1d947f4b8
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:01:55 +0000

    45d79efc3f docs: remove all references to Angular Console (#37608)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+6.sha-09eee5b
+ * @license Angular v10.1.0-next.1+2.sha-45d79ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+6.sha-09eee5b');
+    var VERSION = new core.Version('10.1.0-next.1+2.sha-45d79ef');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+6.sha-09eee5b
+ * @license Angular v10.1.0-next.1+2.sha-45d79ef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+6.sha-09eee5b');
+const VERSION = new Version('10.1.0-next.1+2.sha-45d79ef');
 
 /**
  * @license
