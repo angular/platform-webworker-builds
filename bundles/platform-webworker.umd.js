d51dc0ba5cdad3e59150c72efc1253b953c63b20
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:04:56 +0000

    545f1dbac9 feat(dev-infra): add bazel firefox browser with RBE compatibility (#38029)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+2.sha-45d79ef
+ * @license Angular v10.1.0-next.1+4.sha-545f1db
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+2.sha-45d79ef');
+    var VERSION = new core.Version('10.1.0-next.1+4.sha-545f1db');
 
     /**
      * @license
