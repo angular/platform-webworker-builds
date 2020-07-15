d742a17465882edc85057adc5f5a757ca1e06785
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:10:00 +0000

    bf641e1b4b fix(core): incorrectly validating properties on ng-content and ng-container (#37773)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+4.sha-545f1db
+ * @license Angular v10.1.0-next.1+10.sha-bf641e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+4.sha-545f1db');
+    var VERSION = new i0.Version('10.1.0-next.1+10.sha-bf641e1');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+4.sha-545f1db
+ * @license Angular v10.1.0-next.1+10.sha-bf641e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+4.sha-545f1db');
+const VERSION = new Version('10.1.0-next.1+10.sha-bf641e1');
 
 /**
  * @license
