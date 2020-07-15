121d741ae45c914c95f0eaec9ebe077547c3e372
Andrew Kushnir <akushnir@google.com>
2020-07-15 21:17:46 +0000

    469aba0140 docs: add Emma Twersky to DevRel Contributor page (#38084)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+12.sha-d8c47b9
+ * @license Angular v10.1.0-next.1+13.sha-469aba0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+12.sha-d8c47b9');
+    var VERSION = new i0.Version('10.1.0-next.1+13.sha-469aba0');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+12.sha-d8c47b9
+ * @license Angular v10.1.0-next.1+13.sha-469aba0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+12.sha-d8c47b9');
+const VERSION = new Version('10.1.0-next.1+13.sha-469aba0');
 
 /**
  * @license
