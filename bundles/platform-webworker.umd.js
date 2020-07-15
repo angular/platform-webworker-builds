9008fe51a01ba2c8b052514e20c10dfd72c66988
Andrew Kushnir <akushnir@google.com>
2020-07-15 23:49:04 +0000

    737506e79c fix(core): Allow modification of lifecycle hooks any time before bootstrap (#35464)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+13.sha-469aba0
+ * @license Angular v10.1.0-next.1+14.sha-737506e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+13.sha-469aba0');
+    var VERSION = new i0.Version('10.1.0-next.1+14.sha-737506e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+13.sha-469aba0
+ * @license Angular v10.1.0-next.1+14.sha-737506e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+13.sha-469aba0');
+const VERSION = new Version('10.1.0-next.1+14.sha-737506e');
 
 /**
  * @license
