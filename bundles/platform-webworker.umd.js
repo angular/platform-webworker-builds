910dff829059555a4b92a786f92b28f07f9de5af
Andrew Kushnir <akushnir@google.com>
2020-07-15 23:46:36 +0000

    737506e79c fix(core): Allow modification of lifecycle hooks any time before bootstrap (#35464)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+13.sha-469aba0
+ * @license Angular v10.1.0-next.1+14.sha-737506e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+13.sha-469aba0');
+    var VERSION = new core.Version('10.1.0-next.1+14.sha-737506e');
 
     /**
      * @license
