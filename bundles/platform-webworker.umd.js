cae925ebf3955eac4584eec2209e6f9a4fe556e7
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:02:16 +0000

    b00121c9a6 docs: update reference doc for router guards and resolvers (#38079)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+1.sha-1d9e56e
+ * @license Angular v10.1.0-next.1+3.sha-b00121c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+1.sha-1d9e56e');
+    var VERSION = new i0.Version('10.1.0-next.1+3.sha-b00121c');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
