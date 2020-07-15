59d62325c188c9e5c1dede3af28c169e7ab19c24
Andrew Kushnir <akushnir@google.com>
2020-07-15 20:10:52 +0000

    c8e77d8536 ci(docs-infra): increase minimum a11y scores for various pages (#37899)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+10.sha-bf641e1
+ * @license Angular v10.1.0-next.1+8.sha-c8e77d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+10.sha-bf641e1');
+    var VERSION = new i0.Version('10.1.0-next.1+8.sha-c8e77d8');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
