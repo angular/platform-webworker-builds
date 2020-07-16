c9d35f2e9bba0dd84abce1f5e3d0dc4897d6236f
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:26:37 +0000

    99960a98d2 docs: update router api documentation (#37980)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+15.sha-9ac3383
+ * @license Angular v10.0.4+16.sha-99960a9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4+15.sha-9ac3383');
+const VERSION = new Version('10.0.4+16.sha-99960a9');
 
 /**
  * @license
