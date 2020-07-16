ffc7e782f39c7f85ab8853fc87bed65ddeb6ba4e
Andrew Kushnir <akushnir@google.com>
2020-07-16 18:29:19 +0000

    c1539b16ae docs: add Ajit Singh to the collaborators (#37792)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+14.sha-737506e
+ * @license Angular v10.1.0-next.1+15.sha-c1539b1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+14.sha-737506e');
+const VERSION = new Version('10.1.0-next.1+15.sha-c1539b1');
 
 /**
  * @license
