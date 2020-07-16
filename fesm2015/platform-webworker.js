df37717a160b2c314176e72f541977aa3510c4ee
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:23:39 +0000

    e382632473 build: fix symbol extractor not dealing with ES2015 classes (#38093)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+16.sha-e0b35bf
+ * @license Angular v10.1.0-next.1+23.sha-e382632
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+16.sha-e0b35bf');
+const VERSION = new Version('10.1.0-next.1+23.sha-e382632');
 
 /**
  * @license
