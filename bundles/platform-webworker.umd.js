520bc88ee3bf268fbad9b89ef971e208ea71ea56
Andrew Kushnir <akushnir@google.com>
2020-07-16 21:29:28 +0000

    cf9a47ba53 feat(localize): allow duplicate messages to be handled during extraction (#38082)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+20.sha-6c9401c
+ * @license Angular v10.1.0-next.1+22.sha-cf9a47b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+20.sha-6c9401c');
+    var VERSION = new i0.Version('10.1.0-next.1+22.sha-cf9a47b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+20.sha-6c9401c
+ * @license Angular v10.1.0-next.1+22.sha-cf9a47b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+20.sha-6c9401c');
+const VERSION = new Version('10.1.0-next.1+22.sha-cf9a47b');
 
 /**
  * @license
