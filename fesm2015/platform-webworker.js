8ad703bbac80c2f104e18658742b381281c2a48c
Andrew Kushnir <akushnir@google.com>
2020-07-17 19:09:14 +0000

    fd45f7485d docs(docs-infra): reformat redundant sentence (#38109)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+24.sha-58f6079
+ * @license Angular v10.1.0-next.1+25.sha-fd45f74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+24.sha-58f6079');
+const VERSION = new Version('10.1.0-next.1+25.sha-fd45f74');
 
 /**
  * @license
