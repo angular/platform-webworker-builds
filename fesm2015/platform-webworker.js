89c25b51069581fbf762bc53e5b5700660d7c41a
Andrew Kushnir <akushnir@google.com>
2020-07-17 21:06:23 +0000

    481df830ad build: Ignore .history for the xyz.local-history VSCode extension (#38121)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+19.sha-f2ca463
+ * @license Angular v10.0.4+22.sha-481df83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4+19.sha-f2ca463');
+const VERSION = new Version('10.0.4+22.sha-481df83');
 
 /**
  * @license
