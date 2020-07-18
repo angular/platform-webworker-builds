07da40ccb17f05579b5c64bad13daaa2445aab9e
Andrew Kushnir <akushnir@google.com>
2020-07-18 00:02:01 +0000

    136acdfab6 docs: correct flag default values in `--strict` (#37982)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+28.sha-095cfd9
+ * @license Angular v10.1.0-next.1+29.sha-136acdf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+28.sha-095cfd9');
+const VERSION = new Version('10.1.0-next.1+29.sha-136acdf');
 
 /**
  * @license
