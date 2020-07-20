c210849d43b9d8030b6e351e29c4e0910fe98e97
Andrew Kushnir <akushnir@google.com>
2020-07-20 17:25:53 +0000

    e80278cf02 fix(compiler): properly associate source spans for implicitly closed elements (#38126)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+23.sha-8e305e7
+ * @license Angular v10.0.4+27.sha-e80278c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.4+23.sha-8e305e7');
+    var VERSION = new core.Version('10.0.4+27.sha-e80278c');
 
     /**
      * @license
