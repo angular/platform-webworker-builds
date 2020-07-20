09a0509e86b045e0a11ebf3baa52571599dc9845
Andrew Kushnir <akushnir@google.com>
2020-07-20 18:40:44 +0000

    5d3d8b8330 docs: add known issue for bazel (#38074)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+34.sha-c8c7415
+ * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.1+34.sha-c8c7415');
+    var VERSION = new i0.Version('10.1.0-next.1+35.sha-5d3d8b8');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+34.sha-c8c7415
+ * @license Angular v10.1.0-next.1+35.sha-5d3d8b8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.1+34.sha-c8c7415');
+const VERSION = new Version('10.1.0-next.1+35.sha-5d3d8b8');
 
 /**
  * @license
