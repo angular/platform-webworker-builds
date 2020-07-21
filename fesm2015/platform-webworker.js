23a0885485db9150087b10c881659138e7cec5c2
Andrew Kushnir <akushnir@google.com>
2020-07-21 21:55:14 +0000

    3f8f3a2daa docs: fix type in lazy-load callout (#38153)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+37.sha-f5eeb1a
+ * @license Angular v10.0.4+38.sha-3f8f3a2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4+37.sha-f5eeb1a');
+const VERSION = new Version('10.0.4+38.sha-3f8f3a2');
 
 /**
  * @license
