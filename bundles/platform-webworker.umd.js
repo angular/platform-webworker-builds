f420d6bb17f72716eb945bd20dceab4c0401d45f
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:57:46 +0000

    f5eeb1a714 docs: fix DOCUMENT import path (#38158) (#38159)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+36.sha-2af3d9c
+ * @license Angular v10.0.4+37.sha-f5eeb1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.4+36.sha-2af3d9c');
+    var VERSION = new core.Version('10.0.4+37.sha-f5eeb1a');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+36.sha-2af3d9c
+ * @license Angular v10.0.4+37.sha-f5eeb1a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4+36.sha-2af3d9c');
+const VERSION = new Version('10.0.4+37.sha-f5eeb1a');
 
 /**
  * @license
