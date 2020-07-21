34c26a26fd7fca6f4998aaf29563dacc8d9b3fcd
Andrew Kushnir <akushnir@google.com>
2020-07-21 00:34:46 +0000

    eae13e42f3 docs: add routing terms to glossary (#38053)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+39.sha-4a6dcd0
+ * @license Angular v10.1.0-next.1+41.sha-eae13e4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+39.sha-4a6dcd0');
+    var VERSION = new core.Version('10.1.0-next.1+41.sha-eae13e4');
 
     /**
      * @license
