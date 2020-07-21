44dbf2b5ee00a208942eb6886b68ec7c95ff5bdd
Andrew Kushnir <akushnir@google.com>
2020-07-21 18:03:09 +0000

    ad7046b934 feat(forms): AbstractControl to store raw validators in addition to combined validators function (#37881)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+42.sha-d72b1e4
+ * @license Angular v10.1.0-next.1+43.sha-ad7046b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+42.sha-d72b1e4');
+    var VERSION = new core.Version('10.1.0-next.1+43.sha-ad7046b');
 
     /**
      * @license
