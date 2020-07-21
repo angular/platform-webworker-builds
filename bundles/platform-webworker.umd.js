4d7e09e0a0b34fc09b9c2c1c37497c8cb9e3536b
Andrew Kushnir <akushnir@google.com>
2020-07-21 21:54:36 +0000

    7f82a2c1c8 docs: fix type in lazy-load callout (#38153)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.1+44.sha-a9910f5
+ * @license Angular v10.1.0-next.1+45.sha-7f82a2c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.1+44.sha-a9910f5');
+    var VERSION = new core.Version('10.1.0-next.1+45.sha-7f82a2c');
 
     /**
      * @license
