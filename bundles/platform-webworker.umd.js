1299f486a9e085afb31d05be542e9e119c006f26
Andrew Kushnir <akushnir@google.com>
2020-07-22 17:11:25 +0000

    2c7ff82f31 release: cut the v10.0.5 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+38.sha-3f8f3a2
+ * @license Angular v10.0.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.4+38.sha-3f8f3a2');
+    var VERSION = new core.Version('10.0.5');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.4+38.sha-3f8f3a2
+ * @license Angular v10.0.5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.4+38.sha-3f8f3a2');
+const VERSION = new Version('10.0.5');
 
 /**
  * @license
