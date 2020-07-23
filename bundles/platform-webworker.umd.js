4ca7213b4f4f755cb33d366bc3bb157b6b04d36f
Misko Hevery <misko@hevery.com>
2020-07-23 18:34:14 +0000

    d0060dcfdc refactor(dev-infra): Add support for groups in the conditions evaluator (#38164)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+20.sha-88d4b26
+ * @license Angular v10.1.0-next.2+17.sha-d0060dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+20.sha-88d4b26');
+    var VERSION = new i0.Version('10.1.0-next.2+17.sha-d0060dc');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+20.sha-88d4b26
+ * @license Angular v10.1.0-next.2+17.sha-d0060dc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+20.sha-88d4b26');
+const VERSION = new Version('10.1.0-next.2+17.sha-d0060dc');
 
 /**
  * @license
