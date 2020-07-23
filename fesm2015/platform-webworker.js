c875b54b81d0fc93622990eced4818db6cd01077
Misko Hevery <misko@hevery.com>
2020-07-23 03:51:41 +0000

    778ad3776a docs: create coding standards doc (#37700)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+2.sha-d88711c
+ * @license Angular v10.1.0-next.2+9.sha-778ad37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+2.sha-d88711c');
+const VERSION = new Version('10.1.0-next.2+9.sha-778ad37');
 
 /**
  * @license
