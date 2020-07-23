b38f317f3c65d8bc16b4841fdd637a96a1504af6
Misko Hevery <misko@hevery.com>
2020-07-23 03:54:04 +0000

    778ad3776a docs: create coding standards doc (#37700)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+2.sha-d88711c
+ * @license Angular v10.1.0-next.2+9.sha-778ad37
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+2.sha-d88711c');
+    var VERSION = new i0.Version('10.1.0-next.2+9.sha-778ad37');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
