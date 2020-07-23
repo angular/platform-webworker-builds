15f625effb7c75f954c0ca7ec20dc7c2a7a38dee
Misko Hevery <misko@hevery.com>
2020-07-23 04:06:40 +0000

    062b8d90af refactor(forms): refactor common validators used in unit tests (#38020)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+10.sha-8df888d
+ * @license Angular v10.1.0-next.2+11.sha-062b8d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.2+10.sha-8df888d');
+    var VERSION = new core.Version('10.1.0-next.2+11.sha-062b8d9');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+10.sha-8df888d
+ * @license Angular v10.1.0-next.2+11.sha-062b8d9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+10.sha-8df888d');
+const VERSION = new Version('10.1.0-next.2+11.sha-062b8d9');
 
 /**
  * @license
