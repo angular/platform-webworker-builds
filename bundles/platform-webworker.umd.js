90a1baf86b4b39a3c3fda6d2a6c0f35e05084c8a
Misko Hevery <misko@hevery.com>
2020-07-23 04:20:24 +0000

    3c1866b5bb docs: update api reference for router outlet directive (#38166)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+11.sha-062b8d9
+ * @license Angular v10.1.0-next.2+12.sha-3c1866b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.2+11.sha-062b8d9');
+    var VERSION = new core.Version('10.1.0-next.2+12.sha-3c1866b');
 
     /**
      * @license
