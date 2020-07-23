7aaa6a2e7c8842656446c68267bbfe36b804a8ec
Misko Hevery <misko@hevery.com>
2020-07-23 18:30:25 +0000

    b518b30dae docs: add Kevin Kreuzer to GDE page (#37929)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+16.sha-7b2e2f5
+ * @license Angular v10.1.0-next.2+15.sha-b518b30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+16.sha-7b2e2f5');
+    var VERSION = new i0.Version('10.1.0-next.2+15.sha-b518b30');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
