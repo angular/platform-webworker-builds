05abc76b8d316394e6bb0f93275b233b9a26adf9
Misko Hevery <misko@hevery.com>
2020-07-23 18:33:16 +0000

    88d4b269b5 build(docs-infra): simplify `ExampleZipper` by removing `PackageJsonCustomizer` (#38192)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+18.sha-dd09fb5
+ * @license Angular v10.1.0-next.2+20.sha-88d4b26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+18.sha-dd09fb5');
+    var VERSION = new i0.Version('10.1.0-next.2+20.sha-88d4b26');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
