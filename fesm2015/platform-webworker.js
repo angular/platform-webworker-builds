1141de501032fa1dbabc1cc45b593604019b94e6
Misko Hevery <misko@hevery.com>
2020-07-23 18:31:11 +0000

    f411c9e5b9 build(docs-infra): simplify `ExampleZipper` by removing `PackageJsonCustomizer` (#38192)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+8.sha-a50a688
+ * @license Angular v10.0.5+15.sha-f411c9e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.5+8.sha-a50a688');
+const VERSION = new Version('10.0.5+15.sha-f411c9e');
 
 /**
  * @license
