b483d99f3e2be7bc5b1e0dd0cb305f9673f697b2
Misko Hevery <misko@hevery.com>
2020-07-23 18:27:53 +0000

    b518b30dae docs: add Kevin Kreuzer to GDE page (#37929)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+16.sha-7b2e2f5
+ * @license Angular v10.1.0-next.2+15.sha-b518b30
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+16.sha-7b2e2f5');
+const VERSION = new Version('10.1.0-next.2+15.sha-b518b30');
 
 /**
  * @license
