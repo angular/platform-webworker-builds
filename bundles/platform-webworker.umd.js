6dfbdb2fa3880259dce0d3e5e12ec1d664b30f99
Misko Hevery <misko@hevery.com>
2020-07-24 23:17:55 +0000

    6a3e68b606 docs(zone.js): update zone.js bundle doc since the APF change (#37919)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+26.sha-1822cbc
+ * @license Angular v10.1.0-next.2+27.sha-6a3e68b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+26.sha-1822cbc');
+    var VERSION = new i0.Version('10.1.0-next.2+27.sha-6a3e68b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
