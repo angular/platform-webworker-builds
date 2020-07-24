e8d54cda5085b275cec7269d865cb238350921b6
Misko Hevery <misko@hevery.com>
2020-07-24 23:09:35 +0000

    c4e6f585c5 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+19.sha-7467fd3
+ * @license Angular v10.0.5+20.sha-c4e6f58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.5+19.sha-7467fd3');
+    var VERSION = new core.Version('10.0.5+20.sha-c4e6f58');
 
     /**
      * @license
