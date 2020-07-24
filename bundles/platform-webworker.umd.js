41dbe7241d914e7249d51515a0dace4cba918b49
Misko Hevery <misko@hevery.com>
2020-07-24 15:50:09 +0000

    307db74e93 docs: fixed that class attribute is not closed (#38219)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+17.sha-d0060dc
+ * @license Angular v10.1.0-next.2+21.sha-307db74
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+17.sha-d0060dc');
+    var VERSION = new i0.Version('10.1.0-next.2+21.sha-307db74');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
