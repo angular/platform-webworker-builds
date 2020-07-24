76660d2691087a8f00453b4a58751e00328436cb
Misko Hevery <misko@hevery.com>
2020-07-24 23:08:12 +0000

    1822cbcd46 fix(zone.js): patch nodejs EventEmtter.prototype.off (#37863)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+25.sha-a71f114
+ * @license Angular v10.1.0-next.2+26.sha-1822cbc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+25.sha-a71f114');
+const VERSION = new Version('10.1.0-next.2+26.sha-1822cbc');
 
 /**
  * @license
