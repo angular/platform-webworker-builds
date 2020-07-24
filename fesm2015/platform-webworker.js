39f35bb1754a8fb6d3a633f9e21aea16ef216711
Misko Hevery <misko@hevery.com>
2020-07-24 15:39:53 +0000

    b8af10902f docs: fixed that class attribute is not closed (#38219)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+15.sha-f411c9e
+ * @license Angular v10.0.5+16.sha-b8af109
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.5+15.sha-f411c9e');
+const VERSION = new Version('10.0.5+16.sha-b8af109');
 
 /**
  * @license
