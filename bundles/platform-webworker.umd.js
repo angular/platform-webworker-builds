a3033518471ce3319ac2492fef76340b4f32bfee
Misko Hevery <misko@hevery.com>
2020-07-24 17:23:06 +0000

    aca01985fd docs: Fix link by removing a space (#38214) PR Close #38214

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+16.sha-b8af109
+ * @license Angular v10.0.5+18.sha-aca0198
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.5+16.sha-b8af109');
+    var VERSION = new core.Version('10.0.5+18.sha-aca0198');
 
     /**
      * @license
