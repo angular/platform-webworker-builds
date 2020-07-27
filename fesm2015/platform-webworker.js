cf1a44c917e53312f77a2ec4adc850bb3c96697a
Misko Hevery <misko@hevery.com>
2020-07-27 17:16:32 +0000

    4dfc3fe6a2 refactor(router): extract Router config utils to a separate file (#38229)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+32.sha-1296003
+ * @license Angular v10.1.0-next.2+31.sha-4dfc3fe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+32.sha-1296003');
+const VERSION = new Version('10.1.0-next.2+31.sha-4dfc3fe');
 
 /**
  * @license
