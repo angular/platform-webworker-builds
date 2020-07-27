03ad8b1bc9e453198c2b8ff6e0073f64074a97c4
Misko Hevery <misko@hevery.com>
2020-07-27 17:12:05 +0000

    1296003445 docs: add ng-add save option (#38198)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+30.sha-67e3ecc
+ * @license Angular v10.1.0-next.2+32.sha-1296003
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.2+30.sha-67e3ecc');
+const VERSION = new Version('10.1.0-next.2+32.sha-1296003');
 
 /**
  * @license
