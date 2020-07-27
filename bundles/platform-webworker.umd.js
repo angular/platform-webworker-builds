43e61aac7b3f263804ed076d4be425b4aea13d28
Misko Hevery <misko@hevery.com>
2020-07-27 17:46:10 +0000

    f74cfadf64 docs: clarify the description of pipes (#37950)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+35.sha-8e5969b
+ * @license Angular v10.1.0-next.2+36.sha-f74cfad
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.2+35.sha-8e5969b');
+    var VERSION = new core.Version('10.1.0-next.2+36.sha-f74cfad');
 
     /**
      * @license
