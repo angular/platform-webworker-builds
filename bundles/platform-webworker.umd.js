2f79a395d1d9f2e2e6af4f41b5582a51db7d4198
Misko Hevery <misko@hevery.com>
2020-07-27 19:37:00 +0000

    96aa14df01 fix(zone.js): zone patch rxjs should return null _unsubscribe correctly. (#37091)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+38.sha-2b6ab57
+ * @license Angular v10.1.0-next.2+39.sha-96aa14d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.2+38.sha-2b6ab57');
+    var VERSION = new core.Version('10.1.0-next.2+39.sha-96aa14d');
 
     /**
      * @license
