1ba74e556ace707e0dd31cd39efed2b934a66d46
Misko Hevery <misko@hevery.com>
2020-07-27 19:29:41 +0000

    2b6ab57d78 docs: add template ref var to glossary (#36743)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.2+37.sha-b65b616
+ * @license Angular v10.1.0-next.2+38.sha-2b6ab57
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.2+37.sha-b65b616');
+    var VERSION = new i0.Version('10.1.0-next.2+38.sha-2b6ab57');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
