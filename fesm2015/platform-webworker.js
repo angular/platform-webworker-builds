afe3ab20282dcf0ed1d6088fc3b14a56f458c6ad
Misko Hevery <misko@hevery.com>
2020-07-28 19:56:15 +0000

    7570356bfa build: fix broken build (#38274)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.5+38.sha-b1e7775
+ * @license Angular v10.0.5+51.sha-7570356
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.5+38.sha-b1e7775');
+const VERSION = new Version('10.0.5+51.sha-7570356');
 
 /**
  * @license
