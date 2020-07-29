49b6f421e21380886df70febce5a6b1d6779a09e
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 21:03:14 +0000

    7f8c2225f2 fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+17.sha-8effc83
+ * @license Angular v10.1.0-next.3+16.sha-7f8c222
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.3+17.sha-8effc83');
+    var VERSION = new i0.Version('10.1.0-next.3+16.sha-7f8c222');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
