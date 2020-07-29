eca486fd6069a8b16082edc157fd3f006442d760
Misko Hevery <misko@hevery.com>
2020-07-29 18:00:48 +0000

    1b17722091 refactor(docs-infra): Lazy-loads SVG icons (#38268)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+2.sha-9c8bc4a
+ * @license Angular v10.1.0-next.3+13.sha-1b17722
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+2.sha-9c8bc4a');
+const VERSION = new Version('10.1.0-next.3+13.sha-1b17722');
 
 /**
  * @license
