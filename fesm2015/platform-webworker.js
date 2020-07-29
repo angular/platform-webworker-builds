e58b910c041da8b7be2aecddf1fb5da124ab3dfd
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 20:59:03 +0000

    8effc83b92 docs(router): clarify how base href is used to construct targets (#38123)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+15.sha-887c350
+ * @license Angular v10.1.0-next.3+17.sha-8effc83
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+15.sha-887c350');
+const VERSION = new Version('10.1.0-next.3+17.sha-8effc83');
 
 /**
  * @license
