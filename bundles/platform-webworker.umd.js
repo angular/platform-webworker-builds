a7e5a532a823d7bed704ac0cc64d08e5faf654c2
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-29 21:00:35 +0000

    7f8c2225f2 fix(compiler): mark `NgModuleFactory` construction as not side effectful (#38147)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+17.sha-8effc83
+ * @license Angular v10.1.0-next.3+16.sha-7f8c222
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.3+17.sha-8effc83');
+    var VERSION = new core.Version('10.1.0-next.3+16.sha-7f8c222');
 
     /**
      * @license
