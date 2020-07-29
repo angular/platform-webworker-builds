df4916a35e65096678899bc8c445d28a967f62e3
Misko Hevery <misko@hevery.com>
2020-07-29 17:56:31 +0000

    9c8bc4a239 fix(common): narrow `NgIf` context variables in template type checker (#36627)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+1.sha-cf4da82
+ * @license Angular v10.1.0-next.3+2.sha-9c8bc4a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.3+1.sha-cf4da82');
+    var VERSION = new i0.Version('10.1.0-next.3+2.sha-9c8bc4a');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
