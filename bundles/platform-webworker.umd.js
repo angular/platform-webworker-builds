7713e4c841ea61acacb7c4ca5b8ebe8cef29f592
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-30 22:45:32 +0000

    879ff08f0e fix(compiler): Metadata should not include methods on Object.prototype (#38292)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+20.sha-a15d7ac
+ * @license Angular v10.0.6+21.sha-879ff08f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.6+20.sha-a15d7ac');
+    var VERSION = new core.Version('10.0.6+21.sha-879ff08f');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+20.sha-a15d7ac
+ * @license Angular v10.0.6+21.sha-879ff08f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.6+20.sha-a15d7ac');
+const VERSION = new Version('10.0.6+21.sha-879ff08f');
 
 /**
  * @license
