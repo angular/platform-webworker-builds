515bc25aa127dff8d111bcf68ed7442567328b84
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 22:57:50 +0000

    ff9f4de4f1 fix(compiler): update unparsable character reference entity error messages (#38319)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+38.sha-3a46c2d
+ * @license Angular v10.1.0-next.3+39.sha-ff9f4de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.3+38.sha-3a46c2d');
+    var VERSION = new i0.Version('10.1.0-next.3+39.sha-ff9f4de');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+38.sha-3a46c2d
+ * @license Angular v10.1.0-next.3+39.sha-ff9f4de
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+38.sha-3a46c2d');
+const VERSION = new Version('10.1.0-next.3+39.sha-ff9f4de');
 
 /**
  * @license
