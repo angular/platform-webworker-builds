73e7fc8e85c305665f6b82b0cff82394f9143963
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:19:48 +0000

    e8896b9de3 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+22.sha-6f6102d
+ * @license Angular v10.1.0-next.3+23.sha-e8896b9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.3+22.sha-6f6102d');
+    var VERSION = new core.Version('10.1.0-next.3+23.sha-e8896b9');
 
     /**
      * @license
