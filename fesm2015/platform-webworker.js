e7045e665ab0ecea998845333eec983451dcd58c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 18:28:18 +0000

    9c5fc1693a refactor(docs-infra): update docs examples `tslint.json` to match CLI and fix failures (#38143)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.7+2.sha-7ff5ef2
+ * @license Angular v10.0.7+17.sha-9c5fc16
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.7+2.sha-7ff5ef2');
+const VERSION = new Version('10.0.7+17.sha-9c5fc16');
 
 /**
  * @license
