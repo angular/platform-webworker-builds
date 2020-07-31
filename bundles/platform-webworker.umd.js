1805c6bd0bb3e70f503a2e772b153bfd94841c0b
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 18:33:26 +0000

    3a46c2da7c refactor(docs-infra): update docs examples `tslint.json` to match CLI and fix failures (#38143)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+23.sha-e8896b9
+ * @license Angular v10.1.0-next.3+38.sha-3a46c2d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.3+23.sha-e8896b9');
+    var VERSION = new i0.Version('10.1.0-next.3+38.sha-3a46c2d');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
