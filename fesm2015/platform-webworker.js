7996009c8e63bd4003b9620b6ea169907ed0150f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-07-31 00:22:51 +0000

    7ff5ef27c7 fix(language-service): [ivy] do not retrieve ts.Program at startup (#38120)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.6+21.sha-879ff08f
+ * @license Angular v10.0.7+2.sha-7ff5ef2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.6+21.sha-879ff08f');
+const VERSION = new Version('10.0.7+2.sha-7ff5ef2');
 
 /**
  * @license
