51603fc325c7bde05b16fada6c37f07182cc4948
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:24:47 +0000

    8fbf40bf40 feat(core): update reference and doc to change `async` to `waitAsync`. (#37583)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+40.sha-87baa06
+ * @license Angular v10.1.0-next.3+43.sha-8fbf40b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.3+40.sha-87baa06');
+    var VERSION = new i0.Version('10.1.0-next.3+43.sha-8fbf40b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
