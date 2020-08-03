548c9afddb82ec853748b63b4aac15ae0462e7be
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-03 20:19:33 +0000

    87baa06cc6 revert: docs(core): correct SomeService to SomeComponent (#38325)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+41.sha-e49b053
+ * @license Angular v10.1.0-next.3+40.sha-87baa06
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.3+41.sha-e49b053');
+    var VERSION = new i0.Version('10.1.0-next.3+40.sha-87baa06');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
