a10173708aa6a13bd9cd4079d546fa95c7032bde
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-08-04 23:32:44 +0000

    192ef42304 docs: release notes for the v10.0.8 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.3+43.sha-8fbf40b
+ * @license Angular v10.1.0-next.3+44.sha-192ef42
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.3+43.sha-8fbf40b');
+const VERSION = new Version('10.1.0-next.3+44.sha-192ef42');
 
 /**
  * @license
