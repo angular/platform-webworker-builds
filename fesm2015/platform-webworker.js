e90880f51ad213da7b382fafef5db76b07b8cd28
Andrew Kushnir <akushnir@google.com>
2020-08-06 23:37:52 +0000

    df7f3b04b5 fix(service-worker): fix the chrome debugger syntax highlighter (#38332)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+21.sha-7525f3a
+ * @license Angular v10.1.0-next.4+22.sha-df7f3b0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+21.sha-7525f3a');
+const VERSION = new Version('10.1.0-next.4+22.sha-df7f3b0');
 
 /**
  * @license
