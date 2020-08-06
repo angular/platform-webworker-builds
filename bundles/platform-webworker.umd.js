65d4359e73c800fba6d3497c34955eaddbb90295
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:44:37 +0000

    f5d5bac076 fix(service-worker): fix the chrome debugger syntax highlighter (#38332)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+16.sha-ad16e2f
+ * @license Angular v10.0.8+17.sha-f5d5bac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.8+16.sha-ad16e2f');
+    var VERSION = new core.Version('10.0.8+17.sha-f5d5bac');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+16.sha-ad16e2f
+ * @license Angular v10.0.8+17.sha-f5d5bac
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+16.sha-ad16e2f');
+const VERSION = new Version('10.0.8+17.sha-f5d5bac');
 
 /**
  * @license
