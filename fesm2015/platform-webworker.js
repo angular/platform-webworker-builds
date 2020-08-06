466f69efd03c7111eb085b1619bb60083c1f9038
Andrew Kushnir <akushnir@google.com>
2020-08-06 01:57:57 +0000

    d7c043ba35 docs: add a page with the Angular roadmap (#38358)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+10.sha-0c24903
+ * @license Angular v10.1.0-next.4+11.sha-d7c043b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+10.sha-0c24903');
+const VERSION = new Version('10.1.0-next.4+11.sha-d7c043b');
 
 /**
  * @license
