112a2604d63d1afe6d32bc753957671d95f6dcb5
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:28:59 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+9.sha-bb88c9f
+ * @license Angular v10.1.0-next.4+10.sha-0c24903
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+9.sha-bb88c9f');
+const VERSION = new Version('10.1.0-next.4+10.sha-0c24903');
 
 /**
  * @license
