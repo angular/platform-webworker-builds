6ea3c6a430f7186168629910068647d0b0614adb
Andrew Kushnir <akushnir@google.com>
2020-08-06 01:15:16 +0000

    58f4b3ad80 fix(common): ensure scrollRestoration is writable (#30630) (#38357)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+8.sha-db897f4
+ * @license Angular v10.0.8+10.sha-58f4b3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+8.sha-db897f4');
+const VERSION = new Version('10.0.8+10.sha-58f4b3a');
 
 /**
  * @license
