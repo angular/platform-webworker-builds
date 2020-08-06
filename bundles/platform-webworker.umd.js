d1bf8171826fe815f8915a5e65c8df190de6db35
Andrew Kushnir <akushnir@google.com>
2020-08-06 22:47:48 +0000

    7525f3afc1 fix(compiler-cli): type-check inputs that include undefined when there's coercion members (#38273)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+19.sha-26be5b4
+ * @license Angular v10.1.0-next.4+21.sha-7525f3a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.4+19.sha-26be5b4');
+    var VERSION = new core.Version('10.1.0-next.4+21.sha-7525f3a');
 
     /**
      * @license
