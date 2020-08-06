290fc08f09d895f81c1cf458d78a594653f8e307
Andrew Kushnir <akushnir@google.com>
2020-08-06 00:31:26 +0000

    0c2490368e refactor(platform-browser): specify return type of parseEventName (#38089)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+9.sha-bb88c9f
+ * @license Angular v10.1.0-next.4+10.sha-0c24903
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.4+9.sha-bb88c9f');
+    var VERSION = new i0.Version('10.1.0-next.4+10.sha-0c24903');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
