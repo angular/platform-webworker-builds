5393b2be1d5e8e295ea2c32e1284f3348481459b
Andrew Kushnir <akushnir@google.com>
2020-08-06 16:23:01 +0000

    dca4443a8e fix(compiler-cli): mark eager `NgModuleFactory` construction as not side effectful (#38320)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+11.sha-d7c043b
+ * @license Angular v10.1.0-next.4+15.sha-dca4443
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.4+11.sha-d7c043b');
+    var VERSION = new core.Version('10.1.0-next.4+15.sha-dca4443');
 
     /**
      * @license
