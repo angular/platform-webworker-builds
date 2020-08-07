4030a442aa33f98b8358364be9209e8546f2b827
Andrew Kushnir <akushnir@google.com>
2020-08-07 18:41:30 +0000

    4107abba2d refactor(common): use getElementById in ViewportScroller.scrollToAnchor (#38372)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+20.sha-ef7b70a
+ * @license Angular v10.0.8+22.sha-4107abb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+20.sha-ef7b70a');
+const VERSION = new Version('10.0.8+22.sha-4107abb');
 
 /**
  * @license
