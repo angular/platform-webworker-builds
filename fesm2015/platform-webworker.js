a1c3669a3ece003ec0d587f0d0c435f91ca642d1
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:25:47 +0000

    445ac15a78 docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+25.sha-856db56
+ * @license Angular v10.1.0-next.4+26.sha-445ac15
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+25.sha-856db56');
+const VERSION = new Version('10.1.0-next.4+26.sha-445ac15');
 
 /**
  * @license
