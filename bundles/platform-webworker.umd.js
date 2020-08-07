c153d73da1657d1a3514521c1d1c486ad57b810f
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:23:43 +0000

    702958e968 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+22.sha-df7f3b0
+ * @license Angular v10.1.0-next.4+23.sha-702958e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.4+22.sha-df7f3b0');
+    var VERSION = new core.Version('10.1.0-next.4+23.sha-702958e');
 
     /**
      * @license
