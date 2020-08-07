16bc2938dd097006947798909c4407d328c00906
Andrew Kushnir <akushnir@google.com>
2020-08-07 18:40:51 +0000

    354e66efad refactor(common): use getElementById in ViewportScroller.scrollToAnchor (#30143)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+23.sha-702958e
+ * @license Angular v10.1.0-next.4+24.sha-354e66e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.4+23.sha-702958e');
+    var VERSION = new i0.Version('10.1.0-next.4+24.sha-354e66e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+23.sha-702958e
+ * @license Angular v10.1.0-next.4+24.sha-354e66e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+23.sha-702958e');
+const VERSION = new Version('10.1.0-next.4+24.sha-354e66e');
 
 /**
  * @license
