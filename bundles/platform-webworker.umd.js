0bd82a323a799e1d5053dcddf24d7b44f40cb600
Andrew Kushnir <akushnir@google.com>
2020-08-07 00:26:25 +0000

    702958e968 refactor(core): add debug ranges to `LViewDebug` with matchers (#38359)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+22.sha-df7f3b0
+ * @license Angular v10.1.0-next.4+23.sha-702958e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.4+22.sha-df7f3b0');
+    var VERSION = new i0.Version('10.1.0-next.4+23.sha-702958e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
