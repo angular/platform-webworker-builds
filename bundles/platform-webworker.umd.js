f45692745ca703eeea164d60c343b9f1f3326a14
Andrew Kushnir <akushnir@google.com>
2020-08-07 22:30:22 +0000

    42a649d80f docs: fix purpose description of "builders.json" (#36830) PR Close #36830

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+23.sha-47fbfb3
+ * @license Angular v10.0.8+24.sha-42a649d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.8+23.sha-47fbfb3');
+    var VERSION = new core.Version('10.0.8+24.sha-42a649d');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+23.sha-47fbfb3
+ * @license Angular v10.0.8+24.sha-42a649d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+23.sha-47fbfb3');
+const VERSION = new Version('10.0.8+24.sha-42a649d');
 
 /**
  * @license
