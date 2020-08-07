566fe98d471fb3b604e22d9621fef85be99e9eb5
Andrew Kushnir <akushnir@google.com>
2020-08-07 19:06:45 +0000

    856db56cca refactor(forms): get rid of duplicate functions (#38371)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+24.sha-354e66e
+ * @license Angular v10.1.0-next.4+25.sha-856db56
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.4+24.sha-354e66e');
+    var VERSION = new i0.Version('10.1.0-next.4+25.sha-856db56');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
