2c79d8a2ca6937eab2e67a59a48425d36d338672
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:51:07 +0000

    6da9e5851a fix(compiler-cli): preserve quotes in class member names (#38387)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+32.sha-250e299
+ * @license Angular v10.1.0-next.4+33.sha-6da9e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.4+32.sha-250e299');
+    var VERSION = new core.Version('10.1.0-next.4+33.sha-6da9e58');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+32.sha-250e299
+ * @license Angular v10.1.0-next.4+33.sha-6da9e58
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+32.sha-250e299');
+const VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
 
 /**
  * @license
