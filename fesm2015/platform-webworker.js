a3902c4efa47165546bb5d531306bac89c4f15ea
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:09:28 +0000

    6d8c73a4d6 fix(core): Store the currently selected ICU in `LView` (#38345)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+27.sha-0de93fd
+ * @license Angular v10.1.0-next.4+29.sha-6d8c73a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+27.sha-0de93fd');
+const VERSION = new Version('10.1.0-next.4+29.sha-6d8c73a');
 
 /**
  * @license
