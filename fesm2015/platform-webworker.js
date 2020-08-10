89cfa58c49722586c41ef9d13f3608296ac1bbf5
Andrew Kushnir <akushnir@google.com>
2020-08-10 22:53:14 +0000

    5dc8d287aa fix(core): queries not matching string injection tokens (#38321)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+33.sha-6da9e58
+ * @license Angular v10.1.0-next.4+34.sha-5dc8d28
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+33.sha-6da9e58');
+const VERSION = new Version('10.1.0-next.4+34.sha-5dc8d28');
 
 /**
  * @license
