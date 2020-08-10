0ece541b9b0e26da4e18d1a9c25fdefe0c13b99a
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:35:16 +0000

    7f111491a8 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+27.sha-ee5123f
+ * @license Angular v10.0.8+28.sha-7f11149
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.8+27.sha-ee5123f');
+    var VERSION = new core.Version('10.0.8+28.sha-7f11149');
 
     /**
      * @license
