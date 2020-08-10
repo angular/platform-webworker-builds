9ed6ea0ddee80272b7c47b790240956bb90e1167
Andrew Kushnir <akushnir@google.com>
2020-08-10 20:36:53 +0000

    e34c33cd46 fix(platform-server): remove styles added by ServerStylesHost on destruction (#38367)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+29.sha-6d8c73a
+ * @license Angular v10.1.0-next.4+30.sha-e34c33c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+29.sha-6d8c73a');
+const VERSION = new Version('10.1.0-next.4+30.sha-e34c33c');
 
 /**
  * @license
