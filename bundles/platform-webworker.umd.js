6d3d135a4c2d7951f5cf5531c3cb6582bfcb4315
Andrew Kushnir <akushnir@google.com>
2020-08-11 00:49:44 +0000

    3d156162af fix(dev-infra): update i18n-related file locations in PullApprove config (#38403)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+34.sha-5dc8d28
+ * @license Angular v10.1.0-next.4+35.sha-3d15616
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.4+34.sha-5dc8d28');
+    var VERSION = new core.Version('10.1.0-next.4+35.sha-3d15616');
 
     /**
      * @license
