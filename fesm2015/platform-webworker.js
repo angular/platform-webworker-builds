ee6bfba74714843ece56fbd5928363b223d525a7
Andrew Kushnir <akushnir@google.com>
2020-08-11 18:31:04 +0000

    f6cfc9294b ci: update payload size limits for Closure tests (#38411)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+32.sha-02aca13
+ * @license Angular v10.0.8+36.sha-f6cfc92
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+32.sha-02aca13');
+const VERSION = new Version('10.0.8+36.sha-f6cfc92');
 
 /**
  * @license
