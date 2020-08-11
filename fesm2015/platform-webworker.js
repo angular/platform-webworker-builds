d350aee05c88ceaeafa90074f8612fb58ce56610
Andrew Kushnir <akushnir@google.com>
2020-08-11 16:05:21 +0000

    df76a2048b fix(router): restore 'history.state' object for navigations coming from Angular router (#28108) (#28176)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.4+35.sha-3d15616
+ * @license Angular v10.1.0-next.4+36.sha-df76a20
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.4+35.sha-3d15616');
+const VERSION = new Version('10.1.0-next.4+36.sha-df76a20');
 
 /**
  * @license
