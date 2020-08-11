a79fa315225dd6b0d0ac30d08a8249dc7d07212c
Andrew Kushnir <akushnir@google.com>
2020-08-11 20:27:55 +0000

    ca2b4bcd4b fix(compiler-cli): avoid creating value expressions for symbols from type-only imports (#38415)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+36.sha-f6cfc92
+ * @license Angular v10.0.8+37.sha-ca2b4bc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+36.sha-f6cfc92');
+const VERSION = new Version('10.0.8+37.sha-ca2b4bc');
 
 /**
  * @license
