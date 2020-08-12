11c8f104b653c556b9d0d6a2b1c69121eda48caa
Andrew Kushnir <akushnir@google.com>
2020-08-12 15:48:31 +0000

    2590a9b535 docs: delete one superfluous sentence (#38339)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.8+39.sha-f68a1d3
+ * @license Angular v10.0.8+40.sha-2590a9b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.8+39.sha-f68a1d3');
+const VERSION = new Version('10.0.8+40.sha-2590a9b');
 
 /**
  * @license
