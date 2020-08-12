1910e0f6522a1440f2004c05b06c3f295f07e833
Andrew Scott <atscott01@gmail.com>
2020-08-12 19:02:22 +0000

    5f2e475abf docs: remove unused Input decorator (#38306)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+2.sha-aa3520e
+ * @license Angular v10.1.0-next.5+3.sha-5f2e475
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+2.sha-aa3520e');
+const VERSION = new Version('10.1.0-next.5+3.sha-5f2e475');
 
 /**
  * @license
