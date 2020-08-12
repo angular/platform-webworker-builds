45f7e12b752f58cd8adcb0f3950d7303da2c3265
Andrew Scott <atscott01@gmail.com>
2020-08-12 23:33:40 +0000

    afd4417a7b refactor(dev-infra): extract the commit message parsing function into its own file (#38429)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+3.sha-2f53bbb
+ * @license Angular v10.0.9+4.sha-afd4417
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.9+3.sha-2f53bbb');
+    var VERSION = new core.Version('10.0.9+4.sha-afd4417');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+3.sha-2f53bbb
+ * @license Angular v10.0.9+4.sha-afd4417
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.9+3.sha-2f53bbb');
+const VERSION = new Version('10.0.9+4.sha-afd4417');
 
 /**
  * @license
