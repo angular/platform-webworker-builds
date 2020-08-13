3d1c512f9096ff46800f8dfb233ff54cc19cf5bd
Andrew Scott <atscott01@gmail.com>
2020-08-13 19:05:10 +0000

    aa816d3887 ci: disable closure size tracking test (#38449)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+9.sha-cb83b8a
+ * @license Angular v10.0.9+11.sha-aa816d3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.9+9.sha-cb83b8a');
+    var VERSION = new core.Version('10.0.9+11.sha-aa816d3');
 
     /**
      * @license
