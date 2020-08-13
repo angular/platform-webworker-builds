a56d70683fc14c306a2f939681f00be58dded72b
Andrew Scott <atscott01@gmail.com>
2020-08-13 17:04:46 +0000

    aa847cb014 build: run browsers tests on chromium locally (#38435)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+8.sha-8763d82
+ * @license Angular v10.1.0-next.5+9.sha-aa847cb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+8.sha-8763d82');
+const VERSION = new Version('10.1.0-next.5+9.sha-aa847cb');
 
 /**
  * @license
