8537ccc0afeced73d35e447e6d70c68144485816
Andrew Scott <atscott01@gmail.com>
2020-08-13 20:18:04 +0000

    692e34d4a2 test(docs-infra): remove deprecated `ReflectiveInjector` (#38408)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+11.sha-aa816d3
+ * @license Angular v10.0.9+14.sha-692e34d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.9+11.sha-aa816d3');
+const VERSION = new Version('10.0.9+14.sha-692e34d');
 
 /**
  * @license
