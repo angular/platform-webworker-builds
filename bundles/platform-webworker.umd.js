982bbf14662a155213223b265632d9f9d9c7e5ca
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:04:22 +0000

    b071495f92 fix(core): fix multiple nested views removal from ViewContainerRef (#38317)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+17.sha-d5f819e
+ * @license Angular v10.1.0-next.5+18.sha-b071495
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.5+17.sha-d5f819e');
+    var VERSION = new i0.Version('10.1.0-next.5+18.sha-b071495');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
