b346cd25f9082364d240ff416cb1ad3cbb03cb61
Andrew Scott <atscott01@gmail.com>
2020-08-13 16:33:13 +0000

    773f7908c0 feat(dev-infra): update to latest benchpress version (#38440)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+5.sha-f4ced74
+ * @license Angular v10.1.0-next.5+6.sha-773f790
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+5.sha-f4ced74');
+const VERSION = new Version('10.1.0-next.5+6.sha-773f790');
 
 /**
  * @license
