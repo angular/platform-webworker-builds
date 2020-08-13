ec6c0a9ead4e62fc35fc1be3aa7e42c4a607b0b8
Andrew Scott <atscott01@gmail.com>
2020-08-13 21:02:37 +0000

    d5e09f4d62 fix(core): fix multiple nested views removal from ViewContainerRef (#38317)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+14.sha-692e34d
+ * @license Angular v10.0.9+16.sha-d5e09f4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.9+14.sha-692e34d');
+const VERSION = new Version('10.0.9+16.sha-d5e09f4');
 
 /**
  * @license
