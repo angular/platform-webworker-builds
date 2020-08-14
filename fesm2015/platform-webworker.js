f3243227befc0f5b5073fb731104e73b9843efc0
Andrew Scott <atscott01@gmail.com>
2020-08-14 19:05:54 +0000

    71079ce47e fix(common): Allow scrolling when browser supports scrollTo (#38468)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+19.sha-ca79880
+ * @license Angular v10.1.0-next.5+20.sha-71079ce
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+19.sha-ca79880');
+const VERSION = new Version('10.1.0-next.5+20.sha-71079ce');
 
 /**
  * @license
