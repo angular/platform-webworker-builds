cf8753d320cc4e4ef4add5150c286a0a4b95d1d9
Andrew Scott <atscott@google.com>
2020-08-17 18:55:22 +0000

    84d1ba792b refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.9+17.sha-b32126c
+ * @license Angular v10.0.9+18.sha-84d1ba7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.9+17.sha-b32126c');
+const VERSION = new Version('10.0.9+18.sha-84d1ba7');
 
 /**
  * @license
