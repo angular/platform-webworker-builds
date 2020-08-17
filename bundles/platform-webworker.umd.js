27455a54c9d6019790fe63cba77322ee27048c7a
Andrew Scott <atscott@google.com>
2020-08-17 18:09:19 +0000

    81c3e809aa fix(localize): render ICU placeholders in extracted translation files (#38484)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+22.sha-cb05c01
+ * @license Angular v10.1.0-next.5+24.sha-81c3e80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.5+22.sha-cb05c01');
+    var VERSION = new core.Version('10.1.0-next.5+24.sha-81c3e80');
 
     /**
      * @license
