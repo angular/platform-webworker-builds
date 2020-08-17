62f8edbb9375b56de337b8e471bbe6c9a71213b7
Andrew Scott <atscott@google.com>
2020-08-17 18:54:53 +0000

    cfe424e875 refactor(language-service): [Ivy] remove temporary compiler (#38310)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+26.sha-3b9c802
+ * @license Angular v10.1.0-next.5+27.sha-cfe424e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.5+26.sha-3b9c802');
+    var VERSION = new i0.Version('10.1.0-next.5+27.sha-cfe424e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+26.sha-3b9c802
+ * @license Angular v10.1.0-next.5+27.sha-cfe424e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.5+26.sha-3b9c802');
+const VERSION = new Version('10.1.0-next.5+27.sha-cfe424e');
 
 /**
  * @license
