b61e6d7b2ff4c7c532bd56cc8492edf37dc54e87
atscott <atscott01@gmail.com>
2020-08-17 20:54:43 +0000

    64cf087ae5 release: cut the v10.1.0-next.6 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+29.sha-fec9dcb
+ * @license Angular v10.1.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.5+29.sha-fec9dcb');
+    var VERSION = new core.Version('10.1.0-next.6');
 
     /**
      * @license
