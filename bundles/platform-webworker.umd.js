11cea3a01e0b5747042ae3dbe5b711433eb4f10f
atscott <atscott01@gmail.com>
2020-08-17 20:57:07 +0000

    64cf087ae5 release: cut the v10.1.0-next.6 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.5+29.sha-fec9dcb
+ * @license Angular v10.1.0-next.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.5+29.sha-fec9dcb');
+    var VERSION = new i0.Version('10.1.0-next.6');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
