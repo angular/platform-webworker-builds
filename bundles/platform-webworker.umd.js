b27b3778a233a8b783fd59cbd4b8008e7cea827d
Andrew Scott <atscott@google.com>
2020-08-18 17:35:45 +0000

    723a9ff095 docs(common): Wrong parameter description on TrackBy (#38495)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+3.sha-e472f5f
+ * @license Angular v10.1.0-next.6+4.sha-723a9ff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.6+3.sha-e472f5f');
+    var VERSION = new i0.Version('10.1.0-next.6+4.sha-723a9ff');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
