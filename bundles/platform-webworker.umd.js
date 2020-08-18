fd54720e183bdf72c72fabf80d522ab5b53adcfc
Andrew Scott <atscott@google.com>
2020-08-18 17:47:55 +0000

    dbfb50e9f4 fix(router): ensure routerLinkActive updates when associated routerLinks change (#38511)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+4.sha-723a9ff
+ * @license Angular v10.1.0-next.6+6.sha-dbfb50e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.6+4.sha-723a9ff');
+    var VERSION = new i0.Version('10.1.0-next.6+6.sha-dbfb50e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
