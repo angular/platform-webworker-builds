831f19d9f699879aa6a7919a455179f08ffd4472
Andrew Scott <atscott@google.com>
2020-08-18 23:45:37 +0000

    63ba74fe4e feat(dev-infra): tooling to check out pending PR (#38474)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+7.sha-aaa1d8e
+ * @license Angular v10.1.0-next.6+8.sha-63ba74f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.6+7.sha-aaa1d8e');
+    var VERSION = new core.Version('10.1.0-next.6+8.sha-63ba74f');
 
     /**
      * @license
