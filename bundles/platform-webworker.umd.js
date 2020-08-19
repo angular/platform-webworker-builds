2a59151d520e59ae5533474d28ba7b14b7bc4f98
atscott <atscott01@gmail.com>
2020-08-19 16:41:35 +0000

    0270020ac2 docs: release notes for the v10.0.11 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+12.sha-0270020
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.6+10.sha-55fd725');
+    var VERSION = new i0.Version('10.1.0-next.6+12.sha-0270020');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+12.sha-0270020
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.6+10.sha-55fd725');
+const VERSION = new Version('10.1.0-next.6+12.sha-0270020');
 
 /**
  * @license
