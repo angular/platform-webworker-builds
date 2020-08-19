55f4d57cc13d4056aa8ab47c15f28615e8a06e0c
Misko Hevery <misko@hevery.com>
2020-08-19 21:35:05 +0000

    0b54c0c6b4 refactor(compiler-cli): add getTemplateOfComponent to TemplateTypeChecker (#38355)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+3.sha-1ec6099
+ * @license Angular v10.1.0-next.7+4.sha-0b54c0c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.7+3.sha-1ec6099');
+    var VERSION = new core.Version('10.1.0-next.7+4.sha-0b54c0c');
 
     /**
      * @license
