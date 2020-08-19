7d9a46b3b4b43e47d574e36ebcb0122400735eca
Misko Hevery <misko@hevery.com>
2020-08-19 21:32:38 +0000

    1ec609946f docs: Typos fixes in the binding syntax guide (#38519)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+2.sha-7ad3264
+ * @license Angular v10.1.0-next.7+3.sha-1ec6099
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+2.sha-7ad3264');
+const VERSION = new Version('10.1.0-next.7+3.sha-1ec6099');
 
 /**
  * @license
