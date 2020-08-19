b6fbc7968ebaee8c2eeff5b2e623ae031c489241
Misko Hevery <misko@hevery.com>
2020-08-19 21:42:59 +0000

    68a9a01a64 fix(localize): parse all parts of a translation with nested HTML (#38452)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+4.sha-0b54c0c
+ * @license Angular v10.1.0-next.7+6.sha-68a9a01
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+4.sha-0b54c0c');
+const VERSION = new Version('10.1.0-next.7+6.sha-68a9a01');
 
 /**
  * @license
