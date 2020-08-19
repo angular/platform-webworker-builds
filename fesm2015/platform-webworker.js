c3fd90a4fbd46ab693bc342f0fa0af8121d18c13
Andrew Scott <atscott@google.com>
2020-08-19 00:25:45 +0000

    f77fd5e02a feat(dev-infra): create a wizard for building commit messages (#38457)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+8.sha-63ba74f
+ * @license Angular v10.1.0-next.6+9.sha-f77fd5e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.6+8.sha-63ba74f');
+const VERSION = new Version('10.1.0-next.6+9.sha-f77fd5e');
 
 /**
  * @license
