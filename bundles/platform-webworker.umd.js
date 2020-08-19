ae8687530339db24af26f5a3313b58d804d2a2c7
Andrew Scott <atscott@google.com>
2020-08-19 16:30:13 +0000

    6b662d10c1 fix(zone.js): zone.js package.json should not include files/directories field (#38528)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+10.sha-55fd725
+ * @license Angular v10.1.0-next.6+11.sha-6b662d1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.6+10.sha-55fd725');
+    var VERSION = new core.Version('10.1.0-next.6+11.sha-6b662d1');
 
     /**
      * @license
