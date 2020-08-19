b066e0bca284aa56b51281f0ef0d5d9c39ef88fb
Misko Hevery <misko@hevery.com>
2020-08-19 19:03:45 +0000

    7ad32649c0 fix(router): support lazy loading for empty path named outlets (#38379)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.6+12.sha-0270020
+ * @license Angular v10.1.0-next.7+2.sha-7ad3264
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.6+12.sha-0270020');
+    var VERSION = new i0.Version('10.1.0-next.7+2.sha-7ad3264');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
