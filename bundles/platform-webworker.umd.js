bc982c36efca7fbc0ad1e0d152dd52b56b573373
Misko Hevery <misko@hevery.com>
2020-08-19 21:48:11 +0000

    ac461e1efd fix(localize): extract the correct message ids (#38498)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+7.sha-f245c6b
+ * @license Angular v10.1.0-next.7+8.sha-ac461e1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.7+7.sha-f245c6b');
+    var VERSION = new i0.Version('10.1.0-next.7+8.sha-ac461e1');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
