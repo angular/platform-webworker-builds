e035f1c7f42c464175af4f8c65ae0c41a132dd0c
Misko Hevery <misko@hevery.com>
2020-08-19 21:46:22 +0000

    f245c6bb15 fix(core): remove closing body tag from inert DOM builder (#38454)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+6.sha-68a9a01
+ * @license Angular v10.1.0-next.7+7.sha-f245c6b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.7+6.sha-68a9a01');
+    var VERSION = new i0.Version('10.1.0-next.7+7.sha-f245c6b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
