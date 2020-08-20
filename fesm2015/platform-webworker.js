96326a80230a19ae1c70d24ce40844db9c992d2b
Misko Hevery <misko@hevery.com>
2020-08-20 16:56:06 +0000

    6442875c99 docs(core): Fix typo in JSDoc for AbstractType<T> (#38541)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+11.sha-4ee5e73
+ * @license Angular v10.1.0-next.7+10.sha-6442875
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+11.sha-4ee5e73');
+const VERSION = new Version('10.1.0-next.7+10.sha-6442875');
 
 /**
  * @license
