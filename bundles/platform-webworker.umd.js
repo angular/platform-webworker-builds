646e82bd2ba16d35c386d24c9db8a47e2ba528d5
Misko Hevery <misko@hevery.com>
2020-08-20 17:04:41 +0000

    1775f351d6 fix(docs-infra): fix vertical alignment of external link icons (#38410)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+7.sha-c90262e
+ * @license Angular v10.0.11+10.sha-1775f35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.11+7.sha-c90262e');
+    var VERSION = new core.Version('10.0.11+10.sha-1775f35');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+7.sha-c90262e
+ * @license Angular v10.0.11+10.sha-1775f35
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.11+7.sha-c90262e');
+const VERSION = new Version('10.0.11+10.sha-1775f35');
 
 /**
  * @license
