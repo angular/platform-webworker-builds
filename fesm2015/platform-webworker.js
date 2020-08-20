bbe387a94e773dbc913ecb8d7c80b43625aabd1b
Misko Hevery <misko@hevery.com>
2020-08-20 20:26:37 +0000

    956b25a100 docs: apply code styling in template reference variables guide (#38522)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+12.sha-22f1ac3
+ * @license Angular v10.1.0-next.7+14.sha-956b25a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+12.sha-22f1ac3');
+const VERSION = new Version('10.1.0-next.7+14.sha-956b25a');
 
 /**
  * @license
