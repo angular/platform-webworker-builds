e38d7e0b43a575181555f9b969d05405edc25e06
Misko Hevery <misko@hevery.com>
2020-08-20 17:03:19 +0000

    22f1ac3e37 docs: udpate the details (#37967)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+13.sha-8017ca4
+ * @license Angular v10.1.0-next.7+12.sha-22f1ac3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+13.sha-8017ca4');
+const VERSION = new Version('10.1.0-next.7+12.sha-22f1ac3');
 
 /**
  * @license
