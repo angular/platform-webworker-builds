a05108c3ccd949c348ab19b1be56927c512872d4
Misko Hevery <misko@hevery.com>
2020-08-21 19:47:00 +0000

    e7da4040d6 fix(compiler-cli): adding references to const enums in runtime code (#38542)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+16.sha-2a643e1
+ * @license Angular v10.1.0-next.7+17.sha-e7da404
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+16.sha-2a643e1');
+const VERSION = new Version('10.1.0-next.7+17.sha-e7da404');
 
 /**
  * @license
