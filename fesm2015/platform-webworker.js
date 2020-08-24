2d4777a37c7e89da32800f630146ec2fb4d456f1
Misko Hevery <misko@hevery.com>
2020-08-24 16:50:15 +0000

    8224e65e70 docs(localize): fix angular.json syntax (#38553)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.11+13.sha-814b436
+ * @license Angular v10.0.11+16.sha-8224e65
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.11+13.sha-814b436');
+const VERSION = new Version('10.0.11+16.sha-8224e65');
 
 /**
  * @license
