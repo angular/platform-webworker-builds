0324fe0d20de4e69ea9882c232a44acdfb0774ae
Misko Hevery <misko@hevery.com>
2020-08-24 20:39:46 +0000

    375f0a6f67 build: update tslint to 6.1.3 (#38076)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+19.sha-b48cc6e
+ * @license Angular v10.1.0-next.7+25.sha-375f0a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.7+19.sha-b48cc6e');
+    var VERSION = new i0.Version('10.1.0-next.7+25.sha-375f0a6');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
