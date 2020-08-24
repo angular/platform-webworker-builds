403756e517528a670d2f26129d3da396d727733a
Misko Hevery <misko@hevery.com>
2020-08-24 16:53:27 +0000

    b48cc6ead5 feat(language-service): introduce hybrid visitor to locate AST node (#38540)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+22.sha-201a546
+ * @license Angular v10.1.0-next.7+19.sha-b48cc6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.7+22.sha-201a546');
+    var VERSION = new core.Version('10.1.0-next.7+19.sha-b48cc6e');
 
     /**
      * @license
