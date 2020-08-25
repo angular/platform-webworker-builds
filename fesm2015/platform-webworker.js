b5b7dad4c3b00ce93c751ed042371d7265a935e3
Misko Hevery <misko@hevery.com>
2020-08-25 17:25:59 +0000

    94a3e0e81d docs: add correct path for karma.conf.js file (#38571)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+30.sha-9cbde86
+ * @license Angular v10.1.0-next.7+31.sha-94a3e0e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+30.sha-9cbde86');
+const VERSION = new Version('10.1.0-next.7+31.sha-94a3e0e');
 
 /**
  * @license
