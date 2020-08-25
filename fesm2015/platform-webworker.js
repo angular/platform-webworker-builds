0aa00c051ba071278fa25d962b9a6a645fb90bf5
Misko Hevery <misko@hevery.com>
2020-08-25 17:21:05 +0000

    9cbde86534 docs: fix typo in the testing component basics guide (#38573)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+29.sha-18e474f
+ * @license Angular v10.1.0-next.7+30.sha-9cbde86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-next.7+29.sha-18e474f');
+const VERSION = new Version('10.1.0-next.7+30.sha-9cbde86');
 
 /**
  * @license
