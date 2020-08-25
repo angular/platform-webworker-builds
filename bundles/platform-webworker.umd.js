82b17e96c8c591db6077db26391ca3fa9abc38e2
Misko Hevery <misko@hevery.com>
2020-08-25 17:18:31 +0000

    9cbde86534 docs: fix typo in the testing component basics guide (#38573)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.7+29.sha-18e474f
+ * @license Angular v10.1.0-next.7+30.sha-9cbde86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-next.7+29.sha-18e474f');
+    var VERSION = new core.Version('10.1.0-next.7+30.sha-9cbde86');
 
     /**
      * @license
