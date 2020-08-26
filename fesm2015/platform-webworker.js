00e7a482ba3beaa6354df0e12914f80cf1690246
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:18:43 +0000

    89ff4467fd docs: add Sam Vloeberghs to GDE list (#37970)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14
+ * @license Angular v10.0.14+3.sha-89ff446
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.14');
+const VERSION = new Version('10.0.14+3.sha-89ff446');
 
 /**
  * @license
