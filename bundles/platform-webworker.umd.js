59207c3010195d9e1289cd9b2477acdb532744ee
Joey Perrott <josephperrott@gmail.com>
2020-08-26 20:20:09 +0000

    618cb32407 docs: add Sam Vloeberghs to GDE list (#37970)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+1.sha-0681a20
+ * @license Angular v10.1.0-rc.0+3.sha-618cb32
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+1.sha-0681a20');
+    var VERSION = new i0.Version('10.1.0-rc.0+3.sha-618cb32');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
