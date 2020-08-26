301ba7d5084421ae579b6cbc26ee56b0c8603b82
Misko Hevery <misko@hevery.com>
2020-08-26 19:09:11 +0000

    80cab26023 docs: Minor fixes in NgModules section (#36177)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-next.8+11.sha-f0af387
+ * @license Angular v10.1.0-next.8+13.sha-80cab26
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-next.8+11.sha-f0af387');
+    var VERSION = new i0.Version('10.1.0-next.8+13.sha-80cab26');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
