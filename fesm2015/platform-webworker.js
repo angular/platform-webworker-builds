7b366986f5c9cd5bc610aa827194689c4dd326a4
Joey Perrott <josephperrott@gmail.com>
2020-08-27 16:28:12 +0000

    164cd274a4 test(docs-infra): add commands to run the unit tests (#34537)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+2.sha-4aee008
+ * @license Angular v10.1.0-rc.0+5.sha-164cd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+2.sha-4aee008');
+const VERSION = new Version('10.1.0-rc.0+5.sha-164cd27');
 
 /**
  * @license
