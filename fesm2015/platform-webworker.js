dd5ffb336932f56d30ee45b6ac412a84ec96fa4a
Joey Perrott <josephperrott@gmail.com>
2020-08-27 18:23:05 +0000

    43ddfbaf31 build: bump to lastest sha of angular/dev-infra's lock-closed action (#38615)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+5.sha-94fd5c7
+ * @license Angular v10.0.14+6.sha-43ddfba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.14+5.sha-94fd5c7');
+const VERSION = new Version('10.0.14+6.sha-43ddfba');
 
 /**
  * @license
