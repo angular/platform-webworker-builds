77b5fa92a073bdee38a4251ecf77b961c20b424a
Joey Perrott <josephperrott@gmail.com>
2020-08-27 16:34:17 +0000

    94fd5c772b test(docs-infra): add commands to run the unit tests (#34537)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+3.sha-89ff446
+ * @license Angular v10.0.14+5.sha-94fd5c7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.0.14+3.sha-89ff446');
+    var VERSION = new core.Version('10.0.14+5.sha-94fd5c7');
 
     /**
      * @license
