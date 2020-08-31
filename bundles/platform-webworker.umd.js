010672ca77643dec04276d5c6e23ab8f94aae319
Joey Perrott <josephperrott@gmail.com>
2020-08-31 20:57:45 +0000

    f5a148b1b7 fix(compiler): incorrectly inferring namespace for HTML nodes inside SVG (#38477)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+30.sha-4f28192
+ * @license Angular v10.1.0-rc.0+31.sha-f5a148b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+30.sha-4f28192');
+    var VERSION = new i0.Version('10.1.0-rc.0+31.sha-f5a148b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
