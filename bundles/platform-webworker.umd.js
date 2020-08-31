c085f835a1fd3e5373437b2ff4b72784c5c5b384
Joey Perrott <josephperrott@gmail.com>
2020-08-31 17:50:58 +0000

    0a7a5e3aff docs: Remove confusion between do/avoid templates (#38647) PR Close #38647

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+22.sha-ebc0e46
+ * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-rc.0+22.sha-ebc0e46');
+    var VERSION = new core.Version('10.1.0-rc.0+24.sha-0a7a5e3');
 
     /**
      * @license
