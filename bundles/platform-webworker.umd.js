3b6263d5ad9714ef60258e16b476b3678076ca7f
Joey Perrott <josephperrott@gmail.com>
2020-08-31 19:08:52 +0000

    0fc2bef0cd docs(service-worker): add links to service worker communication guide (#36847)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+24.sha-0a7a5e3
+ * @license Angular v10.1.0-rc.0+29.sha-0fc2bef
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+24.sha-0a7a5e3');
+    var VERSION = new i0.Version('10.1.0-rc.0+29.sha-0fc2bef');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
