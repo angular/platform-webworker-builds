baf1e3ab77c029134315d38c779ab86762fe896b
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:06:07 +0000

    5d5caf21b8 docs: fix broken markdown in start/start-data (#38644) PR Close #38644

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+15.sha-930eeaf
+ * @license Angular v10.1.0-rc.0+17.sha-5d5caf2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-rc.0+15.sha-930eeaf');
+    var VERSION = new core.Version('10.1.0-rc.0+17.sha-5d5caf2');
 
     /**
      * @license
