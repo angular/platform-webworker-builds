331feb7fd4db0b8f76403aadbd921854ebd28407
Joey Perrott <josephperrott@gmail.com>
2020-08-31 16:07:16 +0000

    827ba05914 docs: remove first person and space in CircleCI in the testing guide. (#38631)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+17.sha-5d5caf2
+ * @license Angular v10.1.0-rc.0+19.sha-827ba05
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+17.sha-5d5caf2');
+const VERSION = new Version('10.1.0-rc.0+19.sha-827ba05');
 
 /**
  * @license
