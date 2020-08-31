eb45904943765cb5ac9c31cd534cb613d923a391
Joey Perrott <josephperrott@gmail.com>
2020-08-31 20:55:10 +0000

    f5a148b1b7 fix(compiler): incorrectly inferring namespace for HTML nodes inside SVG (#38477)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+30.sha-4f28192
+ * @license Angular v10.1.0-rc.0+31.sha-f5a148b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+30.sha-4f28192');
+const VERSION = new Version('10.1.0-rc.0+31.sha-f5a148b');
 
 /**
  * @license
