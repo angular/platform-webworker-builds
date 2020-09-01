0ff31de7974c9951e1ea46283d98e6df117f906e
Joey Perrott <josephperrott@gmail.com>
2020-09-01 16:25:32 +0000

    c142b071eb build: upgrade cli command docs sources to 32391604b (#38652)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+32.sha-71acf9d
+ * @license Angular v10.1.0-rc.0+33.sha-c142b07
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+32.sha-71acf9d');
+const VERSION = new Version('10.1.0-rc.0+33.sha-c142b07');
 
 /**
  * @license
