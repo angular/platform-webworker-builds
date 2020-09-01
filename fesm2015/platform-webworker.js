b6bacd8979f217e57803415e8a23e55e91a97cdb
GitHub <noreply@github.com>
2020-09-01 17:58:14 +0000

    03dbcc7a56 build: update the package.json to 11.0.0-next.0 (#38667)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+33.sha-c142b07
+ * @license Angular v10.1.0-rc.0+34.sha-03dbcc7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0-rc.0+33.sha-c142b07');
+const VERSION = new Version('10.1.0-rc.0+34.sha-03dbcc7');
 
 /**
  * @license
