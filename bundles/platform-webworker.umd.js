7856ab00c31ccad71880e5315b7c92543499cbff
Joey Perrott <josephperrott@gmail.com>
2020-09-01 20:34:58 +0000

    59c234cfb4 build: add configuration for the caretaker command (#38601)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+35.sha-d9fea85
+ * @license Angular v10.1.0-rc.0+37.sha-59c234c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('10.1.0-rc.0+35.sha-d9fea85');
+    var VERSION = new i0.Version('10.1.0-rc.0+37.sha-59c234c');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
