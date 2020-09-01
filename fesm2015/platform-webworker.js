6e27cab0a00bcb2fa6e1e043a467db1b736c77de
Joey Perrott <josephperrott@gmail.com>
2020-09-01 00:33:35 +0000

    626ac45482 docs: Restructure table of contents to provide a more streamlined experience (#38658)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.0.14+24.sha-a938180
+ * @license Angular v10.0.14+25.sha-626ac45
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.0.14+24.sha-a938180');
+const VERSION = new Version('10.0.14+25.sha-626ac45');
 
 /**
  * @license
