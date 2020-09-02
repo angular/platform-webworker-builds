45850248ffba2804b3817422e32a6602804d5c3c
Joey Perrott <josephperrott@gmail.com>
2020-09-02 20:21:56 +0000

    c01bd0fe8e release: cut the v10.1.0 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0-rc.0+19.sha-5588324
+ * @license Angular v10.1.0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0-rc.0+19.sha-5588324');
+    var VERSION = new core.Version('10.1.0');
 
     /**
      * @license
