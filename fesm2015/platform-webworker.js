7e9b8bbf9ccfc1d097f5d66c7f9914be4e43bff0
Andrew Scott <atscott@google.com>
2020-09-02 22:14:29 +0000

    1d8c5d88cd refactor(compiler): `element.sourceSpan` should span the `outerHTML` (#38581)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+1.sha-86e7cd8
+ * @license Angular v11.0.0-next.0+5.sha-1d8c5d8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+1.sha-86e7cd8');
+const VERSION = new Version('11.0.0-next.0+5.sha-1d8c5d8');
 
 /**
  * @license
