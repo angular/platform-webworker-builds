88075a6ddd7e9e1db33ba44da76825122b4e184d
Andrew Scott <atscott@google.com>
2020-09-02 22:18:15 +0000

    5da1934115 fix(localize): render context of translation file parse errors (#38673)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+5.sha-1d8c5d8
+ * @license Angular v11.0.0-next.0+2.sha-5da1934
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+5.sha-1d8c5d8');
+    var VERSION = new i0.Version('11.0.0-next.0+2.sha-5da1934');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+5.sha-1d8c5d8
+ * @license Angular v11.0.0-next.0+2.sha-5da1934
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+5.sha-1d8c5d8');
+const VERSION = new Version('11.0.0-next.0+2.sha-5da1934');
 
 /**
  * @license
