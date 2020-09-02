344c58bfe7c52b3fd272784755d79c83cd1b6e98
atscott <atscott01@gmail.com>
2020-09-02 23:02:04 +0000

    6aac499ee7 docs: Restructure table of contents to provide a more streamlined experience (#38689)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+2.sha-32f33f0
+ * @license Angular v10.1.0+3.sha-6aac499
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0+2.sha-32f33f0');
+    var VERSION = new core.Version('10.1.0+3.sha-6aac499');
 
     /**
      * @license
