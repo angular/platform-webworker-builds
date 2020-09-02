baeeeeffb8e02bc135fb6e7fd567f6a943abab90
Andrew Scott <atscott@google.com>
2020-09-02 22:12:13 +0000

    32f33f095f fix(localize): render context of translation file parse errors (#38673)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0
+ * @license Angular v10.1.0+2.sha-32f33f0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0');
+const VERSION = new Version('10.1.0+2.sha-32f33f0');
 
 /**
  * @license
