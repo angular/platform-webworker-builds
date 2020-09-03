0139e220a54f1bb0854818d1f2a750086eae6a3c
atscott <atscott01@gmail.com>
2020-09-03 19:23:45 +0000

    25afbcc459 docs: add dayjs date adapter to resources page (#38031)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+6.sha-29c89c9
+ * @license Angular v10.1.0+7.sha-25afbcc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0+6.sha-29c89c9');
+    var VERSION = new core.Version('10.1.0+7.sha-25afbcc');
 
     /**
      * @license
