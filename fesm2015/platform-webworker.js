e8521d5f487a8e97003e203f314a9cc3bd5356e1
atscott <atscott01@gmail.com>
2020-09-08 17:33:57 +0000

    d9da7e5a18 docs: fix result of sanitization example (#38724)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+22.sha-9f28f82
+ * @license Angular v11.0.0-next.0+19.sha-d9da7e5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+22.sha-9f28f82');
+const VERSION = new Version('11.0.0-next.0+19.sha-d9da7e5');
 
 /**
  * @license
