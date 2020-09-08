64a59a7616e3a813e50bb19b82f1d7d7cc17882d
Andrew Scott <atscott@google.com>
2020-09-08 16:34:43 +0000

    73001b42fe docs: remove reverted bug fix from 10.1 change log (#38718)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+12.sha-c90eb54
+ * @license Angular v11.0.0-next.0+13.sha-73001b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.0+12.sha-c90eb54');
+    var VERSION = new core.Version('11.0.0-next.0+13.sha-73001b4');
 
     /**
      * @license
