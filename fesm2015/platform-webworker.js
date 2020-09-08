d166f98e5425e1294927bbc847627192e2f5ccf0
atscott <atscott01@gmail.com>
2020-09-08 21:35:29 +0000

    b0c79f2373 docs: Describe a scenario in which ngOnChanges is not called before ngOnInit. (#38625)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+39.sha-bfb7eec
+ * @license Angular v11.0.0-next.0+41.sha-b0c79f2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+39.sha-bfb7eec');
+const VERSION = new Version('11.0.0-next.0+41.sha-b0c79f2');
 
 /**
  * @license
