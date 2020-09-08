77879c2181479500c5584ec5d205f7d115d3bd0e
atscott <atscott01@gmail.com>
2020-09-08 17:30:02 +0000

    3e57ca1d98 docs: fix typos in deployment guide (#38727)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+18.sha-79d8795
+ * @license Angular v11.0.0-next.0+17.sha-3e57ca1
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+18.sha-79d8795');
+    var VERSION = new i0.Version('11.0.0-next.0+17.sha-3e57ca1');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
