7cdb095aa72acf7cd52d50867e3d795d7b24134a
atscott <atscott01@gmail.com>
2020-09-08 19:11:10 +0000

    7869de6136 fix(ngcc): use aliased exported types correctly (#38666)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+27.sha-92ff6d9
+ * @license Angular v11.0.0-next.0+29.sha-7869de6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+27.sha-92ff6d9');
+    var VERSION = new i0.Version('11.0.0-next.0+29.sha-7869de6');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
