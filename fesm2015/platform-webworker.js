1fe143cf44f89e89bfa295ebfbbb3170dee85bf9
Andrew Scott <atscott@google.com>
2020-09-08 16:35:30 +0000

    0a277c6c40 docs: remove reverted bug fix from 10.1 change log (#38718)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+9.sha-9bf32c4
+ * @license Angular v10.1.0+10.sha-0a277c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.0+9.sha-9bf32c4');
+const VERSION = new Version('10.1.0+10.sha-0a277c6');
 
 /**
  * @license
