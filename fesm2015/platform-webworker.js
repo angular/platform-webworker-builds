33c9c10194133e8a7f0abc617a1fcbbf1c7f0e45
atscott <atscott01@gmail.com>
2020-09-08 17:31:26 +0000

    c2d017de83 docs: word correction (#38729) PR Close #38729

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+23.sha-a29f9a9
+ * @license Angular v11.0.0-next.0+16.sha-c2d017d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+23.sha-a29f9a9');
+const VERSION = new Version('11.0.0-next.0+16.sha-c2d017d');
 
 /**
  * @license
