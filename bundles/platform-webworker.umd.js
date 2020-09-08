785f0782bf274dcfdc8b52e12cbac4d6cd9845a7
atscott <atscott01@gmail.com>
2020-09-08 23:01:31 +0000

    e156e29edd perf(compiler-cli): optimize computation of type-check scope information (#38749)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+34.sha-190dca0
+ * @license Angular v10.1.0+36.sha-e156e29
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0+34.sha-190dca0');
+    var VERSION = new core.Version('10.1.0+36.sha-e156e29');
 
     /**
      * @license
