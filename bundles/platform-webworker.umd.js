7edd77c84bc1048e67c241f85873f2bb18ad9eea
atscott <atscott01@gmail.com>
2020-09-08 17:32:02 +0000

    261f689e9b docs: remove duplicate trans-unit element closing tag (#38715)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+19.sha-d9da7e5
+ * @license Angular v11.0.0-next.0+21.sha-261f689
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.0+19.sha-d9da7e5');
+    var VERSION = new core.Version('11.0.0-next.0+21.sha-261f689');
 
     /**
      * @license
