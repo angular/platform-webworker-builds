939dd95b647afc1e57240cd1f69cfd6a7001938e
atscott <atscott01@gmail.com>
2020-09-08 22:16:54 +0000

    ba95b79a21 perf(compiler-cli): optimize computation of type-check scope information (#38539)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+43.sha-4360eed
+ * @license Angular v11.0.0-next.0+45.sha-ba95b79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+43.sha-4360eed');
+    var VERSION = new i0.Version('11.0.0-next.0+45.sha-ba95b79');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
