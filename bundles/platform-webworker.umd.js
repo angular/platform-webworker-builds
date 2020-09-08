f4bcc8de6e3ef7d3d1a28a94fbc46034b3af7086
atscott <atscott01@gmail.com>
2020-09-08 19:13:37 +0000

    a1c34c6f0a fix(compiler): correct confusion between field and property names (#38685)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.0+20.sha-5863537
+ * @license Angular v10.1.0+26.sha-a1c34c6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.0+20.sha-5863537');
+    var VERSION = new core.Version('10.1.0+26.sha-a1c34c6');
 
     /**
      * @license
