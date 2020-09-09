6ec66bc4fd40c211077afe94bfb1aaa26cf2fb30
atscott <atscott01@gmail.com>
2020-09-09 20:48:20 +0000

    886f58d4fe docs: release notes for the v10.1.1 release

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+55.sha-18f84a0
+ * @license Angular v11.0.0-next.0+56.sha-886f58d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+55.sha-18f84a0');
+    var VERSION = new i0.Version('11.0.0-next.0+56.sha-886f58d');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
