4db7815100344f5bce459bac9c81dbe775d94901
atscott <atscott01@gmail.com>
2020-09-09 16:32:18 +0000

    3cb2a79399 docs: update v9 support status to LTS (#38744)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+45.sha-ba95b79
+ * @license Angular v11.0.0-next.0+49.sha-3cb2a79
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+45.sha-ba95b79');
+    var VERSION = new i0.Version('11.0.0-next.0+49.sha-3cb2a79');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
