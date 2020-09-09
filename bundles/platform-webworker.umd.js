575ee8390ad03417b80825e9881783539c6569d5
atscott <atscott01@gmail.com>
2020-09-09 19:48:22 +0000

    18f84a0328 Revert "perf(compiler-cli): only emit directive/pipe references that are used (#38539)" (#38765)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+53.sha-3d77b64
+ * @license Angular v11.0.0-next.0+55.sha-18f84a0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.0+53.sha-3d77b64');
+    var VERSION = new i0.Version('11.0.0-next.0+55.sha-18f84a0');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
