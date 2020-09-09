50d716cf9962856b2a8defcd45c5a05d6795aa4a
atscott <atscott01@gmail.com>
2020-09-09 16:33:36 +0000

    6d9bfb8368 docs(router): fixed PreloadAllModules comment typo (#38758) PR Close #38758

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.0+47.sha-19a4843
+ * @license Angular v11.0.0-next.0+51.sha-6d9bfb8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.0+47.sha-19a4843');
+const VERSION = new Version('11.0.0-next.0+51.sha-6d9bfb8');
 
 /**
  * @license
