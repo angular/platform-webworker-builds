a03008334c48b6a155f8353af08916f7473de75b
atscott <atscott01@gmail.com>
2020-09-09 23:48:49 +0000

    c6ebb77cec test(language-service): [ivy] remove all markers from test (#38777)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+2.sha-f942118
+ * @license Angular v11.0.0-next.1+1.sha-c6ebb77
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+2.sha-f942118');
+    var VERSION = new i0.Version('11.0.0-next.1+1.sha-c6ebb77');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
