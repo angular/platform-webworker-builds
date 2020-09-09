a2910b1c38a6b49559e1e999e253704bcf2f7dfe
atscott <atscott01@gmail.com>
2020-09-09 23:56:27 +0000

    1373a98e25 feat(dev-infra): Allow local user ng-dev configuration to disable commit message wizard (#38701)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+3.sha-3c4b8b9
+ * @license Angular v11.0.0-next.1+5.sha-1373a98
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+3.sha-3c4b8b9');
+    var VERSION = new i0.Version('11.0.0-next.1+5.sha-1373a98');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
