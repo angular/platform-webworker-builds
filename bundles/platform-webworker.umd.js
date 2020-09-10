e9c653d43e9cc549084accb341d0a0d074fe3083
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:54:39 +0000

    03447ba52f docs: Move Displaying data topic and ToH tutorial (#38774)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+19.sha-19598b4
+ * @license Angular v11.0.0-next.1+21.sha-03447ba
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.1+19.sha-19598b4');
+    var VERSION = new core.Version('11.0.0-next.1+21.sha-03447ba');
 
     /**
      * @license
