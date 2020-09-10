4539dc89b61a142a1bc8dcc098c37b3e60d2f663
Andrew Kushnir <akushnir@google.com>
2020-09-10 21:58:39 +0000

    2d52c80332 test(compiler): Add back tests for renamed inputs and outputs (#38798)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+20.sha-8f349b2
+ * @license Angular v11.0.0-next.1+22.sha-2d52c80
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+20.sha-8f349b2');
+const VERSION = new Version('11.0.0-next.1+22.sha-2d52c80');
 
 /**
  * @license
