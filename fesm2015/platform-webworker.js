e2cddaf1cf3c27128787d161c706b7d7a7d25ffe
Andrew Kushnir <akushnir@google.com>
2020-09-10 16:34:26 +0000

    edb7f90363 fix(core): clear the `RefreshTransplantedView` when detached (#38768)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+5.sha-7669bd8
+ * @license Angular v10.1.1+9.sha-edb7f90
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+5.sha-7669bd8');
+const VERSION = new Version('10.1.1+9.sha-edb7f90');
 
 /**
  * @license
