8282993492099d5d26ce488acac265e4ae6f3193
Andrew Kushnir <akushnir@google.com>
2020-09-10 22:59:02 +0000

    6acea54f62 fix(common): mark locale data arrays as readonly (#30397)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+22.sha-2d52c80
+ * @license Angular v11.0.0-next.1+23.sha-6acea54
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+22.sha-2d52c80');
+const VERSION = new Version('11.0.0-next.1+23.sha-6acea54');
 
 /**
  * @license
