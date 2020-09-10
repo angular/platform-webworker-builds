4d93c2b09e370d5cc020aff763288587ae1e3cde
Andrew Kushnir <akushnir@google.com>
2020-09-10 22:00:49 +0000

    8f349b2375 fix(compiler): source span for microsyntax text att should be key span (#38766)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+21.sha-03447ba
+ * @license Angular v11.0.0-next.1+20.sha-8f349b2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+21.sha-03447ba');
+    var VERSION = new i0.Version('11.0.0-next.1+20.sha-8f349b2');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
