c2e271c4c4118194c12d56807bc737e13c355b81
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:22:16 +0000

    a47383d1e8 fix(localize): ensure that `formatOptions` is optional (#38787)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+11.sha-9078187
+ * @license Angular v10.1.1+12.sha-a47383d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.1+11.sha-9078187');
+    var VERSION = new core.Version('10.1.1+12.sha-a47383d');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+11.sha-9078187
+ * @license Angular v10.1.1+12.sha-a47383d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+11.sha-9078187');
+const VERSION = new Version('10.1.1+12.sha-a47383d');
 
 /**
  * @license
