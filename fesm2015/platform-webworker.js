b15e11e70f347c3be294642f35a4d7f1341249c6
Andrew Kushnir <akushnir@google.com>
2020-09-10 20:10:55 +0000

    19598b47ca feat(compiler-cli): add ability to get symbol of reference or variable (#38618)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+13.sha-26f2820
+ * @license Angular v11.0.0-next.1+19.sha-19598b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+13.sha-26f2820');
+const VERSION = new Version('11.0.0-next.1+19.sha-19598b4');
 
 /**
  * @license
