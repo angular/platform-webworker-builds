b4ccffed7ee4e106c203e3a9d25e08e58c3026f7
Andrew Kushnir <akushnir@google.com>
2020-09-10 18:20:30 +0000

    26f28200bf fix(common): do not round up fractions of a millisecond in `DatePipe` (#38009)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+12.sha-ce1efc1
+ * @license Angular v11.0.0-next.1+13.sha-26f2820
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+12.sha-ce1efc1');
+const VERSION = new Version('11.0.0-next.1+13.sha-26f2820');
 
 /**
  * @license
