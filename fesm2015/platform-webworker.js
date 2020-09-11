26d23ac1118252710c7bc7f9461b8ad46db5fca6
Andrew Kushnir <akushnir@google.com>
2020-09-11 19:31:23 +0000

    15207e3c9c fix(compiler): detect pipes in ICUs in template binder (#38810)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+26.sha-66129f8
+ * @license Angular v11.0.0-next.1+27.sha-15207e3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+26.sha-66129f8');
+const VERSION = new Version('11.0.0-next.1+27.sha-15207e3');
 
 /**
  * @license
