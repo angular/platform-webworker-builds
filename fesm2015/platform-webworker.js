af5fb35d04db73ee0f38a6852e8c28bfc566d7f6
Andrew Kushnir <akushnir@google.com>
2020-09-11 21:22:57 +0000

    aa43cbf8c5 fix(dev-infra): correct build setup for dev-infra (#38815)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+18.sha-b05d79d
+ * @license Angular v10.1.1+19.sha-aa43cbf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+18.sha-b05d79d');
+const VERSION = new Version('10.1.1+19.sha-aa43cbf');
 
 /**
  * @license
