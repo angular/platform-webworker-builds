ad3cd143a7327fb4de02a7c261fea228ec3106fe
Andrew Kushnir <akushnir@google.com>
2020-09-11 16:06:50 +0000

    8096c63c66 docs: clarify what fixes are merged to LTS versions (#38788)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+13.sha-31d0ee4
+ * @license Angular v10.1.1+15.sha-8096c63
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.1+13.sha-31d0ee4');
+const VERSION = new Version('10.1.1+15.sha-8096c63');
 
 /**
  * @license
