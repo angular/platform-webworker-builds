fa6fcc0727d3d3d53184a2c4af975d9c7a03593a
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:56:21 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+34.sha-297c060
+ * @license Angular v11.0.0-next.1+35.sha-3934f0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+34.sha-297c060');
+    var VERSION = new i0.Version('11.0.0-next.1+35.sha-3934f0a');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
