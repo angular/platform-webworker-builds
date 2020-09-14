0cea425c55f6e96f996846fe657a44ac551a1738
Andrew Kushnir <akushnir@google.com>
2020-09-14 21:53:33 +0000

    3934f0a833 docs(elements): convert the `ng add` command to code-example (#38834)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+34.sha-297c060
+ * @license Angular v11.0.0-next.1+35.sha-3934f0a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.1+34.sha-297c060');
+    var VERSION = new core.Version('11.0.0-next.1+35.sha-3934f0a');
 
     /**
      * @license
