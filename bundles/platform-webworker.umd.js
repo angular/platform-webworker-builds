c9a9bef8579013bab0b94761c7f86d343252d73e
Andrew Kushnir <akushnir@google.com>
2020-09-14 16:28:49 +0000

    e162da0753 refactor(dev-infra): do not validate config file multiple times (#38808)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+31.sha-354138e
+ * @license Angular v11.0.0-next.1+32.sha-e162da0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+31.sha-354138e');
+    var VERSION = new i0.Version('11.0.0-next.1+32.sha-e162da0');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
