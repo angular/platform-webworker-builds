c74eb09b64d5828fc8f24f958fbdb4f3f63d0b2b
Andrew Kushnir <akushnir@google.com>
2020-09-15 01:29:12 +0000

    593bd594e3 build: create temporary script for symbol extractor tests (#38819)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+36.sha-78e1ecb
+ * @license Angular v11.0.0-next.1+38.sha-593bd59
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+36.sha-78e1ecb');
+    var VERSION = new i0.Version('11.0.0-next.1+38.sha-593bd59');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
