8a19b1c6a191147979483bcb25f54808283ea8c5
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:55:05 +0000

    3817e5f1df fix(router): Fix arguments order for call to shouldReuseRoute (#26949)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+44.sha-171a0d0
+ * @license Angular v11.0.0-next.1+45.sha-3817e5f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.1+44.sha-171a0d0');
+    var VERSION = new core.Version('11.0.0-next.1+45.sha-3817e5f');
 
     /**
      * @license
