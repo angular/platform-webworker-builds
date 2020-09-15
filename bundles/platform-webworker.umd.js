ff13c0238823d4e78b4b61dd5540a5eb9ecc1301
Andrew Kushnir <akushnir@google.com>
2020-09-15 18:55:28 +0000

    171a0d0696 docs: add ngc to glossary (#36781)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+43.sha-a1c1c45
+ * @license Angular v11.0.0-next.1+44.sha-171a0d0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+43.sha-a1c1c45');
+    var VERSION = new i0.Version('11.0.0-next.1+44.sha-171a0d0');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
