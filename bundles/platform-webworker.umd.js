a348788ea751017212cbef91d94249d832903920
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:30:27 +0000

    d192c87f6a refactor(dev-infra): refactor commit-message files (#38845)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+45.sha-3817e5f
+ * @license Angular v11.0.0-next.1+46.sha-d192c87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.1+45.sha-3817e5f');
+    var VERSION = new i0.Version('11.0.0-next.1+46.sha-d192c87');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
