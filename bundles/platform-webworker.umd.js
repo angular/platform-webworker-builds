19669693a8fb49f24340e40c5b99ce975587611e
Andrew Kushnir <akushnir@google.com>
2020-09-15 16:09:10 +0000

    e00535a2a4 fix(dev-infra): remove ANSI escape codes from log file outputs (#38792)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+38.sha-593bd59
+ * @license Angular v11.0.0-next.1+39.sha-e00535a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.1+38.sha-593bd59');
+    var VERSION = new core.Version('11.0.0-next.1+39.sha-e00535a');
 
     /**
      * @license
