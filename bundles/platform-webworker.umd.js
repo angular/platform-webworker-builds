a1f2e5575297f698f0f3691a2c264266c6abc6cc
Andrew Kushnir <akushnir@google.com>
2020-09-15 23:27:52 +0000

    d192c87f6a refactor(dev-infra): refactor commit-message files (#38845)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+45.sha-3817e5f
+ * @license Angular v11.0.0-next.1+46.sha-d192c87
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.1+45.sha-3817e5f');
+    var VERSION = new core.Version('11.0.0-next.1+46.sha-d192c87');
 
     /**
      * @license
