1a05bc968f8667b9cc3eea360c2dfcffb8e530a7
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:43:00 +0000

    e64c0c3730 fix(dev-infra): handle no caretaker config being defined (#38862)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.1+33.sha-3d94919
+ * @license Angular v10.1.2+9.sha-e64c0c3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.1+33.sha-3d94919');
+    var VERSION = new core.Version('10.1.2+9.sha-e64c0c3');
 
     /**
      * @license
