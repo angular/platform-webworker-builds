be21cce93d48e383ea14859c89a25a144cd96ab6
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:51:36 +0000

    dd8d8c8289 fix(common): add `params` and `reportProgress` options to `HttpClient.put()` overload (#37873)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+11.sha-722699f
+ * @license Angular v11.0.0-next.2+13.sha-dd8d8c8
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+11.sha-722699f');
+const VERSION = new Version('11.0.0-next.2+13.sha-dd8d8c8');
 
 /**
  * @license
