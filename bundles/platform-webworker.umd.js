c71ca391335bbbb593b5b6234ed85e14ce838b3e
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:45:13 +0000

    49ee90b1b5 docs: fix typo in architecture.md guide (#38853)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+9.sha-e62a918
+ * @license Angular v11.0.0-next.2+8.sha-49ee90b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+9.sha-e62a918');
+    var VERSION = new core.Version('11.0.0-next.2+8.sha-49ee90b');
 
     /**
      * @license
