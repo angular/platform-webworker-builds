53993bf62452448267361c31780f90e0fcc0df6a
Joey Perrott <josephperrott@gmail.com>
2020-09-16 22:18:45 +0000

    85a2626620 docs: release notes for the v10.1.2 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.1+46.sha-d192c87
+ * @license Angular v11.0.0-next.1+47.sha-85a2626
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.1+46.sha-d192c87');
+const VERSION = new Version('11.0.0-next.1+47.sha-85a2626');
 
 /**
  * @license
