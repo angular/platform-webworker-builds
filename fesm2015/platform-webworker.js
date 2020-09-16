77214f3a773f25d9391517e3bf01d9a3aa70ea13
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:55:25 +0000

    129107191c refactor(compiler): always return a mutable clone from `Scope#resolve` (#38857)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+13.sha-dd8d8c8
+ * @license Angular v11.0.0-next.2+12.sha-1291071
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+13.sha-dd8d8c8');
+const VERSION = new Version('11.0.0-next.2+12.sha-1291071');
 
 /**
  * @license
