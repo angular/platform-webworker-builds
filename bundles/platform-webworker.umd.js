c7f0795ab7a4382f1b3bf78f5ee58649bd6aef6b
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:47:48 +0000

    49ee90b1b5 docs: fix typo in architecture.md guide (#38853)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+9.sha-e62a918
+ * @license Angular v11.0.0-next.2+8.sha-49ee90b
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.2+9.sha-e62a918');
+    var VERSION = new i0.Version('11.0.0-next.2+8.sha-49ee90b');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
