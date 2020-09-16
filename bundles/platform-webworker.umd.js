4f91ea7967746ed5cc41b564793800edd41c734f
Andrew Kushnir <akushnir@google.com>
2020-09-16 22:39:04 +0000

    97adc27207 docs(bazel): fix typo in BUILD.bazel comments (#38054)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+2.sha-4407449
+ * @license Angular v11.0.0-next.2+6.sha-97adc27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+2.sha-4407449');
+    var VERSION = new core.Version('11.0.0-next.2+6.sha-97adc27');
 
     /**
      * @license
