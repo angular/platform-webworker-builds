b4eec9ea7c37ba5225ab86fd47151172006680a7
Misko Hevery <misko@hevery.com>
2020-09-17 16:56:21 +0000

    a33d630a21 fix(zone.js): should have better backward compatibilities (#38797)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+12.sha-1291071');
+    var VERSION = new core.Version('11.0.0-next.2+15.sha-a33d630');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+12.sha-1291071
+ * @license Angular v11.0.0-next.2+15.sha-a33d630
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+12.sha-1291071');
+const VERSION = new Version('11.0.0-next.2+15.sha-a33d630');
 
 /**
  * @license
