f7b340cb9f6e5b92f0f51eb2e116817c243e6067
Misko Hevery <misko@hevery.com>
2020-09-18 18:46:15 +0000

    2d6105a784 feat(dev-infra): output the number of new and fixed cycles (#38805)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+22.sha-5548571
+ * @license Angular v11.0.0-next.2+25.sha-2d6105a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.2+22.sha-5548571');
+    var VERSION = new i0.Version('11.0.0-next.2+25.sha-2d6105a');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
