957e0229398e891f61bc947f289b9612c0457419
Misko Hevery <misko@hevery.com>
2020-09-18 18:43:37 +0000

    2d6105a784 feat(dev-infra): output the number of new and fixed cycles (#38805)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+22.sha-5548571
+ * @license Angular v11.0.0-next.2+25.sha-2d6105a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+22.sha-5548571');
+const VERSION = new Version('11.0.0-next.2+25.sha-2d6105a');
 
 /**
  * @license
