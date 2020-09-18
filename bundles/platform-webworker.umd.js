11d67da20402a8016ba70ed495e277a0d4440997
Misko Hevery <misko@hevery.com>
2020-09-18 15:28:52 +0000

    02c6bff9cf Update aio/content/marketing/events.json (#38874)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+19.sha-d795a00
+ * @license Angular v11.0.0-next.2+17.sha-02c6bff
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+19.sha-d795a00');
+    var VERSION = new core.Version('11.0.0-next.2+17.sha-02c6bff');
 
     /**
      * @license
