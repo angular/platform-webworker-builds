c9c760e265e8dc0174cb7a593547cad67b964c8d
Misko Hevery <misko@hevery.com>
2020-09-19 00:10:14 +0000

    b68fab547a ci: do not require g3 checks for the changes in `packages/zone.js/dist` folder (#38901)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+23.sha-d92a0dd
+ * @license Angular v11.0.0-next.2+28.sha-b68fab5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+23.sha-d92a0dd');
+const VERSION = new Version('11.0.0-next.2+28.sha-b68fab5');
 
 /**
  * @license
