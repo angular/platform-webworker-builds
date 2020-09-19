53b4e9a4792ac4b2ebd998b07db2d42de0d68183
Misko Hevery <misko@hevery.com>
2020-09-19 00:29:14 +0000

    a91f0f6b82 refactor(compiler): refactor ast spans test to be more human readable (#38902)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+28.sha-b68fab5
+ * @license Angular v11.0.0-next.2+30.sha-a91f0f6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+28.sha-b68fab5');
+const VERSION = new Version('11.0.0-next.2+30.sha-a91f0f6');
 
 /**
  * @license
