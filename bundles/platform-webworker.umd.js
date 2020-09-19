571debb750a17a691790b077693d45fd5e8a57cb
Misko Hevery <misko@hevery.com>
2020-09-19 00:18:56 +0000

    7d5b5153cf docs: Grammar fixes (#38900)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.2+21.sha-3902ec0
+ * @license Angular v10.1.2+24.sha-7d5b515
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.2+21.sha-3902ec0');
+    var VERSION = new core.Version('10.1.2+24.sha-7d5b515');
 
     /**
      * @license
