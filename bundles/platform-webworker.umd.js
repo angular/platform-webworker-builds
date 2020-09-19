206d3f79bdd8846a06df860b39e204a0aeade020
Misko Hevery <misko@hevery.com>
2020-09-19 00:56:18 +0000

    d5ddb9f340 docs: Grammar fixes (#38900)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+30.sha-a91f0f6
+ * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.2+30.sha-a91f0f6');
+    var VERSION = new i0.Version('11.0.0-next.2+29.sha-d5ddb9f');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
