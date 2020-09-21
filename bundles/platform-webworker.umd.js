5e2a6ef6ef2796d9bc45d81aabf49ed286577a94
Misko Hevery <misko@hevery.com>
2020-09-21 19:51:49 +0000

    fdd4fa0009 docs: change wrong default app module by ng new (#38549)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+32.sha-f3f6a42
+ * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.2+32.sha-f3f6a42');
+    var VERSION = new i0.Version('11.0.0-next.2+36.sha-fdd4fa0');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+32.sha-f3f6a42
+ * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+32.sha-f3f6a42');
+const VERSION = new Version('11.0.0-next.2+36.sha-fdd4fa0');
 
 /**
  * @license
