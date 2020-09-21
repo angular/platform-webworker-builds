3c83d7252b73ccf345405b5fa6e5ba09e7af5b9c
Misko Hevery <misko@hevery.com>
2020-09-21 19:54:39 +0000

    dfbfabc052 fix(docs-infra): remove scrollbar styles for accessibility (#38852)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+35.sha-984ed39
+ * @license Angular v11.0.0-next.2+33.sha-dfbfabc
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.2+35.sha-984ed39');
+    var VERSION = new i0.Version('11.0.0-next.2+33.sha-dfbfabc');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
