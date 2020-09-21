4140f9e543d905b79154f3d8b3c5e908adc20359
Misko Hevery <misko@hevery.com>
2020-09-21 23:55:44 +0000

    3082f7378b test(compiler-cli): make typescript_ast_factory_spec tests resilient to line-endings (#38925)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+47.sha-297b123
+ * @license Angular v11.0.0-next.2+48.sha-3082f73
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+47.sha-297b123');
+const VERSION = new Version('11.0.0-next.2+48.sha-3082f73');
 
 /**
  * @license
