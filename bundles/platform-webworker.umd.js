db8aa8c35f3260521df3cb57bb8d93d4072b1cb5
Misko Hevery <misko@hevery.com>
2020-09-21 19:53:25 +0000

    0c0c54d615 refactor(compiler): simplify visitor logic for attributes (#38899)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+33.sha-dfbfabc
+ * @license Angular v11.0.0-next.2+34.sha-0c0c54d
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+33.sha-dfbfabc');
+    var VERSION = new core.Version('11.0.0-next.2+34.sha-0c0c54d');
 
     /**
      * @license
