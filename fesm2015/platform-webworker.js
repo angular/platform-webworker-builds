188ad4f5ffed37d93ee41f192163689e11f2f837
Misko Hevery <misko@hevery.com>
2020-09-21 19:52:56 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
+ * @license Angular v11.0.0-next.2+37.sha-2b1b718
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.2+36.sha-fdd4fa0');
+const VERSION = new Version('11.0.0-next.2+37.sha-2b1b718');
 
 /**
  * @license
