7250f71f5efc9aa2894c4d9fe9c10512d81aa1e0
Misko Hevery <misko@hevery.com>
2020-09-21 19:50:11 +0000

    2b1b7180db fix(forms): type NG_VALUE_ACCESSOR injection token as array (#29723)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+36.sha-fdd4fa0
+ * @license Angular v11.0.0-next.2+37.sha-2b1b718
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+36.sha-fdd4fa0');
+    var VERSION = new core.Version('11.0.0-next.2+37.sha-2b1b718');
 
     /**
      * @license
