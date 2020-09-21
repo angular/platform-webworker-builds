237e529b5e7d3d73feeb263abd181f6f163b1891
Misko Hevery <misko@hevery.com>
2020-09-21 16:56:57 +0000

    e498ea9b5a revert: feat(router): better warning message when a router outlet has not been instantiated (#38920)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+29.sha-d5ddb9f
+ * @license Angular v11.0.0-next.2+31.sha-e498ea9
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+29.sha-d5ddb9f');
+    var VERSION = new core.Version('11.0.0-next.2+31.sha-e498ea9');
 
     /**
      * @license
