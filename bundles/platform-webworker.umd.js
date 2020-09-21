f0b3ac6e4043004d8b6626223299ebe09fd5b39f
Misko Hevery <misko@hevery.com>
2020-09-21 19:57:40 +0000

    49f27e31ed test(compiler-cli): re-enable dynamic value diagnostic tests on Windows CI (#37782)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+34.sha-0c0c54d
+ * @license Angular v11.0.0-next.2+39.sha-49f27e3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.2+34.sha-0c0c54d');
+    var VERSION = new i0.Version('11.0.0-next.2+39.sha-49f27e3');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
