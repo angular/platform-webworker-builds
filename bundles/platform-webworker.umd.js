1da9005951bc9ce8c8ec2dc89956b286ad07e127
Misko Hevery <misko@hevery.com>
2020-09-22 22:34:18 +0000

    b1682526dd refactor(dev-infra): simplify runBenchmark (#38941)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+52.sha-ba3f4c2
+ * @license Angular v11.0.0-next.2+54.sha-b168252
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+52.sha-ba3f4c2');
+    var VERSION = new core.Version('11.0.0-next.2+54.sha-b168252');
 
     /**
      * @license
