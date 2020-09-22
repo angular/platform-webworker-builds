064a9552b8d31373eab6415983f5f87fde9a162e
Misko Hevery <misko@hevery.com>
2020-09-22 22:31:19 +0000

    ba3f4c26bb refactor(compiler): make `keySpan` available for `BoundAttributes` (#38898)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.2+50.sha-a206852
+ * @license Angular v11.0.0-next.2+52.sha-ba3f4c2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.2+50.sha-a206852');
+    var VERSION = new core.Version('11.0.0-next.2+52.sha-ba3f4c2');
 
     /**
      * @license
