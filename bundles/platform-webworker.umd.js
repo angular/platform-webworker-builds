16aa73eff2556f58d3173f4b538f9fcffb5517a8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-23 20:11:30 +0000

    f979914d4e test(platform-browser): remove usage of blacklist in test naming (#38928)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3
+ * @license Angular v11.0.0-next.3+4.sha-f979914
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.3');
+    var VERSION = new core.Version('11.0.0-next.3+4.sha-f979914');
 
     /**
      * @license
