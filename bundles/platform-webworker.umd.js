5e3f174eb85f73346cc314e8bb75937d0e6fa560
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:05:15 +0000

    19d543f71e fix(zone.js): disable wrap uncaught promise rejection should handle primitive value (#38476)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+2.sha-ffe89fb
+ * @license Angular v11.0.0-next.3+5.sha-19d543f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.3+2.sha-ffe89fb');
+    var VERSION = new i0.Version('11.0.0-next.3+5.sha-19d543f');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+2.sha-ffe89fb
+ * @license Angular v11.0.0-next.3+5.sha-19d543f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+2.sha-ffe89fb');
+const VERSION = new Version('11.0.0-next.3+5.sha-19d543f');
 
 /**
  * @license
