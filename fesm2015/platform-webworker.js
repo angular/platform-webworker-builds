6f1f25e3799a281e391a0389ebc2e247235533c9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:03:58 +0000

    8422633b8a test(docs-infra): add missing unit tests for backoff example (#38896)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+5.sha-19d543f
+ * @license Angular v11.0.0-next.3+7.sha-8422633
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+5.sha-19d543f');
+const VERSION = new Version('11.0.0-next.3+7.sha-8422633');
 
 /**
  * @license
