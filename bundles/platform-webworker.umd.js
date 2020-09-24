7ef06a1e72f5d22b9b32f7023ce06aaeaaa1632f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:02:36 +0000

    19d543f71e fix(zone.js): disable wrap uncaught promise rejection should handle primitive value (#38476)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+2.sha-ffe89fb
+ * @license Angular v11.0.0-next.3+5.sha-19d543f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.3+2.sha-ffe89fb');
+    var VERSION = new core.Version('11.0.0-next.3+5.sha-19d543f');
 
     /**
      * @license
