ccd134bf54ba2ae14cab5f4252923d7267a3301a
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-24 16:04:15 +0000

    4c8766573d refactor(language-service): Update hybrid visitor to use keySpan for bound attributes (#38955)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+7.sha-8422633
+ * @license Angular v11.0.0-next.3+8.sha-4c87665
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.3+7.sha-8422633');
+    var VERSION = new core.Version('11.0.0-next.3+8.sha-4c87665');
 
     /**
      * @license
