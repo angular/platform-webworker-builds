7d1ea1a7c7700117dbb6533fba4b644137d73f6d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 15:06:04 +0000

    e4f4d18e7e refactor(router): Adjust type of parameter in navigateByUrl and createUrlTree to be more accurate (#38227)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+10.sha-837889f
+ * @license Angular v11.0.0-next.3+11.sha-e4f4d18
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.3+10.sha-837889f');
+    var VERSION = new i0.Version('11.0.0-next.3+11.sha-e4f4d18');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
