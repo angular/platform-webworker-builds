4ce8c67ac788e46a2111504af5214bf91ed606a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 18:57:22 +0000

    cf9176eb46 docs(docs-infra): remove usage of whitelist in aio-builds-setup docs (#38964)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+18.sha-2f6777d
+ * @license Angular v11.0.0-next.3+19.sha-cf9176e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.3+18.sha-2f6777d');
+    var VERSION = new i0.Version('11.0.0-next.3+19.sha-cf9176e');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
