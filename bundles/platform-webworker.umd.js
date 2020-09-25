880478a16b8b4b5b09b0026f4925d18bc1bcf463
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-25 19:01:48 +0000

    e790c8547e test(compiler-cli): load test files into memory only once (#38909)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+17.sha-4ca1c73
+ * @license Angular v11.0.0-next.3+14.sha-e790c85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.3+17.sha-4ca1c73');
+    var VERSION = new i0.Version('11.0.0-next.3+14.sha-e790c85');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+17.sha-4ca1c73
+ * @license Angular v11.0.0-next.3+14.sha-e790c85
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1032,7 +1032,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+17.sha-4ca1c73');
+const VERSION = new Version('11.0.0-next.3+14.sha-e790c85');
 
 /**
  * @license
