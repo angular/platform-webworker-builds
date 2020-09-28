df11411da556976d95bbe621e9e35d57250d0b29
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:53:23 +0000

    c5e178997c build(docs-infra): update docs examples to Angular v10 (#38993)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+69.sha-4fd635a
+ * @license Angular v11.0.0-next.3+65.sha-c5e1789
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+69.sha-4fd635a');
+const VERSION = new Version('11.0.0-next.3+65.sha-c5e1789');
 
 /**
  * @license
