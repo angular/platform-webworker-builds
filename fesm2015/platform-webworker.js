5b898f574d4f9ca853fb41baaad6440ed14d32a9
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:48:24 +0000

    914d7099ee build: remove usage of blacklist in benchmark tooling (#38926)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+64.sha-cb7164a
+ * @license Angular v11.0.0-next.3+62.sha-914d709
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+64.sha-cb7164a');
+const VERSION = new Version('11.0.0-next.3+62.sha-914d709');
 
 /**
  * @license
