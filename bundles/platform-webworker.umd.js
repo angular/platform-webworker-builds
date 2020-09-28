84046a1d21c89163e22f89fbd1d6003c8b452ec8
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 21:02:37 +0000

    875fcfe5a9 ci: note Paul and Pawel as OOO in pullapprove (#39028)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.3+13.sha-5bf5513
+ * @license Angular v10.1.3+49.sha-875fcfe
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.3+13.sha-5bf5513');
+    var VERSION = new core.Version('10.1.3+49.sha-875fcfe');
 
     /**
      * @license
