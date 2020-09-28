e493c79cc4843d4a0c6525fe35eb7772310d314d
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:40:26 +0000

    f96dcc5ce0 feat(dev-infra): tool for staging and publishing releases (#38656)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+29.sha-4744c22
+ * @license Angular v11.0.0-next.3+46.sha-f96dcc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('11.0.0-next.3+29.sha-4744c22');
+    var VERSION = new core.Version('11.0.0-next.3+46.sha-f96dcc5');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+29.sha-4744c22
+ * @license Angular v11.0.0-next.3+46.sha-f96dcc5
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+29.sha-4744c22');
+const VERSION = new Version('11.0.0-next.3+46.sha-f96dcc5');
 
 /**
  * @license
