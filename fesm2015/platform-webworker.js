de06e194708454f0f57ada5d5449fa96dc58db4c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 21:00:18 +0000

    e2997ed5ee build(docs-infra): update TypeScript and other deps to align with latest CLI (#39017)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+72.sha-06525cf
+ * @license Angular v11.0.0-next.3+76.sha-e2997ed
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+72.sha-06525cf');
+const VERSION = new Version('11.0.0-next.3+76.sha-e2997ed');
 
 /**
  * @license
