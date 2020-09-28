44fda2d702cdbe9c9b60b6e7c37cdd957b294e1c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:55:44 +0000

    06525cfed3 test(compiler-cli): fix tests to have at least one component (#39011)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+77.sha-3ba97ab
+ * @license Angular v11.0.0-next.3+72.sha-06525cf
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+77.sha-3ba97ab');
+const VERSION = new Version('11.0.0-next.3+72.sha-06525cf');
 
 /**
  * @license
