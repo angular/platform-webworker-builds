382ca64cc2959e984b1dc8b88469865865c9d185
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:49:54 +0000

    cb7164a236 refactor(zone.js): rename BlacklistedStackFrames to InternalZoneJsStackFrames (#38978)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+46.sha-f96dcc5
+ * @license Angular v11.0.0-next.3+64.sha-cb7164a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1377,7 +1377,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new i0.Version('11.0.0-next.3+46.sha-f96dcc5');
+    var VERSION = new i0.Version('11.0.0-next.3+64.sha-cb7164a');
 
     var MessageBasedPlatformLocation = /** @class */ (function () {
         function MessageBasedPlatformLocation(_brokerFactory, _platformLocation, bus, _serializer) {
