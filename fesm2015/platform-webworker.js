0197af0c49cedbdff5aed4dbe501c3c3ce1e4c5c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-09-28 20:53:10 +0000

    4fd635a03a build(docs-infra): enable AOT in development mode for all docs examples (#39001)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v11.0.0-next.3+63.sha-4a9f452
+ * @license Angular v11.0.0-next.3+69.sha-4fd635a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('11.0.0-next.3+63.sha-4a9f452');
+const VERSION = new Version('11.0.0-next.3+69.sha-4fd635a');
 
 /**
  * @license
