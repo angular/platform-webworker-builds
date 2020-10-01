02d4ad911af1c296b80fff1a9a702e5a33a9cba8
Joey Perrott <josephperrott@gmail.com>
2020-10-01 16:58:09 +0000

    50e83e2566 refactor(docs-infra): leave TODO comments for aligning tslint with CLI (#39018)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+7.sha-bb11fd9
+ * @license Angular v10.1.4+14.sha-50e83e2
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.4+7.sha-bb11fd9');
+const VERSION = new Version('10.1.4+14.sha-50e83e2');
 
 /**
  * @license
