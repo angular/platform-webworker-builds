8d3d170a78e95f75c3a1b45f90c097ebd60fff38
Joey Perrott <josephperrott@gmail.com>
2020-10-02 22:37:50 +0000

    6791cd79af refactor(compiler): iteratively parse interpolations (#38977)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+21.sha-f50313f
+ * @license Angular v10.1.4+22.sha-6791cd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.4+21.sha-f50313f');
+    var VERSION = new core.Version('10.1.4+22.sha-6791cd7');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+21.sha-f50313f
+ * @license Angular v10.1.4+22.sha-6791cd7
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.4+21.sha-f50313f');
+const VERSION = new Version('10.1.4+22.sha-6791cd7');
 
 /**
  * @license
