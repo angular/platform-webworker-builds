c7834f8427558f6946ad1631d5e734142c248330
Joey Perrott <josephperrott@gmail.com>
2020-10-05 20:21:22 +0000

    072b707b38 ci: update to latest version of sauce-connect (#39073)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.4+24.sha-878e2f0
+ * @license Angular v10.1.4+27.sha-072b707
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.4+24.sha-878e2f0');
+const VERSION = new Version('10.1.4+27.sha-072b707');
 
 /**
  * @license
