299710ec2652c9ea3c0e9f8bce86b95dd01a768f
atscott <atscott01@gmail.com>
2020-10-09 17:29:20 +0000

    952fd8662c fix(elements): detect matchesSelector prototype without IIFE (#37799)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+8.sha-52a0c6b
+ * @license Angular v10.1.5+10.sha-952fd86
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.5+8.sha-52a0c6b');
+const VERSION = new Version('10.1.5+10.sha-952fd86');
 
 /**
  * @license
