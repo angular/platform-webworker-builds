add48b92125966b6f7d0fbab405cbaba53c77bc7
atscott <atscott01@gmail.com>
2020-10-13 22:23:02 +0000

    23fc2b43ac updated sajee's profile (#39019)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+18.sha-3487d5a
+ * @license Angular v10.1.5+20.sha-23fc2b4
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.5+18.sha-3487d5a');
+const VERSION = new Version('10.1.5+20.sha-23fc2b4');
 
 /**
  * @license
