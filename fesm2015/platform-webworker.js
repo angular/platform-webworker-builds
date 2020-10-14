d2d4db6bc4ab69a9b2799e6b5af102d3eb13a705
atscott <atscott01@gmail.com>
2020-10-14 21:05:14 +0000

    5db7b4c354 release: cut the v10.1.6 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+42.sha-e08d021
+ * @license Angular v10.1.6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.5+42.sha-e08d021');
+const VERSION = new Version('10.1.6');
 
 /**
  * @license
