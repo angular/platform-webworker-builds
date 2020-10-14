d9cac3883f3ee33b0c798ae3201b6bc33e5054ab
atscott <atscott01@gmail.com>
2020-10-14 20:23:30 +0000

    e08d02145e fix(ngcc): ensure that "inline exports" can be interpreted correctly (#39272)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.5+23.sha-df09415
+ * @license Angular v10.1.5+42.sha-e08d021
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.5+23.sha-df09415');
+const VERSION = new Version('10.1.5+42.sha-e08d021');
 
 /**
  * @license
