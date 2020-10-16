8edf7107170057e6de82ef052c969c621b41d04b
Andrew Kushnir <akushnir@google.com>
2020-10-16 22:11:38 +0000

    8fd59dabba ci: separate the windows CI tests into build and test (#39289)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+20.sha-d8e313b
+ * @license Angular v10.1.6+21.sha-8fd59da
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+20.sha-d8e313b');
+const VERSION = new Version('10.1.6+21.sha-8fd59da');
 
 /**
  * @license
