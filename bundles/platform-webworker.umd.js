d292ec39aa336907808a81a80526f55894821dfc
Andrew Kushnir <akushnir@google.com>
2020-10-16 00:23:33 +0000

    94d7ef3909 refactor(core): renames checkNoChangesMode to be clearer (#39277)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+14.sha-af17038
+ * @license Angular v10.1.6+16.sha-94d7ef3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.6+14.sha-af17038');
+    var VERSION = new core.Version('10.1.6+16.sha-94d7ef3');
 
     /**
      * @license
