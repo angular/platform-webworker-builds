4fce877329984d65db244e3b2253bbb9cfc45f2e
Andrew Kushnir <akushnir@google.com>
2020-10-16 23:31:52 +0000

    10db99588f refactor(docs-infra): fix `strictTemplates` failures in `accessibility` docs example (#39248)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+21.sha-8fd59da
+ * @license Angular v10.1.6+22.sha-10db995
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+21.sha-8fd59da');
+const VERSION = new Version('10.1.6+22.sha-10db995');
 
 /**
  * @license
