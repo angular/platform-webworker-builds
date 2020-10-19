8a709c8c443576a84d9e989c3a3e71f12634b9b8
Andrew Kushnir <akushnir@google.com>
2020-10-19 16:18:53 +0000

    38db976617 docs: add Johannes Hoppe to GDE resources (#34694)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+22.sha-10db995
+ * @license Angular v10.1.6+24.sha-38db976
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.6+22.sha-10db995');
+    var VERSION = new core.Version('10.1.6+24.sha-38db976');
 
     /**
      * @license
