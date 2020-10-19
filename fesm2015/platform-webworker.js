62addf68dcb1363b45543597f2dc4bfd7cf8983d
Andrew Kushnir <akushnir@google.com>
2020-10-19 18:48:41 +0000

    7827d3ee73 docs: change definition of NgModules in Angular concepts guide (#36179)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+24.sha-38db976
+ * @license Angular v10.1.6+25.sha-7827d3e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+24.sha-38db976');
+const VERSION = new Version('10.1.6+25.sha-7827d3e');
 
 /**
  * @license
