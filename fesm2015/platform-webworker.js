adf5f5bcede701630102b13a8e4255d4419d1254
Andrew Kushnir <akushnir@google.com>
2020-10-19 21:55:51 +0000

    02b87505ee docs: clarify grammatical error (#39279)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+26.sha-32a8e72
+ * @license Angular v10.1.6+27.sha-02b8750
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+26.sha-32a8e72');
+const VERSION = new Version('10.1.6+27.sha-02b8750');
 
 /**
  * @license
