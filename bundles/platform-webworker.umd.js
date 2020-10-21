4e400c574316f3e18f73d67ed61d40e274305d3f
Andrew Kushnir <akushnir@google.com>
2020-10-21 16:26:27 +0000

    7ca17a6249 build(docs-infra): upgrade cli command docs sources to cba6d86ca (#39360)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+29.sha-823200c
+ * @license Angular v10.1.6+32.sha-7ca17a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.1.6+29.sha-823200c');
+    var VERSION = new core.Version('10.1.6+32.sha-7ca17a6');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.1.6+29.sha-823200c
+ * @license Angular v10.1.6+32.sha-7ca17a6
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.1.6+29.sha-823200c');
+const VERSION = new Version('10.1.6+32.sha-7ca17a6');
 
 /**
  * @license
