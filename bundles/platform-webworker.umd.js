a896312d1b4eed2a3f38e5b8a5defbd1126eb99f
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-10-27 22:23:06 +0000

    055f7ebecb ci: bust cache of stored node_modules on CircleCI (#39461)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.0+17.sha-308b930
+ * @license Angular v10.2.0+31.sha-055f7eb
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.2.0+17.sha-308b930');
+    var VERSION = new core.Version('10.2.0+31.sha-055f7eb');
 
     /**
      * @license
