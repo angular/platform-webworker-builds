19440bef1513a0dffb423b2f961402f4a3edcd07
Joey Perrott <josephperrott@gmail.com>
2020-10-31 01:27:13 +0000

    71d0063285 fix(compiler-cli): report missing pipes when `fullTemplateTypeCheck` is disabled (#39320)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+14.sha-85d5242
+ * @license Angular v10.2.1+15.sha-71d0063
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+14.sha-85d5242');
+const VERSION = new Version('10.2.1+15.sha-71d0063');
 
 /**
  * @license
