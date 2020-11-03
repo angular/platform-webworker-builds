de208d54cb38bc42f9d9dfb0b4390a4c7a267b81
Joey Perrott <josephperrott@gmail.com>
2020-11-03 22:12:55 +0000

    aa3bd27cbf build: update dev dependency on @angular/cli to 10.2.0 (#39553)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+15.sha-71d0063
+ * @license Angular v10.2.1+29.sha-aa3bd27
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+15.sha-71d0063');
+const VERSION = new Version('10.2.1+29.sha-aa3bd27');
 
 /**
  * @license
