61e508c1e0e77779554f1bb334a9c903c72ee58d
Joey Perrott <josephperrott@gmail.com>
2020-11-04 00:58:53 +0000

    7ed0f0f7d0 fix(dev-infra): run caretaker checks asyncronously (#39086)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+30.sha-250858a
+ * @license Angular v10.2.1+32.sha-7ed0f0f
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.2.1+30.sha-250858a');
+    var VERSION = new core.Version('10.2.1+32.sha-7ed0f0f');
 
     /**
      * @license
