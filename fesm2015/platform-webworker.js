0f26f7c4378b0f57e28b916eddfbb7b2a66a577b
Joey Perrott <josephperrott@gmail.com>
2020-11-04 20:06:53 +0000

    8929355a9d docs: fix typos in the "Attribute Binding" guide (#39562)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.1+32.sha-7ed0f0f
+ * @license Angular v10.2.1+33.sha-8929355
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.1+32.sha-7ed0f0f');
+const VERSION = new Version('10.2.1+33.sha-8929355');
 
 /**
  * @license
