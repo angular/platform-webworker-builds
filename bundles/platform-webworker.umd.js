21f84e61a19b2f6565b437fdd34cf4e1e6cc8302
Misko Hevery <misko@hevery.com>
2020-11-06 17:48:57 +0000

    1c6cf8a8c2 fix(compiler-cli): do not drop non-Angular decorators when downleveling (#39577)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+1.sha-08e077c
+ * @license Angular v10.2.2+7.sha-1c6cf8a
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.2.2+1.sha-08e077c');
+    var VERSION = new core.Version('10.2.2+7.sha-1c6cf8a');
 
     /**
      * @license
