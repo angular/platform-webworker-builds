de695bdbbaab4924199596f105079980f728694f
Misko Hevery <misko@hevery.com>
2020-11-09 16:03:42 +0000

    ae71b76089 ci: log commands output when deploying angular.io to Firebase (#39596)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+8.sha-861e4fa
+ * @license Angular v10.2.2+10.sha-ae71b76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.2.2+8.sha-861e4fa');
+    var VERSION = new core.Version('10.2.2+10.sha-ae71b76');
 
     /**
      * @license
--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+8.sha-861e4fa
+ * @license Angular v10.2.2+10.sha-ae71b76
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.2+8.sha-861e4fa');
+const VERSION = new Version('10.2.2+10.sha-ae71b76');
 
 /**
  * @license
