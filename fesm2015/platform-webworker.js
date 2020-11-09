179d7d512a6ae842b96e00c3762339bcb352fd17
Misko Hevery <misko@hevery.com>
2020-11-09 21:29:45 +0000

    0786c59835 release: cut the v10.2.3 release

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.2+10.sha-ae71b76
+ * @license Angular v10.2.3
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1040,7 +1040,7 @@ function spawnWebWorker(uri, instance) {
  * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
  *     of Angular
  */
-const VERSION = new Version('10.2.2+10.sha-ae71b76');
+const VERSION = new Version('10.2.3');
 
 /**
  * @license
