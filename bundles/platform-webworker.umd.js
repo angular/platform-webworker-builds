3ef15248928b9995c2776528967f75c2b0e584cd
Pete Bacon Darwin <pete@bacondarwin.com>
2020-11-19 00:40:02 +0000

    1806d6e0c5 build: update to latest "yargs" package

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.3
+ * @license Angular v10.2.3+1.sha-1806d6e
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.2.3');
+    var VERSION = new core.Version('10.2.3+1.sha-1806d6e');
 
     /**
      * @license
