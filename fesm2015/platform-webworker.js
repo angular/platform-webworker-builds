52d965ad6643d437af67c6fb58e52db81f74d444
Alex Rickabaugh <alx+alxhub@alxandria.net>
2020-12-16 22:57:40 +0000

    9ae216ced5 build: upgrade typescript (dev dep) in zone.js to 3.8.3 (#40159)

--- fesm2015/platform-webworker.js
+++ fesm2015/platform-webworker.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v9.1.12+10.sha-5b38050
+ * @license Angular v9.1.12+13.sha-9ae216c
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -2038,7 +2038,7 @@ function spawnWebWorker(uri, instance) {
  *     of Angular
  * @type {?}
  */
-const VERSION = new Version('9.1.12+10.sha-5b38050');
+const VERSION = new Version('9.1.12+13.sha-9ae216c');
 
 /**
  * @fileoverview added by tsickle
