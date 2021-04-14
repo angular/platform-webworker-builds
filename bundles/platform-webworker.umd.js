8ea9653939899291c703e5d714fa384ac0003e7f
Zach Arend <zachzach@google.com>
2021-04-14 17:02:53 +0000

    f630f331de fix(compiler-cli): show a more specific error for Ivy NgModules (#41534) (#41598)

--- bundles/platform-webworker.umd.js
+++ bundles/platform-webworker.umd.js
@@ -1,5 +1,5 @@
 /**
- * @license Angular v10.2.4+1.sha-ba8da74
+ * @license Angular v10.2.4+2.sha-f630f33
  * (c) 2010-2020 Google LLC. https://angular.io/
  * License: MIT
  */
@@ -1371,7 +1371,7 @@
      * @deprecated platform-webworker is deprecated in Angular and will be removed in a future version
      *     of Angular
      */
-    var VERSION = new core.Version('10.2.4+1.sha-ba8da74');
+    var VERSION = new core.Version('10.2.4+2.sha-f630f33');
 
     /**
      * @license
